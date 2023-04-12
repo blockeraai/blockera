17a7ad4c68b25aaaebe53306ad68bb814503e647
reza elahi <relahi1373@gmail.com>
2023-04-12 19:02:26 +0430

    updated packages.json and webpack config

--- tools/webpack/packages.js
+++ tools/webpack/packages.js
@@ -51,6 +51,10 @@ module.exports = {
 		rules: [
 			{
 				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				resolve: {
+					extensions: ['.js', '.jsx'],
+				},
 				use: {
 					loader: 'babel-loader',
 				},
