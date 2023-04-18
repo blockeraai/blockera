e592c062cda89f0c0920028801aa92eb07c416b0
reza elahi <relahi1373@gmail.com>
2023-04-18 21:59:22 +0430

    Dev: exported Button component

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,4 +1,5 @@
 export { default as Icon } from './icon';
 export { default as Modal } from './modal';
+export { default as Button } from './button';
 export { default as PanelTab } from './panel-tab';
 export { default as InspectElement } from './inspect-element';
