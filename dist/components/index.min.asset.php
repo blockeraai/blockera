f785d969b64fa8fb0bd399194691642c0e3b1918
reza elahi <relahi1373@gmail.com>
2023-04-19 14:36:43 +0430

    build of packages!

--- dist/components/index.min.asset.php
+++ dist/components/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => 'afc70628d052b0f908fe');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '7dd948ad6a2531d0e72e');
--- dist/controls/index.min.asset.php
+++ dist/controls/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '52d4ed905553140c30a4');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '1df0e728bae907293b88');
