780df91cce99437edf008610c9aefa5cd9591cf9
reza elahi <relahi1373@gmail.com>
2023-04-19 14:33:08 +0430

    Dev: exported vstack and hstack componetns

--- packages/components/src/index.js
+++ packages/components/src/index.js
@@ -1,5 +1,7 @@
 export { default as Icon } from './icon';
 export { default as Modal } from './modal';
 export { default as Button } from './button';
+export { default as VStack } from './v-stack';
+export { default as HStack } from './h-stack';
 export { default as PanelTab } from './panel-tab';
 export { default as InspectElement } from './inspect-element';
