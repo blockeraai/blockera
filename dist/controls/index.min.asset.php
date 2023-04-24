63e9424a456af747822856a136794723ef41b2af
reza elahi <relahi1373@gmail.com>
2023-04-24 12:10:05 +0430

    build of controls package!

--- dist/controls/index.min.asset.php
+++ dist/controls/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '371fe2add3fbc342d794');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-primitives'), 'version' => '7dcc85d0fa193ce4deac');
