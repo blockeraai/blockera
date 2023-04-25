f5959f013325014fccfdb19c9f7a0655bfc25f14
reza elahi <relahi1373@gmail.com>
2023-04-25 20:28:36 +0430

    Revert "Dev: webpack config improved!"
    This reverts commit d0e57d8037e25918e0eb67c3e5249790d20d7df0.

--- tools/webpack/packages.js
+++ tools/webpack/packages.js
@@ -2,9 +2,6 @@
  * External dependencies
  */
 const { join } = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { isArray, mergeWith } = require('lodash');
 
 /**
  * WordPress dependencies
@@ -18,7 +15,6 @@ const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extract
  * Internal dependencies
  */
 const { dependencies } = require('../../package');
-const styleDependencies = require('./packages-styles');
 
 const PUBLISHER_NAMESPACE = '@publisher/';
 
@@ -28,94 +24,55 @@ const publisherPackages = Object.keys(dependencies)
 
 const exportDefaultPackages = [];
 
-module.exports = (env, argv) => {
-	const isProduction = argv.mode === 'production';
-
-	return {
-		mode: argv.mode,
-		name: 'packages',
-		entry: {
-			...mergeWith(
-				publisherPackages.reduce((memo, packageName) => {
-					return {
-						...memo,
-						[packageName]: {
-							import: `./packages/${packageName}`,
-							library: {
-								name: ['publisher', camelCaseDash(packageName)],
-								type: 'var',
-								export: exportDefaultPackages.includes(
-									packageName
-								)
-									? 'default'
-									: undefined,
-							},
-						},
-					};
-				}, {}),
-				styleDependencies.entry,
-				(objValue, srcValue) => {
-					if (isArray(objValue)) {
-						return objValue.concat(srcValue);
-					}
-				}
-			),
-		},
-		output: {
-			devtoolNamespace: 'publisher',
-			filename: isProduction
-				? './dist/[name]/index.min.js'
-				: './dist/[name]/index.js',
-			path: join(__dirname, '..', '..'),
-		},
-		module: {
-			rules: [
-				{
-					test: /\.(js|jsx)$/,
-					exclude: /node_modules/,
-					resolve: {
-						extensions: ['.js', '.jsx'],
-					},
-					use: {
-						loader: 'babel-loader',
-					},
+module.exports = {
+	mode: 'development',
+	name: 'packages',
+	entry: publisherPackages.reduce((memo, packageName) => {
+		return {
+			...memo,
+			[packageName]: {
+				import: `./packages/${packageName}`,
+				library: {
+					name: ['publisher', camelCaseDash(packageName)],
+					type: 'var',
+					export: exportDefaultPackages.includes(packageName)
+						? 'default'
+						: undefined,
 				},
-				{
-					test: /\.s[ac]ss$/i,
-					exclude: /node_modules/,
-					use: [
-						// Creates `style` nodes from JS strings
-						'style-loader',
-						// Translates CSS into CommonJS
-						'css-loader',
-						// Compiles Sass to CSS
-						'sass-loader',
-					],
+			},
+		};
+	}, {}),
+	output: {
+		devtoolNamespace: 'publisher',
+		filename: './dist/[name]/index.min.js',
+		path: join(__dirname, '..', '..'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				resolve: {
+					extensions: ['.js', '.jsx'],
 				},
-				{
-					test: /\.scss$/,
-					use: [
-						MiniCssExtractPlugin.loader,
-						'css-loader',
-						'sass-loader',
-					],
+				use: {
+					loader: 'babel-loader',
 				},
-			],
-		},
-		plugins: [
-			new DependencyExtractionWebpackPlugin({ injectPolyfill: true }),
-			new MiniCssExtractPlugin({
-				filename: isProduction
-					? './dist/[name]/style.min.css'
-					: './dist/[name]/style.css',
-			}),
-		].filter(Boolean),
-		optimization: {
-			minimize: isProduction,
-			minimizer: [
-				new TerserPlugin(),
-				...styleDependencies.optimization.minimizer,
-			],
-		},
-	};
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					// Creates `style` nodes from JS strings
+					'style-loader',
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new DependencyExtractionWebpackPlugin({ injectPolyfill: true }),
+	].filter(Boolean),
 };
