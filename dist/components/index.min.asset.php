cc0bf92b228ba12ba94b4ee57ee2c168a08ac965
reza elahi <relahi1373@gmail.com>
2023-04-25 13:58:46 +0430

    build of packages! ✅

--- dist/components/index.min.asset.php
+++ dist/components/index.min.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'dde53bd98a568402ddeb');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'e1b4907b1bc9c59d2507');
