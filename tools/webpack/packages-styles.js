8a9d45d6bf5030effdf5af84988aed2a54da25ba
reza elahi <relahi1373@gmail.com>
2023-05-02 12:02:35 +0430

    Dev: improved webpack and package.json configuration!

---
+++ tools/webpack/packages-styles.js
@@ -0,0 +1,55 @@
+/**
+ * External dependencies
+ */
+const glob = require('glob');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const styleEntries = {};
+const styleFiles = glob.sync('./packages/**/*.scss');
+
+function getPackage(name) {
+	const packageFiles = [];
+
+	styleFiles.forEach((file) => {
+		if (-1 === file.indexOf(name)) {
+			return;
+		}
+
+		packageFiles.push(file);
+	});
+
+	return packageFiles;
+}
+
+styleFiles.map((currentEntry) => {
+	const regex = new RegExp('packages\\/(\\w+)', 'g');
+
+	let m;
+
+	while ((m = regex.exec(currentEntry)) !== null) {
+		// This is necessary to avoid infinite loops with zero-width matches
+		if (m.index === regex.lastIndex) {
+			regex.lastIndex++;
+		}
+
+		// The result can be accessed through the `m`-variable.
+		m.forEach((match, groupIndex) => {
+			if (groupIndex !== 1) {
+				return;
+			}
+
+			Object.assign(styleEntries, {
+				[`${match}-styles`]: getPackage(match),
+			});
+		});
+	}
+
+	return currentEntry;
+});
+
+module.exports = {
+	entry: styleEntries,
+	optimization: {
+		minimizer: [new CssMinimizerPlugin()],
+	},
+};
--- tools/webpack/packages.js
+++ tools/webpack/packages.js
@@ -2,6 +2,9 @@
  * External dependencies
  */
 const { join } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+// const { isArray, merge, mergeWith } = require('lodash');
 
 /**
  * WordPress dependencies
@@ -15,64 +18,95 @@ const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extract
  * Internal dependencies
  */
 const { dependencies } = require('../../package');
+const styleDependencies = require('./packages-styles');
 
+const exportDefaultPackages = [];
 const PUBLISHER_NAMESPACE = '@publisher/';
-
 const publisherPackages = Object.keys(dependencies)
 	.filter((packageName) => packageName.startsWith(PUBLISHER_NAMESPACE))
 	.map((packageName) => packageName.replace(PUBLISHER_NAMESPACE, ''));
+const publisherEntries = publisherPackages.reduce((memo, packageName) => {
+	return {
+		...memo,
+		[packageName]: {
+			import: `./packages/${packageName}`,
+			library: {
+				name: ['publisher', camelCaseDash(packageName)],
+				type: 'var',
+				export: exportDefaultPackages.includes(packageName)
+					? 'default'
+					: undefined,
+			},
+		},
+	};
+}, {});
 
-const exportDefaultPackages = [];
+module.exports = (env, argv) => {
+	const isProduction = argv.mode === 'production';
 
-module.exports = {
-	mode: 'development',
-	name: 'packages',
-	entry: publisherPackages.reduce((memo, packageName) => {
-		return {
-			...memo,
-			[packageName]: {
-				import: `./packages/${packageName}`,
-				library: {
-					name: ['publisher', camelCaseDash(packageName)],
-					type: 'var',
-					export: exportDefaultPackages.includes(packageName)
-						? 'default'
-						: undefined,
+	return {
+		mode: argv.mode,
+		name: 'packages',
+		entry: {
+			...publisherEntries,
+			// ...styleDependencies.entry
+		},
+		output: {
+			devtoolNamespace: 'publisher',
+			filename: isProduction
+				? './dist/[name]/index.min.js'
+				: './dist/[name]/index.js',
+			path: join(__dirname, '..', '..'),
+		},
+		module: {
+			rules: [
+				{
+					test: /\.(js|jsx)$/,
+					exclude: /node_modules/,
+					resolve: {
+						extensions: ['.js', '.jsx'],
+					},
+					use: {
+						loader: 'babel-loader',
+					},
 				},
-			},
-		};
-	}, {}),
-	output: {
-		devtoolNamespace: 'publisher',
-		filename: './dist/[name]/index.min.js',
-		path: join(__dirname, '..', '..'),
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				resolve: {
-					extensions: ['.js', '.jsx'],
+				{
+					test: /\.s[ac]ss$/i,
+					exclude: /node_modules/,
+					use: [
+						// Creates `style` nodes from JS strings
+						'style-loader',
+						// Translates CSS into CommonJS
+						'css-loader',
+						// Compiles Sass to CSS
+						'sass-loader',
+					],
 				},
-				use: {
-					loader: 'babel-loader',
+				{
+					test: /\.scss$/,
+					use: [
+						MiniCssExtractPlugin.loader,
+						'css-loader',
+						'sass-loader',
+					],
 				},
-			},
-			{
-				test: /\.s[ac]ss$/i,
-				use: [
-					// Creates `style` nodes from JS strings
-					'style-loader',
-					// Translates CSS into CommonJS
-					'css-loader',
-					// Compiles Sass to CSS
-					'sass-loader',
-				],
-			},
-		],
-	},
-	plugins: [
-		new DependencyExtractionWebpackPlugin({ injectPolyfill: true }),
-	].filter(Boolean),
+			],
+		},
+		plugins: [
+			new DependencyExtractionWebpackPlugin({ injectPolyfill: true }),
+			new MiniCssExtractPlugin({
+				filename: isProduction
+					? './dist/[name]/style.min.css'
+					: './dist/[name]/style.css',
+				ignoreOrder: true,
+			}),
+		].filter(Boolean),
+		optimization: {
+			minimize: isProduction,
+			minimizer: [
+				new TerserPlugin(),
+				...styleDependencies.optimization.minimizer,
+			],
+		},
+	};
 };
