bba4ec0276c354633439890dffc14b792941af11
reza.develop <relahi1373@gmail.com>
2023-05-20 12:42:15 +0330

    Removed: extra files

--- packages/extensions/src/wrappers/index.js
+++
@@ -1,5 +0,0 @@
-export { default as withSaveProps } from './save-props';
-export { default as withEditorProps } from './editor-props';
-export { default as withBlockControls } from './block-controls';
-export { default as withBlockAttributes } from './block-attributes';
-export { default as withCustomizeSaveElement } from './save-element';
