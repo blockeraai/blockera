b508b7f8b960dc3530c1e8680c91cf9d776c3789
reza.develop <relahi1373@gmail.com>
2023-05-20 12:49:38 +0330

    Dev: added extensions controls

--- .dev/tests/jest/jest.config.js
+++ .dev/tests/jest/jest.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable sort-keys */
 module.exports = {
-	preset: '@wordpress/jest-preset-default',
 	rootDir: '../../../',
+	preset: '@wordpress/jest-preset-default',
 	collectCoverageFrom: ['<rootDir>/src/**/*.js'],
 	setupFilesAfterEnv: [
 		require.resolve(
@@ -14,5 +14,10 @@ module.exports = {
 		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
 			'<rootDir>/.dev/tests/jest/assets-transformer.js',
 		'^.+\\.(js|jsx)$': 'babel-jest',
+	},
+	globals: {
+		'ts-jest': {
+			isolatedModules: true,
+		},
 	},
 };
