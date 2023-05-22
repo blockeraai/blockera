632df6094119626a10e8554a898e7a02d08dccd6
reza.develop <relahi1373@gmail.com>
2023-05-22 16:47:05 +0330

    initialized extensions libs and fields package 🔥

--- packages/extensions/src/index.js
+++ packages/extensions/src/index.js
@@ -1,3 +1,21 @@
+/**
+ * Internal dependencies
+ */
+import { getExtensions } from './libs';
+import { getFields } from '@publisher/fields';
+import { registerBlockExtension } from './api';
+
+const register = ({ name, ...extension }) =>
+	registerBlockExtension(name, extension);
+
+function initExtensions() {
+	getExtensions().forEach(register);
+	getFields().forEach(register);
+}
+
+initExtensions();
+
 export { store } from './store';
 export * from './api';
+export { getExtensions } from './libs';
 export { default as applyHooks } from './hooks';
