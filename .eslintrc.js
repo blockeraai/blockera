87ac5d0fbefc2170ee9bd2f6ce9515fb928beeb1
reza.develop <relahi1373@gmail.com>
2023-06-06 15:20:12 +0330

    modified and improved input control stories!

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	extends: ['plugin:@wordpress/eslint-plugin/recommended'],
+	rules: {
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+			},
+		],
+		'import/no-extraneous-dependencies': 'off',
+		'@wordpress/no-unsafe-wp-apis': 'off',
+		'jsdoc/check-line-alignment': 'off',
+		'jsdoc/require-param': 'off',
+		'jsdoc/check-param-names': 'off',
+		'no-shadow': 'off',
+		'no-console': 'off',
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector:
+					"CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+				message: 'Unexpected property on console object was called',
+			},
+		],
+		'jsx-a11y/no-static-element-interactions': 'off',
+		'jsx-a11y/click-events-have-key-events': 'off',
+	},
+	env: {
+		jest: true,
+	},
+};
--- packages/controls/src/input-control/stories/Input.stories.js
+++ packages/controls/src/input-control/stories/Input.stories.js
@@ -1,28 +1,108 @@
+/**
+ * Storybook dependencies
+ */
+import { within, userEvent } from '@storybook/testing-library';
+
+/**
+ * Internal dependencies
+ */
 import { InputControl } from '../input';
+import { BlockEditContext } from '@publisher/extensions';
 
-// const units = ['px', '%', 'em'];
+let attributes = {
+	publisherInput: '20',
+};
+const setAttributes = (newAttributes) => {
+	attributes = newAttributes;
+};
+const { publisherInput: value } = attributes;
+const units = [
+	{ value: 'px', label: 'px', default: 0 },
+	{ value: '%', label: '%', default: 10 },
+	{ value: 'em', label: 'em', default: 0 },
+];
+const onChange = (newValue) => {
+	attributes = {
+		publisherInput: newValue,
+	};
+};
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
 	title: 'Controls/InputControl',
 	component: InputControl,
 	tags: ['autodocs'],
-	argTypes: {
-		initValue: '',
-		attribute: null,
-		className: 'publisher-input',
-		units: [],
-		suffix: '',
+};
+
+const WithMockBlockEditContext = (story) => (
+	<BlockEditContext.Provider
+		value={{
+			attributes,
+			setAttributes,
+		}}
+	>
+		{story()}
+	</BlockEditContext.Provider>
+);
+
+export const Default = {
+	args: {
 		range: false,
+		onChange,
+		initValue: '10px',
+		className: 'publisher-input',
+		value,
+		attribute: 'publisherInput',
+	},
+	decorators: [WithMockBlockEditContext],
+};
+
+export const InputWithUnit = {
+	args: {
+		units,
+		range: false,
+		onChange,
+		initValue: '10px',
+		className: 'publisher-input',
+		value,
+		// repeaterAttribute: 'test',
+		// repeaterAttributeIndex: 0,
+		attribute: 'publisherInput',
+	},
+	decorators: [WithMockBlockEditContext],
+	play: async ({ canvasElement, step }) => {
+		const canvas = within(canvasElement);
+
+		await step('Enter input value', async () => {
+			await userEvent.type(canvas.getByDisplayValue('20'), '60');
+		});
 	},
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const primary = {
+export const InputWithSlider = {
 	args: {
-		primary: true,
+		range: true,
+		onChange,
 		initValue: '10px',
+		className: 'publisher-input',
+		value,
+		// repeaterAttribute: 'test',
+		// repeaterAttributeIndex: 0,
 		attribute: 'publisherInput',
-		range: false,
 	},
+	decorators: [WithMockBlockEditContext],
+};
+
+export const InputWithUnitAndSlider = {
+	args: {
+		units,
+		range: true,
+		onChange,
+		initValue: '10px',
+		className: 'publisher-input',
+		value,
+		// repeaterAttribute: 'test',
+		// repeaterAttributeIndex: 0,
+		attribute: 'publisherInput',
+	},
+	decorators: [WithMockBlockEditContext],
 };
