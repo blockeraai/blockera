9ec51245fcd9c0db751076e7ae9211abae36906a
Ali Aghdam <betteraliaghdam@gmail.com>
2023-06-08 07:55:25 +0330

    Popover refactored
    - Title bar added
    - close button added
    - onClose implemented

---
+++ packages/components/src/popover/icons/close.js
@@ -0,0 +1,15 @@
+export default function CloseIcon({}) {
+	return (
+		<svg
+			width="16"
+			height="16"
+			viewBox="0 0 16 16"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<path
+				d="M8 9.11041L11.8885 13L13 11.8896L9.11146 8L13 4.11146L11.8896 3.00105L8 6.88749L4.11146 3L3 4.11041L6.88854 8L3 11.8885L4.11041 13L8 9.11041Z"
+				fill="currentColor"
+			/>
+		</svg>
+	);
+}
--- packages/components/src/popover/index.jsx
+++ packages/components/src/popover/index.jsx
@@ -2,19 +2,68 @@
  * WordPress dependencies
  */
 import { Popover as WPPopover } from '@wordpress/components';
+import { useState } from '@wordpress/element';
+
+/**
+ * Publisher dependencies
+ */
+import {
+	componentClassNames,
+	componentInnerClassNames,
+} from '@publisher/classnames';
 
 /**
  * Internal dependencies
  */
-import { componentClassNames } from '@publisher/classnames';
+import { Button } from '../button';
+import CloseIcon from './icons/close';
+import './style.scss';
 
-export default function Popover({ children, className, ...props }) {
+export default function Popover({
+	label = '',
+	onClose = () => {},
+	children,
+	className,
+	...props
+}) {
+	const [isVisible, setIsVisible] = useState(true);
+
 	return (
-		<WPPopover
-			className={componentClassNames('popover', className)}
-			{...props}
-		>
-			{children}
-		</WPPopover>
+		<>
+			{isVisible && (
+				<WPPopover
+					className={componentClassNames(
+						'popover',
+						label && 'with-header',
+						className
+					)}
+					onClose={onClose}
+					{...props}
+				>
+					{label && (
+						<div className={componentInnerClassNames('header')}>
+							{label}
+
+							<Button
+								className={componentInnerClassNames(
+									'header-close',
+									'no-border'
+								)}
+								size="extra-small"
+								align="center"
+								onClick={() => {
+									setIsVisible(false);
+									onClose();
+								}}
+								tabindex="-1"
+							>
+								<CloseIcon />
+							</Button>
+						</div>
+					)}
+					{children}
+				</WPPopover>
+			)}
+		</>
 	);
 }
