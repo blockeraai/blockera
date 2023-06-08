e28827d9f81176b2c1a5ddb004a47a6431af2690
Ali Aghdam <betteraliaghdam@gmail.com>
2023-06-08 17:17:50 +0330

    Icon support added to InspectElement

--- packages/components/src/inspect-element/index.js
+++ packages/components/src/inspect-element/index.js
@@ -5,23 +5,38 @@ import { PanelBody } from '@wordpress/components';
 import { InspectorControls } from '@wordpress/block-editor';
 
 /**
+ * Internal dependencies
+ */
+import './style.scss';
+
+/**
  * Render Inspect of Element.
  *
  * @param {string} title
+ * @param titleIcon
  * @param {boolean} initialOpen
- * @param {object} children
- * @returns
+ * @param {Object} children
+ * @param className
+ * @param props
+ * @return
  */
 export default function InspectElement({
 	title,
+	icon,
 	initialOpen,
 	children,
+	className = '',
 	...props
 }) {
 	return (
 		<div {...props}>
 			<InspectorControls>
-				<PanelBody title={title} initialOpen={initialOpen}>
+				<PanelBody
+					title={title}
+					initialOpen={initialOpen}
+					className={className}
+					icon={icon}
+				>
 					{children}
 				</PanelBody>
 			</InspectorControls>
