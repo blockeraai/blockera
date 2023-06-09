786f65742ddc789bdd63ec25817e3d467735c8b1
Ali Aghdam <betteraliaghdam@gmail.com>
2023-06-10 02:36:58 +0330

    minimal type added

--- packages/controls/src/color-control/index.js
+++ packages/controls/src/color-control/index.js
@@ -19,6 +19,7 @@ import { Button } from '@publisher/components';
 
 export default function ColorControl({
 	value,
+	type = 'normal',
 	//
 	onValueChange = (newValue) => {
 		return newValue;
@@ -29,24 +30,33 @@ export default function ColorControl({
 	const [isOpen, setOpen] = useState(false);
 	const [color, setColor] = useState(value);
 
+	let label = '';
+
+	if (type !== 'minimal') {
+		label = color || __('None', 'publisher-core');
+	}
+
 	return (
 		<>
 			<Button
 				size="input"
 				style="primary"
-				className={
-					isOpen
-						? 'is-focus toggle-focus ' +
-						  controlClassNames('color-indicator')
-						: 'toggle-focus ' + controlClassNames('color-indicator')
-				}
+				className={controlClassNames(
+					'color-indicator',
+					'color-type-' + type,
+					'toggle-focus',
+					isOpen ? 'is-focus' : '',
+					className
+				)}
 				onClick={() => setOpen(!isOpen)}
+				{...props}
 			>
 				<ColorIndicator
 					colorValue={color}
 					className="color-indicator"
 				/>
-				{color || __('None', 'publisher-core')}
+
+				{label}
 			</Button>
 
 			<ColorPickerPopover
