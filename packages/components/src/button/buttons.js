bde1a07b9b8dc4e0040e077627d57ad0a42f2927
Ali Aghdam <betteraliaghdam@gmail.com>
2023-06-22 10:56:35 +0330

    Buttons component changed to Flex and Fixed

--- packages/components/src/button/buttons.js
+++ packages/components/src/button/buttons.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { PropTypes } from 'prop-types';
+
+/**
  * Publisher dependencies
  */
 import { componentClassNames } from '@publisher/classnames';
@@ -6,21 +11,60 @@ import { componentClassNames } from '@publisher/classnames';
 /**
  * Internal dependencies
  */
-import { HStack } from '../index';
+import { Flex } from '../index';
 
 export default function Buttons({
+	direction,
+	gap,
+	justifyContent,
+	alignItems,
 	className,
 	children,
-	spacing = 2,
 	...props
 }) {
 	return (
-		<HStack
-			spcaing={spacing}
+		<Flex
+			gap={gap}
+			direction={direction}
+			alignItems={alignItems}
 			className={componentClassNames('buttons', className)}
 			{...props}
 		>
 			{children}
-		</HStack>
+		</Flex>
 	);
 }
+
+Buttons.propTypes = {
+	direction: PropTypes.oneOf([
+		'column',
+		'column-reverse',
+		'row',
+		'row-reverse',
+	]),
+	gap: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+	justifyContent: PropTypes.oneOf([
+		'flex-start',
+		'center',
+		'flex-end',
+		'space-between',
+		'space-around',
+		'space-evenly',
+	]),
+	alignItems: PropTypes.oneOf([
+		'flex-start',
+		'center',
+		'flex-end',
+		'stretch',
+	]),
+	className: PropTypes.string,
+	children: PropTypes.element,
+};
+
+Buttons.defaultProps = {
+	direction: 'row',
+	gap: '8px',
+	justifyContent: '',
+	alignItems: '',
+	className: '',
+};
