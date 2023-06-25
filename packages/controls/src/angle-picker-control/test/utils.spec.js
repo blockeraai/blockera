25886bbb8c5abeeb1f234b05ba63b0ee303a60cb
Ali Aghdam <betteraliaghdam@gmail.com>
2023-06-25 09:36:32 +0330

    tests added

---
+++ packages/controls/src/angle-picker-control/test/utils.spec.js
@@ -0,0 +1,43 @@
+import { addAngle, subtractAngle } from '../utils';
+
+describe('Testing util functions', () => {
+	describe('addAngle testing', () => {
+		test('testing when passed values as String!', () => {
+			const value = addAngle('45', '45');
+
+			expect(value).toBe(0);
+		});
+
+		test('testing when passed as number!', () => {
+			const value = addAngle(45, 45);
+
+			expect(value).toBe(90);
+		});
+
+		test('testing when passed as negative number!', () => {
+			const value = addAngle(45, -90);
+
+			expect(value).toBe(315);
+		});
+	});
+
+	describe('subtractAngle testing', () => {
+		test('testing when passed values as String!', () => {
+			const value = subtractAngle('45', '45');
+
+			expect(value).toBe(0);
+		});
+
+		test('testing when passed as number!', () => {
+			const value = subtractAngle(45, 45);
+
+			expect(value).toBe(0);
+		});
+
+		test('testing when passed as negative number!', () => {
+			const value = subtractAngle(45, -90);
+
+			expect(value).toBe(135);
+		});
+	});
+});
--- packages/controls/src/angle-picker-control/utils.js
+++ packages/controls/src/angle-picker-control/utils.js
@@ -1,6 +1,12 @@
-export function addAngle(angle, value) {
+import { isNumber } from '@publisher/utils';
+
+export function addAngle(angle, addValue) {
+	if (!isNumber(angle) || !isNumber(addValue)) {
+		return 0;
+	}
+
 	// Add the value to the angle
-	let result = angle + value;
+	let result = angle + addValue;
 
 	// Normalize the result within the range of 0 to 360
 	if (result > 360) {
@@ -12,9 +18,13 @@ export function addAngle(angle, value) {
 	return result;
 }
 
-export function subtractAngle(angle, value) {
+export function subtractAngle(angle, addValue) {
+	if (!isNumber(angle) || !isNumber(addValue)) {
+		return 0;
+	}
+
 	// Subtract the value from the angle
-	let result = angle - value;
+	let result = angle - addValue;
 
 	// Normalize the result within the range of 0 to 359
 	if (result < 0) {
