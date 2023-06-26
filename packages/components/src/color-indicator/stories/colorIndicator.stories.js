d78d73a2c3edf553769d39d452b588aa9539c95c
Ali Aghdam <betteraliaghdam@gmail.com>
2023-06-26 14:45:29 +0330

    ColorIndicator: Screenshot story + layout improvement

--- packages/components/src/color-indicator/stories/colorIndicator.stories.js
+++ packages/components/src/color-indicator/stories/colorIndicator.stories.js
@@ -42,22 +42,28 @@ export const Color = {
 	args: Default.args,
 	argTypes: Default.argTypes,
 	render: (args) => (
-		<>
-			<h2 className="story-heading">Color</h2>
-			<ColorIndicator {...args} />
-
-			<h2 className="story-heading">Empty & None</h2>
-			<Flex direction="row" gap="20px">
-				<ColorIndicator value="" />
-				<ColorIndicator value="none" />
+		<Flex direction="column" gap="30px">
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Color</h2>
+				<ColorIndicator {...args} />
 			</Flex>
 
-			<h2 className="story-heading">Custom Size</h2>
-			<Flex direction="row" gap="20px">
-				<ColorIndicator size="30" {...args} />
-				<ColorIndicator size="30" value="" />
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Color → Empty & None</h2>
+				<Flex direction="row" gap="20px">
+					<ColorIndicator value="" />
+					<ColorIndicator value="none" />
+				</Flex>
 			</Flex>
-		</>
+
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Color → Custom Size</h2>
+				<Flex direction="row" gap="20px">
+					<ColorIndicator size="30" {...args} />
+					<ColorIndicator size="30" value="" />
+				</Flex>
+			</Flex>
+		</Flex>
 	),
 	decorators: [WithInspectorStyles, ...SharedDecorators],
 };
@@ -74,22 +80,28 @@ export const Image = {
 		},
 	},
 	render: (args) => (
-		<>
-			<h2 className="story-heading">Image</h2>
-			<ColorIndicator {...args} />
-
-			<h2 className="story-heading">Empty & None</h2>
-			<Flex direction="row" gap="20px">
-				<ColorIndicator {...args} type="image" value="" />
-				<ColorIndicator {...args} type="image" value="none" />
+		<Flex direction="column" gap="30px">
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Image</h2>
+				<ColorIndicator {...args} />
 			</Flex>
 
-			<h2 className="story-heading">Custom Size</h2>
-			<Flex direction="row" gap="20px">
-				<ColorIndicator {...args} type="image" size="30" />
-				<ColorIndicator {...args} type="image" size="30" value="" />
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Image → Empty & None</h2>
+				<Flex direction="row" gap="20px">
+					<ColorIndicator {...args} type="image" value="" />
+					<ColorIndicator {...args} type="image" value="none" />
+				</Flex>
 			</Flex>
-		</>
+
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Image → Custom Size</h2>
+				<Flex direction="row" gap="20px">
+					<ColorIndicator {...args} type="image" size="30" />
+					<ColorIndicator {...args} type="image" size="30" value="" />
+				</Flex>
+			</Flex>
+		</Flex>
 	),
 	decorators: [WithInspectorStyles, ...SharedDecorators],
 };
@@ -107,22 +119,33 @@ export const Gradient = {
 		},
 	},
 	render: (args) => (
-		<div>
-			<h2 className="story-heading">Gradient</h2>
-			<ColorIndicator {...args} />
-
-			<h2 className="story-heading">Empty & None</h2>
-			<Flex direction="row" gap="20px">
-				<ColorIndicator {...args} type="gradient" value="" />
-				<ColorIndicator {...args} type="gradient" value="none" />
+		<Flex direction="column" gap="30px">
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Gradient</h2>
+				<ColorIndicator {...args} />
 			</Flex>
 
-			<h2 className="story-heading">Custom Size</h2>
-			<Flex direction="row" gap="20px">
-				<ColorIndicator {...args} type="gradient" size="30" />
-				<ColorIndicator {...args} type="gradient" size="30" value="" />
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Gradient → Empty & None</h2>
+				<Flex direction="row" gap="20px">
+					<ColorIndicator {...args} type="gradient" value="" />
+					<ColorIndicator {...args} type="gradient" value="none" />
+				</Flex>
 			</Flex>
-		</div>
+
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Gradient → Custom Size</h2>
+				<Flex direction="row" gap="20px">
+					<ColorIndicator {...args} type="gradient" size="30" />
+					<ColorIndicator
+						{...args}
+						type="gradient"
+						size="30"
+						value=""
+					/>
+				</Flex>
+			</Flex>
+		</Flex>
 	),
 	decorators: [WithInspectorStyles, ...SharedDecorators],
 };
@@ -132,123 +155,161 @@ export const Stack = {
 	args: {},
 	argTypes: Default.argTypes,
 	render: (args) => (
-		<div>
-			<h2 className="story-heading">Flat Value Stack</h2>
-			<ColorIndicatorStack
-				{...args}
-				value={['#1183ff', '#ff6363', '#2cc800']}
-			/>
+		<Flex direction="column" gap="30px">
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Stack → Flat Value</h2>
+				<ColorIndicatorStack
+					{...args}
+					value={['#1183ff', '#ff6363', '#2cc800']}
+				/>
+			</Flex>
 
-			<h2 className="story-heading">Object Value Stack </h2>
-			<ColorIndicatorStack
-				{...args}
-				value={[
-					{
-						type: 'color',
-						value: '#1183ff',
-					},
-					{
-						type: 'image',
-						value: 'https://betterstudio.com/wp-content/uploads/2022/09/publisher-theme.svg',
-					},
-					{
-						type: 'gradient',
-						value: 'linear-gradient(315deg,rgb(143,0,0) 26%,rgb(117,48,255) 26%,rgb(0,39,179) 58%,rgb(255,122,0) 58%,rgb(150,30,0) 81%,rgb(204,167,0) 100%)',
-					},
-				]}
-			/>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Stack → Object Value</h2>
+				<ColorIndicatorStack
+					{...args}
+					value={[
+						{
+							type: 'color',
+							value: '#1183ff',
+						},
+						{
+							type: 'image',
+							value: 'https://betterstudio.com/wp-content/uploads/2022/09/publisher-theme.svg',
+						},
+						{
+							type: 'gradient',
+							value: 'linear-gradient(315deg,rgb(143,0,0) 26%,rgb(117,48,255) 26%,rgb(0,39,179) 58%,rgb(255,122,0) 58%,rgb(150,30,0) 81%,rgb(204,167,0) 100%)',
+						},
+					]}
+				/>
+			</Flex>
 
-			<h2 className="story-heading">Custom Size</h2>
-			<ColorIndicatorStack
-				{...args}
-				size="30"
-				value={[
-					{
-						type: 'color',
-						value: '#1183ff',
-					},
-					{
-						type: 'image',
-						value: 'https://betterstudio.com/wp-content/uploads/2022/09/publisher-theme.svg',
-					},
-					{
-						type: 'gradient',
-						value: 'linear-gradient(315deg,rgb(143,0,0) 26%,rgb(117,48,255) 26%,rgb(0,39,179) 58%,rgb(255,122,0) 58%,rgb(150,30,0) 81%,rgb(204,167,0) 100%)',
-					},
-				]}
-			/>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Stack → Custom Size</h2>
+				<ColorIndicatorStack
+					{...args}
+					size="30"
+					value={[
+						{
+							type: 'color',
+							value: '#1183ff',
+						},
+						{
+							type: 'image',
+							value: 'https://betterstudio.com/wp-content/uploads/2022/09/publisher-theme.svg',
+						},
+						{
+							type: 'gradient',
+							value: 'linear-gradient(315deg,rgb(143,0,0) 26%,rgb(117,48,255) 26%,rgb(0,39,179) 58%,rgb(255,122,0) 58%,rgb(150,30,0) 81%,rgb(204,167,0) 100%)',
+						},
+					]}
+				/>
+			</Flex>
 
-			<h2 className="story-heading">1 Item</h2>
-			<ColorIndicatorStack {...args} value={['#1183ff']} />
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Stack → 1 Item</h2>
+				<ColorIndicatorStack {...args} value={['#1183ff']} />
+			</Flex>
 
-			<h2 className="story-heading">2 Items</h2>
-			<ColorIndicatorStack {...args} value={['#1183ff', '#ff6363']} />
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Stack → 2 Items</h2>
+				<ColorIndicatorStack {...args} value={['#1183ff', '#ff6363']} />
+			</Flex>
 
-			<h2 className="story-heading">5 Items</h2>
-			<ColorIndicatorStack
-				{...args}
-				value={['#1183ff', '#ff6363', '#2cc800', '#becd00', '#ff2e63']}
-			/>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Stack → 5 Items</h2>
+				<ColorIndicatorStack
+					{...args}
+					value={[
+						'#1183ff',
+						'#ff6363',
+						'#2cc800',
+						'#becd00',
+						'#ff2e63',
+					]}
+				/>
+			</Flex>
 
-			<h2 className="story-heading">10 Items</h2>
-			<ColorIndicatorStack
-				{...args}
-				value={[
-					'#1183ff',
-					'#ff1313b3',
-					'#2cc800',
-					'#becd00',
-					'#ff2e63',
-					'#ad8400',
-					'#0a9cba',
-					'#d31000',
-					'#0949e8',
-					'#5100df',
-				]}
-				maxItems={10}
-			/>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Stack → 10 Items</h2>
+				<ColorIndicatorStack
+					{...args}
+					value={[
+						'#1183ff',
+						'#ff1313b3',
+						'#2cc800',
+						'#becd00',
+						'#ff2e63',
+						'#ad8400',
+						'#0a9cba',
+						'#d31000',
+						'#0949e8',
+						'#5100df',
+					]}
+					maxItems={10}
+				/>
+			</Flex>
 
-			<h2 className="story-heading">15 Items</h2>
-			<ColorIndicatorStack
-				{...args}
-				value={[
-					'#1183ff',
-					'#ff1313b3',
-					'#2cc800',
-					'#becd00',
-					'#ff2e63',
-					'#ad8400',
-					'#0a9cba',
-					'#d31000',
-					'#0949e8',
-					'#5100df',
-					'#40a700',
-					'#231bff',
-					'#f62c1b',
-					'#e8a909',
-					'#a0df00',
-				]}
-				maxItems={15}
-			/>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Stack → 15 Items</h2>
+				<ColorIndicatorStack
+					{...args}
+					value={[
+						'#1183ff',
+						'#ff1313b3',
+						'#2cc800',
+						'#becd00',
+						'#ff2e63',
+						'#ad8400',
+						'#0a9cba',
+						'#d31000',
+						'#0949e8',
+						'#5100df',
+						'#40a700',
+						'#231bff',
+						'#f62c1b',
+						'#e8a909',
+						'#a0df00',
+					]}
+					maxItems={15}
+				/>
+			</Flex>
 
-			<h2 className="story-heading">10 Items (Max Items → 6)</h2>
-			<ColorIndicatorStack
-				{...args}
-				value={[
-					'#1183ff',
-					'#ff1313b3',
-					'#2cc800',
-					'#becd00',
-					'#ff2e63',
-					'#ad8400',
-					'#0a9cba',
-					'#d31000',
-					'#0949e8',
-					'#5100df',
-				]}
-				maxItems={6}
-			/>
-		</div>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">
+					Stack → 10 Items (Max Items → 6)
+				</h2>
+				<ColorIndicatorStack
+					{...args}
+					value={[
+						'#1183ff',
+						'#ff1313b3',
+						'#2cc800',
+						'#becd00',
+						'#ff2e63',
+						'#ad8400',
+						'#0a9cba',
+						'#d31000',
+						'#0949e8',
+						'#5100df',
+					]}
+					maxItems={6}
+				/>
+			</Flex>
+		</Flex>
+	),
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+};
+
+export const Screenshot = {
+	render: () => (
+		<Flex direction="column" gap="30px">
+			<Color.render {...Color.args} />
+			<Image.render {...Image.args} />
+			<Gradient.render {...Gradient.args} />
+			<Stack.render {...Stack.args} />
+		</Flex>
 	),
 	decorators: [WithInspectorStyles, ...SharedDecorators],
 };
