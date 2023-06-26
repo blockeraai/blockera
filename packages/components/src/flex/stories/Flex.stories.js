e2bf3fd60f8aec57a26adf0d70d4ea695ce0f1ed
Ali Aghdam <betteraliaghdam@gmail.com>
2023-06-26 14:52:55 +0330

    Flex: screenshot update + relayout

--- packages/components/src/flex/stories/Flex.stories.js
+++ packages/components/src/flex/stories/Flex.stories.js
@@ -19,253 +19,95 @@ export default {
 	tags: ['autodocs'],
 };
 
+const FlexChildren = () => {
+	return (
+		<>
+			<div
+				className="story-flex-item"
+				style={{
+					backgroundColor: '#0047eb',
+					color: '#fff',
+					width: '60px',
+				}}
+			>
+				Item 1
+			</div>
+			<div
+				className="story-flex-item"
+				style={{
+					backgroundColor: '#0047eb',
+					color: '#fff',
+					width: '90px',
+				}}
+			>
+				Item 2
+			</div>
+			<div
+				className="story-flex-item"
+				style={{
+					backgroundColor: '#0047eb',
+					color: '#fff',
+					width: '110px',
+				}}
+			>
+				Item 3
+			</div>
+		</>
+	);
+};
+
 export const Default = {
 	args: {
-		children: (
-			<>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '60px',
-					}}
-				>
-					Item 1
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '90px',
-					}}
-				>
-					Item 2
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '110px',
-					}}
-				>
-					Item 3
-				</div>
-			</>
-		),
+		children: <FlexChildren />,
 	},
 	decorators: [WithInspectorStyles, ...SharedDecorators],
 };
 
 export const Screenshot = {
 	render: (args) => (
-		<>
-			<h2 className="story-heading">Row Flex</h2>
-			<Flex direction="row" {...args}>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '60px',
-					}}
-				>
-					Item 1
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '90px',
-					}}
-				>
-					Item 2
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '110px',
-					}}
-				>
-					Item 3
-				</div>
+		<Flex direction="column" gap="30px">
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Row Flex</h2>
+				<Flex direction="row" {...args}>
+					<FlexChildren />
+				</Flex>
 			</Flex>
 
-			<h2 className="story-heading">20px Gap Flex</h2>
-			<Flex gap="20px" {...args}>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '60px',
-					}}
-				>
-					Item 1
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '90px',
-					}}
-				>
-					Item 2
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '110px',
-					}}
-				>
-					Item 3
-				</div>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">20px Gap Flex</h2>
+				<Flex gap="20px" {...args}>
+					<FlexChildren />
+				</Flex>
 			</Flex>
 
-			<h2 className="story-heading">Flex Wrap</h2>
-			<Flex flexWrap="wrap" {...args}>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '60px',
-					}}
-				>
-					Item 1
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '90px',
-					}}
-				>
-					Item 2
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '110px',
-					}}
-				>
-					Item 3
-				</div>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Flex Wrap</h2>
+				<Flex flexWrap="wrap" {...args}>
+					<FlexChildren />
+				</Flex>
 			</Flex>
 
-			<h2 className="story-heading">Column Flex</h2>
-			<Flex direction="column" {...args}>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '60px',
-					}}
-				>
-					Item 1
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '90px',
-					}}
-				>
-					Item 2
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '110px',
-					}}
-				>
-					Item 3
-				</div>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Column Flex</h2>
+				<Flex direction="column" {...args}>
+					<FlexChildren />
+				</Flex>
 			</Flex>
 
-			<h2 className="story-heading">alignItems=center</h2>
-			<Flex direction="column" alignItems="center" {...args}>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '60px',
-					}}
-				>
-					Item 1
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '90px',
-					}}
-				>
-					Item 2
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '110px',
-					}}
-				>
-					Item 3
-				</div>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">alignItems=center</h2>
+				<Flex direction="column" alignItems="center" {...args}>
+					<FlexChildren />
+				</Flex>
 			</Flex>
 
-			<h2 className="story-heading">alignItems=flex-end</h2>
-			<Flex direction="column" alignItems="flex-end" {...args}>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '60px',
-					}}
-				>
-					Item 1
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '90px',
-					}}
-				>
-					Item 2
-				</div>
-				<div
-					className="story-flex-item"
-					style={{
-						backgroundColor: '#0047eb',
-						color: '#fff',
-						width: '110px',
-					}}
-				>
-					Item 3
-				</div>
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">alignItems=flex-end</h2>
+				<Flex direction="column" alignItems="flex-end" {...args}>
+					<FlexChildren />
+				</Flex>
 			</Flex>
-		</>
+		</Flex>
 	),
 	decorators: [WithInspectorStyles, ...SharedDecorators],
 };
