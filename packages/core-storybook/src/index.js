af56b2f09376d10cb603f81823e594966ca283f3
reza.develop <relahi1373@gmail.com>
2023-06-26 11:52:39 +0330

    perf: deleted core-storybook package

--- packages/core-storybook/src/index.js
+++
@@ -1,6 +0,0 @@
-export * from './adapters/jest';
-export { Playground } from './components';
-export * from './adapters/testing-library';
-export const testId = 'publisher-playground';
-export { blocksInitializer } from './api/init';
-export { createBlockEditorContent } from './api/create-block';
