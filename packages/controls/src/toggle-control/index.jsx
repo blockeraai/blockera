5bbccfa6ce9131d65dc04db67503f9552d135a9d
Ali Aghdam <betteraliaghdam@gmail.com>
2023-06-27 12:19:27 +0330

    ToggleControl: Refactor + design improvement + Stories

--- packages/controls/src/toggle-control/index.jsx
+++ packages/controls/src/toggle-control/index.jsx
@@ -1,33 +1,60 @@
 /**
- * WordPress dependencies
+ * External dependencies
  */
 import { ToggleControl as WPToggleControl } from '@wordpress/components';
+import PropTypes from 'prop-types';
 
 /**
  * Publisher dependencies
  */
 import { controlClassNames } from '@publisher/classnames';
+import { useValue } from '@publisher/utils';
 
 export default function ToggleControl({
-	value,
-	//
+	label,
+	defaultValue,
+	value: initialValue,
+	onChange,
 	className,
-	onChange = (newValue) => {
-		return newValue;
-	},
-	onValueChange = () => {},
-
 	...props
 }) {
+	const { value, setValue } = useValue({
+		initialValue,
+		defaultValue,
+		onChange,
+	});
+
 	return (
 		<WPToggleControl
-			value={value}
-			onChange={(newValue) => {
-				newValue = onChange(newValue);
-				onValueChange(newValue);
-			}}
-			className={controlClassNames('toggle-control', className)}
+			label={label}
+			checked={value}
+			onChange={setValue}
+			className={controlClassNames('toggle', className)}
 			{...props}
 		/>
 	);
 }
+
+ToggleControl.propTypes = {
+	/**
+	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
+	 */
+	defaultValue: PropTypes.bool,
+	/**
+	 * The current value.
+	 */
+	value: PropTypes.bool,
+	/**
+	 * Function that will be fired while the control value state changes.
+	 */
+	onChange: PropTypes.func,
+	/**
+	 * Label after toggle
+	 */
+	label: PropTypes.string,
+};
+
+ToggleControl.defaultProps = {
+	value: false,
+	label: '',
+};
---
+++ packages/controls/src/toggle-control/stories/index.stories.js
@@ -0,0 +1,110 @@
+/**
+ * External dependencies
+ */
+import { useContext } from '@wordpress/element';
+import { userEvent, waitFor, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+/**
+ * Publisher dependencies
+ */
+import { Flex } from '@publisher/components';
+import { default as Decorators } from '@publisher/storybook/decorators';
+
+/**
+ * Internal dependencies
+ */
+import { ToggleControl } from '../../index';
+
+const {
+	WithInspectorStyles,
+	StoryDataContext,
+	WithStoryContextProvider,
+	SharedDecorators,
+} = Decorators;
+
+export default {
+	title: 'Controls/ToggleControl',
+	component: ToggleControl,
+	tags: ['autodocs'],
+};
+
+export const Default = {
+	args: {
+		value: false,
+	},
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+};
+
+export const States = {
+	args: {
+		label: 'test',
+		value: false,
+	},
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+	render: () => {
+		return (
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Toggle</h2>
+				<ToggleControl label="Not Checked" value={false} />
+				<ToggleControl label="Checked" value={true} />
+			</Flex>
+		);
+	},
+};
+
+const ControlWithHooks = (args) => {
+	const { storyValue, setStoryValue } = useContext(StoryDataContext);
+
+	return (
+		<ToggleControl {...args} onChange={setStoryValue} value={storyValue} />
+	);
+};
+
+export const Play = {
+	args: {
+		label: '',
+		value: false,
+	},
+	decorators: [
+		WithStoryContextProvider,
+		WithInspectorStyles,
+		...SharedDecorators,
+	],
+	render: (args) => <ControlWithHooks {...args} />,
+	play: async ({ canvasElement, step }) => {
+		const canvas = within(canvasElement);
+		const currentValue = canvas.getByTestId('current-value');
+		const checkbox = canvas.getByRole('checkbox');
+
+		await step('Story Data', async () => {
+			await expect(currentValue).toBeInTheDocument();
+			await expect(currentValue).toHaveTextContent('false');
+		});
+
+		await step('Change Test', async () => {
+			await userEvent.click(checkbox);
+			await waitFor(
+				async () =>
+					await expect(currentValue).toHaveTextContent('true'),
+				{ timeout: 1000 }
+			);
+
+			await userEvent.click(checkbox);
+			await waitFor(
+				async () =>
+					await expect(currentValue).toHaveTextContent('false'),
+				{ timeout: 1000 }
+			);
+		});
+	},
+};
+
+export const Screenshot = {
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+	render: () => (
+		<Flex direction="column" gap="50px">
+			<States.render {...States.args} />
+		</Flex>
+	),
+};
