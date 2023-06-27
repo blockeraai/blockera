60084d20e9b0f731256a5a1c1326c70c8db44e5f
Ali Aghdam <betteraliaghdam@gmail.com>
2023-06-27 12:54:50 +0330

    SearchControl: Refactor + Design improvement + stories

--- packages/controls/src/search-control/index.js
+++ packages/controls/src/search-control/index.js
@@ -1,17 +1,60 @@
 /**
- * WordPress dependencies
+ * External dependencies
  */
+import { __ } from '@wordpress/i18n';
 import { SearchControl as WPSearchControl } from '@wordpress/components';
+import PropTypes from 'prop-types';
 
 /**
  * Publisher dependencies
  */
 import { controlClassNames } from '@publisher/classnames';
+import { useValue } from '@publisher/utils';
 
-const SearchControl = ({ ...props }) => {
+export default function SearchControl({
+	value: initialValue,
+	defaultValue,
+	onChange,
+	className,
+	...props
+}) {
+	const { value, setValue } = useValue({
+		initialValue,
+		defaultValue,
+		onChange,
+	});
+
 	return (
-		<WPSearchControl className={controlClassNames('search')} {...props} />
+		<WPSearchControl
+			value={value}
+			onChange={setValue}
+			className={controlClassNames('search', className)}
+			{...props}
+		/>
 	);
+}
+
+SearchControl.propTypes = {
+	/**
+	 * A placeholder for the input.
+	 */
+	placeholder: PropTypes.string,
+	/**
+	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
+	 */
+	defaultValue: PropTypes.string,
+	/**
+	 * The current value.
+	 */
+	value: PropTypes.string,
+	/**
+	 * Function that will be fired while the control value state changes.
+	 */
+	onChange: PropTypes.func,
 };
 
-export default SearchControl;
+SearchControl.defaultProps = {
+	placeholder: __('Search…', 'publisher-core'),
+	value: '',
+	defaultValue: '',
+};
---
+++ packages/controls/src/search-control/stories/index.stories.js
@@ -0,0 +1,132 @@
+/**
+ * External dependencies
+ */
+import { useContext } from '@wordpress/element';
+import { fireEvent, waitFor, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
+/**
+ * Publisher dependencies
+ */
+import { Flex } from '@publisher/components';
+import { default as Decorators } from '@publisher/storybook/decorators';
+
+/**
+ * Internal dependencies
+ */
+import { SearchControl } from '../../index';
+
+const {
+	WithInspectorStyles,
+	StoryDataContext,
+	WithStoryContextProvider,
+	SharedDecorators,
+} = Decorators;
+
+export default {
+	title: 'Controls/SearchControl',
+	component: SearchControl,
+	tags: ['autodocs'],
+};
+
+export const Default = {
+	args: {
+		placeholder: 'Enter to search...',
+		value: '',
+	},
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+};
+
+export const States = {
+	args: {
+		placeholder: 'Enter to search...',
+		value: '',
+	},
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+	render: (args) => {
+		return (
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Search</h2>
+				<SearchControl {...args} />
+				<SearchControl {...args} className="is-hovered" />
+				<SearchControl {...args} className="is-focused" />
+				<SearchControl {...args} value="term" />
+			</Flex>
+		);
+	},
+};
+
+const ControlWithHooks = (args) => {
+	const { storyValue, setStoryValue } = useContext(StoryDataContext);
+
+	return (
+		<SearchControl {...args} onChange={setStoryValue} value={storyValue} />
+	);
+};
+
+export const Play = {
+	args: {
+		value: '',
+	},
+	decorators: [
+		WithStoryContextProvider,
+		WithInspectorStyles,
+		...SharedDecorators,
+	],
+	render: (args) => <ControlWithHooks {...args} />,
+	play: async ({ canvasElement, step }) => {
+		const canvas = within(canvasElement);
+		const currentValue = canvas.getByTestId('current-value');
+		const input = canvas.getByRole('searchbox');
+
+		await step('Story Data', async () => {
+			await expect(currentValue).toBeInTheDocument();
+			await expect(currentValue).toHaveTextContent("''");
+		});
+
+		await step('Search for term', async () => {
+			fireEvent.change(input, { target: { value: 'term' } });
+			await waitFor(
+				async () =>
+					await expect(currentValue).toHaveTextContent("'term'"),
+				{ timeout: 1000 }
+			);
+
+			fireEvent.change(input, { target: { value: '' } });
+			await waitFor(
+				async () => await expect(currentValue).toHaveTextContent("''"),
+				{ timeout: 1000 }
+			);
+		});
+
+		await step('Search and close', async () => {
+			fireEvent.change(input, { target: { value: 'term' } });
+			await waitFor(
+				async () =>
+					await expect(currentValue).toHaveTextContent("'term'"),
+				{ timeout: 1000 }
+			);
+
+			await canvas.getByLabelText('Reset search');
+			await expect(
+				canvas.getByLabelText('Reset search')
+			).toBeInTheDocument();
+			fireEvent.click(canvas.getByLabelText('Reset search'));
+
+			// value should be reset to empty
+			await waitFor(
+				async () => await expect(currentValue).toHaveTextContent("''"),
+				{ timeout: 1000 }
+			);
+		});
+	},
+};
+
+export const Screenshot = {
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+	render: () => (
+		<Flex direction="column" gap="50px">
+			<States.render {...States.args} />
+		</Flex>
+	),
+};
