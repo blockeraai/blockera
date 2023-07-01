d291a808627660e6fdcb9fa2779483898d8d6a58
Ali Aghdam <betteraliaghdam@gmail.com>
2023-07-01 08:48:35 +0330

    TransitionControl: propTypes + defaultProps + stories

--- packages/controls/src/transition-control/index.js
+++ packages/controls/src/transition-control/index.js
@@ -1,7 +1,8 @@
 /**
- * WordPress dependencies
+ * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import PropTypes from 'prop-types';
 
 /**
  * Publisher dependencies
@@ -16,15 +17,8 @@ import RepeaterControl from '../repeater-control';
 import Fields from './components/fields';
 
 export default function TransitionControl({
-	defaultRepeaterItemValue = {
-		type: 'all',
-		duration: '500ms',
-		timing: 'ease',
-		delay: '0ms',
-		isVisible: true,
-	},
-	popoverLabel = __('Transition', 'publisher-core'),
-	//
+	defaultRepeaterItemValue,
+	popoverLabel,
 	className,
 	...props
 }) {
@@ -39,3 +33,45 @@ export default function TransitionControl({
 		/>
 	);
 }
+
+TransitionControl.propTypes = {
+	...RepeaterControl.propTypes,
+	/**
+	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
+	 */
+	defaultValue: PropTypes.array,
+	/**
+	 * The current value.
+	 */
+	value: PropTypes.array,
+	/**
+	 * Function that will be fired while the control value state changes.
+	 */
+	onChange: PropTypes.func,
+	/**
+	 * Default value of each repeater item
+	 */
+	defaultRepeaterItemValue: PropTypes.shape({
+		type: PropTypes.string,
+		duration: PropTypes.string,
+		timing: PropTypes.string,
+		delay: PropTypes.string,
+		isVisible: PropTypes.bool,
+	}),
+	/**
+	 * Label for popover
+	 */
+	popoverLabel: PropTypes.string,
+};
+
+TransitionControl.defaultProps = {
+	value: [],
+	defaultRepeaterItemValue: {
+		type: 'all',
+		duration: '500ms',
+		timing: 'ease',
+		delay: '0ms',
+		isVisible: true,
+	},
+	popoverLabel: __('Transition', 'publisher-core'),
+};
---
+++ packages/controls/src/transition-control/stories/index.stories.js
@@ -0,0 +1,178 @@
+/**
+ * External dependencies
+ */
+import { useContext } from '@wordpress/element';
+import { expect } from '@storybook/jest';
+import {
+	fireEvent,
+	userEvent,
+	waitFor,
+	within,
+} from '@storybook/testing-library';
+
+/**
+ * Publisher dependencies
+ */
+import { Flex } from '@publisher/components';
+import { default as Decorators } from '@publisher/storybook/decorators';
+
+/**
+ * Internal dependencies
+ */
+import TransitionControl from '../index';
+
+const {
+	WithInspectorStyles,
+	StoryDataContext,
+	WithStoryContextProvider,
+	SharedDecorators,
+} = Decorators;
+
+export default {
+	title: 'Controls/TransitionControl',
+	component: TransitionControl,
+	tags: ['autodocs'],
+};
+
+export const Empty = {
+	args: {
+		label: 'Transitions',
+	},
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+};
+
+export const Fill = {
+	args: {
+		label: 'Transitions',
+	},
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+	render: (args) => {
+		return (
+			<Flex direction="column" gap="50px">
+				<Flex direction="column" gap="15px">
+					<h2 className="story-heading">Filled</h2>
+					<TransitionControl
+						{...args}
+						label="Transitions"
+						value={[
+							{
+								type: 'all',
+								duration: '250ms',
+								timing: 'ease',
+								delay: '10ms',
+								isVisible: true,
+							},
+							{
+								type: 'opacity',
+								duration: '600ms',
+								timing: 'ease',
+								delay: '0ms',
+								isVisible: true,
+							},
+						]}
+					/>
+				</Flex>
+			</Flex>
+		);
+	},
+};
+
+const ControlWithHooks = (args) => {
+	const { storyValue, setStoryValue } = useContext(StoryDataContext);
+
+	return (
+		<TransitionControl
+			{...args}
+			onChange={setStoryValue}
+			value={storyValue}
+		/>
+	);
+};
+
+export const Play = {
+	args: {
+		label: 'Transitions',
+	},
+	decorators: [
+		WithStoryContextProvider,
+		WithInspectorStyles,
+		...SharedDecorators,
+	],
+	render: (args) => <ControlWithHooks {...args} />,
+	play: async ({ canvasElement, step }) => {
+		const canvas = within(canvasElement);
+
+		const currentValue = canvas.getByTestId('current-value');
+		const button = canvas.getByLabelText('Add New');
+		//
+		await step('Story Data', async () => {
+			await expect(currentValue).toBeInTheDocument();
+			await expect(currentValue).toBeEmptyDOMElement();
+		});
+
+		await step('Click Add Button', async () => {
+			await expect(button).toBeInTheDocument();
+
+			await userEvent.click(button);
+			await waitFor(
+				async () =>
+					await expect(currentValue).toHaveTextContent(
+						'[ { "type": "all", "duration": "500ms", "timing": "ease", "delay": "0ms", "isVisible": true } ]'
+					),
+				{ timeout: 1000 }
+			);
+		});
+
+		await step('Open Popover', async () => {
+			await userEvent.click(canvas.getByLabelText('Item 1'));
+		});
+
+		await step('Change Duration', async () => {
+			const inputs = canvas.getAllByRole('textbox');
+
+			userEvent.type(
+				inputs[0],
+				'{backspace}{backspace}{backspace}100{enter}'
+			);
+
+			await waitFor(
+				async () =>
+					await expect(currentValue).toHaveTextContent(
+						'[ { "type": "all", "duration": "100ms", "timing": "ease", "delay": "0ms", "isVisible": true } ]'
+					),
+				{ timeout: 1000 }
+			);
+		});
+
+		await step('Change Type', async () => {
+			const selects = canvas.getAllByRole('combobox', {});
+
+			fireEvent.change(selects[0], {
+				target: { value: 'opacity' },
+			});
+
+			await waitFor(
+				async () =>
+					await expect(currentValue).toHaveTextContent(
+						'[ { "type": "opacity", "duration": "100ms", "timing": "ease", "delay": "0ms", "isVisible": true } ]'
+					),
+				{ timeout: 1000 }
+			);
+		});
+	},
+};
+
+export const Screenshot = {
+	args: {},
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+	render: () => (
+		<Flex direction="column" gap="50px">
+			<Flex direction="column" gap="15px">
+				<h2 className="story-heading">Empty</h2>
+				<TransitionControl {...Empty.args} />
+			</Flex>
+
+			<Fill.render />
+		</Flex>
+	),
+};
