c4b146872580f8aa54b528a0cbec8641d6d316c8
Ali Aghdam <betteraliaghdam@gmail.com>
2023-07-03 10:23:11 +0330

    aria-label support

--- packages/controls/src/label-control/index.js
+++ packages/controls/src/label-control/index.js
@@ -3,13 +3,19 @@
  */
 import { controlClassNames } from '@publisher/classnames';
 
-export default function LabelControl({ label, className, ...props }) {
+export default function LabelControl({
+	label,
+	className,
+	ariaLabel = '',
+	...props
+}) {
 	return (
 		<>
 			{label && (
 				<span
 					{...props}
 					className={controlClassNames('label', className)}
+					aria-label={ariaLabel || label}
 				>
 					{label}
 				</span>
