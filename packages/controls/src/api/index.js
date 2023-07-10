91a87076f945d18c292653279d454e4294d51ee4
reza.develop <relahi1373@gmail.com>
2023-07-10 19:32:19 +0330

    perf: explanation of registerControl API

--- packages/controls/src/api/index.js
+++ packages/controls/src/api/index.js
@@ -13,17 +13,21 @@ import { isString } from '@publisher/utils';
  */
 import { store } from '../store';
 
+//Register control into store to use in control context provider
 export function registerControl({ name, ...control }): Object | undefined {
 	if (!isString(name)) {
 		return;
 	}
 
+	//get `publisher-core/controls` store or details of that
 	const { getControl } = select(store);
 
+	//Check is registered control or not!
 	if (getControl(name)) {
 		return;
 	}
 
+	//Create control schema
 	const _control = {
 		name,
 		value: null,
@@ -32,9 +36,12 @@ export function registerControl({ name, ...control }): Object | undefined {
 		...control,
 	};
 
+	//get `addControl` of `publisher-core/controls` store dispatchers
 	const { addControl } = dispatch(store);
 
+	//fire add control action
 	addControl({ name, ..._control });
 
+	//retrieved control information ✅
 	return getControl(name);
 }
