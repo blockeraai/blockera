9188156c33423ef866ea16a906057501780b1093
Ali Aghdam <betteraliaghdam@gmail.com>
2023-07-28 12:13:54 +0330

    refactor: MediaImageControl to new Field and useControlContext

--- packages/controls/src/libs/media-image-control/stories/index.stories.js
+++ packages/controls/src/libs/media-image-control/stories/index.stories.js
@@ -1,4 +1,9 @@
 /**
+ * External dependencies
+ */
+import { nanoid } from 'nanoid';
+
+/**
  * Publisher dependencies
  */
 import { Flex } from '@publisher/components';
@@ -8,7 +13,9 @@ import { default as Decorators } from '@publisher/storybook/decorators';
  * Internal dependencies
  */
 import { WithPlaygroundStyles } from '../../../../../../.storybook/preview';
-import { MediaImageControl } from '../../index';
+import { ControlContextProvider, MediaImageControl } from '../../../index';
+import { WithControlDataProvider } from '../../../../../../.storybook/decorators/with-control-data-provider';
+import ControlWithHooks from '../../../../../../.storybook/components/control-with-hooks';
 
 const { WithInspectorStyles, SharedDecorators, WithPopoverDataProvider } =
 	Decorators;
@@ -24,10 +31,21 @@ export default {
 export const Empty = {
 	args: {
 		label: '',
+		controlInfo: {
+			name: nanoid(),
+			value: '',
+		},
 	},
+	render: (args) => (
+		<Flex direction="column" gap="20px">
+			<h2 className="story-heading">Empty Media</h2>
+			<ControlWithHooks Control={MediaImageControl} {...args} />
+		</Flex>
+	),
 	decorators: [
 		WithInspectorStyles,
 		WithPopoverDataProvider,
+		WithControlDataProvider,
 		...SharedDecorators,
 	],
 };
@@ -40,45 +58,86 @@ export const Filled = {
 	render: (args) => {
 		return (
 			<Flex direction="column" gap="50px">
-				<Flex direction="column" gap="15px">
+				<Flex direction="column" gap="20px">
 					<h2 className="story-heading">
 						Image<span>Filled</span>
 					</h2>
-					<MediaImageControl
-						{...args}
-						value="https://betterstudio.com/wp-content/uploads/2022/09/publisher-theme.svg"
-					/>
+
+					<ControlContextProvider
+						value={{
+							name: nanoid(),
+							value: 'https://betterstudio.com/wp-content/uploads/2022/09/publisher-theme.svg',
+						}}
+					>
+						<ControlWithHooks
+							Control={MediaImageControl}
+							{...args}
+						/>
+					</ControlContextProvider>
 				</Flex>
 
-				<Flex direction="column" gap="15px">
+				<Flex direction="column" gap="20px">
 					<h2 className="story-heading">
 						Image<span>Large Image</span>
 					</h2>
-					<MediaImageControl
-						{...args}
-						value="https://betterstudio.com/wp-content/uploads/2023/05/Best-WooCommerce-Currency-Switcher-Plugins.png"
-					/>
+
+					<ControlContextProvider
+						value={{
+							name: nanoid(),
+							value: 'https://betterstudio.com/wp-content/uploads/2023/05/Best-WooCommerce-Currency-Switcher-Plugins.png',
+						}}
+					>
+						<ControlWithHooks
+							Control={MediaImageControl}
+							{...args}
+						/>
+					</ControlContextProvider>
 				</Flex>
 			</Flex>
 		);
 	},
 };
 
-export const Screenshot = {
+export const Field = {
+	args: {
+		label: 'Field',
+	},
+	render: (args) => (
+		<Flex direction="column" gap="20px">
+			<h2 className="story-heading">With Field</h2>
+			<ControlContextProvider
+				value={{
+					name: nanoid(),
+					value: 'https://betterstudio.com/wp-content/uploads/2022/09/publisher-theme.svg',
+				}}
+			>
+				<ControlWithHooks Control={MediaImageControl} {...args} />
+			</ControlContextProvider>
+		</Flex>
+	),
+	decorators: [
+		WithInspectorStyles,
+		WithPopoverDataProvider,
+		WithControlDataProvider,
+		...SharedDecorators,
+	],
+};
+
+export const All = {
 	args: {},
 	decorators: [
 		WithInspectorStyles,
 		WithPopoverDataProvider,
+		WithControlDataProvider,
 		...SharedDecorators,
 	],
 	render: () => (
 		<Flex direction="column" gap="50px">
-			<Flex direction="column" gap="15px">
-				<h2 className="story-heading">Empty</h2>
-				<MediaImageControl {...Empty.args} />
-			</Flex>
+			<Empty.render {...Empty.args} />
 
-			<Filled.render />
+			<Filled.render {...Filled.args} />
+
+			<Field.render {...Field.args} />
 		</Flex>
 	),
 };
