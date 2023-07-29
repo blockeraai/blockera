10e9e3756b6b2771a0c0faebd1a56c5fb8a254a7
Ali Aghdam <betteraliaghdam@gmail.com>
2023-07-29 18:40:03 +0330

    refactor: BorderControl to use new control context and compacted Field

--- packages/controls/src/libs/border-control/index.js
+++ packages/controls/src/libs/border-control/index.js
@@ -7,11 +7,12 @@ import PropTypes from 'prop-types';
  * Publisher dependencies
  */
 import { controlClassNames } from '@publisher/classnames';
-import { useValue } from '@publisher/utils';
+import { Field } from '@publisher/fields';
 
 /**
  * Publisher dependencies
  */
+import { useControlContext } from '../../context';
 import { InputControl, SelectControl, ColorControl } from '../index';
 import BorderStyleHSolidIcon from './icons/style-h-solid';
 import BorderStyleHDashedIcon from './icons/style-h-dashed';
@@ -25,13 +26,14 @@ import BorderStyleVDoubleIcon from './icons/style-v-double';
 export default function BorderControl({
 	linesDirection,
 	customMenuPosition,
-	//
-	defaultValue,
-	value: initialValue,
-	onChange = (newValue) => {
-		return newValue;
-	},
 	style,
+	//
+	id,
+	label,
+	columns,
+	defaultValue,
+	onChange,
+	field,
 	//
 	className,
 	// internal usage for stories
@@ -39,105 +41,136 @@ export default function BorderControl({
 	__isColorFocused,
 	__isStyleFocused,
 }) {
-	const { value, setValue } = useValue({
-		initialValue,
-		defaultValue,
+	const { value, setValue } = useControlContext({
+		id,
 		onChange,
+		defaultValue,
 		mergeInitialAndDefault: true,
 	});
 
 	return (
-		<div className={controlClassNames('border', className)} style={style}>
-			<InputControl
-				min="0"
-				unitType="custom"
-				units={[{ value: 'px', label: 'PX', default: 0 }]}
-				// type="number"
-				className={controlClassNames(
-					'input',
-					__isWidthFocused && 'is-focused'
-				)}
-				noBorder={true}
-				value={value.width}
-				onChange={(newValue) => {
-					setValue({ ...value, width: newValue });
-				}}
-			/>
+		<Field
+			label={label}
+			field={field}
+			columns={columns}
+			className={className}
+		>
+			<div
+				className={controlClassNames('border', className)}
+				style={style}
+			>
+				<InputControl
+					min="0"
+					unitType="custom"
+					units={[{ value: 'px', label: 'PX', default: 0 }]}
+					className={controlClassNames(
+						'input',
+						__isWidthFocused && 'is-focused'
+					)}
+					id="width"
+					noBorder={true}
+					onChange={(newValue) => {
+						setValue({ ...value, width: newValue });
+					}}
+				/>
 
-			<ColorControl
-				type="minimal"
-				noBorder={true}
-				value={value.color}
-				onChange={(newValue) => {
-					setValue({ ...value, color: newValue });
-				}}
-				className={__isColorFocused && 'is-focus'}
-			/>
+				<ColorControl
+					type="minimal"
+					noBorder={true}
+					id="color"
+					onChange={(newValue) => {
+						setValue({ ...value, color: newValue });
+					}}
+					className={__isColorFocused && 'is-focus'}
+				/>
 
-			<SelectControl
-				className={__isStyleFocused && 'is-focused'}
-				customMenuPosition={customMenuPosition}
-				type="custom"
-				customInputCenterContent={true}
-				customHideInputCaret={true}
-				customHideInputLabel={true}
-				noBorder={true}
-				value={value.style}
-				options={[
-					{
-						label: '',
-						icon:
-							linesDirection === 'horizontal' ? (
-								<BorderStyleHSolidIcon />
-							) : (
-								<BorderStyleVSolidIcon />
-							),
-						value: 'solid',
-						className: 'align-center',
-					},
-					{
-						label: '',
-						icon:
-							linesDirection === 'horizontal' ? (
-								<BorderStyleHDashedIcon />
-							) : (
-								<BorderStyleVDashedIcon />
-							),
-						value: 'dashed',
-						className: 'align-center',
-					},
-					{
-						label: '',
-						icon:
-							linesDirection === 'horizontal' ? (
-								<BorderStyleHDottedIcon />
-							) : (
-								<BorderStyleVDottedIcon />
-							),
-						value: 'dotted',
-						className: 'align-center',
-					},
-					{
-						label: '',
-						icon:
-							linesDirection === 'horizontal' ? (
-								<BorderStyleHDoubleIcon />
-							) : (
-								<BorderStyleVDoubleIcon />
-							),
-						value: 'double',
-						className: 'align-center',
-					},
-				]}
-				onChange={(newValue) => {
-					setValue({ ...value, style: newValue });
-				}}
-			/>
-		</div>
+				<SelectControl
+					id="style"
+					className={__isStyleFocused && 'is-focused'}
+					customMenuPosition={customMenuPosition}
+					type="custom"
+					customInputCenterContent={true}
+					customHideInputCaret={true}
+					customHideInputLabel={true}
+					noBorder={true}
+					options={[
+						{
+							label: '',
+							icon:
+								linesDirection === 'horizontal' ? (
+									<BorderStyleHSolidIcon />
+								) : (
+									<BorderStyleVSolidIcon />
+								),
+							value: 'solid',
+							className: 'align-center',
+						},
+						{
+							label: '',
+							icon:
+								linesDirection === 'horizontal' ? (
+									<BorderStyleHDashedIcon />
+								) : (
+									<BorderStyleVDashedIcon />
+								),
+							value: 'dashed',
+							className: 'align-center',
+						},
+						{
+							label: '',
+							icon:
+								linesDirection === 'horizontal' ? (
+									<BorderStyleHDottedIcon />
+								) : (
+									<BorderStyleVDottedIcon />
+								),
+							value: 'dotted',
+							className: 'align-center',
+						},
+						{
+							label: '',
+							icon:
+								linesDirection === 'horizontal' ? (
+									<BorderStyleHDoubleIcon />
+								) : (
+									<BorderStyleVDoubleIcon />
+								),
+							value: 'double',
+							className: 'align-center',
+						},
+					]}
+					onChange={(newValue) => {
+						setValue({ ...value, style: newValue });
+					}}
+				/>
+			</div>
+		</Field>
 	);
 }
 
 BorderControl.propTypes = {
+	/**
+	 * Indicates border-line icons direction
+	 */
+	linesDirection: PropTypes.oneOf(['horizontal', 'vertical']),
+	/**
+	 * Label for field. If you pass empty value the field will not be added and simple control will be rendered
+	 *
+	 * @default ""
+	 */
+	label: PropTypes.string,
+	/**
+	 * Field id for passing into child Field component
+	 *
+	 * @default "toggle-select"
+	 */
+	field: PropTypes.string,
+	/**
+	 * Columns setting for Field grid.
+	 *
+	 * @default "columns-2"
+	 */
+	columns: PropTypes.string,
 	/**
 	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
 	 */
@@ -147,24 +180,13 @@ BorderControl.propTypes = {
 		color: PropTypes.string,
 	}),
 	/**
-	 * The current value.
-	 */
-	value: PropTypes.shape({
-		width: PropTypes.string,
-		style: PropTypes.oneOf(['solid', 'dashed', 'dotted', 'double']),
-		color: PropTypes.string,
-	}),
-	/**
 	 * Function that will be fired while the control value state changes.
 	 */
 	onChange: PropTypes.func,
-	/**
-	 * Indicates border-line icons direction
-	 */
-	linesDirection: PropTypes.oneOf(['horizontal', 'vertical']),
 };
 
 BorderControl.defaultProps = {
+	field: 'border',
 	linesDirection: 'horizontal',
 	defaultValue: {
 		width: '0px',
