ba491edcce7c6e0dfb2ec5410fced48d380ca730
Ali Aghdam <betteraliaghdam@gmail.com>
2023-07-29 17:49:08 +0330

    refactor: ColorControl to use new control context

--- packages/controls/src/libs/color-control/control.js
+++ packages/controls/src/libs/color-control/control.js
@@ -9,38 +9,44 @@ import { useState } from '@wordpress/element';
  */
 import { controlClassNames } from '@publisher/classnames';
 import { ColorIndicator, Button } from '@publisher/components';
+import { Field } from '@publisher/fields';
 
 /**
  * Internal dependencies
  */
 import { ColorPickerControl } from '../index';
 import PropTypes from 'prop-types';
-import { useValue } from '@publisher/utils';
+import { useControlContext } from '../../context';
 
 export default function ColorControl({
 	type,
-	defaultValue,
-	value: initialValue,
-	onChange,
 	noBorder,
 	contentAlign,
+	//
+	id,
+	label,
+	columns,
+	defaultValue,
+	onChange,
+	field,
+	//
 	//
 	className,
 	style,
 	...props
 }) {
-	const { value, setValue } = useValue({
-		defaultValue,
-		initialValue,
+	const { value, setValue } = useControlContext({
+		id,
 		onChange,
+		defaultValue,
 	});
 
 	const [isOpen, setOpen] = useState(false);
 
-	let label = '';
+	let buttonLabel = '';
 
 	if (type === 'normal') {
-		label = value ? (
+		buttonLabel = value ? (
 			<span className="color-label">{value}</span>
 		) : (
 			<span className="color-label">{__('None', 'publisher-core')}</span>
@@ -48,7 +54,12 @@ export default function ColorControl({
 	}
 
 	return (
-		<>
+		<Field
+			label={label}
+			field={field}
+			columns={columns}
+			className={className}
+		>
 			<Button
 				size="input"
 				className={controlClassNames(
@@ -69,11 +80,12 @@ export default function ColorControl({
 			>
 				<ColorIndicator type="color" value={value} />
 
-				{label}
+				{buttonLabel}
 			</Button>
 
 			{isOpen && (
 				<ColorPickerControl
+					id={id}
 					isPopover={true}
 					isOpen={isOpen}
 					onClose={() => setOpen(false)}
@@ -82,23 +94,11 @@ export default function ColorControl({
 					{...props}
 				/>
 			)}
-		</>
+		</Field>
 	);
 }
 
 ColorControl.propTypes = {
-	/**
-	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
-	 */
-	defaultValue: PropTypes.string,
-	/**
-	 * The current value.
-	 */
-	value: PropTypes.string,
-	/**
-	 * Function that will be fired while the control value state changes.
-	 */
-	onChange: PropTypes.func,
 	/**
 	 * By using this you can prevent the control to show the border and outline shape.
 	 */
@@ -111,9 +111,36 @@ ColorControl.propTypes = {
 	 * It is useful for buttons with specified width and allows you to align the content to `left` or `right`. By default, it's `center` and handled by flex justify-content property.
 	 */
 	contentAlign: PropTypes.oneOf(['left', 'center', 'right']),
+	/**
+	 * Label for field. If you pass empty value the field will not be added and simple control will be rendered
+	 *
+	 * @default ""
+	 */
+	label: PropTypes.string,
+	/**
+	 * Field id for passing into child Field component
+	 *
+	 * @default "toggle-select"
+	 */
+	field: PropTypes.string,
+	/**
+	 * Columns setting for Field grid.
+	 *
+	 * @default "columns-2"
+	 */
+	columns: PropTypes.string,
+	/**
+	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
+	 */
+	defaultValue: PropTypes.string,
+	/**
+	 * Function that will be fired while the control value state changes.
+	 */
+	onChange: PropTypes.func,
 };
 
 ColorControl.defaultProps = {
 	type: 'normal',
 	contentAlign: 'left',
+	field: 'color',
 };
