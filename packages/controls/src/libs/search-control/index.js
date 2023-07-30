3520cdee9915ac7d6f8c151726b0303e32c08dfa
Ali Aghdam <betteraliaghdam@gmail.com>
2023-07-30 10:47:23 +0330

    refactor: update SearchControl to new control context and compacted Field inside component

--- packages/controls/src/libs/search-control/index.js
+++ packages/controls/src/libs/search-control/index.js
@@ -9,52 +9,82 @@ import PropTypes from 'prop-types';
  * Publisher dependencies
  */
 import { controlClassNames } from '@publisher/classnames';
-import { useValue } from '@publisher/utils';
+import { Field } from '@publisher/fields';
+
+/**
+ * Internal dependencies
+ */
+import { useControlContext } from '../../context';
 
 export default function SearchControl({
-	value: initialValue,
+	id,
+	label,
+	columns,
 	defaultValue,
 	onChange,
+	field,
+	//
 	className,
 	...props
 }) {
-	const { value, setValue } = useValue({
-		initialValue,
-		defaultValue,
+	const { value, setValue } = useControlContext({
+		id,
 		onChange,
+		defaultValue,
 	});
 
 	return (
-		<WPSearchControl
-			value={value}
-			onChange={setValue}
-			className={controlClassNames('search', className)}
-			{...props}
-		/>
+		<Field
+			label={label}
+			field={field}
+			columns={columns}
+			className={className}
+		>
+			<WPSearchControl
+				value={value}
+				onChange={setValue}
+				className={controlClassNames('search', className)}
+				{...props}
+			/>
+		</Field>
 	);
 }
 
 SearchControl.propTypes = {
 	/**
-	 * A placeholder for the input.
+	 * Label for field. If you pass empty value the field will not be added and simple control will be rendered
+	 *
+	 * @default ""
 	 */
-	placeholder: PropTypes.string,
+	label: PropTypes.string,
+	/**
+	 * Field id for passing into child Field component
+	 *
+	 * @default "toggle-select"
+	 */
+	field: PropTypes.string,
+	/**
+	 * Columns setting for Field grid.
+	 *
+	 * @default "columns-2"
+	 */
+	columns: PropTypes.string,
 	/**
 	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
 	 */
 	defaultValue: PropTypes.string,
 	/**
-	 * The current value.
-	 */
-	value: PropTypes.string,
-	/**
 	 * Function that will be fired while the control value state changes.
 	 */
 	onChange: PropTypes.func,
+	/**
+	 * A placeholder for the input.
+	 */
+	placeholder: PropTypes.string,
 };
 
 SearchControl.defaultProps = {
 	placeholder: __('Search…', 'publisher-core'),
-	value: '',
 	defaultValue: '',
+	field: 'search',
 };
