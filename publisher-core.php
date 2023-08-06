841ba95e3c184630934d386278d593da561528c2
rezaelahidev <rezaelahi.dev@gmail.com>
2023-08-06 10:47:25 +0330

    chor: modified phpcs.xml

--- publisher-core.php
+++ publisher-core.php
@@ -10,6 +10,8 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       publisher-core
+ *
+ * @package PublisherCore
  */
 
 # loading autoloader
