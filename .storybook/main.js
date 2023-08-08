995de41772a6a911f22c2e9293db2db131ea7def
rezaelahidev <rezaelahi.dev@gmail.com>
2023-08-08 13:29:47 +0330

    build(storybook): remove redunant pattern

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,7 @@
 import { defaultConfig } from '@publisher/storybook';
 
-process.env.NODE_ENV = 'DEVELOPMENT';
-
 export default {
 	stories: [
-		'../packages/fields/src/**/stories/*.stories.@(js|jsx|ts|tsx)',
 		'../packages/controls/src/**/stories/*.stories.@(js|jsx|ts|tsx)',
 		'../packages/components/src/**/stories/*.stories.@(js|jsx|ts|tsx)',
 		'../packages/extensions/src/**/stories/*.stories.@(js|jsx|ts|tsx)',
