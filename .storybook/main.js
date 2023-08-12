3fdbd88976bddc434fa63033c7b4bc330b26a696
rezaelahidev <rezaelahi.dev@gmail.com>
2023-08-12 16:45:21 +0330

    chor: modified storybook/main.js to handle webpack problem

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,4 +7,41 @@ export default {
 		'../packages/extensions/src/**/stories/*.stories.@(js|jsx|ts|tsx)',
 	],
 	...defaultConfig,
+	async webpackFinal(config, { configType }) {
+		if (configType === 'DEVELOPMENT') {
+			//TODO: config for development if necessary
+		}
+		if (configType === 'PRODUCTION') {
+			//TODO: config for production if necessary
+		}
+
+		// shared config
+		config.module.rules.push(
+			{
+				test: /\.scss$/,
+				exclude: /\.lazy\.scss$/,
+				use: [
+					{
+						loader: 'style-loader',
+						options: { injectType: 'styleTag' },
+					},
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.lazy\.scss$/,
+				use: [
+					{
+						loader: 'style-loader',
+						options: { injectType: 'lazyStyleTag' },
+					},
+					'css-loader',
+					'sass-loader',
+				],
+			}
+		);
+
+		return config;
+	},
 };
