ff444ecb10f68bc266cb00ae6472e994ca70e7a5
rezaelahidev <rezaelahi.dev@gmail.com>
2023-08-15 09:57:54 +0330

    refactor(context-api): size/extension.js

--- packages/extensions/src/libs/size/extension.js
+++ packages/extensions/src/libs/size/extension.js
@@ -1,5 +1,5 @@
 /**
- * WordPress dependencies
+ * External dependencies
  */
 import { __ } from '@wordpress/i18n';
 import { useContext } from '@wordpress/element';
@@ -7,11 +7,13 @@ import { useContext } from '@wordpress/element';
 /**
  * Publisher dependencies
  */
+import { ControlContextProvider } from '@publisher/controls';
 import { InputField, ToggleSelectField } from '@publisher/fields';
 
 /**
  * Internal dependencies
  */
+import { generateExtensionId } from '../utils';
 import { BlockEditContext } from '../../hooks';
 import { isActiveField } from '../../api/utils';
 import { default as OverflowVisibleIcon } from './icons/overflow-visible';
@@ -28,79 +30,97 @@ export function SizeExtension({ children, config, ...props }) {
 	return (
 		<>
 			{isActiveField(publisherWidth) && (
-				<InputField
-					{...{
-						...props,
-						label: __('Width', 'publisher-core'),
-						settings: {
-							type: 'css',
-							unitType: 'essential',
-							min: 0,
-							defaultValue: '',
-						},
-						//
+				<ControlContextProvider
+					value={{
+						name: generateExtensionId(props.blockName, 'width'),
 						value: attributes.publisherWidth,
-						onChange: (newValue) =>
-							setAttributes({
-								...attributes,
-								publisherWidth: newValue,
-							}),
 					}}
-				/>
+				>
+					<InputField
+						{...{
+							...props,
+							label: __('Width', 'publisher-core'),
+							settings: {
+								type: 'css',
+								unitType: 'essential',
+								min: 0,
+								defaultValue: '',
+							},
+							//
+							onChange: (newValue) =>
+								setAttributes({
+									...attributes,
+									publisherWidth: newValue,
+								}),
+						}}
+					/>
+				</ControlContextProvider>
 			)}
 
 			{isActiveField(publisherHeight) && (
-				<InputField
-					{...{
-						...props,
-						label: __('Height', 'publisher-core'),
-						settings: {
-							type: 'css',
-							unitType: 'essential',
-							min: 0,
-							defaultValue: '',
-						},
-						//
+				<ControlContextProvider
+					value={{
+						name: generateExtensionId(props.blockName, 'height'),
 						value: attributes.publisherHeight,
-						onChange: (newValue) =>
-							setAttributes({
-								...attributes,
-								publisherHeight: newValue,
-							}),
 					}}
-				/>
+				>
+					<InputField
+						{...{
+							...props,
+							label: __('Height', 'publisher-core'),
+							settings: {
+								type: 'css',
+								unitType: 'essential',
+								min: 0,
+								defaultValue: '',
+							},
+							//
+							onChange: (newValue) =>
+								setAttributes({
+									...attributes,
+									publisherHeight: newValue,
+								}),
+						}}
+					/>
+				</ControlContextProvider>
 			)}
 
 			{isActiveField(publisherOverflow) && (
-				<ToggleSelectField
-					label={__('Overflow', 'publisher-core')}
-					options={[
-						{
-							label: __('Visible', 'publisher-core'),
-							value: 'visible',
-							icon: <OverflowVisibleIcon />,
-						},
-						{
-							label: __('Hidden', 'publisher-core'),
-							value: 'hidden',
-							icon: <OverflowHiddenIcon />,
-						},
-						{
-							label: __('Scroll', 'publisher-core'),
-							value: 'scroll',
-							icon: <OverflowScrollIcon />,
-						},
-					]}
-					//
-					defaultValue="visible"
-					value={attributes.publisherOverflow}
-					onChange={(newValue) =>
-						setAttributes({
-							...attributes,
-							publisherOverflow: newValue,
-						})
-					}
-				/>
+				<ControlContextProvider
+					value={{
+						name: generateExtensionId(props.blockName, 'overflow'),
+						value: attributes.publisherOverflow,
+					}}
+				>
+					<ToggleSelectField
+						label={__('Overflow', 'publisher-core')}
+						options={[
+							{
+								label: __('Visible', 'publisher-core'),
+								value: 'visible',
+								icon: <OverflowVisibleIcon />,
+							},
+							{
+								label: __('Hidden', 'publisher-core'),
+								value: 'hidden',
+								icon: <OverflowHiddenIcon />,
+							},
+							{
+								label: __('Scroll', 'publisher-core'),
+								value: 'scroll',
+								icon: <OverflowScrollIcon />,
+							},
+						]}
+						//
+						defaultValue="visible"
+						onChange={(newValue) =>
+							setAttributes({
+								...attributes,
+								publisherOverflow: newValue,
+							})
+						}
+					/>
+				</ControlContextProvider>
 			)}
 		</>
 	);
