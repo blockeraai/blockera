9f31265f0c1732e40f24749b81e6fe7f6120bc80
rezaelahidev <rezaelahi.dev@gmail.com>
2023-08-15 10:07:35 +0330

    refactor: global utils.js into extensions pack

--- packages/extensions/src/libs/index.js
+++ packages/extensions/src/libs/index.js
@@ -1,3 +1,4 @@
+export * from './utils';
 export {
 	LayoutExtensionIcon,
 	LayoutStyles,
--- packages/extensions/src/libs/utils.js
+++ packages/extensions/src/libs/utils.js
@@ -5,6 +5,19 @@
  * @param {Array<any>} b second array
  * @return {boolean} true on success, false when otherwise!
  */
+import { nanoid } from 'nanoid';
+
 export function arrayEquals(a: Array, b: Array): boolean {
 	return JSON.stringify(a) === JSON.stringify(b);
+}
+
+/**
+ * Creation extension id with block name.
+ *
+ * @param {string} blockName
+ * @param {string} id
+ * @return {string} retrieved extension standard identifier.
+ */
+export function generateExtensionId(blockName, id) {
+	return `${blockName}/${id}-${nanoid()}`;
 }
