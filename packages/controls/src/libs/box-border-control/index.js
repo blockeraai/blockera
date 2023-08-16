4727bf5b3dde4185f470a006ea7bbb99d0fcf811
rezaelahidev <rezaelahi.dev@gmail.com>
2023-08-16 08:48:21 +0330

    refactor(context-api): box-border control

--- packages/controls/src/libs/box-border-control/index.js
+++ packages/controls/src/libs/box-border-control/index.js
@@ -15,30 +15,35 @@ import {
 /**
  * Internal dependencies
  */
-import { BorderControl, LabelControl, ToggleSelectControl } from '../index';
 import { useControlContext } from '../../context';
-import { default as CompactIcon } from './icons/compact';
 import { default as CustomIcon } from './icons/custom';
+import { default as CompactIcon } from './icons/compact';
+import { BorderControl, LabelControl, ToggleSelectControl } from '../index';
 
-export default function BoxBorderControl( {
+export default function BoxBorderControl({
 	id,
 	label,
 	defaultValue,
 	onChange,
 	//
 	className,
-} ) {
-	const { value, setValue } = useControlContext( {
+}) {
+	const {
+		value,
+		setValue,
+		controlInfo: { name: controlId },
+		dispatch: { modifyControlValue },
+	} = useControlContext({
 		id,
 		onChange,
 		defaultValue,
 		valueCleanup,
 		mergeInitialAndDefault: true,
-	} );
+	});
 
 	// value clean up for removing extra values to prevent saving extra data!
-	function valueCleanup( value ) {
-		if ( value.type === 'all' ) {
+	function valueCleanup(value) {
+		if (value.type === 'all') {
 			delete value?.top;
 			delete value?.right;
 			delete value?.bottom;
@@ -49,65 +54,87 @@ export default function BoxBorderControl( {
 	}
 
 	return (
-		<div className={ controlClassNames( 'box-border', className ) }>
-			<div className={ controlInnerClassNames( 'border-header' ) }>
-				{ label && (
-					<div className={ controlInnerClassNames( 'label' ) }>
-						<LabelControl label={ label } />
+		<div className={controlClassNames('box-border', className)}>
+			<div className={controlInnerClassNames('border-header')}>
+				{label && (
+					<div className={controlInnerClassNames('label')}>
+						<LabelControl label={label} />
 					</div>
-				) }
+				)}
 
-				{ value.type === 'all' && (
+				{value.type === 'all' && (
 					<BorderControl
 						id="all"
-						onChange={ ( newValue ) => {
-							setValue( { ...value, all: newValue } );
-						} }
+						onChange={(newValue) => {
+							setValue({ ...value, all: newValue });
+							modifyControlValue({
+								controlId,
+								value: { ...value, all: newValue },
+							});
+						}}
 					/>
-				) }
+				)}
 
 				<ToggleSelectControl
 					id="type"
 					defaultValue="compact"
-					options={ [
+					options={[
 						{
-							label: __( 'Compact', 'publisher-core' ),
+							label: __('Compact', 'publisher-core'),
 							value: 'all',
 							icon: <CompactIcon />,
 						},
 						{
-							label: __( 'Custom', 'publisher-core' ),
+							label: __('Custom', 'publisher-core'),
 							value: 'custom',
 							icon: <CustomIcon />,
 						},
-					] }
-					onChange={ ( newValue ) => {
-						if ( newValue === 'custom' ) {
-							setValue( {
+					]}
+					onChange={(newValue) => {
+						if (newValue === 'custom') {
+							setValue({
 								...value,
 								type: newValue,
 								top: value.all,
 								right: value.all,
 								bottom: value.all,
 								left: value.all,
-							} );
+							});
+							modifyControlValue({
+								controlId,
+								value: {
+									...value,
+									type: newValue,
+									top: value.all,
+									right: value.all,
+									bottom: value.all,
+									left: value.all,
+								},
+							});
 						} else {
-							setValue( {
+							setValue({
 								...value,
 								type: newValue,
-							} );
+							});
+							modifyControlValue({
+								controlId,
+								value: {
+									...value,
+									type: newValue,
+								},
+							});
 						}
-					} }
+					}}
 				/>
 			</div>
 
-			{ value.type === 'custom' && (
-				<div className={ controlInnerClassNames( 'border-corners' ) }>
+			{value.type === 'custom' && (
+				<div className={controlInnerClassNames('border-corners')}>
 					<div
-						className={ controlInnerClassNames(
+						className={controlInnerClassNames(
 							'border-corners-preview'
-						) }
-						style={ {
+						)}
+						style={{
 							'--pb-top-width': value.top.width,
 							'--pb-top-style': value.top.style,
 							'--pb-top-color': value.top.color,
@@ -120,61 +147,89 @@ export default function BoxBorderControl( {
 							'--pb-left-width': value.left.width,
 							'--pb-left-style': value.left.style,
 							'--pb-left-color': value.left.color,
-						} }
+						}}
 					>
 						<BorderControl
 							id="top"
-							className={ controlInnerClassNames(
+							className={controlInnerClassNames(
 								'border-corner-top'
-							) }
-							onChange={ ( newValue ) => {
-								setValue( {
+							)}
+							onChange={(newValue) => {
+								setValue({
 									...value,
 									top: newValue,
-								} );
-							} }
+								});
+								modifyControlValue({
+									controlId,
+									value: {
+										...value,
+										top: newValue,
+									},
+								});
+							}}
 						/>
 						<BorderControl
 							id="right"
 							linesDirection="vertical"
-							className={ controlInnerClassNames(
+							className={controlInnerClassNames(
 								'border-corner-right'
-							) }
-							onChange={ ( newValue ) => {
-								setValue( {
+							)}
+							onChange={(newValue) => {
+								setValue({
 									...value,
 									right: newValue,
-								} );
-							} }
+								});
+								modifyControlValue({
+									controlId,
+									value: {
+										...value,
+										right: newValue,
+									},
+								});
+							}}
 						/>
 						<BorderControl
 							id="bottom"
-							className={ controlInnerClassNames(
+							className={controlInnerClassNames(
 								'border-corner-bottom'
-							) }
-							onChange={ ( newValue ) => {
-								setValue( {
+							)}
+							onChange={(newValue) => {
+								setValue({
 									...value,
 									bottom: newValue,
-								} );
-							} }
+								});
+								modifyControlValue({
+									controlId,
+									value: {
+										...value,
+										bottom: newValue,
+									},
+								});
+							}}
 						/>
 						<BorderControl
 							id="left"
 							linesDirection="vertical"
-							className={ controlInnerClassNames(
+							className={controlInnerClassNames(
 								'border-corner-left'
-							) }
-							onChange={ ( newValue ) => {
-								setValue( {
+							)}
+							onChange={(newValue) => {
+								setValue({
 									...value,
 									left: newValue,
-								} );
-							} }
+								});
+								modifyControlValue({
+									controlId,
+									value: {
+										...value,
+										left: newValue,
+									},
+								});
+							}}
 						/>
 					</div>
 				</div>
-			) }
+			)}
 		</div>
 	);
 }
@@ -187,74 +242,44 @@ BoxBorderControl.propTypes = {
 	/**
 	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
 	 */
-	defaultValue: PropTypes.oneOfType( [
-		PropTypes.shape( {
+	defaultValue: PropTypes.oneOfType([
+		PropTypes.shape({
 			type: 'all',
-			all: PropTypes.shape( {
+			all: PropTypes.shape({
 				width: PropTypes.string,
-				style: PropTypes.oneOf( [
-					'solid',
-					'dashed',
-					'dotted',
-					'double',
-				] ),
+				style: PropTypes.oneOf(['solid', 'dashed', 'dotted', 'double']),
 				color: PropTypes.string,
-			} ),
-		} ),
-		PropTypes.shape( {
-			type: PropTypes.oneOf( [ 'all', 'custom' ] ),
-			all: PropTypes.shape( {
+			}),
+		}),
+		PropTypes.shape({
+			type: PropTypes.oneOf(['all', 'custom']),
+			all: PropTypes.shape({
 				width: PropTypes.string,
-				style: PropTypes.oneOf( [
-					'solid',
-					'dashed',
-					'dotted',
-					'double',
-				] ),
+				style: PropTypes.oneOf(['solid', 'dashed', 'dotted', 'double']),
 				color: PropTypes.string,
-			} ),
-			top: PropTypes.shape( {
+			}),
+			top: PropTypes.shape({
 				width: PropTypes.string,
-				style: PropTypes.oneOf( [
-					'solid',
-					'dashed',
-					'dotted',
-					'double',
-				] ),
+				style: PropTypes.oneOf(['solid', 'dashed', 'dotted', 'double']),
 				color: PropTypes.string,
-			} ),
-			right: PropTypes.shape( {
+			}),
+			right: PropTypes.shape({
 				width: PropTypes.string,
-				style: PropTypes.oneOf( [
-					'solid',
-					'dashed',
-					'dotted',
-					'double',
-				] ),
+				style: PropTypes.oneOf(['solid', 'dashed', 'dotted', 'double']),
 				color: PropTypes.string,
-			} ),
-			bottom: PropTypes.shape( {
+			}),
+			bottom: PropTypes.shape({
 				width: PropTypes.string,
-				style: PropTypes.oneOf( [
-					'solid',
-					'dashed',
-					'dotted',
-					'double',
-				] ),
+				style: PropTypes.oneOf(['solid', 'dashed', 'dotted', 'double']),
 				color: PropTypes.string,
-			} ),
-			left: PropTypes.shape( {
+			}),
+			left: PropTypes.shape({
 				width: PropTypes.string,
-				style: PropTypes.oneOf( [
-					'solid',
-					'dashed',
-					'dotted',
-					'double',
-				] ),
+				style: PropTypes.oneOf(['solid', 'dashed', 'dotted', 'double']),
 				color: PropTypes.string,
-			} ),
-		} ),
-	] ),
+			}),
+		}),
+	]),
 	/**
 	 * Function that will be fired while the control value state changes.
 	 */
--- packages/controls/src/libs/box-border-control/stories/control.stories.js
+++ packages/controls/src/libs/box-border-control/stories/control.stories.js
@@ -22,12 +22,12 @@ import { WithControlDataProvider } from '../../../../../../.storybook/decorators
 
 const { WithInspectorStyles, WithStoryContextProvider, SharedDecorators } =
 	Decorators;
-SharedDecorators.push( WithPlaygroundStyles );
+SharedDecorators.push(WithPlaygroundStyles);
 
 export default {
 	title: 'Controls/BoxBorderControl',
 	component: BoxBorderControl,
-	tags: [ 'autodocs' ],
+	tags: ['autodocs'],
 };
 
 export const Default = {
@@ -42,20 +42,20 @@ export const Default = {
 			},
 		},
 	},
-	decorators: [ WithInspectorStyles, ...SharedDecorators ],
-	render: ( args ) => (
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+	render: (args) => (
 		<Flex direction="column" gap="20px">
 			<h2 className="story-heading">
 				Box Border<span>Empty</span>
 			</h2>
 
 			<ControlContextProvider
-				value={ {
+				value={{
 					name: nanoid(),
 					value: args.value,
-				} }
+				}}
 			>
-				<ControlWithHooks Control={ BoxBorderControl } { ...args } />
+				<ControlWithHooks Control={BoxBorderControl} {...args} />
 			</ControlContextProvider>
 		</Flex>
 	),
@@ -73,32 +73,32 @@ export const AllBorders = {
 			},
 		},
 	},
-	decorators: [ WithInspectorStyles, ...SharedDecorators ],
-	render: ( args ) => (
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+	render: (args) => (
 		<Flex direction="column" gap="20px">
 			<h2 className="story-heading">All Borders</h2>
 			<ControlContextProvider
-				value={ {
+				value={{
 					name: nanoid(),
 					value: args.value,
-				} }
+				}}
 			>
 				<ControlWithHooks
-					Control={ BoxBorderControl }
-					{ ...args }
+					Control={BoxBorderControl}
+					{...args}
 					label="All"
 				/>
 			</ControlContextProvider>
 
 			<ControlContextProvider
-				value={ {
+				value={{
 					name: nanoid(),
 					value: {},
-				} }
+				}}
 			>
 				<ControlWithHooks
-					Control={ BoxBorderControl }
-					{ ...args }
+					Control={BoxBorderControl}
+					{...args}
 					label="Empty"
 				/>
 			</ControlContextProvider>
@@ -118,13 +118,13 @@ export const CustomBorders = {
 			},
 		},
 	},
-	decorators: [ WithInspectorStyles, ...SharedDecorators ],
-	render: ( args ) => (
+	decorators: [WithInspectorStyles, ...SharedDecorators],
+	render: (args) => (
 		<Flex direction="column" gap="15px">
 			<h2 className="story-heading">Custom Borders</h2>
 
 			<ControlContextProvider
-				value={ {
+				value={{
 					name: nanoid(),
 					value: {
 						type: 'custom',
@@ -154,17 +154,17 @@ export const CustomBorders = {
 							color: '#0947eb',
 						},
 					},
-				} }
+				}}
 			>
 				<ControlWithHooks
-					Control={ BoxBorderControl }
-					{ ...args }
+					Control={BoxBorderControl}
+					{...args}
 					label="All Same"
 				/>
 			</ControlContextProvider>
 
 			<ControlContextProvider
-				value={ {
+				value={{
 					name: nanoid(),
 					value: {
 						type: 'custom',
@@ -194,11 +194,11 @@ export const CustomBorders = {
 							color: '#a92d00',
 						},
 					},
-				} }
+				}}
 			>
 				<ControlWithHooks
-					Control={ BoxBorderControl }
-					{ ...args }
+					Control={BoxBorderControl}
+					{...args}
 					label="Customized"
 				/>
 			</ControlContextProvider>
@@ -227,43 +227,41 @@ export const PlayAll = {
 		WithControlDataProvider,
 		...SharedDecorators,
 	],
-	render: ( args ) => (
-		<ControlWithHooks Control={ BoxBorderControl } { ...args } />
-	),
-	play: async ( { canvasElement, step } ) => {
-		const canvas = within( canvasElement );
+	render: (args) => <ControlWithHooks Control={BoxBorderControl} {...args} />,
+	play: async ({ canvasElement, step }) => {
+		const canvas = within(canvasElement);
 
-		const currentValue = canvas.getByTestId( 'current-value' );
-		const numberInput = canvas.getByRole( 'spinbutton', {
+		const currentValue = canvas.getByTestId('current-value');
+		const numberInput = canvas.getByRole('spinbutton', {
 			type: 'number',
-		} );
+		});
 
-		await step( 'Story data is available', async () => {
-			await expect( currentValue ).toBeInTheDocument();
-		} );
+		await step('Story data is available', async () => {
+			await expect(currentValue).toBeInTheDocument();
+		});
 
-		await step( 'Input control test', async () => {
-			await expect( numberInput ).toBeInTheDocument();
+		await step('Input control test', async () => {
+			await expect(numberInput).toBeInTheDocument();
 
-			await expect( numberInput ).toHaveValue( 2 );
+			await expect(numberInput).toHaveValue(2);
 			await waitFor(
 				async () =>
-					await expect( currentValue ).toHaveTextContent(
+					await expect(currentValue).toHaveTextContent(
 						'{ "type": "all", "all": { "width": "2px", "style": "solid", "color": "#0947eb" } }'
 					),
 				{ timeout: 1000 }
 			);
 
-			fireEvent.change( numberInput, { target: { value: 20 } } );
-			await expect( numberInput ).toHaveValue( 20 );
+			fireEvent.change(numberInput, { target: { value: 20 } });
+			await expect(numberInput).toHaveValue(20);
 			await waitFor(
 				async () =>
-					await expect( currentValue ).toHaveTextContent(
+					await expect(currentValue).toHaveTextContent(
 						'{ "type": "all", "all": { "width": "20px", "style": "solid", "color": "#0947eb" } }'
 					),
 				{ timeout: 1000 }
 			);
-		} );
+		});
 	},
 };
 PlayAll.storyName = 'Play → All Corners';
@@ -309,56 +307,54 @@ export const PlayCorner = {
 		WithControlDataProvider,
 		...SharedDecorators,
 	],
-	render: ( args ) => (
-		<ControlWithHooks Control={ BoxBorderControl } { ...args } />
-	),
-	play: async ( { canvasElement, step } ) => {
-		const canvas = within( canvasElement );
+	render: (args) => <ControlWithHooks Control={BoxBorderControl} {...args} />,
+	play: async ({ canvasElement, step }) => {
+		const canvas = within(canvasElement);
 
-		const currentValue = canvas.getByTestId( 'current-value' );
-		const numberInput = canvas.getAllByRole( 'spinbutton', {
+		const currentValue = canvas.getByTestId('current-value');
+		const numberInput = canvas.getAllByRole('spinbutton', {
 			type: 'number',
-		} );
+		});
 
-		await step( 'Story data is available', async () => {
-			await expect( currentValue ).toBeInTheDocument();
-		} );
+		await step('Story data is available', async () => {
+			await expect(currentValue).toBeInTheDocument();
+		});
 
-		await step( 'Input control test', async () => {
-			await expect( numberInput[ 1 ] ).toBeInTheDocument();
+		await step('Input control test', async () => {
+			await expect(numberInput[1]).toBeInTheDocument();
 
-			await expect( numberInput[ 1 ] ).toHaveValue( 2 );
+			await expect(numberInput[1]).toHaveValue(2);
 			await waitFor(
 				async () =>
-					await expect( currentValue ).toHaveTextContent(
+					await expect(currentValue).toHaveTextContent(
 						'{ "type": "custom", "all": { "width": "2px", "style": "solid", "color": "#0947eb" }, "top": { "width": "2px", "style": "solid", "color": "#0947eb" }, "right": { "width": "2px", "style": "solid", "color": "#0947eb" }, "bottom": { "width": "2px", "style": "solid", "color": "#0947eb" }, "left": { "width": "2px", "style": "solid", "color": "#0947eb" } }'
 					),
 				{ timeout: 1000 }
 			);
 
-			fireEvent.change( numberInput[ 1 ], { target: { value: 20 } } );
-			await expect( numberInput[ 1 ] ).toHaveValue( 20 );
+			fireEvent.change(numberInput[1], { target: { value: 20 } });
+			await expect(numberInput[1]).toHaveValue(20);
 			await waitFor(
 				async () =>
-					await expect( currentValue ).toHaveTextContent(
+					await expect(currentValue).toHaveTextContent(
 						'{ "type": "custom", "all": { "width": "2px", "style": "solid", "color": "#0947eb" }, "left": { "width": "2px", "style": "solid", "color": "#0947eb" }, "right": { "width": "20px", "style": "solid", "color": "#0947eb" }, "top": { "width": "2px", "style": "solid", "color": "#0947eb" }, "bottom": { "width": "2px", "style": "solid", "color": "#0947eb" } }'
 					),
 				{ timeout: 1000 }
 			);
-		} );
+		});
 	},
 };
 PlayCorner.storyName = 'Play → Custom Corners';
 
 export const All = {
-	decorators: [ WithInspectorStyles, ...SharedDecorators ],
+	decorators: [WithInspectorStyles, ...SharedDecorators],
 	render: () => (
 		<Flex direction="column" gap="50px">
-			<Default.render { ...Default.args } />
+			<Default.render {...Default.args} />
 
-			<AllBorders.render { ...AllBorders.args } />
+			<AllBorders.render {...AllBorders.args} />
 
-			<CustomBorders.render { ...CustomBorders.args } />
+			<CustomBorders.render {...CustomBorders.args} />
 		</Flex>
 	),
 };
