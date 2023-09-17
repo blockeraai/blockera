c94037c93cd0b5cc2ef622c1bbda14c655aee7d8
rezaelahidev <rezaelahi.dev@gmail.com>
2023-09-17 12:51:32 +0330

    feat: add WordPress APIs
    WordPress APIs:
    - Block APIs like: (isSupportBorder, ...) enahncement
    - Editor Default Settings to compatible with WordPress settings

---
+++ packages/extensions/src/wordpress/block-api.js
@@ -0,0 +1,32 @@
+// @flow
+/**
+ * Publisher dependencies
+ */
+import { useBlocksStore } from '../hooks';
+import { isFunction } from '@publisher/utils';
+
+/**
+ * Check is support border feature with WordPress?
+ *
+ * @param {string} blockName the selected block name
+ * @param {boolean} radiusFlag the boolean flag for check border radius is supported by WordPress or not!
+ * @return {boolean} true on success, false on otherwise.
+ */
+export const isSupportBorder = (
+	blockName: string,
+	radiusFlag: boolean = false
+): boolean => {
+	const { hasBlockSupport, getBlockSupport } = useBlocksStore();
+
+	if (!isFunction(hasBlockSupport)) {
+		return false;
+	}
+
+	if (!radiusFlag) {
+		return hasBlockSupport(blockName, '__experimentalBorder');
+	}
+
+	const { radius } = getBlockSupport(blockName, '__experimentalBorder');
+
+	return radius;
+};
