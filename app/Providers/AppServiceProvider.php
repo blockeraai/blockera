a979f9bf71d1e5c9073f27ed380d2075126c8dee
rezaelahidev <rezaelahi.dev@gmail.com>
2023-09-25 18:51:06 +0330

    feat: add ServiceProviders

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace Publisher\Framework\Providers;
+
+/**
+ * External
+ */
+
+use Illuminate\Contracts\Container\BindingResolutionException;
+
+/**
+ * Internal
+ */
+
+use Publisher\Framework\Exceptions\BaseException;
+use Publisher\Framework\Illuminate\{Foundation\Application,
+	StyleEngine\StyleDefinitions\Border,
+	StyleEngine\StyleDefinitions\Layout,
+	StyleEngine\StyleDefinitions\Position,
+	StyleEngine\StyleDefinitions\Size,
+	StyleEngine\StyleDefinitions\Spacing,
+	Support\ServiceProvider,
+	StyleEngine\StyleEngine,
+	Support\Adapters\DomParser,
+	StyleEngine\StyleDefinitions\Effects,
+	StyleEngine\StyleDefinitions\Outline,
+	StyleEngine\StyleDefinitions\BoxShadow,
+	StyleEngine\StyleDefinitions\Background,
+	StyleEngine\StyleDefinitions\TextShadow,
+	StyleEngine\StyleDefinitions\Typography
+};
+use Publisher\Framework\Services\Render\{Render,
+	Parser,
+	Blocks\Icon,
+	Styles\BorderStyle,
+	Styles\EffectsStyle,
+	Styles\LayoutStyle,
+	Styles\OutlineStyle,
+	Styles\BoxShadowStyle,
+	Styles\BackgroundStyle,
+	Styles\PositionStyle,
+	Styles\SizeStyle,
+	Styles\SpacingStyle,
+	Styles\TextShadowStyle,
+	Styles\TypographyStyle
+};
+
+class AppServiceProvider extends ServiceProvider {
+
+	/**
+	 * @throws BaseException
+	 */
+	public function register(): void {
+
+		parent::register();
+
+		try {
+
+			$this->app->singleton( Icon::class, static function ( Application $app ) {
+
+				return new Icon( $app );
+			} );
+
+			$this->app->singleton( LayoutStyle::class, static function () {
+
+				return new LayoutStyle( new Layout() );
+			} );
+
+			$this->app->singleton( PositionStyle::class, static function () {
+
+				return new PositionStyle( new Position() );
+			} );
+
+			$this->app->singleton( BorderStyle::class, static function () {
+
+				return new BorderStyle( new Border() );
+			} );
+
+			$this->app->singleton( SizeStyle::class, static function () {
+
+				return new SizeStyle( new Size() );
+			} );
+
+			$this->app->singleton( SpacingStyle::class, static function () {
+
+				return new SpacingStyle( new Spacing() );
+			} );
+
+			$this->app->singleton( TextShadowStyle::class, static function () {
+
+				return new TextShadowStyle( new TextShadow() );
+			} );
+
+			$this->app->singleton( BackgroundStyle::class, static function () {
+
+				return new BackgroundStyle( new Background() );
+			} );
+
+			$this->app->singleton( TypographyStyle::class, static function () {
+
+				return new TypographyStyle( new Typography() );
+			} );
+
+			$this->app->singleton( BoxShadowStyle::class, static function () {
+
+				return new BoxShadowStyle( new BoxShadow() );
+			} );
+
+			$this->app->singleton( OutlineStyle::class, static function () {
+
+				return new OutlineStyle( new Outline() );
+			} );
+
+			$this->app->singleton( EffectsStyle::class, static function () {
+
+				return new EffectsStyle( new Effects() );
+			} );
+
+			$this->app->bind( StyleEngine::class, static function () {
+
+				return new StyleEngine();
+			} );
+
+			$this->app->singleton( DomParser::class, static function () {
+
+				return new DomParser();
+			} );
+
+			$this->app->singleton( Parser::class, static function ( Application $app ) {
+
+				return new Parser( $app );
+			} );
+
+			$this->app->bind( Render::class, static function ( Application $app, array $params ): Render {
+
+				return new Render( $app, $params['blockName'] );
+			} );
+
+		} catch ( BaseException $handler ) {
+
+			throw new BaseException( 'Binding ' . StyleEngine::class . " Failure! \n" . $handler->getMessage() );
+		}
+	}
+
+	/**
+	 * @throws BindingResolutionException
+	 */
+	public function boot(): void {
+
+		parent::boot();
+
+		$this->app->make( StyleEngine::class );
+
+		foreach ( pb_core_config( 'app.blocks' ) as $block ) {
+
+			if ( empty( $block ) ) {
+
+				continue;
+			}
+
+			$this->app->make( Render::class, [ 'blockName' => $block ] );
+		}
+	}
+
+}
--- app/Providers/AssetsProvider.php
+++ app/Providers/AssetsProvider.php
@@ -1,213 +1,59 @@
 <?php
 
-namespace Publisher\Core\Providers;
+namespace Publisher\Framework\Providers;
 
 /**
- * AssetsProvider is registering all assets of publisher-core into WordPress
+ * External
+ */
+
+use Illuminate\Contracts\Container\BindingResolutionException;
+
+/**
+ * Internal
+ */
+
+use Publisher\Framework\Exceptions\BaseException;
+use Publisher\Framework\Services\PublisherAssets;
+use Publisher\Framework\Illuminate\Foundation\Application;
+use Publisher\Framework\Illuminate\Support\ServiceProvider;
+
+/**
+ * AssetsProvider is registering all assets of publisher app.
  *
  * @since 1.0.0
  */
-class AssetsProvider {
+class AssetsProvider extends ServiceProvider {
 
 	/**
-	 * Holds assets or packages name.
+	 * Register any application services.
 	 *
-	 * @var array
-	 */
-	protected static array $assets = [
-		'utils',
-		'controls',
-		'components',
-		'classnames',
-		'extensions',
-		'style-engine',
-		'data-extractor',
-	];
-
-	/**
-	 * Holds packages dependencies array.
-	 *
-	 * @var string[]
-	 */
-	protected static array $packages_deps = [
-		'extensions' => [
-			'@publisher/controls',
-			'@publisher/components',
-		],
-	];
-
-	/**
-	 * AssetsProvider constructor method,
-	 * when create new instance of current class,
-	 * fire `wp_enqueue_scripts` and `enqueue_block_editor_assets`
-	 *
-	 * @since 1.0.0
-	 */
-	public function __construct() {
-
-		add_action( 'wp_enqueue_scripts', array( $this, 'register_assets' ), 10 );
-		add_action( 'enqueue_block_editor_assets', array( $this, 'register_assets' ), 10 );
-	}
-
-	/**
-	 * Preparing current assets with info!
-	 *
-	 * @return array
-	 */
-	protected function prepare_assets(): array {
-
-		$provider = $this;
-
-		return array_filter(
-			array_map(
-				static function ( string $asset ) use ( $provider ) {
-
-					$assetInfo = $provider->assetInfo( $asset );
-
-					if ( ! $assetInfo ) {
-
-						return null;
-					}
-
-					return $assetInfo;
-
-				},
-				self::$assets
-			)
-		);
-	}
-
-	/**
-	 * Register all assets in WordPress.
-	 *
+	 * @throws BaseException
 	 * @return void
 	 */
-	public function register_assets() {
+	public function register(): void {
 
-		// Registering assets ...
-		foreach ( $this->prepare_assets() as $asset ) {
+		try {
 
-			if ( $asset['style'] ) {
+			$this->app->singleton( PublisherAssets::class, function ( Application $app ) {
 
-				wp_register_style(
-					'@publisher/' . $asset['name'],
-					str_replace( '\\', '/', $asset['style'] ),
-					self::$packages_deps[ $asset['name'] ] ?? [],
-					$asset['version']
-				);
-			}
+				return new PublisherAssets( $app );
+			} );
 
-			if ( ! $asset['script'] ) {
+		} catch ( BaseException $handler ) {
 
-				continue;
-			}
-
-			$deps = $this->exclude_dependencies( $asset['deps'] );
-
-			wp_register_script(
-				'@publisher/' . $asset['name'],
-				str_replace( '\\', '/', $asset['script'] ),
-				$deps,
-				$asset['version'],
-				true
-			);
+			throw new BaseException( 'Binding ' . PublisherAssets::class . " Failure!\n" . $handler->getMessage() );
 		}
 	}
 
 	/**
-	 * Exclude deps before register script!
+	 * Bootstrap any application services.
 	 *
-	 * @param array $dependencies the dependencies of current asset.
-	 *
-	 * @since 1.0.0
-	 * @return array the list of filtered dependencies
+	 * @throws BindingResolutionException
+	 * @return void
 	 */
-	private function exclude_dependencies( array $dependencies ): array {
+	public function boot(): void {
 
-		$excludes = array( '@publisher/storybook' );
-
-		foreach ( $excludes as $item ) {
-
-			if ( ! in_array( $item, $dependencies, true ) ) {
-
-				continue;
-			}
-
-			unset( $dependencies[ array_search( $item, $dependencies, true ) ] );
-		}
-
-		return $dependencies;
-	}
-
-	/**
-	 * Retrieve assets information.
-	 *
-	 * @param string $name the name of current asset.
-	 *
-	 * @return array the asset data
-	 */
-	public function assetInfo( string $name ): array {
-
-		$isDevelopment = pb_core_config( 'app.debug' );
-
-		$assetInfoFile = sprintf(
-			'%s%s/index%s.asset.php',
-			pb_core_config( 'app.dist_path' ),
-			$name,
-			$isDevelopment ? '' : '.min'
-		);
-
-		if ( ! file_exists( $assetInfoFile ) ) {
-
-			return [];
-		}
-
-		$assetInfo = include $assetInfoFile;
-
-		$deps    = $assetInfo['dependencies'] ?? [];
-		$version = $assetInfo['version'] ?? filemtime( $assetInfoFile );
-
-		$js_file = sprintf(
-			'%s%s/index%s.js',
-			pb_core_config( 'app.dist_path' ),
-			$name,
-			$isDevelopment ? '' : '.min'
-		);
-
-		if ( file_exists( $js_file ) ) {
-
-			$script = sprintf(
-				'%s%s/index%s.js',
-				pb_core_config( 'app.dist_url' ),
-				$name,
-				$isDevelopment ? '' : '.min'
-			);
-		} else {
-
-			$script = '';
-		}
-
-		$css_file = sprintf(
-			'%s%s-styles/style%s.css',
-			pb_core_config( 'app.dist_path' ),
-			$name,
-			$isDevelopment ? '' : '.min'
-		);
-
-		if ( file_exists( $css_file ) ) {
-
-			$style = sprintf(
-				'%s%s-styles/style%s.css',
-				pb_core_config( 'app.dist_url' ),
-				$name,
-				$isDevelopment ? '' : '.min'
-			);
-		} else {
-
-			$style = '';
-		}
-
-		return compact( 'name', 'deps', 'script', 'style', 'version' );
+		$this->app->make( PublisherAssets::class );
 	}
 
 }
