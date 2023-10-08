26efdcacaf757ce1feaf6e3e0494650005c87e56
rezaelahidev <rezaelahi.dev@gmail.com>
2023-10-08 12:56:42 +0330

    feat: add wp-env-app tools
    this feature provide custom webpack config for wp-env application of publisher-core

---
+++ tools/wp-env-app/index.js
@@ -0,0 +1,69 @@
+/**
+ * WordPress dependencies
+ */
+import domReady from '@wordpress/dom-ready';
+
+/**
+ * Internal dependencies
+ */
+import {
+	applyHooks,
+	registerBlockExtension,
+	SharedBlockExtension,
+	sharedBlockExtensionAttributes,
+	sharedBlockExtensionSupports,
+} from '@publisher/extensions';
+
+const Shared = {
+	name: 'Shared',
+	attributes: {
+		...sharedBlockExtensionAttributes,
+	},
+	supports: {
+		...sharedBlockExtensionSupports,
+	},
+	edit: ({ children, ...props }) => {
+		return (
+			<SharedBlockExtension {...props}>{children}</SharedBlockExtension>
+		);
+	},
+};
+
+class Setup {
+	addExtensions() {
+		const BlockLibrary = [Shared];
+
+		for (const key in BlockLibrary) {
+			if (!Object.hasOwnProperty.call(BlockLibrary, key)) {
+				continue;
+			}
+
+			const currentBlock = BlockLibrary[key];
+
+			if (!currentBlock?.name && 'Shared' !== key) {
+				console.warn(
+					'Block extension validation: Block extension must contain name param!'
+				);
+				continue;
+			}
+
+			registerBlockExtension(currentBlock.name || key, currentBlock);
+		}
+
+		return this;
+	}
+
+	addBlocks() {
+		//TODO: implements registration publisher custom blocks into WordPress Block Editor (Gutenberg).
+
+		return this;
+	}
+}
+
+domReady(() => {
+	const setupInstance = new Setup();
+
+	setupInstance.addExtensions().addBlocks();
+
+	applyHooks();
+});
