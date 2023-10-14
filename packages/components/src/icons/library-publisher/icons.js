21c312ad86e4df45cb4e04b3f777dfdf1a54eaa0
rezaelahidev <rezaelahi.dev@gmail.com>
2023-10-14 18:48:36 +0330

    feat: add new icon into publisher library

--- packages/components/src/icons/library-publisher/icons.js
+++ packages/components/src/icons/library-publisher/icons.js
@@ -1,9 +1,13 @@
 import { default as publisher } from './icons/publisher';
 import { default as publisherCircle } from './icons/publisher-cricle';
+import { default as publisherInteraction } from './icons/publisher-interaction';
+import { default as publisherSettings } from './icons/publisher-settings';
 
 const PublisherIcons = {
 	publisher,
 	publisherCircle,
+	publisherSettings,
+	publisherInteraction,
 };
 
 export { PublisherIcons };
