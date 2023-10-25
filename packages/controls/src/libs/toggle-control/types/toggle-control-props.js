9601dd9f2066685fbd8c47508492c6ea420548bd
sadegh akbari <akbarisadegh382@gmail.com>
2023-10-25 09:38:38 +0330

    fix: types and typechecking

---
+++ packages/controls/src/libs/toggle-control/types/toggle-control-props.js
@@ -0,0 +1,10 @@
+// @flow
+
+export type TToggleControlProps = {
+	label?: string,
+	defaultValue?: mixed,
+	value?: Boolean,
+	onChange?: (x: Boolean) => any,
+	className?: string,
+	props?: Object,
+};
