dacb335d2b75cc3c348c95d7f4701a057a7d4986
maryamsalamatl <maryamsalamat47@gmail.com>
2023-10-26 22:23:39 +0400

    test:add checking data provider

--- packages/controls/src/libs/select-control/test/custom-select-control.cy.js
+++ packages/controls/src/libs/select-control/test/custom-select-control.cy.js
@@ -1,5 +1,11 @@
 import { SelectControl } from '../..';
 import InheritIcon from '../stories/icons/inherit';
+import { select, dispatch } from '@wordpress/data';
+import { addControl, modifyControlValue } from '../../../store/actions';
+import { controlReducer } from '../../../store/reducers/control-reducer';
+import { getControlValue } from '../../../store/selectors';
+import { nanoid } from 'nanoid';
+
 const selectOptions = [
 	{
 		label: 'All Properties',
@@ -68,52 +74,123 @@ describe('custom select control component testing', () => {
 				component: <SelectControl {...defaultProps} />,
 			});
 		});
+
 		it('select border option', () => {
+			const name = nanoid();
 			cy.withDataProvider({
-				component: <SelectControl {...defaultProps} />,
+				component: (
+					<SelectControl
+						{...defaultProps}
+						onChange={(value) => {
+							controlReducer(
+								select('publisher-core/controls').getControl(
+									name
+								),
+								modifyControlValue({
+									value,
+									controlId: name,
+								})
+							);
+						}}
+					/>
+				),
 				value: 'all',
+				name,
 			});
+
 			cy.get('#downshift-1-toggle-button').click();
 			cy.get('#downshift-1-menu').contains('Border').click();
 			cy.get('#downshift-1-toggle-button').contains('Border');
-		});
-		it('not able to select disabled option', () => {
-			cy.withDataProvider({
-				component: <SelectControl {...defaultProps} />,
-				value: 'all',
+
+			//Check data provider value
+			cy.wait(100).then(() => {
+				expect('border').to.be.equal(getControlValue(name));
 			});
+		});
+
+		it('not able to select disabled option', () => {
+			const name = nanoid();
+			cy.withDataProvider({
+				component: (
+					<SelectControl
+						{...defaultProps}
+						onChange={(value) => {
+							controlReducer(
+								select('publisher-core/controls').getControl(
+									name
+								),
+								modifyControlValue({
+									value,
+									controlId: name,
+								})
+							);
+						}}
+					/>
+				),
+				value: 'all',
+				name,
+			});
+
 			cy.get('#downshift-8-toggle-button').click();
 			cy.get('#downshift-8-menu')
 				.contains('Filter (Disabled)')
 				.should('have.css', 'pointer-events')
 				.should('contain', 'none');
+
+			//Check data provider value
+			cy.wait(100).then(() => {
+				expect('all').to.be.equal(getControlValue(name));
+			});
 		});
+
 		it('pass defaultValue', () => {
+			const name = nanoid();
 			cy.withDataProvider({
 				component: (
 					<SelectControl
 						{...defaultProps}
 						value=""
 						defaultValue="border"
+						onChange={(value) => {
+							controlReducer(
+								select('publisher-core/controls').getControl(
+									name
+								),
+								modifyControlValue({
+									value,
+									controlId: name,
+								})
+							);
+						}}
 					/>
 				),
 				value: '',
+				name,
 			});
+
 			cy.get('#downshift-12-toggle-button').contains('Border');
+
+			//Check data provider value
+			cy.wait(100).then(() => {
+				/**
+				 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
+				so there is no data in provider 
+				*/
+				expect(undefined).to.be.equal(getControlValue(name));
+			});
 		});
 	});
-	describe('test custom styles :', () => {
-		// const doesHaveClass = (id,class ) => {
-		// 	return
-		// }
 
+	describe('test custom styles :', () => {
 		it('With Field', () => {
 			cy.withDataProvider({
 				component: <SelectControl {...defaultProps} label="Field" />,
 				value: 'all',
 			});
+
 			cy.contains('Field');
 		});
+
 		it('hover', () => {
 			cy.withDataProvider({
 				component: (
@@ -121,6 +198,7 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-14-toggle-button')
 				.trigger('mouseover')
 				.parent()
@@ -128,6 +206,7 @@ describe('custom select control component testing', () => {
 				.parent()
 				.should('have.class', 'is-hovered');
 		});
+
 		it('focus', () => {
 			cy.withDataProvider({
 				component: (
@@ -135,6 +214,7 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-15-toggle-button')
 				.parent()
 				.parent()
@@ -147,17 +227,20 @@ describe('custom select control component testing', () => {
 				component: <SelectControl {...defaultProps} noBorder={false} />,
 				value: 'all',
 			});
+
 			cy.get('#downshift-16-toggle-button')
 				.parent()
 				.parent()
 				.parent()
 				.should('not.have.class', 'no-border');
 		});
+
 		it('no border : passing true', () => {
 			cy.withDataProvider({
 				component: <SelectControl {...defaultProps} noBorder={true} />,
 				value: 'all',
 			});
+
 			cy.get('#downshift-17-toggle-button')
 				.parent()
 				.parent()
@@ -176,6 +259,7 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-18-toggle-button')
 				.parent()
 				.parent()
@@ -190,12 +274,14 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-19-toggle-button')
 				.parent()
 				.parent()
 				.parent()
 				.should('have.class', 'menu-position-top');
 		});
+
 		it('customHideInputIcon : passing false', () => {
 			cy.withDataProvider({
 				component: (
@@ -206,12 +292,14 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-20-toggle-button')
 				.parent()
 				.parent()
 				.parent()
 				.should('not.have.class', 'input-hide-icon');
 		});
+
 		it('customHideInputIcon : passing true', () => {
 			cy.withDataProvider({
 				component: (
@@ -222,12 +310,14 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-21-toggle-button')
 				.parent()
 				.parent()
 				.parent()
 				.should('have.class', 'input-hide-icon');
 		});
+
 		it('customHideInputLabel : passing false', () => {
 			cy.withDataProvider({
 				component: (
@@ -238,12 +328,14 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-22-toggle-button')
 				.parent()
 				.parent()
 				.parent()
 				.should('not.have.class', 'input-hide-label');
 		});
+
 		it('customHideInputLabel : passing true', () => {
 			cy.withDataProvider({
 				component: (
@@ -254,12 +346,14 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-23-toggle-button')
 				.parent()
 				.parent()
 				.parent()
 				.should('have.class', 'input-hide-label');
 		});
+
 		it('customHideInputCaret : passing false', () => {
 			cy.withDataProvider({
 				component: (
@@ -270,12 +364,14 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-24-toggle-button')
 				.parent()
 				.parent()
 				.parent()
 				.should('not.have.class', 'input-hide-caret');
 		});
+
 		it('customHideInputCaret : passing true', () => {
 			cy.withDataProvider({
 				component: (
@@ -286,12 +382,14 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-25-toggle-button')
 				.parent()
 				.parent()
 				.parent()
 				.should('have.class', 'input-hide-caret');
 		});
+
 		it('customInputCenterContent : passing false', () => {
 			cy.withDataProvider({
 				component: (
@@ -302,12 +400,14 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-26-toggle-button')
 				.parent()
 				.parent()
 				.parent()
 				.should('not.have.class', 'input-align-center');
 		});
+
 		it('customInputCenterContent : passing true', () => {
 			cy.withDataProvider({
 				component: (
@@ -318,11 +418,42 @@ describe('custom select control component testing', () => {
 				),
 				value: 'all',
 			});
+
 			cy.get('#downshift-27-toggle-button')
 				.parent()
 				.parent()
 				.parent()
 				.should('have.class', 'input-align-center');
+		});
+	});
+
+	describe('test onChange', () => {
+		it('does onChange fire', () => {
+			const name = nanoid();
+			const propsToPass = {
+				...defaultProps,
+				onChange: (value) => {
+					controlReducer(
+						select('publisher-core/controls').getControl(name),
+						modifyControlValue({
+							value,
+							controlId: name,
+						})
+					);
+				},
+			};
+			cy.stub(propsToPass, 'onChange').as('onChange');
+
+			cy.withDataProvider({
+				component: <SelectControl {...propsToPass} />,
+				value: 'all',
+				name,
+			});
+
+			cy.get('#downshift-28-toggle-button').click();
+			cy.get('#downshift-28-menu').contains('Border').click();
+
+			cy.get('@onChange').should('have.been.called');
 		});
 	});
 });
--- packages/controls/src/libs/select-control/types/control-types.js
+++ packages/controls/src/libs/select-control/types/control-types.js
@@ -6,7 +6,7 @@ export type TSelectOption = {
 	icon?: Element,
 	type?: string,
 	disabled?: boolean,
-	options?: Array<TSelectControlProps>,
+	options?: Array<TSelectOption>,
 };
 
 export type TCustomSelectOption = {
@@ -24,12 +24,12 @@ export type TSelectControlProps = {
 	defaultValue?: string,
 	onChange: () => {},
 	type: 'native' | 'custom',
-	options: Array<TSelectOption | TCustomSelectOption>,
+	options: Array<TSelectOption>,
 	customMenuPosition?: 'bottom' | 'top',
 	customHideInputIcon?: boolean,
 	customHideInputLabel?: boolean,
 	customInputCenterContent?: boolean,
 	noBorder?: boolean,
 	multiple?: boolean,
-	classname?: string,
+	className?: string,
 };
