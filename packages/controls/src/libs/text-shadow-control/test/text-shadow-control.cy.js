5c28dd659180f5f3d9d7ee2e3cdfd0f67db53f37
sadegh akbari <akbarisadegh382@gmail.com>
2023-10-31 12:41:54 +0330

    feat: add empty tests

--- packages/controls/src/libs/text-shadow-control/test/text-shadow-control.cy.js
+++ packages/controls/src/libs/text-shadow-control/test/text-shadow-control.cy.js
@@ -18,29 +18,69 @@ describe('ext shadow control component testing', () => {
 	});
 
 	describe('empty', () => {
-		it('test', () => {
+		it('should display default value', () => {
 			const name = nanoid();
+			const defaultValue = [
+				{
+					x: '10px',
+					y: '10px',
+					blur: '4px',
+					color: '#0747eb',
+					isVisible: true,
+				},
+				{
+					x: '2px',
+					y: '3px',
+					blur: '4px',
+					color: '#0947eb',
+					isVisible: true,
+				},
+			];
+			cy.withDataProvider({
+				component: <TextShadowControl label="text shadow" />,
+
+				value: defaultValue,
+				store: STORE_NAME,
+				name,
+			});
+
+			cy.get('.publisher-control-repeater')
+				.find('.publisher-control-repeater-item')
+				.should('have.length', 2);
+
+			// Check data provider value!
+			cy.then(() => {
+				console.log('value: ', getControlValue(name, STORE_NAME));
+				expect(getControlValue(name, STORE_NAME)).to.deep.eq(
+					defaultValue
+				);
+			});
+		});
+	});
+
+	describe.only('fill', () => {
+		it('should display custom repeater header', () => {
 			cy.withDataProvider({
 				component: <TextShadowControl label="text shadow" />,
 
 				value: [
 					{
-						x: '2px',
-						y: '3px',
+						x: '10px',
+						y: '10px',
 						blur: '4px',
-						color: '#0947eb',
+						color: '#0747eb',
 						isVisible: true,
 					},
 				],
 				store: STORE_NAME,
-				name,
 			});
 
-			// Check data provider value!
-			cy.then(() => {
-				console.log('value: ', getControlValue(name, STORE_NAME));
-				expect(getControlValue(name, STORE_NAME)).to.have.length(7);
-			});
+			cy.getByDataCy('text-shadow-repeater-item-header').should(
+				'be.visible'
+			);
+			cy.getByDataCy('header-icon').should('be.visible');
+			cy.getByDataCy('header-label').should('be.visible');
+			cy.getByDataCy('header-values').should('be.visible');
 		});
 	});
 });
