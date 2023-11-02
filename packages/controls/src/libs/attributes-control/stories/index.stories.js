94e1b2d37d414b1a56151f97f6c9d45d8cc79012
maryamsalamatl <maryamsalamat47@gmail.com>
2023-11-02 12:47:05 +0400

    refactor(stories): remove play story

--- packages/controls/src/libs/attributes-control/stories/index.stories.js
+++ packages/controls/src/libs/attributes-control/stories/index.stories.js
@@ -1,13 +1,6 @@
 /**
  * External dependencies
  */
-import { expect } from '@storybook/jest';
-import {
-	fireEvent,
-	userEvent,
-	waitFor,
-	within,
-} from '@storybook/testing-library';
 import { nanoid } from 'nanoid';
 
 /**
@@ -24,10 +17,8 @@ import { STORE_NAME } from '../../repeater-control/store';
 import { WithControlDataProvider } from '../../../../../../.storybook/decorators/with-control-data-provider';
 import { WithPlaygroundStyles } from '../../../../../../.storybook/decorators/with-playground-styles';
 import { ControlContextProvider } from '../../../context';
-import ControlWithHooks from '../../../../../../.storybook/components/control-with-hooks';
 
-const { WithInspectorStyles, WithStoryContextProvider, SharedDecorators } =
-	Decorators;
+const { WithInspectorStyles, SharedDecorators } = Decorators;
 SharedDecorators.push(WithPlaygroundStyles);
 SharedDecorators.push(WithControlDataProvider);
 
@@ -238,158 +229,6 @@ export const Filled = {
 		);
 	},
 };
-
-export const PlayGeneral = {
-	args: {
-		label: 'Attributes',
-		attributeElement: 'general',
-		controlInfo: {
-			name: nanoid(),
-			value: [],
-		},
-		storeName: STORE_NAME,
-	},
-	decorators: [
-		WithStoryContextProvider,
-		WithInspectorStyles,
-		...SharedDecorators,
-	],
-	render: (args) => (
-		<ControlWithHooks Control={AttributesControl} {...args} />
-	),
-	play: async ({ canvasElement, step }) => {
-		const canvas = within(canvasElement);
-
-		const currentValue = canvas.getByTestId('current-value');
-		const button = canvas.getByLabelText('Add New');
-		//
-		await step('Story Data', async () => {
-			await expect(currentValue).toBeInTheDocument();
-			await expect(currentValue).toHaveTextContent('[]');
-		});
-
-		await step('Click Add Button', async () => {
-			await expect(button).toBeInTheDocument();
-
-			await userEvent.click(button);
-			await waitFor(
-				async () =>
-					await expect(currentValue).toHaveTextContent(
-						'[ { "key": "", "__key": "", "value": "", "isVisible": true } ]'
-					),
-				{ timeout: 2000 }
-			);
-		});
-
-		await step('Open Popover', async () => {
-			await userEvent.click(canvas.getByLabelText('Item 1'));
-		});
-
-		await step('Change Input', async () => {
-			const inputs = canvas.getAllByRole('textbox');
-
-			await userEvent.type(inputs[0], 'custom attr{enter}');
-			await waitFor(
-				async () =>
-					await expect(currentValue).toHaveTextContent(
-						'[ { "key": "custom attr", "__key": "custom", "value": "", "isVisible": true } ]'
-					),
-				{ timeout: 2000 }
-			);
-
-			await userEvent.type(inputs[1], 'attr value{enter}');
-			await waitFor(
-				async () =>
-					await expect(currentValue).toHaveTextContent(
-						'[ { "key": "custom attr", "__key": "custom", "value": "attr value", "isVisible": true } ]'
-					),
-				{ timeout: 2000 }
-			);
-		});
-	},
-};
-PlayGeneral.storyName = 'Play → General';
-
-export const PlaySpecial = {
-	args: {
-		label: 'Attributes',
-		attributeElement: 'a',
-		controlInfo: {
-			name: nanoid(),
-			value: [],
-		},
-		storeName: STORE_NAME,
-	},
-	decorators: [
-		WithStoryContextProvider,
-		WithInspectorStyles,
-		...SharedDecorators,
-	],
-	render: (args) => (
-		<ControlWithHooks Control={AttributesControl} {...args} />
-	),
-	play: async ({ canvasElement, step }) => {
-		const canvas = within(canvasElement);
-
-		const currentValue = canvas.getByTestId('current-value');
-		const button = canvas.getByLabelText('Add New');
-		//
-		await step('Story Data', async () => {
-			await expect(currentValue).toBeInTheDocument();
-			await expect(currentValue).toHaveTextContent('[]');
-		});
-
-		await step('Click Add Button', async () => {
-			await expect(button).toBeInTheDocument();
-
-			await userEvent.click(button);
-			await waitFor(
-				async () =>
-					await expect(currentValue).toHaveTextContent(
-						'[ { "key": "",  "__key": "", "value": "", "isVisible": true } ]'
-					),
-				{ timeout: 1000 }
-			);
-		});
-
-		await step('Open Popover', async () => {
-			await userEvent.click(canvas.getByLabelText('Item 1'));
-		});
-
-		await step('Change Input', async () => {
-			const select = canvas.getByRole('combobox', {});
-
-			fireEvent.change(select, {
-				target: { value: 'rel' },
-			});
-
-			await waitFor(
-				async () =>
-					await expect(currentValue).toHaveTextContent(
-						'[ { "key": "rel", "__key": "rel", "value": "", "isVisible": true } ]'
-					),
-				{ timeout: 2000 }
-			);
-
-			await expect(
-				canvas.getAllByRole('combobox', {})[1]
-			).toBeInTheDocument();
-
-			fireEvent.change(canvas.getAllByRole('combobox', {})[1], {
-				target: { value: 'next' },
-			});
-
-			await waitFor(
-				async () =>
-					await expect(currentValue).toHaveTextContent(
-						'[ { "key": "rel", "__key": "rel", "value": "next", "isVisible": true } ]'
-					),
-				{ timeout: 2000 }
-			);
-		});
-	},
-};
-PlaySpecial.storyName = 'Play → Special';
 
 export const All = {
 	args: {},
