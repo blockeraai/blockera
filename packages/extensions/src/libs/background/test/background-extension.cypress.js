5f26d9ba14cff8ebc53f0567f6a2733f3aed6b69
alirezaalatifi <l.alirezalatifi@gmail.com>
2023-11-26 17:36:42 +0330

    test(background extension): remove duplicated function.

--- packages/extensions/src/libs/background/test/background-extension.cypress.js
+++ packages/extensions/src/libs/background/test/background-extension.cypress.js
@@ -4,28 +4,8 @@ import {
 	getWPDataObject,
 	getSelectedBlock,
 	redirectToFrontPage,
+	hexToRGB,
 } from '../../../../../../cypress/helpers';
-
-function hexToRGB(hex) {
-	let r = 0,
-		g = 0,
-		b = 0;
-
-	// 3 digits
-	if (hex.length === 4) {
-		r = '0x' + hex[1] + hex[1];
-		g = '0x' + hex[2] + hex[2];
-		b = '0x' + hex[3] + hex[3];
-
-		// 6 digits
-	} else if (hex.length === 7) {
-		r = '0x' + hex[1] + hex[2];
-		g = '0x' + hex[3] + hex[4];
-		b = '0x' + hex[5] + hex[6];
-	}
-
-	return 'rgb(' + +r + ', ' + +g + ', ' + +b + ')';
-}
 
 describe('background extension', () => {
 	// describe('Extension Initializing', () => {...});
