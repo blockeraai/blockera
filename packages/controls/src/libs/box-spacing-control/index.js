56a650cd965f1aaf0830a677aff757356b9d1fc0
Ali Aghdam <betteraliaghdam@gmail.com>
2023-11-26 16:19:50 +0330

    feat: keep focus on the side box while dragging

--- packages/controls/src/libs/box-spacing-control/index.js
+++ packages/controls/src/libs/box-spacing-control/index.js
@@ -87,24 +87,36 @@ export default function BoxSpacingControl({
 		value: fixLabelToNumber(value.margin.top) || 0,
 		setValue: topMarginDragSetValue,
 		movement: 'vertical',
+		onEnd: () => {
+			setFocusSide('');
+		},
 	});
 
 	const leftMarginDragValueHandler = useDragValue({
 		value: fixLabelToNumber(value.margin.left) || 0,
 		setValue: leftMarginDragSetValue,
 		movement: 'horizontal',
+		onEnd: () => {
+			setFocusSide('');
+		},
 	});
 
 	const rightMarginDragValueHandler = useDragValue({
 		value: fixLabelToNumber(value.margin.right) || 0,
 		setValue: rightMarginDragSetValue,
 		movement: 'horizontal',
+		onEnd: () => {
+			setFocusSide('');
+		},
 	});
 
 	const bottomMarginDragValueHandler = useDragValue({
 		value: fixLabelToNumber(value.margin.bottom) || 0,
 		setValue: bottomMarginDragSetValue,
 		movement: 'vertical',
+		onEnd: () => {
+			setFocusSide('');
+		},
 	});
 
 	const topPaddingDragValueHandler = useDragValue({
@@ -112,6 +124,9 @@ export default function BoxSpacingControl({
 		setValue: topPaddingDragSetValue,
 		movement: 'vertical',
 		min: 0,
+		onEnd: () => {
+			setFocusSide('');
+		},
 	});
 
 	const leftPaddingDragValueHandler = useDragValue({
@@ -119,6 +134,9 @@ export default function BoxSpacingControl({
 		setValue: leftPaddingDragSetValue,
 		movement: 'horizontal',
 		min: 0,
+		onEnd: () => {
+			setFocusSide('');
+		},
 	});
 
 	const rightPaddingDragValueHandler = useDragValue({
@@ -126,6 +144,9 @@ export default function BoxSpacingControl({
 		setValue: rightPaddingDragSetValue,
 		movement: 'horizontal',
 		min: 0,
+		onEnd: () => {
+			setFocusSide('');
+		},
 	});
 
 	const bottomPaddingDragValueHandler = useDragValue({
@@ -133,9 +154,13 @@ export default function BoxSpacingControl({
 		setValue: bottomPaddingDragSetValue,
 		movement: 'vertical',
 		min: 0,
+		onEnd: () => {
+			setFocusSide('');
+		},
 	});
 
 	const [openPopover, setOpenPopover] = useState(openSide);
+	const [focusSide, setFocusSide] = useState('');
 
 	function fixLabelText(value: string | MixedElement): any {
 		if (value === '') {
@@ -196,9 +221,15 @@ export default function BoxSpacingControl({
 							'shape-side',
 							'side-horizontal',
 							'side-margin-top',
-							openPopover === 'margin-top' ? 'selected-side' : ''
+							openPopover === 'margin-top' ||
+								focusSide === 'margin-top'
+								? 'selected-side'
+								: ''
 						)}
-						onMouseDown={topMarginDragValueHandler}
+						onMouseDown={(event) => {
+							topMarginDragValueHandler(event);
+							setFocusSide('margin-top');
+						}}
 						d="M6.242 0.5H243.757C245.094 0.5 245.763 2.11571 244.818 3.06066L218.697 29.182C217.853 30.0259 216.708 30.5 215.515 30.5H34.4846C33.2912 30.5 32.1466 30.0259 31.3027 29.182L5.18134 3.06066C4.2364 2.11571 4.90565 0.5 6.242 0.5Z"
 					/>
 
@@ -207,11 +238,15 @@ export default function BoxSpacingControl({
 							'shape-side',
 							'side-vertical',
 							'side-margin-right',
-							openPopover === 'margin-right'
+							openPopover === 'margin-right' ||
+								focusSide === 'margin-right'
 								? 'selected-side'
 								: ''
 						)}
-						onMouseDown={rightMarginDragValueHandler}
+						onMouseDown={(event) => {
+							rightMarginDragValueHandler(event);
+							setFocusSide('margin-right');
+						}}
 						d="M219.5 124.468V34.4854C219.5 33.2919 219.974 32.1473 220.818 31.3034L246.939 5.18207C247.884 4.23713 249.5 4.90638 249.5 6.24273V152.711C249.5 154.047 247.884 154.716 246.939 153.771L220.818 127.65C219.974 126.806 219.5 125.661 219.5 124.468Z"
 					/>
 
@@ -220,11 +255,15 @@ export default function BoxSpacingControl({
 							'shape-side',
 							'side-horizontal',
 							'side-margin-bottom',
-							openPopover === 'margin-bottom'
+							openPopover === 'margin-bottom' ||
+								focusSide === 'margin-bottom'
 								? 'selected-side'
 								: ''
 						)}
-						onMouseDown={bottomMarginDragValueHandler}
+						onMouseDown={(event) => {
+							bottomMarginDragValueHandler(event);
+							setFocusSide('margin-bottom');
+						}}
 						d="M218.744 129.818L244.865 155.939C245.81 156.884 245.141 158.5 243.804 158.5H6.1961C4.85974 158.5 4.19049 156.884 5.13544 155.939L31.2568 129.818C32.1007 128.974 33.2453 128.5 34.4387 128.5H215.562C216.755 128.5 217.9 128.974 218.744 129.818Z"
 					/>
 
@@ -233,9 +272,15 @@ export default function BoxSpacingControl({
 							'shape-side',
 							'side-vertical',
 							'side-margin-left',
-							openPopover === 'margin-left' ? 'selected-side' : ''
+							openPopover === 'margin-left' ||
+								focusSide === 'margin-left'
+								? 'selected-side'
+								: ''
 						)}
-						onMouseDown={leftMarginDragValueHandler}
+						onMouseDown={(event) => {
+							leftMarginDragValueHandler(event);
+							setFocusSide('margin-left');
+						}}
 						d="M0.5 152.711V6.24322C0.5 4.90687 2.11571 4.23762 3.06066 5.18257L29.182 31.3039C30.0259 32.1478 30.5 33.2924 30.5 34.4859V124.468C30.5 125.661 30.0259 126.806 29.182 127.65L3.06066 153.771C2.11571 154.716 0.5 154.047 0.5 152.711Z"
 					/>
 
@@ -244,9 +289,15 @@ export default function BoxSpacingControl({
 							'shape-side',
 							'side-horizontal',
 							'side-padding-top',
-							openPopover === 'padding-top' ? 'selected-side' : ''
+							openPopover === 'padding-top' ||
+								focusSide === 'padding-top'
+								? 'selected-side'
+								: ''
 						)}
-						onMouseDown={topPaddingDragValueHandler}
+						onMouseDown={(event) => {
+							topPaddingDragValueHandler(event);
+							setFocusSide('padding-top');
+						}}
 						d="M47.242 41.5H202.757C204.094 41.5 204.763 43.1157 203.818 44.0607L178.697 69.182C177.853 70.0259 176.708 70.5 175.515 70.5H74.4846C73.2912 70.5 72.1466 70.0259 71.3027 69.182L46.1813 44.0607C45.2364 43.1157 45.9056 41.5 47.242 41.5Z"
 					/>
 
@@ -255,11 +306,15 @@ export default function BoxSpacingControl({
 							'shape-side',
 							'side-vertical',
 							'side-padding-right',
-							openPopover === 'padding-right'
+							openPopover === 'padding-right' ||
+								focusSide === 'padding-right'
 								? 'selected-side'
 								: ''
 						)}
-						onMouseDown={rightPaddingDragValueHandler}
+						onMouseDown={(event) => {
+							rightPaddingDragValueHandler(event);
+							setFocusSide('padding-right');
+						}}
 						d="M178.5 83.4679V75.4854C178.5 74.2919 178.974 73.1473 179.818 72.3034L204.939 47.1821C205.884 46.2371 207.5 46.9064 207.5 48.2427V110.711C207.5 112.047 205.884 112.716 204.939 111.771L179.818 86.6499C178.974 85.806 178.5 84.6614 178.5 83.4679Z"
 					/>
 
@@ -268,11 +323,15 @@ export default function BoxSpacingControl({
 							'shape-side',
 							'side-horizontal',
 							'side-padding-bottom',
-							openPopover === 'padding-bottom'
+							openPopover === 'padding-bottom' ||
+								focusSide === 'padding-bottom'
 								? 'selected-side'
 								: ''
 						)}
-						onMouseDown={bottomPaddingDragValueHandler}
+						onMouseDown={(event) => {
+							bottomPaddingDragValueHandler(event);
+							setFocusSide('padding-bottom');
+						}}
 						d="M74.4387 88.5H175.562C176.755 88.5 177.9 88.9741 178.744 89.818L203.865 114.939C204.81 115.884 204.141 117.5 202.804 117.5H47.1961C45.8597 117.5 45.1905 115.884 46.1354 114.939L71.2568 89.818C72.1007 88.9741 73.2453 88.5 74.4387 88.5Z"
 					/>
 
@@ -281,11 +340,15 @@ export default function BoxSpacingControl({
 							'shape-side',
 							'side-vertical',
 							'side-padding-left',
-							openPopover === 'padding-left'
+							openPopover === 'padding-left' ||
+								focusSide === 'padding-left'
 								? 'selected-side'
 								: ''
 						)}
-						onMouseDown={leftPaddingDragValueHandler}
+						onMouseDown={(event) => {
+							leftPaddingDragValueHandler(event);
+							setFocusSide('padding-left');
+						}}
 						d="M42.5 110.711V48.2432C42.5 46.9069 44.1157 46.2376 45.0607 47.1826L70.182 72.3039C71.0259 73.1478 71.5 74.2924 71.5 75.4859V83.468C71.5 84.6615 71.0259 85.8061 70.182 86.65L45.0607 111.771C44.1157 112.716 42.5 112.047 42.5 110.711Z"
 					/>
 				</svg>
