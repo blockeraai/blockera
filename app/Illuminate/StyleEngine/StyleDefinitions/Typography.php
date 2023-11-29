703bbf7081e18a97c99543f2318f25d92e73a268
rezaelahidev <rezaelahi.dev@gmail.com>
2023-11-29 12:25:05 +0330

    feat: support typography all css props in backend

--- app/Illuminate/StyleEngine/StyleDefinitions/Typography.php
+++ app/Illuminate/StyleEngine/StyleDefinitions/Typography.php
@@ -11,9 +11,29 @@ class Typography extends BaseStyleDefinition {
 			return $this->properties;
 		}
 
+		$cssProperty   = $this->settings['type'];
+		$propertyValue = $this->settings[ $cssProperty ];
+		
+		if ( 'text-orientation' === $cssProperty ) {
+
+			$this->setProperties(
+				[
+					'writing-mode' => $propertyValue['writing-mode'] . $this->getImportant(),
+					$cssProperty   => $propertyValue['text-orientation'] . $this->getImportant(),
+				]
+			);
+
+			return $this->properties;
+		}
+
+		if ( 'column-count' === $cssProperty ) {
+
+			$propertyValue = 'none' === $propertyValue ? 'initial' : preg_replace( '/\b-columns\b/i', '', $propertyValue );
+		}
+
 		$this->setProperties(
 			[
-				$this->settings['type'] => $this->settings[ $this->settings['type'] ] . $this->getImportant(),
+				$cssProperty => $propertyValue . $this->getImportant(),
 			]
 		);
 
