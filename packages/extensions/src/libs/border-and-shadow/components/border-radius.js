d48251b06fe051dda5fdd6d1de6bc5494ecc6b4c
Ali Aghdam <betteraliaghdam@gmail.com>
2023-12-05 02:59:17 +0330

    feat: refactor to wp compatibility improve + value addon support

--- packages/extensions/src/libs/border-and-shadow/components/border-radius.js
+++ packages/extensions/src/libs/border-and-shadow/components/border-radius.js
@@ -71,6 +71,52 @@ export const BorderRadius = ({
 								setAttributes: (attributes: Object) => void
 							): void => {
 								if ('all' === newValue.type) {
+									if (!newValue.all.endsWith('func')) {
+										setAttributes({
+											...attributes,
+											style: {
+												...(attributes?.style ?? {}),
+												border: {
+													...(attributes?.style
+														?.border ?? {}),
+													radius: newValue.all,
+												},
+											},
+										});
+									} else if (
+										attributes?.style?.border?.radius
+									) {
+										// delete wp old data if exist
+										const newAttrs = { ...attributes };
+
+										delete newAttrs.style.border.radius;
+
+										setAttributes(newAttrs);
+									}
+								} else {
+									let {
+										topLeft,
+										topRight,
+										bottomRight,
+										bottomLeft,
+									} = newValue;
+
+									if (topLeft.endsWith('func')) {
+										topLeft = '';
+									}
+
+									if (topRight.endsWith('func')) {
+										topRight = '';
+									}
+
+									if (bottomRight.endsWith('func')) {
+										bottomRight = '';
+									}
+
+									if (bottomLeft.endsWith('func')) {
+										bottomLeft = '';
+									}
+
 									setAttributes({
 										...attributes,
 										style: {
@@ -78,37 +124,16 @@ export const BorderRadius = ({
 											border: {
 												...(attributes?.style?.border ??
 													{}),
-												radius: newValue.all,
+												radius: {
+													topLeft,
+													topRight,
+													bottomRight,
+													bottomLeft,
+												},
 											},
 										},
 									});
-
-									return;
 								}
-
-								const {
-									topLeft,
-									topRight,
-									bottomRight,
-									bottomLeft,
-								} = newValue;
-
-								setAttributes({
-									...attributes,
-									style: {
-										...(attributes?.style ?? {}),
-										border: {
-											...(attributes?.style?.border ??
-												{}),
-											radius: {
-												topLeft,
-												topRight,
-												bottomRight,
-												bottomLeft,
-											},
-										},
-									},
-								});
 							}
 						)
 					}
