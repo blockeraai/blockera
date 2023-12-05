000213661fb55d98126cad8a6c989990246e2ce7
Ali Aghdam <betteraliaghdam@gmail.com>
2023-12-05 18:46:59 +0330

    feat: typography extension unit value support

--- app/Illuminate/StyleEngine/StyleDefinitions/Typography.php
+++ app/Illuminate/StyleEngine/StyleDefinitions/Typography.php
@@ -13,29 +13,44 @@ class Typography extends BaseStyleDefinition {
 
 		$cssProperty   = $this->settings['type'];
 		$propertyValue = $this->settings[ $cssProperty ];
-		
-		if ( 'text-orientation' === $cssProperty ) {
 
+		switch ( $cssProperty ) {
+
+			case 'text-orientation':
+
+				$this->setProperties(
+					[
+						'writing-mode' => $propertyValue['writing-mode'] . $this->getImportant(),
+						$cssProperty   => $propertyValue['text-orientation'] . $this->getImportant(),
+					]
+				);
+
+				return $this->properties;
+
+			case 'column-count':
+				$propertyValue = 'none' === $propertyValue ? 'initial' : preg_replace( '/\b-columns\b/i', '', $propertyValue );
+				break;
+
+
+			case '-webkit-text-stroke-width':
+			case 'letter-spacing':
+			case 'word-spacing':
+			case 'line-height':
+			case 'text-indent':
+			case 'column-gap':
+			case 'font-size':
+				$propertyValue = $propertyValue ? pb_get_value_addon_real_value( $propertyValue ) : '';
+				break;
+
+		}
+
+		if ( $propertyValue ) {
 			$this->setProperties(
 				[
-					'writing-mode' => $propertyValue['writing-mode'] . $this->getImportant(),
-					$cssProperty   => $propertyValue['text-orientation'] . $this->getImportant(),
+					$cssProperty => $propertyValue . $this->getImportant(),
 				]
 			);
-
-			return $this->properties;
 		}
-
-		if ( 'column-count' === $cssProperty ) {
-
-			$propertyValue = 'none' === $propertyValue ? 'initial' : preg_replace( '/\b-columns\b/i', '', $propertyValue );
-		}
-
-		$this->setProperties(
-			[
-				$cssProperty => $propertyValue . $this->getImportant(),
-			]
-		);
 
 		return $this->properties;
 	}
