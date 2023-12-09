756081c70450347bc13f5b4b7896ff7ce52cacbe
maryamsalamatl <maryamsalamat47@gmail.com>
2023-12-09 13:07:57 +0400

    feat(stories): add advanced extension to storybook

---
+++ packages/extensions/src/libs/advanced/stories/extension.stories.js
@@ -0,0 +1,98 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Publisher Storybook dependencies
+ */
+import { default as Decorators } from '@publisher/storybook/decorators';
+
+/**
+ * Internal dependencies
+ */
+import { BaseExtension, ExtensionStyle } from '@publisher/extensions';
+import {
+	blocksInitializer,
+	createBlockEditorContent,
+} from '@publisher/storybook/block-api';
+import { Playground } from '@publisher/storybook/components';
+import { supports } from '../supports';
+import { attributes } from '../attributes';
+import AdvancedExtensionIcon from '../icons/extension-icon';
+import { WithPlaygroundStyles } from '../../../../../../.storybook/decorators/with-playground-styles';
+import { useAttributes } from '../../shared/use-attributes';
+import { InspectorControls } from '@wordpress/block-editor';
+
+const { SharedDecorators } = Decorators;
+
+SharedDecorators.push(WithPlaygroundStyles);
+
+blocksInitializer({
+	name: 'publisherAdvancedExtension',
+	targetBlock: 'core/paragraph',
+	attributes,
+	supports,
+	edit({ attributes, setAttributes, ...props }) {
+		// eslint-disable-next-line
+		const { handleOnChangeAttributes } = useAttributes(
+			attributes,
+			setAttributes
+		);
+
+		return (
+			<>
+				<InspectorControls>
+					<BaseExtension
+						initialOpen={true}
+						extensionId={'Advanced'}
+						attributes={attributes?.publisherAttributes || []}
+						properties={attributes?.publisherCSSProperties || []}
+						handleOnChangeAttributes={handleOnChangeAttributes}
+						title={__('Advanced', 'publisher-core')}
+						storeName={'publisher-core/controls/repeater'}
+						icon={<AdvancedExtensionIcon />}
+					/>
+				</InspectorControls>
+
+				<ExtensionStyle
+					extensions={['Advanced']}
+					{...{
+						...props,
+						attributes,
+						setAttributes,
+					}}
+				/>
+			</>
+		);
+	},
+});
+
+const wrapperBlock = createBlockEditorContent({
+	blockName: 'core/paragraph',
+	attributes: {
+		content: 'This is test text.',
+	},
+});
+
+export default {
+	title: 'Extensions/Advanced',
+	component: Playground,
+	tags: ['autodocs'],
+};
+
+export const Default = {
+	args: {
+		blocks: [
+			{
+				...wrapperBlock,
+				attributes: {
+					...(wrapperBlock?.attributes || {}),
+					publisherAttributes: [],
+					publisherCSSProperties: [],
+				},
+			},
+		],
+	},
+	decorators: [...SharedDecorators],
+};
