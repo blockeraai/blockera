81ccbd00feb3fc1c4ce5dfd912833e65d453cc3d
Ali Aghdam <betteraliaghdam@gmail.com>
2023-12-12 02:05:28 +0330

    feat: refactor popover to show more buttons easily + usages refactor (color picker) + tests

--- packages/controls/src/libs/border-control/test/border-control.cy.js
+++ packages/controls/src/libs/border-control/test/border-control.cy.js
@@ -147,7 +147,7 @@ describe('border-control component testing', () => {
 					value: {
 						width: '0px',
 						style: 'solid',
-						color: '',
+						color: '#cccccc',
 					},
 					name,
 				});
@@ -155,7 +155,7 @@ describe('border-control component testing', () => {
 				//Check current color
 				cy.getByDataTest('border-control-color')
 					.should('have.attr', 'style')
-					.should('include', 'ccc');
+					.should('include', 'cccccc');
 
 				//Check data provider value!
 				cy.getByDataTest('border-control-color').then(() => {
@@ -164,7 +164,7 @@ describe('border-control component testing', () => {
 
 				//Clear color
 				cy.getByDataTest('border-control-color').click();
-				cy.get('[aria-label="reset-color"]').click();
+				cy.get('[aria-label="Reset Color (Clear)"]').click();
 				cy.getByDataTest('border-control-color')
 					.should('have.attr', 'style')
 					.should('be.empty');
