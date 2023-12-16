315d8de878a060e62610190f07ffc28e9880d7c9
Ali Aghdam <betteraliaghdam@gmail.com>
2023-12-16 20:25:23 +0330

    feat: elseWrapper add to ConditionalWrapper component

--- packages/components/src/conditional-wrapper/types/index.js
+++ packages/components/src/conditional-wrapper/types/index.js
@@ -4,5 +4,6 @@ import type { Node } from 'react';
 export type ConditionalItem = {
 	condition: boolean,
 	wrapper: (children: Node) => Node,
+	elseWrapper?: (children: Node) => Node,
 	children: Node,
 };
