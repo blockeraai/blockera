61890245b5a06233a63074988aa02f6a0a8f2009
rezaelahidev <rezaelahi.dev@gmail.com>
2023-12-18 16:17:25 +0330

    feat: add new helper function on utils package

--- packages/utils/src/string/index.js
+++ packages/utils/src/string/index.js
@@ -1,17 +1,27 @@
-export function ucFirstWord(word: string) {
+// @flow
+
+export function ucFirstWord(word: string): string {
 	return word.charAt(0).toUpperCase() + word.slice(1);
 }
 
-export function getPascalCase(str) {
+export function getPascalCase(str: string): string {
 	return str.replace(/(\w)(\w*)/g, function (_, first, rest) {
 		return first.toUpperCase() + rest.toLowerCase();
 	});
 }
 
-export function getCamelCase(str, removePart) {
+export function getCamelCase(str: string, removePart: string): string {
 	if (removePart) {
 		str = str.replace(removePart, '');
 	}
 
 	return str.substring(0, 1).toLowerCase() + str.substring(1, str.length);
+}
+
+export function isJSONString(str: string): boolean {
+	try {
+		return JSON.parse(str);
+	} catch (error) {
+		return str;
+	}
 }
