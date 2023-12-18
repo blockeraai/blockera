9f7c5a11c321b00549494a262282d158cfb3f03e
Ali Aghdam <betteraliaghdam@gmail.com>
2023-12-18 07:18:46 +0330

    fix: jest config

--- .dev/tests/jest/jest.config.js
+++ .dev/tests/jest/jest.config.js
@@ -20,4 +20,8 @@ module.exports = {
 			isolatedModules: true,
 		},
 	},
+	moduleNameMapper: {
+		// Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
+		uuid: require.resolve('uuid'),
+	},
 };
