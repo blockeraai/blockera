33dfad600edaa6528ad9c2b47483479cb8212232
maryamsalamatl <maryamsalamat47@gmail.com>
2023-12-24 17:26:46 +0400

    story: add mouse extension to storybook

---
+++ packages/extensions/src/libs/mouse/stories/extension.stories.js
@@ -0,0 +1,104 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Publisher Storybook dependencies
+ */
+import { default as Decorators } from '@publisher/storybook/decorators';
+
+/**
+ * Internal dependencies
+ */
+import { BaseExtension, ExtensionStyle } from '@publisher/extensions';
+import {
+	blocksInitializer,
+	createBlockEditorContent,
+} from '@publisher/storybook/block-api';
+import { Playground } from '@publisher/storybook/components';
+import { supports } from '../supports';
+import { attributes } from '../attributes';
+import MouseExtensionIcon from '../icons/extension-icon';
+import { WithPlaygroundStyles } from '../../../../../../.storybook/decorators/with-playground-styles';
+import { useAttributes } from '../../shared/use-attributes';
+import { InspectorControls } from '@wordpress/block-editor';
+
+const { SharedDecorators } = Decorators;
+
+SharedDecorators.push(WithPlaygroundStyles);
+
+blocksInitializer({
+	name: 'publisherMouseExtension',
+	targetBlock: 'core/paragraph',
+	attributes,
+	supports,
+	edit({ attributes, setAttributes, ...props }) {
+		// eslint-disable-next-line
+		const { handleOnChangeAttributes } = useAttributes(
+			attributes,
+			setAttributes
+		);
+
+		return (
+			<>
+				<InspectorControls>
+					<BaseExtension
+						{...{ ...props, attributes, setAttributes }}
+						initialOpen={true}
+						values={{
+							cursor: attributes?.publisherCursor || '',
+							userSelect: attributes?.publisherUserSelect || '',
+							pointerEvents:
+								attributes?.publisherPointerEvents || '',
+						}}
+						extensionId={'Mouse'}
+						icon={<MouseExtensionIcon />}
+						storeName={'publisher-core/controls'}
+						handleOnChangeAttributes={handleOnChangeAttributes}
+						title={__('Mouse', 'publisher-core')}
+					/>
+				</InspectorControls>
+
+				<ExtensionStyle
+					extensions={['Mouse']}
+					{...{
+						...props,
+						attributes,
+						setAttributes,
+					}}
+				/>
+			</>
+		);
+	},
+});
+
+const wrapperBlock = createBlockEditorContent({
+	blockName: 'core/paragraph',
+	attributes: {
+		content: 'This is test text.',
+	},
+});
+
+export default {
+	title: 'Extensions/Mouse',
+	component: Playground,
+	tags: ['autodocs'],
+};
+
+export const Default = {
+	args: {
+		blocks: [
+			{
+				...wrapperBlock,
+				attributes: {
+					...(wrapperBlock?.attributes || {}),
+					publisherCursor: '',
+					publisherUserSelect: '',
+					publisherPointerEvents: '',
+				},
+			},
+		],
+	},
+	decorators: [...SharedDecorators],
+};
