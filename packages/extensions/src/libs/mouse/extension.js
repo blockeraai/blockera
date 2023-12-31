9a74a18b35dfc6cf289b077b52edd354f1f8a67c
Ali Aghdam <betteraliaghdam@gmail.com>
2023-12-31 13:37:42 +0330

    feat: Fature help add to Mouse extension

--- packages/extensions/src/libs/mouse/extension.js
+++ packages/extensions/src/libs/mouse/extension.js
@@ -52,8 +52,24 @@ export const MouseExtension: MixedElement = memo<TMouseProps>(
 						}}
 					>
 						<SelectControl
-							controlName="select"
 							label={__('Cursor', 'publisher-core')}
+							labelPopoverTitle={__('Cursor', 'publisher-core')}
+							labelDescription={
+								<>
+									<p>
+										{__(
+											"It allows to change the mouse cursor when it's over block.",
+											'publisher-core'
+										)}
+									</p>
+									<p>
+										{__(
+											"It provides visual feedback to the user about the nature of the block they're interacting with – whether it's clickable, text-selectable, disabled, or used for resizing.",
+											'publisher-core'
+										)}
+									</p>
+								</>
+							}
 							columns="columns-2"
 							options={cursorFieldOptions()}
 							type="custom"
@@ -80,8 +96,33 @@ export const MouseExtension: MixedElement = memo<TMouseProps>(
 						}}
 					>
 						<SelectControl
-							controlName="select"
 							label={__('User Select', 'publisher-core')}
+							labelPopoverTitle={__(
+								'User Select',
+								'publisher-core'
+							)}
+							labelDescription={
+								<>
+									<p>
+										{__(
+											'It controls how text can be selected by the user.',
+											'publisher-core'
+										)}
+									</p>
+									<p>
+										{__(
+											"It's useful for improving user experience.",
+											'publisher-core'
+										)}
+									</p>
+									<p>
+										{__(
+											'For instance, disabling text selection on buttons or icons can prevent confusion, while enabling it on textual content improves usability.',
+											'publisher-core'
+										)}
+									</p>
+								</>
+							}
 							columns="columns-2"
 							options={userSelectOptions()}
 							type="native"
@@ -107,8 +148,33 @@ export const MouseExtension: MixedElement = memo<TMouseProps>(
 						}}
 					>
 						<SelectControl
-							controlName="select"
 							label={__('Pointer Events', 'publisher-core')}
+							labelPopoverTitle={__(
+								'User Select',
+								'publisher-core'
+							)}
+							labelDescription={
+								<>
+									<p>
+										{__(
+											'It specifies how a block reacts to pointer interactions, such as mouse clicks or touch.',
+											'publisher-core'
+										)}
+									</p>
+									<p>
+										{__(
+											'It allows you to create blocks that are either interactive or completely non-interactive with pointer actions.',
+											'publisher-core'
+										)}
+									</p>
+									<p>
+										{__(
+											'For example, disabling pointer events on an overlay of block can allow users to interact with underlying block.',
+											'publisher-core'
+										)}
+									</p>
+								</>
+							}
 							columns="columns-2"
 							options={pointerEventsOptions()}
 							type="native"
