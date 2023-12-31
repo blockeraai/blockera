072127630479fad983c318f3b56fc12775643dff
Ali Aghdam <betteraliaghdam@gmail.com>
2023-12-31 18:04:40 +0330

    feat: propTypes remove

--- packages/controls/src/libs/repeater-control/index.js
+++ packages/controls/src/libs/repeater-control/index.js
@@ -2,7 +2,6 @@
 /**
  * External dependencies
  */
-import PropTypes from 'prop-types';
 import { __ } from '@wordpress/i18n';
 import type { MixedElement } from 'react';
 
@@ -140,7 +139,7 @@ export default function RepeaterControl({
 				data-cy="publisher-repeater-control"
 			>
 				<div className={controlInnerClassNames('header')}>
-					{!withoutAdvancedLabel && (
+					{!withoutAdvancedLabel ? (
 						<LabelControl
 							label={label}
 							labelPopoverTitle={labelPopoverTitle}
@@ -157,8 +156,7 @@ export default function RepeaterControl({
 									: defaultValue
 							}
 						/>
-					)}
-					{withoutAdvancedLabel && (
+					) : (
 						<LabelControl label={label} mode={'simple'} />
 					)}
 
@@ -269,117 +267,3 @@ export default function RepeaterControl({
 		</RepeaterContextProvider>
 	);
 }
-
-RepeaterControl.propTypes = {
-	/**
-	 * It sets the default value of repeater. Please note for defining the value of repeater items you have to use `defaultRepeaterItemValue`
-	 */
-	defaultValue: PropTypes.array,
-	// /**
-	//  * Function that will be fired while the control value state changes.
-	//  */
-	// onChange: PropTypes.func,
-	/**
-	 * Function that runs before firing onChange. You can use it cleanup values
-	 */
-	valueCleanup: PropTypes.func,
-	/**
-	 * It sets the default of each repeater item.
-	 */
-	defaultRepeaterItemValue: PropTypes.object,
-	/**
-	 * It specifies the design of repeater control.
-	 */
-	// $FlowFixMe
-	design: PropTypes.oneOf(['minimal']),
-	/**
-	 * Specifies that repeater item should use popover or accordion
-	 *
-	 * @default true
-	 */
-	// $FlowFixMe
-	mode: PropTypes.oneOf(['popover', 'accordion']),
-	/**
-	 * Specifies the popover title if `mode` was `popover`. by default the repeater label will be shown as popover title.
-	 */
-	popoverTitle: PropTypes.string,
-	/**
-	 * Specifies custom css classes that should be added to popover
-	 *
-	 * @default true
-	 */
-	popoverClassName: PropTypes.string,
-	/**
-	 * Specifies maximum number of repeater items. -1 means unlimited.
-	 *
-	 * @default -1
-	 */
-	maxItems: PropTypes.number,
-	/**
-	 * Specifies minimum number of repeater items.
-	 *
-	 * @default 0
-	 */
-	minItems: PropTypes.number,
-	/**
-	 * Specifies the add button should be shown for repeater items.
-	 *
-	 * @default true
-	 */
-	actionButtonAdd: PropTypes.bool,
-	/**
-	 * Specifies the visibility or activation control should be shown for repeater items.
-	 *
-	 * @default true
-	 */
-	actionButtonVisibility: PropTypes.bool,
-	/**
-	 * Specifies delete or remove control should be shown for repeater items.
-	 *
-	 * @default true
-	 */
-	actionButtonDelete: PropTypes.bool,
-	/**
-	 * Specifies clone or copy control should be shown for repeater items.
-	 *
-	 * @default true
-	 */
-	actionButtonClone: PropTypes.bool,
-	/**
-	 * A placeholder that you can use inject items at the beginning of header buttons.
-	 */
-	// $FlowFixMe
-	injectHeaderButtonsStart: PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.element,
-	]),
-	/**
-	 * A placeholder that you can use inject items after header buttons.
-	 */
-	// $FlowFixMe
-	injectHeaderButtonsEnd: PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.element,
-	]),
-	/**
-	 * Header component for each repeater item
-	 */
-	// $FlowFixMe
-	repeaterItemHeader: PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.element,
-		PropTypes.func,
-		PropTypes.object,
-	]),
-	/**
-	 * Children components for each repeater item
-	 */
-	// $FlowFixMe
-	repeaterItemChildren: PropTypes.oneOfType([
-		PropTypes.string,
-		PropTypes.element,
-		PropTypes.func,
-		PropTypes.object,
-	]),
-	getDynamicDefaultRepeaterItem: PropTypes.func,
-};
--- packages/controls/src/libs/repeater-control/types/index.js
+++ packages/controls/src/libs/repeater-control/types/index.js
@@ -22,25 +22,87 @@ type ID = string | number;
 export type RepeaterControlProps = {
 	...Object,
 	...ControlGeneralTypes,
+	/**
+	 * It specifies the design of repeater control.
+	 */
 	design?: 'minimal',
+	/**
+	 * Specifies that repeater item should use popover or accordion
+	 *
+	 * @default true
+	 */
 	mode?: GroupControlMode,
 	withoutAdvancedLabel?: boolean,
+	/**
+	 * Specifies the popover title if `mode` was `popover`. by default the repeater label will be shown as popover title.
+	 */
 	popoverTitle?: string | MixedElement,
 	addNewButtonLabel?: string,
+	/**
+	 * Specifies custom css classes that should be added to popover
+	 *
+	 * @default true
+	 */
 	popoverClassName?: string,
+	/**
+	 * Specifies maximum number of repeater items. -1 means unlimited.
+	 *
+	 * @default -1
+	 */
 	maxItems?: number,
+	/**
+	 * Specifies minimum number of repeater items.
+	 *
+	 * @default 0
+	 */
 	minItems?: number,
+	/**
+	 * Specifies the add button should be shown for repeater items.
+	 *
+	 * @default true
+	 */
 	actionButtonAdd?: boolean,
+	/**
+	 * Specifies the visibility or activation control should be shown for repeater items.
+	 *
+	 * @default true
+	 */
 	actionButtonVisibility?: boolean,
+	/**
+	 * Specifies delete or remove control should be shown for repeater items.
+	 *
+	 * @default true
+	 */
 	actionButtonDelete?: boolean,
+	/**
+	 * Specifies clone or copy control should be shown for repeater items.
+	 *
+	 * @default true
+	 */
 	actionButtonClone?: boolean,
+	/**
+	 * A placeholder that you can use inject items at the beginning of header buttons.
+	 */
 	injectHeaderButtonsStart?: MixedElement | null | string,
+	/**
+	 * A placeholder that you can use inject items after header buttons.
+	 */
 	injectHeaderButtonsEnd?: MixedElement | null | string,
-	//
+	/**
+	 * Header component for each repeater item
+	 */
 	repeaterItemHeader?: MixedElement | any,
+	/**
+	 * Children components for each repeater item
+	 */
 	repeaterItemChildren?: MixedElement | any,
-	//
+	/**
+	 * It sets the default value of repeater. Please note for defining the value of repeater items you have to use `defaultRepeaterItemValue`
+	 */
 	defaultValue?: Array<Object> | [],
+	/**
+	 * It sets the default of each repeater item.
+	 */
 	defaultRepeaterItemValue?: Object,
 	getDynamicDefaultRepeaterItem?: (
 		itemsCount: number,
@@ -49,6 +111,9 @@ export type RepeaterControlProps = {
 	onSelect?: (event: MouseEvent, item: Object) => boolean,
 	overrideItem?: (item: Object) => Object,
 	repeaterItemOpener?: (props: Object) => boolean | MixedElement,
+	/**
+	 * Function that runs before firing onChange. You can use it cleanup values
+	 */
 	valueCleanup?: (any | Array<Object>) => any | Array<Object>,
 };
 
