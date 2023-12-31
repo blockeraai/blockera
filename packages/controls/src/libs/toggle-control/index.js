a15cd8331fd4d383e8f4febaeaf833b21be77375
Ali Aghdam <betteraliaghdam@gmail.com>
2023-12-31 13:34:10 +0330

    fix: extra label bug + code care

--- packages/controls/src/libs/toggle-control/index.js
+++ packages/controls/src/libs/toggle-control/index.js
@@ -2,7 +2,6 @@
 /**
  * External dependencies
  */
-import PropTypes from 'prop-types';
 import type { MixedElement } from 'react';
 import { ToggleControl as WPToggleControl } from '@wordpress/components';
 
@@ -30,6 +29,7 @@ export default function ToggleControl({
 	columns,
 	onChange,
 	className,
+	children,
 	...props
 }: TToggleControlProps): MixedElement {
 	const {
@@ -68,31 +68,13 @@ export default function ToggleControl({
 			{...labelProps}
 		>
 			<WPToggleControl
-				label={label}
+				label={columns ? '' : label}
 				checked={value}
 				onChange={setValue}
 				className={controlClassNames('toggle', className)}
 				{...props}
 			/>
+			{children}
 		</BaseControl>
 	);
 }
-
-ToggleControl.propTypes = {
-	/**
-	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
-	 */
-	defaultValue: PropTypes.bool,
-	/**
-	 * The current value.
-	 */
-	value: PropTypes.bool,
-	/**
-	 * Function that will be fired while the control value state changes.
-	 */
-	onChange: PropTypes.func,
-	/**
-	 * Label after toggle
-	 */
-	label: PropTypes.string,
-};
