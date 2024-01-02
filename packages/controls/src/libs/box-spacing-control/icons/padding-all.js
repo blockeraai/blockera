7771d84610ceb8127628cc3a97754a603a566b59
Ali Aghdam <betteraliaghdam@gmail.com>
2024-01-02 04:58:02 +0330

    feat: icons design improvement

--- packages/controls/src/libs/box-spacing-control/icons/padding-all.js
+++ packages/controls/src/libs/box-spacing-control/icons/padding-all.js
@@ -12,11 +12,10 @@ export default function PaddingAllIcon({}) {
 				clipRule="evenodd"
 				d="M18 0H0V18H18V0ZM17 1H1V17H17V1Z"
 			/>
-			<path
-				fillRule="evenodd"
-				clipRule="evenodd"
-				d="M16 2H2V16H16V2ZM6 12H12V6H6V12Z"
-			/>
+			<path d="M2 3.41421V14.34L6 10.34V7.41421L2 3.41421Z" />
+			<path d="M7.41421 6L3.41421 2H14.3399L10.3399 6H7.41421Z" />
+			<path d="M12 7.16836V10.5859L16 14.5859V3.16836L12 7.16836Z" />
+			<path d="M14.5857 16L10.5857 12H7.16842L3.16842 16H14.5857Z" />
 		</svg>
 	);
 }
