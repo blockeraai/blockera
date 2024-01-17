4391689d408ac69b93ec699e6863fca910ae53f2
rezaelahidev <rezaelahi.dev@gmail.com>
2024-01-17 22:30:36 +0330

    feat: add new hook into memoization props

--- packages/extensions/src/components/utils.js
+++ packages/extensions/src/components/utils.js
@@ -1,5 +1,7 @@
 // @flow
 
+import { applyFilters } from '@wordpress/hooks';
+
 /**
  * Publisher dependencies
  */
@@ -11,10 +13,23 @@ import { omitWithPattern } from '@publisher/utils';
 import { hasSameProps } from '../libs';
 
 export const propsAreEqual = (
-	prev: { attributes: Object },
+	prev: { attributes: Object, name: string },
 	next: { attributes: Object }
 ): boolean => {
-	const regexp = /\b(?!publisher)\w+\b/i;
+	const normalizedBlockName = prev.name.replace('/', '.');
+
+	const excludedAttributeKeys = applyFilters(
+		`publisherCore.blockEdit.${normalizedBlockName}.memoization.excludedAttributeKeys`,
+		[]
+	).map((attributeId: string): string => `\b${attributeId}\b`);
+
+	//FIXME: we needs to declaration excludedAttributeKeys for WP Compatibilities and prevent of redundant re-rendering process,
+	// please double check all extensions bootstrap files to define these!
+	if (!excludedAttributeKeys.length) {
+		return hasSameProps(prev.attributes, next.attributes);
+	}
+
+	const regexp = new RegExp(excludedAttributeKeys.join('|'));
 
 	return hasSameProps(
 		omitWithPattern(prev.attributes, regexp),
