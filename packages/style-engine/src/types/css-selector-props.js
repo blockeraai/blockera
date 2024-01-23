8511b52108a8fc2f3bab08f747f79adcff3700b4
rezaelahidev <rezaelahi.dev@gmail.com>
2024-01-23 13:30:24 +0330

    feat: add inner blocks style support

--- packages/style-engine/src/types/css-selector-props.js
+++ packages/style-engine/src/types/css-selector-props.js
@@ -1,8 +1,15 @@
 // @flow
+
+/**
+ * Publisher dependencies
+ */
+import type { InnerBlockModel } from '@publisher/extensions/src/libs/inner-blocks/types';
+
 export type TUseCssSelectorProps = {
 	query?: string,
 	blockName?: string,
 	supportId?: string,
 	currentState: string,
+	innerBlocks: Array<InnerBlockModel>,
 	fallbackSupportId?: string,
 };
