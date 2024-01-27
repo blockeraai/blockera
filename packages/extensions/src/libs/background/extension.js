90e118ab9a9e31cacd4ac234f811ff53d804ee48
Ali Aghdam <betteraliaghdam@gmail.com>
2024-01-27 11:45:01 +0330

    feat: FeatureWrapper add to Background Extension

--- packages/extensions/src/libs/background/extension.js
+++ packages/extensions/src/libs/background/extension.js
@@ -34,6 +34,7 @@ import { generateExtensionId, hasSameProps } from '../utils';
 import type { TBackgroundProps } from './types/background-props';
 import { default as BackgroundExtensionIcon } from './icons/extension-icon';
 import { ExtensionSettings } from '../settings';
+import { FeatureWrapper } from '@publisher/components';
 
 export const BackgroundExtension: ComponentType<TBackgroundProps> = memo(
 	({
@@ -108,47 +109,61 @@ export const BackgroundExtension: ComponentType<TBackgroundProps> = memo(
 						}}
 						storeName={'publisher-core/controls/repeater'}
 					>
-						<BaseControl
-							controlName="background"
-							columns="columns-1"
+						<FeatureWrapper
+							isActive={isActiveBackground}
+							isActiveOnStates={
+								publisherBackground.isActiveOnStates
+							}
+							isActiveOnBreakpoints={
+								publisherBackground.isActiveOnBreakpoints
+							}
 						>
-							<BackgroundControl
-								label={__('Image & Gradient', 'publisher-core')}
-								onChange={(newValue) => {
-									const toWPCompatibleValue =
-										isArray(newValue) &&
-										!isEmpty(newValue) &&
-										newValue[0]?.image
-											? {
-													style: {
-														...(block?.attributes
-															?.style ?? {}),
-														background: {
+							<BaseControl
+								controlName="background"
+								columns="columns-1"
+							>
+								<BackgroundControl
+									label={__(
+										'Image & Gradient',
+										'publisher-core'
+									)}
+									onChange={(newValue) => {
+										const toWPCompatibleValue =
+											isArray(newValue) &&
+											!isEmpty(newValue) &&
+											newValue[0]?.image
+												? {
+														style: {
 															...(block
 																?.attributes
-																?.style
-																?.background ??
-																{}),
-															backgroundImage:
-																newValue[0]
-																	.image,
+																?.style ?? {}),
+															background: {
+																...(block
+																	?.attributes
+																	?.style
+																	?.background ??
+																	{}),
+																backgroundImage:
+																	newValue[0]
+																		.image,
+															},
 														},
-													},
-											  }
-											: {};
+												  }
+												: {};
 
-									handleOnChangeAttributes(
-										'publisherBackground',
-										newValue,
-										{
-											addOrModifyRootItems:
-												toWPCompatibleValue,
-										}
-									);
-								}}
-								{...extensionProps.publisherBackground}
-							/>
-						</BaseControl>
+										handleOnChangeAttributes(
+											'publisherBackground',
+											newValue,
+											{
+												addOrModifyRootItems:
+													toWPCompatibleValue,
+											}
+										);
+									}}
+									{...extensionProps.publisherBackground}
+								/>
+							</BaseControl>
+						</FeatureWrapper>
 					</ControlContextProvider>
 				)}
 
@@ -165,40 +180,50 @@ export const BackgroundExtension: ComponentType<TBackgroundProps> = memo(
 								blockName: block.blockName,
 							}}
 						>
-							<ColorControl
-								controlName="color"
-								label={__('BG Color', 'publisher-core')}
-								labelPopoverTitle={__(
-									'Background Color',
-									'publisher-core'
-								)}
-								labelDescription={
-									<>
-										<p>
-											{__(
-												'It sets the color of the block’s background, providing a simple yet powerful way to apply solid color.',
-												'publisher-core'
-											)}
-										</p>
-										<p>
-											{__(
-												'You can use variables to use color from your site design system.',
-												'publisher-core'
-											)}
-										</p>
-									</>
+							<FeatureWrapper
+								isActive={isActiveBackgroundColor}
+								isActiveOnStates={
+									publisherBackgroundColor.isActiveOnStates
 								}
-								columns="columns-2"
-								onChange={(newValue) =>
-									handleOnChangeAttributes(
-										'publisherBackgroundColor',
-										newValue
-									)
+								isActiveOnBreakpoints={
+									publisherBackgroundColor.isActiveOnBreakpoints
 								}
-								controlAddonTypes={['variable']}
-								variableTypes={['color']}
-								{...extensionProps.publisherBackgroundColor}
-							/>
+							>
+								<ColorControl
+									controlName="color"
+									label={__('BG Color', 'publisher-core')}
+									labelPopoverTitle={__(
+										'Background Color',
+										'publisher-core'
+									)}
+									labelDescription={
+										<>
+											<p>
+												{__(
+													'It sets the color of the block’s background, providing a simple yet powerful way to apply solid color.',
+													'publisher-core'
+												)}
+											</p>
+											<p>
+												{__(
+													'You can use variables to use color from your site design system.',
+													'publisher-core'
+												)}
+											</p>
+										</>
+									}
+									columns="columns-2"
+									onChange={(newValue) =>
+										handleOnChangeAttributes(
+											'publisherBackgroundColor',
+											newValue
+										)
+									}
+									controlAddonTypes={['variable']}
+									variableTypes={['color']}
+									{...extensionProps.publisherBackgroundColor}
+								/>
+							</FeatureWrapper>
 						</ControlContextProvider>
 					)}
 
@@ -215,144 +240,160 @@ export const BackgroundExtension: ComponentType<TBackgroundProps> = memo(
 								blockName: block.blockName,
 							}}
 						>
-							<SelectControl
-								controlName="select"
-								label={__('Clipping', 'publisher-core')}
-								labelPopoverTitle={__(
-									'Background Clipping',
-									'publisher-core'
-								)}
-								labelDescription={
-									<>
-										<p>
-											{__(
-												'It defines how far the background (color or image) extends within the block.',
-												'publisher-core'
-											)}
-										</p>
-										<p>
-											{__(
-												'It is useful for creating special effects with backgrounds, such as having a background only within the content area or under the borders.',
-												'publisher-core'
-											)}
-										</p>
-										<h3>
-											<ClipPaddingIcon />
-											{__(
+							<FeatureWrapper
+								isActive={isActiveBackgroundClip}
+								isActiveOnStates={
+									publisherBackgroundClip.isActiveOnStates
+								}
+								isActiveOnBreakpoints={
+									publisherBackgroundClip.isActiveOnBreakpoints
+								}
+							>
+								<SelectControl
+									controlName="select"
+									label={__('Clipping', 'publisher-core')}
+									labelPopoverTitle={__(
+										'Background Clipping',
+										'publisher-core'
+									)}
+									labelDescription={
+										<>
+											<p>
+												{__(
+													'It defines how far the background (color or image) extends within the block.',
+													'publisher-core'
+												)}
+											</p>
+											<p>
+												{__(
+													'It is useful for creating special effects with backgrounds, such as having a background only within the content area or under the borders.',
+													'publisher-core'
+												)}
+											</p>
+											<h3>
+												<ClipPaddingIcon />
+												{__(
+													'Clip to Padding',
+													'publisher-core'
+												)}
+											</h3>
+											<p>
+												{__(
+													'The background stops at the padding edge, not extending behind the border.',
+													'publisher-core'
+												)}
+											</p>
+											<h3>
+												<ClipContentIcon />
+												{__(
+													'Clip to Content',
+													'publisher-core'
+												)}
+											</h3>
+											<p>
+												{__(
+													'The background is applied only to the content area.',
+													'publisher-core'
+												)}
+											</p>
+											<h3>
+												<ClipTextIcon />
+												{__(
+													'Clip to Text',
+													'publisher-core'
+												)}
+											</h3>
+											<p>
+												{__(
+													'Advanced feature that allows the background to only be visible through the text of block.',
+													'publisher-core'
+												)}
+											</p>
+											<p>
+												{__(
+													'This creates an eye-catching effect where the text acts as a mask for the background image or video.',
+													'publisher-core'
+												)}
+											</p>
+											<h3>
+												<InheritIcon />
+												{__(
+													'Inherit',
+													'publisher-core'
+												)}
+											</h3>
+											<p>
+												{__(
+													'Clipping inherit from the parent block.',
+													'publisher-core'
+												)}
+											</p>
+										</>
+									}
+									columns="columns-2"
+									options={[
+										{
+											label: __('None', 'publisher-core'),
+											value: 'none',
+											icon: <ClipNoneIcon />,
+										},
+										{
+											label: __(
 												'Clip to Padding',
 												'publisher-core'
-											)}
-										</h3>
-										<p>
-											{__(
-												'The background stops at the padding edge, not extending behind the border.',
-												'publisher-core'
-											)}
-										</p>
-										<h3>
-											<ClipContentIcon />
-											{__(
+											),
+											value: 'padding-box',
+											icon: <ClipPaddingIcon />,
+										},
+										{
+											label: __(
 												'Clip to Content',
 												'publisher-core'
-											)}
-										</h3>
-										<p>
-											{__(
-												'The background is applied only to the content area.',
-												'publisher-core'
-											)}
-										</p>
-										<h3>
-											<ClipTextIcon />
-											{__(
+											),
+											value: 'content-box',
+											icon: <ClipContentIcon />,
+										},
+										{
+											label: __(
 												'Clip to Text',
 												'publisher-core'
-											)}
-										</h3>
-										<p>
+											),
+											value: 'text',
+											icon: <ClipTextIcon />,
+										},
+										{
+											label: __(
+												'Inherit',
+												'publisher-core'
+											),
+											value: 'inherit',
+											icon: <InheritIcon />,
+										},
+									]}
+									type="custom"
+									defaultValue="none"
+									onChange={(newValue) =>
+										handleOnChangeAttributes(
+											'publisherBackgroundClip',
+											newValue
+										)
+									}
+									{...extensionProps.publisherBackgroundClip}
+								/>
+								{!visibleBackgroundLength &&
+									!backgroundColor &&
+									backgroundClip === 'text' && (
+										<NoticeControl
+											type="error"
+											style={{ marginTop: '10px' }}
+										>
 											{__(
-												'Advanced feature that allows the background to only be visible through the text of block.',
+												`You've applied text clipping without setting a background color or image. Make sure to add a background to the block.`,
 												'publisher-core'
 											)}
-										</p>
-										<p>
-											{__(
-												'This creates an eye-catching effect where the text acts as a mask for the background image or video.',
-												'publisher-core'
-											)}
-										</p>
-										<h3>
-											<InheritIcon />
-											{__('Inherit', 'publisher-core')}
-										</h3>
-										<p>
-											{__(
-												'Clipping inherit from the parent block.',
-												'publisher-core'
-											)}
-										</p>
-									</>
-								}
-								columns="columns-2"
-								options={[
-									{
-										label: __('None', 'publisher-core'),
-										value: 'none',
-										icon: <ClipNoneIcon />,
-									},
-									{
-										label: __(
-											'Clip to Padding',
-											'publisher-core'
-										),
-										value: 'padding-box',
-										icon: <ClipPaddingIcon />,
-									},
-									{
-										label: __(
-											'Clip to Content',
-											'publisher-core'
-										),
-										value: 'content-box',
-										icon: <ClipContentIcon />,
-									},
-									{
-										label: __(
-											'Clip to Text',
-											'publisher-core'
-										),
-										value: 'text',
-										icon: <ClipTextIcon />,
-									},
-									{
-										label: __('Inherit', 'publisher-core'),
-										value: 'inherit',
-										icon: <InheritIcon />,
-									},
-								]}
-								type="custom"
-								defaultValue="none"
-								onChange={(newValue) =>
-									handleOnChangeAttributes(
-										'publisherBackgroundClip',
-										newValue
-									)
-								}
-								{...extensionProps.publisherBackgroundClip}
-							/>
-							{!visibleBackgroundLength &&
-								!backgroundColor &&
-								backgroundClip === 'text' && (
-									<NoticeControl
-										type="error"
-										style={{ marginTop: '10px' }}
-									>
-										{__(
-											`You've applied text clipping without setting a background color or image. Make sure to add a background to the block.`,
-											'publisher-core'
-										)}
-									</NoticeControl>
-								)}
+										</NoticeControl>
+									)}
+							</FeatureWrapper>
 						</ControlContextProvider>
 					)}
 			</PanelBodyControl>
