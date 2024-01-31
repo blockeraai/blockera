f7197e4b2b656f9a55b8c56694e5dd0fc3d1b0d6
Ali Aghdam <betteraliaghdam@gmail.com>
2024-01-31 06:29:40 +0330

    feat: drop propTypes

--- packages/controls/src/libs/range-control/index.js
+++ packages/controls/src/libs/range-control/index.js
@@ -3,7 +3,6 @@
 /**
  * External dependencies
  */
-import PropTypes from 'prop-types';
 import { RangeControl as WordPressRangeControl } from '@wordpress/components';
 import type { MixedElement } from 'react';
 
@@ -132,64 +131,3 @@ export default function RangeControl({
 		</BaseControl>
 	);
 }
-
-RangeControl.propTypes = {
-	/**
-	 * ID for retrieving value from control context
-	 */
-	id: PropTypes.string,
-	/**
-	 * Label for field. If you pass empty value the field will not be added and simple control will be rendered
-	 *
-	 * @default ""
-	 */
-	label: PropTypes.string,
-	/**
-	 * Field id for passing into child Field component
-	 *
-	 * @default "toggle-select"
-	 */
-	field: PropTypes.string,
-	/**
-	 * Columns setting for Field grid.
-	 *
-	 * @default "columns-2"
-	 */
-	columns: PropTypes.string,
-	/**
-	 * if when sideEffect is true calling setValue to modify value of context provider,
-	 * else just calling onChange handler!
-	 *
-	 * @default true
-	 */
-	sideEffect: PropTypes.bool,
-	/**
-	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
-	 */
-	// $FlowFixMe
-	defaultValue: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-	/**
-	 * Function that will be fired while the control value state changes.
-	 */
-	onChange: PropTypes.func,
-	/**
-	 * The minimum `value` allowed.
-	 */
-	min: PropTypes.number,
-	/**
-	 * The maximum `value` allowed.
-	 */
-	max: PropTypes.number,
-	/**
-	 * Passed as a prop to the `NumberControl` component and is only
-	 * applicable if `withInputField` and `isShiftStepEnabled` are both true
-	 * and while the number input has focus. Acts as a multiplier of `step`.
-	 */
-	withInputField: PropTypes.bool,
-	/**
-	 * The slider starting position, used when no `value` is passed.
-	 * The `initialPosition` will be clamped between the provided `min`
-	 * and `max` prop values.
-	 */
-	initialPosition: PropTypes.number,
-};
--- packages/controls/src/libs/range-control/types/control-types.js
+++ packages/controls/src/libs/range-control/types/control-types.js
@@ -8,13 +8,41 @@ export type TValueCleanup = string | number;
 
 export type TRangeControlProps = {
 	...ControlGeneralTypes,
+	/**
+	 * It sets the control default value if the value not provided. By using it the control will not fire onChange event for this default value on control first render,
+	 */
 	defaultValue?: number | string,
+	/**
+	 * Function that will be fired while the control value state changes.
+	 */
 	onChange: (number | string) => number | string,
+	/**
+	 * The minimum `value` allowed.
+	 */
 	min?: number,
+	/**
+	 * The maximum `value` allowed.
+	 */
 	max?: number,
+	/**
+	 * Passed as a prop to the `NumberControl` component and is only
+	 * applicable if `withInputField` and `isShiftStepEnabled` are both true
+	 * and while the number input has focus. Acts as a multiplier of `step`.
+	 */
 	withInputField?: boolean,
 	disabled?: boolean,
+	/**
+	 * The slider starting position, used when no `value` is passed.
+	 * The `initialPosition` will be clamped between the provided `min`
+	 * and `max` prop values.
+	 */
 	initialPosition?: number,
+	/**
+	 * if when sideEffect is true calling setValue to modify value of context provider,
+	 * else just calling onChange handler!
+	 *
+	 * @default true
+	 */
 	sideEffect?: boolean,
 	className?: string,
 };
