dc07adf5571f7d9c73ac098c743542dae32618eb
rezaelahidev <rezaelahi.dev@gmail.com>
2024-02-06 12:12:45 +0330

    refactor: canvas-editor display with observer

--- packages/extensions/src/scripts/index.js
+++ packages/extensions/src/scripts/index.js
@@ -1,5 +1,15 @@
 // @flow
 
+import { select } from '@wordpress/data';
+
+/**
+ * Publisher dependencies
+ */
+import { Observer, CanvasEditor } from '@publisher/editor';
+
+/**
+ * Internal dependencies
+ */
 import {
 	bootstrapSizeExtension,
 	bootstrapTypographyExtension,
@@ -8,17 +18,82 @@ import {
 	bootstrapBorderAndShadowExtension,
 } from '../libs';
 
-/**
- * Publisher dependencies
- */
-import { CanvasEditor } from '@publisher/editor';
-
 export default function (wp: Object) {
 	const registerPlugin = wp.plugins.registerPlugin;
+	const getPlugin = wp.plugins.getPlugin;
 
-	registerPlugin('publisher-core-canvas-editor', {
+	registerPlugin('publisher-core-editor-observer', {
 		render() {
-			return <CanvasEditor />;
+			return (
+				<Observer
+					ancestors={[
+						{
+							options: {
+								root: document.querySelector(
+									'div[aria-label="Editor top bar"]'
+								),
+								threshold: 1.0,
+							},
+							callback(
+								entries: Array<IntersectionObserverEntry>
+							) {
+								const editPost = select('core/edit-post');
+
+								if (!editPost) {
+									return;
+								}
+
+								const plugin =
+									'publisher-core-post-editor-top-bar';
+
+								if (getPlugin(plugin)) {
+									return;
+								}
+
+								registerPlugin(plugin, {
+									render() {
+										return (
+											<CanvasEditor entry={entries[0]} />
+										);
+									},
+								});
+							},
+							target: '.edit-post-header__center',
+						},
+						{
+							options: {
+								root: document.querySelector('body'),
+								threshold: 1.0,
+							},
+							callback(
+								entries: Array<IntersectionObserverEntry>
+							) {
+								const editSite = select('core/edit-site');
+
+								if (!editSite) {
+									return;
+								}
+
+								const plugin =
+									'publisher-core-site-editor-top-bar';
+
+								if (getPlugin(plugin)) {
+									return;
+								}
+
+								registerPlugin(plugin, {
+									render() {
+										return (
+											<CanvasEditor entry={entries[0]} />
+										);
+									},
+								});
+							},
+							target: 'div[aria-label="Editor top bar"]',
+						},
+					]}
+				/>
+			);
 		},
 	});
 
