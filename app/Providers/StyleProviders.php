0baa05764200321e8c5f373ad737de451ce12bbb
rezaelahidev <rezaelahi.dev@gmail.com>
2024-02-09 03:46:12 +0330

    refactor: removed redundant PHP classes

--- app/Providers/StyleProviders.php
+++ app/Providers/StyleProviders.php
@@ -3,20 +3,6 @@
 namespace Publisher\Framework\Providers;
 
 use Publisher\Framework\Services\Render\Blocks\Icon;
-use Publisher\Framework\Services\Render\Styles\{
-	SizeStyle,
-	MouseStyle,
-	BorderStyle,
-	LayoutStyle,
-	EffectsStyle,
-	OutlineStyle,
-	SpacingStyle,
-	PositionStyle,
-	BoxShadowStyle,
-	BackgroundStyle,
-	TextShadowStyle,
-	TypographyStyle
-};
 use Publisher\Framework\Illuminate\StyleEngine\StyleDefinitions\{
 	Size,
 	Mouse,
@@ -56,101 +42,18 @@ class StyleProviders extends ServiceProvider {
 			}
 		);
 
-		$this->app->singleton(
-			EffectsStyle::class,
-			static function () {
-
-				return new EffectsStyle( new Effects() );
-			}
-		);
-
-		$this->app->singleton(
-			LayoutStyle::class,
-			static function () {
-
-				return new LayoutStyle( new Layout() );
-			}
-		);
-
-		$this->app->singleton(
-			PositionStyle::class,
-			static function () {
-
-				return new PositionStyle( new Position() );
-			}
-		);
-
-		$this->app->singleton(
-			BorderStyle::class,
-			static function () {
-
-				return new BorderStyle( new Border() );
-			}
-		);
-
-		$this->app->singleton(
-			SizeStyle::class,
-			static function () {
-
-				return new SizeStyle( new Size() );
-			}
-		);
-
-		$this->app->singleton(
-			SpacingStyle::class,
-			static function () {
-
-				return new SpacingStyle( new Spacing() );
-			}
-		);
-
-		$this->app->singleton(
-			TextShadowStyle::class,
-			static function () {
-
-				return new TextShadowStyle( new TextShadow() );
-			}
-		);
-
-		$this->app->singleton(
-			BackgroundStyle::class,
-			static function () {
-
-				return new BackgroundStyle( new Background() );
-			}
-		);
-
-		$this->app->singleton(
-			TypographyStyle::class,
-			static function () {
-
-				return new TypographyStyle( new Typography() );
-			}
-		);
-
-		$this->app->singleton(
-			BoxShadowStyle::class,
-			static function () {
-
-				return new BoxShadowStyle( new BoxShadow() );
-			}
-		);
-
-		$this->app->singleton(
-			OutlineStyle::class,
-			static function () {
-
-				return new OutlineStyle( new Outline() );
-			}
-		);
-
-		$this->app->singleton(
-			MouseStyle::class,
-			static function () {
-
-				return new MouseStyle( new Mouse() );
-			}
-		);
+		$this->app->singleton( Size::class );
+		$this->app->singleton( Mouse::class );
+		$this->app->singleton( Layout::class );
+		$this->app->singleton( Border::class );
+		$this->app->singleton( Effects::class );
+		$this->app->singleton( Outline::class );
+		$this->app->singleton( Spacing::class );
+		$this->app->singleton( Position::class );
+		$this->app->singleton( BoxShadow::class );
+		$this->app->singleton( TextShadow::class );
+		$this->app->singleton( Background::class );
+		$this->app->singleton( Typography::class );
 	}
 
 }
