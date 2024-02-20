2387746596bb85496722da59d48de092ceb18d46
Ali Aghdam <betteraliaghdam@gmail.com>
2024-02-20 19:26:41 +0330

    feat: drop propTypes

--- packages/controls/src/libs/color-control/types/index.js
+++ packages/controls/src/libs/color-control/types/index.js
@@ -11,8 +11,17 @@ import type {
 export type ColorControlProps = {
 	...ControlGeneralTypes,
 	...ControlValueAddonTypes,
+	/**
+	 * Type of CSS units from presets
+	 */
 	type?: 'normal' | 'minimal',
+	/**
+	 * By using this you can prevent the control to show the border and outline shape.
+	 */
 	noBorder?: boolean,
+	/**
+	 * It is useful for buttons with specified width and allows you to align the content to `left` or `right`. By default, it's `center` and handled by flex justify-content property.
+	 */
 	contentAlign?: 'left' | 'center' | 'right',
 	size?: ControlSize,
 };
