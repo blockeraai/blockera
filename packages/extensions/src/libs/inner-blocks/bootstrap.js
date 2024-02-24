8f264885cb3ce005ae2afaab901c9d212a82469d
Ali Aghdam <betteraliaghdam@gmail.com>
2024-02-24 10:58:40 +0330

    feat: headings color and bg color functionality

--- packages/extensions/src/libs/inner-blocks/bootstrap.js
+++ packages/extensions/src/libs/inner-blocks/bootstrap.js
@@ -22,6 +22,10 @@ import {
 	elementHoverFontColorToWPCompatibility,
 } from './compatibility/element-font-color';
 import { elementsSupportedBlocks } from './compatibility/elements';
+import {
+	elementNormalBackgroundColorFromWPCompatibility,
+	elementNormalBackgroundColorToWPCompatibility,
+} from './compatibility/element-bg-color';
 
 export const bootstrap = (): void => {
 	addFilter(
@@ -91,6 +95,71 @@ export const bootstrap = (): void => {
 							}
 						}
 						break;
+
+					//
+					// Heading elements
+					//
+					case 'h6':
+					case 'h5':
+					case 'h4':
+					case 'h3':
+					case 'h2':
+					case 'h1':
+					case 'heading':
+						if (
+							!attributes?.style?.elements[element] ||
+							!elementsSupportedBlocks[element].includes(blockId)
+						) {
+							return; // Skip this element
+						}
+
+						//
+						// Normal font color
+						//
+						if (
+							!attributes.publisherInnerBlocks[element] ||
+							!attributes.publisherInnerBlocks[element]
+								?.attributes?.publisherFontColor
+						) {
+							const newAttributes =
+								elementNormalFontColorFromWPCompatibility({
+									element,
+									attributes,
+								});
+
+							if (newAttributes) {
+								attributes = mergeObject(
+									attributes,
+									newAttributes
+								);
+							}
+						}
+
+						//
+						// Normal background color
+						//
+						if (
+							!attributes.publisherInnerBlocks[element] ||
+							!attributes.publisherInnerBlocks[element]
+								?.attributes?.publisherBackgroundColor
+						) {
+							const newAttributes =
+								elementNormalBackgroundColorFromWPCompatibility(
+									{
+										element,
+										attributes,
+									}
+								);
+
+							if (newAttributes) {
+								attributes = mergeObject(
+									attributes,
+									newAttributes
+								);
+							}
+						}
+
+						break;
 				}
 			});
 
@@ -158,7 +227,7 @@ export const bootstrap = (): void => {
 								return mergeObject(
 									nextState,
 									elementNormalFontColorToWPCompatibility({
-										element: 'link',
+										element: currentBlock,
 										newValue,
 										ref,
 									})
@@ -178,13 +247,66 @@ export const bootstrap = (): void => {
 								return mergeObject(
 									nextState,
 									elementHoverFontColorToWPCompatibility({
-										element: 'link',
+										element: currentBlock,
 										newValue,
 										ref,
 									})
 								);
 						}
 					}
+					break;
+
+				//
+				// Heading elements
+				//
+				case 'h6':
+				case 'h5':
+				case 'h4':
+				case 'h3':
+				case 'h2':
+				case 'h1':
+				case 'heading':
+					if (
+						!elementsSupportedBlocks[currentBlock].includes(blockId)
+					) {
+						return nextState;
+					}
+
+					//
+					// Normal state
+					//
+					if (currentState === 'normal') {
+						switch (featureId) {
+							//
+							// Font color
+							//
+							case 'publisherFontColor':
+								return mergeObject(
+									nextState,
+									elementNormalFontColorToWPCompatibility({
+										element: currentBlock,
+										newValue,
+										ref,
+									})
+								);
+
+							//
+							// Background color
+							//
+							case 'publisherBackgroundColor':
+								return mergeObject(
+									nextState,
+									elementNormalBackgroundColorToWPCompatibility(
+										{
+											element: currentBlock,
+											newValue,
+											ref,
+										}
+									)
+								);
+						}
+					}
+
 					break;
 			}
 
