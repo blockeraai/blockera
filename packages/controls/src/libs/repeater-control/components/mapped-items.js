8a6827c30bc5c0dd1223ac48ea380a732e582828
rezaelahidev <rezaelahi.dev@gmail.com>
2024-02-28 12:23:58 +0330

    refactor: repeater mapped items component

--- packages/controls/src/libs/repeater-control/components/mapped-items.js
+++ packages/controls/src/libs/repeater-control/components/mapped-items.js
@@ -6,23 +6,25 @@ import type { MixedElement } from 'react';
 import { useContext, useCallback, useMemo } from '@wordpress/element';
 
 /**
+ * Publisher dependencies
+ */
+import { isUndefined } from '@publisher/utils';
+
+/**
  * Internal dependencies
  */
 import RepeaterItem from './repeater-item';
 import { RepeaterContext } from '../context';
-import { isUndefined } from '@publisher/utils';
+import { getSortedRepeater } from '../utils';
 
 const MappedItems = (): MixedElement => {
 	const { repeaterItems: items, repeaterId } = useContext(RepeaterContext);
 
 	// Sorting repeater items based on "order" property value of each item.
-	const sortedRepeaterItems = useMemo(() => {
-		const dataArray = Object.entries(items);
-
-		dataArray.sort(([, a], [, b]) => (a.order || 0) - (b.order || 0));
-
-		return dataArray;
-	}, [items]);
+	const sortedRepeaterItems = useMemo(
+		() => getSortedRepeater(items),
+		[items]
+	);
 
 	const render = useCallback(() => {
 		const getUniqueId = (itemId: string | number) =>
