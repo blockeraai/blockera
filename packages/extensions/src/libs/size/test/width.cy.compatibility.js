74831bab42bb9e631bf473c88fc7343cd6c7be8e
Ali Aghdam <betteraliaghdam@gmail.com>
2024-03-13 18:37:41 +0330

    test: core/site-logo block width feature WP data compatibility

--- packages/extensions/src/libs/size/test/width.cy.compatibility.js
+++ packages/extensions/src/libs/size/test/width.cy.compatibility.js
@@ -8,7 +8,7 @@ import {
 } from '../../../../../../cypress/helpers';
 
 describe('Width → WP Compatibility', () => {
-	describe('Search Block', () => {
+	describe('core/search Block', () => {
 		it('Simple Value', () => {
 			appendBlocks(
 				'<!-- wp:search {"label":"Search","width":500,"widthUnit":"px","buttonText":"Search"} /-->'
@@ -130,6 +130,119 @@ describe('Width → WP Compatibility', () => {
 				expect(100).to.be.equal(getSelectedBlock(data, 'width'));
 
 				expect('%').to.be.equal(getSelectedBlock(data, 'widthUnit'));
+			});
+		});
+	});
+
+	describe('core/site-logo Block', () => {
+		it('Simple Value', () => {
+			appendBlocks(
+				'<!-- wp:site-logo {"width":100,"shouldSyncIcon":false} /-->'
+			);
+
+			// Select target block
+			cy.get('[data-type="core/site-logo"]').click();
+
+			// add alias to the feature container
+			cy.get('[aria-label="Width"]')
+				.closest('[data-cy="base-control"]')
+				.as('widthContainer');
+
+			//
+			// Test 1: WP data to Blockera
+			//
+
+			// WP data should come to Blockera
+			getWPDataObject().then((data) => {
+				expect('100px').to.be.equal(
+					getSelectedBlock(data, 'publisherWidth')
+				);
+			});
+
+			//
+			// Test 2: Blockera value to WP data
+			//
+
+			// change value
+			cy.get('@widthContainer').within(() => {
+				cy.get('input').as('widthInput');
+				cy.get('@widthInput').clear();
+				cy.get('@widthInput').type('200', { force: true });
+			});
+
+			// Blockera value should be moved to WP data
+			getWPDataObject().then((data) => {
+				expect(200).to.be.equal(getSelectedBlock(data, 'width'));
+			});
+
+			//
+			// Test 3: Clear Blockera value and check WP data
+			//
+
+			// clear
+			cy.get('@widthContainer').within(() => {
+				cy.get('input').clear({ force: true });
+			});
+
+			// WP data should be removed too
+			getWPDataObject().then((data) => {
+				expect(undefined).to.be.equal(getSelectedBlock(data, 'width'));
+			});
+		});
+
+		it('Use WP not supported value', () => {
+			appendBlocks(
+				'<!-- wp:site-logo {"width":100,"shouldSyncIcon":false} /-->'
+			);
+
+			// Select target block
+			cy.get('[data-type="core/site-logo"]').click();
+
+			// add alias to the feature container
+			cy.get('[aria-label="Width"]')
+				.closest('[data-cy="base-control"]')
+				.as('widthContainer');
+
+			//
+			// Test 1: WP data to Blockera
+			//
+
+			// WP data should come to Blockera
+			getWPDataObject().then((data) => {
+				expect('100px').to.be.equal(
+					getSelectedBlock(data, 'publisherWidth')
+				);
+			});
+
+			//
+			// Test 2: Blockera value to WP data
+			//
+
+			// change value
+			cy.get('@widthContainer').within(() => {
+				cy.get('input').as('widthInput');
+				cy.get('@widthInput').clear();
+				cy.get('@widthInput').type('200', { force: true });
+				cy.get('[aria-label="Select Unit"]').select('%');
+			});
+
+			// Blockera value should be moved to WP data
+			getWPDataObject().then((data) => {
+				expect(undefined).to.be.equal(getSelectedBlock(data, 'width'));
+			});
+
+			//
+			// Test 3: Clear Blockera value and check WP data
+			//
+
+			// clear
+			cy.get('@widthContainer').within(() => {
+				cy.get('input').clear({ force: true });
+			});
+
+			// WP data should be removed too
+			getWPDataObject().then((data) => {
+				expect(undefined).to.be.equal(getSelectedBlock(data, 'width'));
 			});
 		});
 	});
