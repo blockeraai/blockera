19f0c54e8631bfba1be8f12841ea79c63eab69c9
Ali Aghdam <betteraliaghdam@gmail.com>
2024-03-14 16:35:20 +0330

    test: core/post-featured-image block Height feature WP data compatibility

--- packages/extensions/src/libs/size/test/height.cy.compatibility.js
+++ packages/extensions/src/libs/size/test/height.cy.compatibility.js
@@ -132,4 +132,100 @@ describe('Height → WP Compatibility', () => {
 			});
 		});
 	});
+
+	describe('core/post-featured-image Block', () => {
+		it('Simple Value', () => {
+			appendBlocks(
+				'<!-- wp:post-featured-image {"aspectRatio":"auto","height":"200px"} /-->'
+			);
+
+			// Select target block
+			cy.get('[data-type="core/post-featured-image"]').click();
+
+			// add alias to the feature container
+			cy.get('[aria-label="Height"]')
+				.closest('[data-cy="base-control"]')
+				.as('container');
+
+			//
+			// Test 1: WP data to Blockera
+			//
+
+			// WP data should come to Blockera
+			getWPDataObject().then((data) => {
+				expect('200px').to.be.equal(
+					getSelectedBlock(data, 'publisherHeight')
+				);
+			});
+
+			//
+			// Test 2: Blockera value to WP data
+			//
+
+			// change value
+			cy.get('@container').within(() => {
+				cy.get('input').as('containerInput');
+				cy.get('@containerInput').clear();
+				cy.get('@containerInput').type('300', { force: true });
+			});
+
+			// Blockera value should be moved to WP data
+			getWPDataObject().then((data) => {
+				expect('300px').to.be.equal(getSelectedBlock(data, 'height'));
+			});
+
+			//
+			// Test 3: Clear Blockera value and check WP data
+			//
+
+			// clear
+			cy.get('@container').within(() => {
+				cy.get('input').clear({ force: true });
+			});
+
+			// WP data should be removed too
+			getWPDataObject().then((data) => {
+				expect(undefined).to.be.equal(getSelectedBlock(data, 'height'));
+			});
+		});
+
+		it('Use WP not supported value', () => {
+			appendBlocks(
+				'<!-- wp:post-featured-image {"aspectRatio":"auto","height":"30%"} /-->'
+			);
+
+			// Select target block
+			cy.get('[data-type="core/post-featured-image"]').click();
+
+			// add alias to the feature container
+			cy.get('[aria-label="Height"]')
+				.closest('[data-cy="base-control"]')
+				.as('container');
+
+			//
+			// Test 1: Blockera dat to WP
+			//
+
+			// WP data should come to Blockera
+			getWPDataObject().then((data) => {
+				expect('30%').to.be.equal(
+					getSelectedBlock(data, 'publisherHeight')
+				);
+			});
+
+			//
+			// Test 2: Blockera value to WP data
+			//
+
+			// change value
+			cy.get('@container').within(() => {
+				cy.get('[aria-label="Select Unit"]').select('auto');
+			});
+
+			// Blockera value should be moved to WP data
+			getWPDataObject().then((data) => {
+				expect(undefined).to.be.equal(getSelectedBlock(data, 'height'));
+			});
+		});
+	});
 });
