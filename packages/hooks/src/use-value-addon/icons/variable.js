a4c9f45cfa50e8b13d1f7bb2aa6db4b0a5661632
Ali Aghdam <betteraliaghdam@gmail.com>
2024-03-22 16:53:23 +0330

    feat: test id

--- packages/hooks/src/use-value-addon/icons/variable.js
+++ packages/hooks/src/use-value-addon/icons/variable.js
@@ -6,6 +6,7 @@ export default function VariableIcon({ className = '' }) {
 			viewBox="0 0 16 16"
 			xmlns="http://www.w3.org/2000/svg"
 			className={className}
+			data-cy="value-addon-btn-open"
 		>
 			<path d="M7.13012 12L7.29412 9.1875L4.86988 10.7361L4 9.26389L6.58824 8L4 6.73611L4.86988 5.26389L7.29412 6.8125L7.13012 4H8.86274L8.70588 6.8125L11.1301 5.26389L12 6.73611L9.40463 8L12 9.26389L11.1301 10.7361L8.70588 9.1875L8.86274 12H7.13012Z" />
 		</svg>
