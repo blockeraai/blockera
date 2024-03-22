bbfa32d40534865744daf286b1d88507965a10e5
Ali Aghdam <betteraliaghdam@gmail.com>
2024-03-22 17:04:10 +0330

    test: Font Color → Functionality

---
+++ packages/extensions/src/libs/typography/test/font-color.cypress.js
@@ -0,0 +1,97 @@
+import {
+	savePage,
+	addBlockToPost,
+	getWPDataObject,
+	getSelectedBlock,
+	redirectToFrontPage,
+} from '../../../../../../cypress/helpers';
+
+describe('Font Color → Functionality', () => {
+	beforeEach(() => {
+		addBlockToPost('core/paragraph', true, 'publisher-paragraph');
+
+		cy.getBlock(`core/paragraph`).type('This is test text.');
+
+		cy.getByDataTest('style-tab').click();
+	});
+
+	it('simple value', () => {
+		cy.getParentContainer('Text Color', 'base-control').within(() => {
+			cy.getByDataCy('color-btn').click();
+		});
+
+		cy.getByDataTest('popover-body').within(() => {
+			cy.get('input[maxlength="9"]').clear();
+			cy.get('input[maxlength="9"]').type('70ca9e');
+		});
+
+		//Check block
+		cy.getBlock(`core/paragraph`).should(
+			'have.css',
+			'color',
+			'rgb(112, 202, 158)'
+		);
+
+		//Check store
+		getWPDataObject().then((data) => {
+			expect('#70ca9e').to.be.equal(
+				getSelectedBlock(data, 'publisherFontColor')
+			);
+		});
+
+		//Check frontend
+		savePage();
+
+		redirectToFrontPage();
+
+		cy.get('.publisher-core-block').should(
+			'have.css',
+			'color',
+			'rgb(112, 202, 158)'
+		);
+	});
+
+	it('Variable value', () => {
+		cy.getParentContainer('Text Color', 'base-control').within(() => {
+			cy.openValueAddon();
+		});
+
+		// select variable
+		cy.selectValueAddonItem('contrast');
+
+		cy.getBlock(`core/paragraph`).hasCssVar(
+			'color',
+			'--wp--preset--color--contrast'
+		);
+
+		//Check store
+		getWPDataObject().then((data) => {
+			expect({
+				settings: {
+					name: 'Contrast',
+					id: 'contrast',
+					value: '#111111',
+					reference: {
+						type: 'theme',
+						theme: 'Twenty Twenty-Four',
+					},
+					type: 'color',
+					var: '--wp--preset--color--contrast',
+				},
+				name: 'Contrast',
+				isValueAddon: true,
+				valueType: 'variable',
+			}).to.be.deep.equal(getSelectedBlock(data, 'publisherFontColor'));
+		});
+
+		//Check frontend
+		savePage();
+
+		redirectToFrontPage();
+
+		cy.get('.publisher-core-block').hasCssVar(
+			'color',
+			'--wp--preset--color--contrast'
+		);
+	});
+});
