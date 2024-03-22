a9cb6887a1be521e524da076fb815f5718b7f6b3
Ali Aghdam <betteraliaghdam@gmail.com>
2024-03-22 17:03:55 +0330

    feat: Direction → Functionality

---
+++ packages/extensions/src/libs/typography/test/direction.cypress.js
@@ -0,0 +1,40 @@
+import {
+	savePage,
+	addBlockToPost,
+	getWPDataObject,
+	getSelectedBlock,
+	redirectToFrontPage,
+} from '../../../../../../cypress/helpers';
+
+describe('Direction → Functionality', () => {
+	beforeEach(() => {
+		addBlockToPost('core/paragraph', true, 'publisher-paragraph');
+
+		cy.getBlock(`core/paragraph`).type('This is test text.');
+
+		cy.getByDataTest('style-tab').click();
+	});
+
+	it('Simple value', () => {
+		cy.openMoreFeatures('More typography settings');
+
+		cy.getByAriaLabel('Right to Left').click();
+
+		//Check block
+		cy.getBlock(`core/paragraph`).should('have.css', 'direction', 'rtl');
+
+		//Check store
+		getWPDataObject().then((data) => {
+			expect('rtl').to.be.equal(
+				getSelectedBlock(data, 'publisherDirection')
+			);
+		});
+
+		//Check frontend
+		savePage();
+
+		redirectToFrontPage();
+
+		cy.get('.publisher-core-block').should('have.css', 'direction', 'rtl');
+	});
+});
