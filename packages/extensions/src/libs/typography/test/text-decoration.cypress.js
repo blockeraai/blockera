4f0f2aa15d6c7d372380e4d5a1eeac0aa3306f16
Ali Aghdam <betteraliaghdam@gmail.com>
2024-03-22 17:05:21 +0330

    test: Text Decoration → Functionality

---
+++ packages/extensions/src/libs/typography/test/text-decoration.cypress.js
@@ -0,0 +1,44 @@
+import {
+	savePage,
+	addBlockToPost,
+	getWPDataObject,
+	getSelectedBlock,
+	redirectToFrontPage,
+} from '../../../../../../cypress/helpers';
+
+describe('Text Decoration → Functionality', () => {
+	beforeEach(() => {
+		addBlockToPost('core/paragraph', true, 'publisher-paragraph');
+
+		cy.getBlock(`core/paragraph`).type('This is test text.');
+
+		cy.getByDataTest('style-tab').click();
+	});
+
+	it('simple value', () => {
+		cy.openMoreFeatures('More typography settings');
+
+		cy.getByAriaLabel('Overline').click();
+
+		//Check block
+		cy.getBlock(`core/paragraph`)
+			.should('have.css', 'text-decoration')
+			.should('include', 'overline');
+
+		//Check store
+		getWPDataObject().then((data) => {
+			expect('overline').to.be.equal(
+				getSelectedBlock(data, 'publisherTextDecoration')
+			);
+		});
+
+		//Check frontend
+		savePage();
+
+		redirectToFrontPage();
+
+		cy.get('.publisher-core-block')
+			.should('have.css', 'text-decoration')
+			.should('include', 'overline');
+	});
+});
