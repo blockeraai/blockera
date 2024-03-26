4fd2e0eeac84d059ec09abafe4414a5474be0c19
Ali Aghdam <betteraliaghdam@gmail.com>
2024-03-26 18:32:43 +0330

    test: Aspect Ratio → Functionality

---
+++ packages/extensions/src/libs/size/test/aspect-ratio.cypress.js
@@ -0,0 +1,79 @@
+/**
+ * Internal dependencies
+ */
+import {
+	savePage,
+	addBlockToPost,
+	getWPDataObject,
+	getSelectedBlock,
+	redirectToFrontPage,
+} from '../../../../../../cypress/helpers';
+
+describe('Aspect Ratio → Functionality', () => {
+	beforeEach(() => {
+		addBlockToPost('core/paragraph', true, 'publisher-paragraph');
+
+		cy.getBlock('core/paragraph').type('This is a test text.');
+
+		cy.getByDataTest('style-tab').click();
+	});
+
+	it('should update aspect-ratio correctly, when add value', () => {
+		// activate ratio
+		// cy.openMoreFeatures('More Size Settings');
+		cy.activateMoreSettingsItem('More Size Settings', 'Aspect Ratio');
+
+		// Standard 1:1
+		cy.getParentContainer('Aspect Ratio').within(() => {
+			cy.get('select').select('1', { force: true });
+		});
+
+		// Check block
+		cy.getBlock('core/paragraph').should(
+			'have.css',
+			'aspect-ratio',
+			'1 / 1'
+		);
+
+		// Check store
+		getWPDataObject().then((data) => {
+			expect({
+				value: '1',
+				width: '',
+				height: '',
+			}).to.be.deep.equal(getSelectedBlock(data, 'publisherRatio'));
+		});
+
+		// Custom
+		cy.getParentContainer('Aspect Ratio').within(() => {
+			cy.get('select').select('custom');
+			cy.get('input').eq(0).type(2);
+			cy.get('input').eq(1).type(5);
+		});
+
+		// Check block
+		cy.getBlock('core/paragraph').should(
+			'have.css',
+			'aspect-ratio',
+			'2 / 5'
+		);
+
+		// Check store
+		getWPDataObject().then((data) => {
+			expect({
+				value: 'custom',
+				width: 2,
+				height: 5,
+			}).to.be.deep.equal(getSelectedBlock(data, 'publisherRatio'));
+		});
+
+		// Check frontend
+		savePage();
+		redirectToFrontPage();
+		cy.get('.publisher-core-block').should(
+			'have.css',
+			'aspect-ratio',
+			'2 / 5'
+		);
+	});
+});
