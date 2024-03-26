829ce354bb7184f36c1e5a0242449e606aca0e91
Ali Aghdam <betteraliaghdam@gmail.com>
2024-03-26 11:11:57 +0330

    test: Background Color → Functionality

---
+++ packages/extensions/src/libs/background/test/background-color.cypress.js
@@ -0,0 +1,108 @@
+import {
+	savePage,
+	addBlockToPost,
+	getWPDataObject,
+	getSelectedBlock,
+	redirectToFrontPage,
+} from '../../../../../../cypress/helpers';
+
+describe('Background Color → Functionality', () => {
+	beforeEach(() => {
+		addBlockToPost('core/paragraph', true, 'publisher-paragraph');
+
+		cy.getBlock('core/paragraph').click();
+
+		cy.get('[aria-label="Settings"]').eq(1).click({ force: true });
+
+		cy.getByDataTest('style-tab').click();
+
+		// add alias to the feature container
+		cy.getParentContainer('BG Color', 'base-control').as(
+			'bgColorContainer'
+		);
+	});
+
+	it('simple value background color', () => {
+		// act: clicking on color button
+		cy.get('@bgColorContainer').within(() => {
+			cy.get('button').as('colorBtn');
+			cy.get('@colorBtn').click();
+		});
+
+		// act: entering new hexColor
+		cy.get('.components-popover').within(() => {
+			cy.get('input').as('hexColorInput');
+			cy.get('@hexColorInput').clear();
+			cy.get('@hexColorInput').type('666');
+		});
+
+		//assert data
+		getWPDataObject().then((data) => {
+			expect(
+				getSelectedBlock(data, 'publisherBackgroundColor')
+			).to.be.equal('#666666');
+		});
+
+		// assert editor
+		cy.getBlock('core/paragraph').should(
+			'have.css',
+			'backgroundColor',
+			'rgb(102, 102, 102)'
+		);
+
+		//assert frontend
+		savePage();
+		redirectToFrontPage();
+		cy.get('.publisher-core-block').should(
+			'have.css',
+			'background-color',
+			'rgb(102, 102, 102)'
+		);
+	});
+
+	it('variable background color', () => {
+		// act: clicking on color button
+		cy.get('@bgColorContainer').within(() => {
+			cy.openValueAddon();
+		});
+
+		// select variable
+		cy.selectValueAddonItem('accent-4');
+
+		//assert data
+		getWPDataObject().then((data) => {
+			expect({
+				settings: {
+					name: 'Accent / Four',
+					id: 'accent-4',
+					value: '#b1c5a4',
+					reference: {
+						type: 'theme',
+						theme: 'Twenty Twenty-Four',
+					},
+					type: 'color',
+					var: '--wp--preset--color--accent-4',
+				},
+				name: 'Accent / Four',
+				isValueAddon: true,
+				valueType: 'variable',
+			}).to.be.deep.equal(
+				getSelectedBlock(data, 'publisherBackgroundColor')
+			);
+		});
+
+		// assert editor
+		cy.getBlock('core/paragraph').hasCssVar(
+			'background-color',
+			'--wp--preset--color--accent-4'
+		);
+
+		//assert frontend
+		savePage();
+		redirectToFrontPage();
+		cy.get('.publisher-core-block').hasCssVar(
+			'background-color',
+			'--wp--preset--color--accent-4'
+		);
+	});
+});
