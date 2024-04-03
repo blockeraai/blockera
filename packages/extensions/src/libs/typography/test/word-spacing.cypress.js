4eff28fd9df410cfafea0290654a5008ac41d928
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-03 15:42:01 +0330

    test: Word Spacing → Functionality

---
+++ packages/extensions/src/libs/typography/test/word-spacing.cypress.js
@@ -0,0 +1,48 @@
+import {
+	savePage,
+	addBlockToPost,
+	getWPDataObject,
+	getSelectedBlock,
+	redirectToFrontPage,
+} from '../../../../../../cypress/helpers';
+
+describe('Word Spacing → Functionality', () => {
+	beforeEach(() => {
+		addBlockToPost('core/paragraph', true, 'publisher-paragraph');
+
+		cy.getBlock('core/paragraph').type('This is test text.', {
+			delay: 0,
+		});
+
+		cy.getByDataTest('style-tab').click();
+	});
+
+	it('Simple value', () => {
+		cy.openMoreFeatures('More typography settings');
+
+		cy.getParentContainer('Words').within(() => {
+			cy.get('input').type(5, { force: true });
+		});
+
+		//Check letter spacing
+		cy.getBlock('core/paragraph').should('have.css', 'word-spacing', '5px');
+
+		//Check store
+		getWPDataObject().then((data) => {
+			expect('5px').to.be.equal(
+				getSelectedBlock(data, 'publisherWordSpacing')
+			);
+		});
+
+		//Check frontend
+		savePage();
+
+		redirectToFrontPage();
+
+		cy.get('.publisher-core-block').should(
+			'have.css',
+			'word-spacing',
+			'5px'
+		);
+	});
+});
