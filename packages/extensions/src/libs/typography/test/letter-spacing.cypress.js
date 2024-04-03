9d408d2b57134c7d6c41f9b79cd4241420211372
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-03 15:41:27 +0330

    test: Letter Spacing → Functionality

---
+++ packages/extensions/src/libs/typography/test/letter-spacing.cypress.js
@@ -0,0 +1,52 @@
+import {
+	savePage,
+	addBlockToPost,
+	getWPDataObject,
+	getSelectedBlock,
+	redirectToFrontPage,
+} from '../../../../../../cypress/helpers';
+
+describe('Letter Spacing → Functionality', () => {
+	beforeEach(() => {
+		addBlockToPost('core/paragraph', true, 'publisher-paragraph');
+
+		cy.getBlock('core/paragraph').type('This is test text.', {
+			delay: 0,
+		});
+
+		cy.getByDataTest('style-tab').click();
+	});
+
+	it('Simple value', () => {
+		cy.openMoreFeatures('More typography settings');
+
+		cy.getParentContainer('Letters').within(() => {
+			cy.get('input').type(5, { force: true });
+		});
+
+		//Check letter spacing
+		cy.getBlock('core/paragraph').should(
+			'have.css',
+			'letter-spacing',
+			'5px'
+		);
+
+		//Check store
+		getWPDataObject().then((data) => {
+			expect('5px').to.be.equal(
+				getSelectedBlock(data, 'publisherLetterSpacing')
+			);
+		});
+
+		//Check frontend
+		savePage();
+
+		redirectToFrontPage();
+
+		cy.get('.publisher-core-block').should(
+			'have.css',
+			'letter-spacing',
+			'5px'
+		);
+	});
+});
