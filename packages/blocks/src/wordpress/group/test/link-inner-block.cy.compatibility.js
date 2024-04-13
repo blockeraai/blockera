8ffeab91357a029b08c6d7286797d1f26c0bfa64
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-13 13:12:57 +0330

    test: Link Inner Block → WP Data Compatibility (variable value)

--- packages/blocks/src/wordpress/group/test/link-inner-block.cy.compatibility.js
+++ packages/blocks/src/wordpress/group/test/link-inner-block.cy.compatibility.js
@@ -11,7 +11,7 @@ import {
 } from '../../../../../../cypress/helpers';
 
 describe('Group Block → Link Inner Block → WP Data Compatibility', () => {
-	it('Simple value inner block value (normal + hover)', () => {
+	it('Simple color for inner block (normal + hover)', () => {
 		appendBlocks(
 			'<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"#ffbaba"},":hover":{"color":{"text":"#ff1d1d"}}}}},"layout":{"type":"constrained"}} -->\n' +
 				'<div class="wp-block-group has-link-color"><!-- wp:paragraph -->\n' +
@@ -169,6 +169,252 @@ describe('Group Block → Link Inner Block → WP Data Compatibility', () => {
 
 		cy.get('.components-popover').within(() => {
 			cy.get('button[aria-label="Reset Color (Clear)"]').click();
+		});
+
+		getWPDataObject().then((data) => {
+			expect({
+				publisherFontColor: '', // !! should be removed
+				publisherBlockStates: {
+					normal: {
+						isVisible: true,
+						breakpoints: {
+							laptop: {
+								attributes: {},
+							},
+						},
+					},
+					hover: {
+						isVisible: true,
+						breakpoints: {
+							laptop: {
+								attributes: {
+									publisherFontColor: '', // !! should be removed
+								},
+							},
+						},
+					},
+				},
+			}).to.be.deep.equal(
+				getSelectedBlock(data, 'publisherInnerBlocks').link.attributes
+			);
+		});
+
+		getWPDataObject().then((data) => {
+			expect(undefined).to.be.equal(
+				getSelectedBlock(data, 'style')?.elements?.link?.color?.text
+			);
+		});
+
+		getWPDataObject().then((data) => {
+			expect(undefined).to.be.equal(
+				getSelectedBlock(data, 'style')?.elements?.link[':hover']?.color
+					?.text
+			);
+		});
+	});
+
+	it('Variable color value for inner block (normal + hover)', () => {
+		appendBlocks(
+			'<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|accent-3"},":hover":{"color":{"text":"var:preset|color|accent-4"}}}}},"layout":{"type":"constrained"}} -->\n' +
+				'<div class="wp-block-group has-link-color"><!-- wp:paragraph -->\n' +
+				'<p>Paragraph 1</p>\n' +
+				'<!-- /wp:paragraph -->\n' +
+				'\n' +
+				'<!-- wp:paragraph -->\n' +
+				'<p>Paragraph 2 with <a href="#a">link</a></p>\n' +
+				'<!-- /wp:paragraph --></div>\n' +
+				'<!-- /wp:group -->'
+		);
+
+		// Select target block
+		cy.getBlock('core/paragraph').first().click();
+
+		// Switch to parent block
+		cy.getByAriaLabel('Select Group').click();
+
+		//
+		// Test 1: WP data to Blockera
+		//
+
+		// WP data should come to Blockera
+		getWPDataObject().then((data) => {
+			expect({
+				publisherFontColor: {
+					settings: {
+						name: 'Accent / Three',
+						id: 'accent-3',
+						value: '#d8613c',
+						reference: {
+							type: 'theme',
+							theme: 'Twenty Twenty-Four',
+						},
+						type: 'color',
+						var: '--wp--preset--color--accent-3',
+					},
+					name: 'Accent / Three',
+					isValueAddon: true,
+					valueType: 'variable',
+				},
+				publisherBlockStates: {
+					normal: {
+						breakpoints: {
+							laptop: {},
+						},
+						isVisible: true,
+					},
+					hover: {
+						isVisible: true,
+						breakpoints: {
+							laptop: {
+								attributes: {
+									publisherFontColor: {
+										settings: {
+											name: 'Accent / Four',
+											id: 'accent-4',
+											value: '#b1c5a4',
+											reference: {
+												type: 'theme',
+												theme: 'Twenty Twenty-Four',
+											},
+											type: 'color',
+											var: '--wp--preset--color--accent-4',
+										},
+										name: 'Accent / Four',
+										isValueAddon: true,
+										valueType: 'variable',
+									},
+								},
+							},
+						},
+					},
+				},
+			}).to.be.deep.equal(
+				getSelectedBlock(data, 'publisherInnerBlocks').link.attributes
+			);
+		});
+
+		//
+		// Test 2: Blockera value to WP data
+		//
+
+		setBlockType('Link');
+
+		//
+		// Normal → Text Color
+		//
+
+		setBlockState('Normal');
+
+		cy.getParentContainer('Text Color').within(() => {
+			cy.clickValueAddonButton();
+		});
+
+		cy.selectValueAddonItem('contrast-2');
+
+		//
+		// Hover → Text Color
+		//
+		setBlockState('Hover');
+
+		cy.getParentContainer('Text Color').within(() => {
+			cy.clickValueAddonButton();
+		});
+
+		cy.selectValueAddonItem('accent');
+
+		getWPDataObject().then((data) => {
+			expect({
+				publisherFontColor: {
+					settings: {
+						name: 'Contrast / Two',
+						id: 'contrast-2',
+						value: '#636363',
+						reference: {
+							type: 'theme',
+							theme: 'Twenty Twenty-Four',
+						},
+						type: 'color',
+						var: '--wp--preset--color--contrast-2',
+					},
+					name: 'Contrast / Two',
+					isValueAddon: true,
+					valueType: 'variable',
+				},
+				publisherBlockStates: {
+					normal: {
+						breakpoints: {
+							laptop: {
+								attributes: {},
+							},
+						},
+						isVisible: true,
+					},
+					hover: {
+						isVisible: true,
+						breakpoints: {
+							laptop: {
+								attributes: {
+									publisherFontColor: {
+										settings: {
+											name: 'Accent',
+											id: 'accent',
+											value: '#cfcabe',
+											reference: {
+												type: 'theme',
+												theme: 'Twenty Twenty-Four',
+											},
+											type: 'color',
+											var: '--wp--preset--color--accent',
+										},
+										name: 'Accent',
+										isValueAddon: true,
+										valueType: 'variable',
+									},
+								},
+							},
+						},
+					},
+				},
+			}).to.be.deep.equal(
+				getSelectedBlock(data, 'publisherInnerBlocks').link.attributes
+			);
+		});
+
+		getWPDataObject().then((data) => {
+			expect('var:preset|color|contrast-2').to.be.equal(
+				getSelectedBlock(data, 'style')?.elements?.link?.color?.text
+			);
+		});
+
+		getWPDataObject().then((data) => {
+			expect('var:preset|color|accent').to.be.equal(
+				getSelectedBlock(data, 'style')?.elements?.link[':hover']?.color
+					?.text
+			);
+		});
+
+		//
+		// Test 3: Clear Blockera value and check WP data
+		//
+
+		//
+		// Normal → Text Color
+		//
+
+		setBlockState('Normal');
+
+		cy.getParentContainer('Text Color').within(() => {
+			cy.removeValueAddon();
+		});
+
+		//
+		// Hover → Text Color
+		//
+
+		setBlockState('Hover');
+
+		cy.getParentContainer('Text Color').within(() => {
+			cy.removeValueAddon();
 		});
 
 		getWPDataObject().then((data) => {
