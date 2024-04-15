7110873e9daa59a13cbed0fed7911d6678e92769
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-15 13:15:41 +0330

    test: Post Excerpt block → Inner blocks → existence test

---
+++ packages/blocks/src/wordpress/post-excerpt/test/inner-blocks.cypress.js
@@ -0,0 +1,29 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	openInnerBlocksExtension,
+	openMoreFeaturesControl,
+} from '../../../../../../cypress/helpers';
+
+describe('Post Excerpt Block → Inner Blocks', () => {
+	it('Should add all inner blocks to block settings', () => {
+		appendBlocks('<!-- wp:post-excerpt {"moreText":"Read More"} /--> ');
+
+		// Select target block
+		cy.getBlock('core/post-excerpt').click();
+
+		// open inner block settings
+		openInnerBlocksExtension();
+
+		cy.get('.publisher-extension.publisher-extension-inner-blocks').within(
+			() => {
+				cy.getByAriaLabel('Link Customize').should('exist');
+
+				// no other item
+				cy.getByAriaLabel('Headings Customize').should('not.exist');
+			}
+		);
+	});
+});
