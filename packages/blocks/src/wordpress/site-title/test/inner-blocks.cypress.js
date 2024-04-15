201943378632e2b66074acdb2ce33f472c8d7a86
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-15 12:55:26 +0330

    test: Site Title block → Inner blocks → existence test

---
+++ packages/blocks/src/wordpress/site-title/test/inner-blocks.cypress.js
@@ -0,0 +1,29 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	openInnerBlocksExtension,
+	openMoreFeaturesControl,
+} from '../../../../../../cypress/helpers';
+
+describe('Site Title Block → Inner Blocks', () => {
+	it('Should add all inner blocks to block settings', () => {
+		appendBlocks('<!-- wp:site-title /--> ');
+
+		// Select target block
+		cy.getBlock('core/site-title').click();
+
+		// open inner block settings
+		openInnerBlocksExtension();
+
+		cy.get('.publisher-extension.publisher-extension-inner-blocks').within(
+			() => {
+				cy.getByAriaLabel('Link Customize').should('exist');
+
+				// no other item
+				cy.getByAriaLabel('Headings Customize').should('not.exist');
+			}
+		);
+	});
+});
