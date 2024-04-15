7d3854a470ae8d1dc5eed1ee8ce751209e893dd7
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-15 17:57:46 +0330

    test: Post Terms block → Inner blocks → existence test

---
+++ packages/blocks/src/wordpress/post-terms/test/inner-blocks.cypress.js
@@ -0,0 +1,32 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	openInnerBlocksExtension,
+	openMoreFeaturesControl,
+} from '../../../../../../cypress/helpers';
+
+describe('Post Terms → Inner Blocks', () => {
+	it('Should add all inner blocks to block settings', () => {
+		appendBlocks('<!-- wp:post-terms {"term":"category"} /--> ');
+
+		// Select target block
+		cy.getBlock('core/post-terms').click();
+
+		// open inner block settings
+		openInnerBlocksExtension();
+
+		cy.get('.publisher-extension.publisher-extension-inner-blocks').within(
+			() => {
+				cy.getByAriaLabel('Term Items Customize').should('exist');
+				cy.getByAriaLabel('Separator Customize').should('exist');
+				cy.getByAriaLabel('Prefix Text Customize').should('exist');
+				cy.getByAriaLabel('Suffix Text Customize').should('exist');
+
+				// no other item
+				cy.getByAriaLabel('Headings Customize').should('not.exist');
+			}
+		);
+	});
+});
