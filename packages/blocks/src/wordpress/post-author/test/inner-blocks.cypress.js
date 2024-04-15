27d8c20fcc91933f703f0e8539488276874694b6
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-15 17:25:18 +0330

    test: Post Author block → Inner blocks → existence test

---
+++ packages/blocks/src/wordpress/post-author/test/inner-blocks.cypress.js
@@ -0,0 +1,32 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	openInnerBlocksExtension,
+	openMoreFeaturesControl,
+} from '../../../../../../cypress/helpers';
+
+describe('Post Author Block → Inner Blocks', () => {
+	it('Should add all inner blocks to block settings', () => {
+		appendBlocks('<!-- wp:post-author {"byline":"Author"} /--> ');
+
+		// Select target block
+		cy.getBlock('core/post-author').click();
+
+		// open inner block settings
+		openInnerBlocksExtension();
+
+		cy.get('.publisher-extension.publisher-extension-inner-blocks').within(
+			() => {
+				cy.getByAriaLabel('Avatar Customize').should('exist');
+				cy.getByAriaLabel('Byline Customize').should('exist');
+				cy.getByAriaLabel('Name Customize').should('exist');
+				cy.getByAriaLabel('Link Customize').should('exist');
+
+				// no other item
+				cy.getByAriaLabel('Headings Customize').should('not.exist');
+			}
+		);
+	});
+});
