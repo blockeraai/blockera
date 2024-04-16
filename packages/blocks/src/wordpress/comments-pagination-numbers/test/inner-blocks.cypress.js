608d87c109eae419c65bfe2efcd0d47ff66e3c48
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-16 18:24:16 +0330

    test: Comments Pagination Numbers block → Inner blocks → existence test

---
+++ packages/blocks/src/wordpress/comments-pagination-numbers/test/inner-blocks.cypress.js
@@ -0,0 +1,40 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	createPost,
+	openInnerBlocksExtension,
+} from '../../../../../../cypress/helpers';
+
+/**
+ * Internal dependencies
+ */
+import { testContent } from './test-content';
+
+describe('Comments Pagination Numbers Block → Inner Blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Should add all inner blocks to block settings', () => {
+		appendBlocks(testContent);
+
+		// Select target block
+		cy.getBlock('core/comments-pagination-numbers').click();
+
+		// open inner block settings
+		openInnerBlocksExtension();
+
+		cy.get('.publisher-extension.publisher-extension-inner-blocks').within(
+			() => {
+				cy.getByAriaLabel('Numbers Customize').should('exist');
+				cy.getByAriaLabel('Current Page Customize').should('exist');
+				cy.getByAriaLabel('Dots Customize').should('exist');
+
+				// no other item
+				cy.getByAriaLabel('Headings Customize').should('not.exist');
+			}
+		);
+	});
+});
