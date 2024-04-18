7678c33546b150e589ca37716c39a7d5d050630b
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-19 02:48:00 +0330

    feat: setParentBlock cypress helper fnc for switching to parent block from inner block

--- cypress/helpers/inner-blocks.js
+++ cypress/helpers/inner-blocks.js
@@ -4,6 +4,12 @@ export function openInnerBlocksExtension() {
 	openSettingsPanel('Inner Blocks');
 }
 
+export function setParentBlock() {
+	cy.get(
+		'.publisher-extension.publisher-extension-block-card .publisher-extension-block-card__title .publisher-extension-block-card__title__block'
+	).click();
+}
+
 export function setInnerBlock(blockType, suffix = ' Customize') {
 	openInnerBlocksExtension();
 
