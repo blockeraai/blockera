985e2ee869143d1f21eaba602afdee56827b3ca7
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-19 03:27:19 +0330

    test: Paragraph block → Inner blocks → existence test

---
+++ packages/blocks/src/wordpress/paragraph/test/inner-blocks.cypress.js
@@ -0,0 +1,37 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	createPost,
+	openInnerBlocksExtension,
+} from '../../../../../../cypress/helpers';
+
+describe('Paragraph Block → Inner Blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Should add all inner blocks to block settings', () => {
+		appendBlocks(`
+		<!-- wp:paragraph -->
+<p>This is a test paragraph...</p>
+<!-- /wp:paragraph -->
+		`);
+
+		// Select target block
+		cy.getBlock('core/paragraph').click();
+
+		// open inner block settings
+		openInnerBlocksExtension();
+
+		cy.get('.publisher-extension.publisher-extension-inner-blocks').within(
+			() => {
+				cy.getByAriaLabel('Link Customize').should('exist');
+
+				// no other item
+				cy.getByAriaLabel('Headings Customize').should('not.exist');
+			}
+		);
+	});
+});
