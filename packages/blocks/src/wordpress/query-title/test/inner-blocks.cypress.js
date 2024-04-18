262efa8b6d97380664492918eca538dbc232fc7f
Ali Aghdam <betteraliaghdam@gmail.com>
2024-04-18 18:39:30 +0330

    test: Query Title block → Inner blocks → existence test

---
+++ packages/blocks/src/wordpress/query-title/test/inner-blocks.cypress.js
@@ -0,0 +1,33 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	createPost,
+	openInnerBlocksExtension,
+} from '../../../../../../cypress/helpers';
+
+describe('Query Title Block → Inner Blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Should add all inner blocks to block settings', () => {
+		appendBlocks('<!-- wp:query-title {"type":"search"} /--> ');
+
+		// Select target block
+		cy.getBlock('core/query-title').click();
+
+		// open inner block settings
+		openInnerBlocksExtension();
+
+		cy.get('.publisher-extension.publisher-extension-inner-blocks').within(
+			() => {
+				cy.getByAriaLabel('Link Customize').should('exist');
+
+				// no other item
+				cy.getByAriaLabel('Headings Customize').should('not.exist');
+			}
+		);
+	});
+});
