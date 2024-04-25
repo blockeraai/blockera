fa0f11220c456366c658bcd1cc048aacf1c9b132
rezaelahidev <rezaelahi.dev@gmail.com>
2024-04-25 18:26:29 +0330

    refactor: fix shared architecture

---
+++ packages/extensions/src/libs/shared/supports.js
@@ -0,0 +1,29 @@
+// @flow
+
+import { supports as sizeSupports } from '../size';
+import { supports as iconSupports } from '../icon';
+import { supports as mouseSupports } from '../mouse';
+import { supports as layoutSupports } from '../layout';
+import { supports as effectsSupports } from '../effects';
+import { supports as spacingSupports } from '../spacing';
+import { supports as positionSupports } from '../position';
+import { supports as flexChildSupports } from '../flex-child';
+import { supports as typographySupports } from '../typography';
+import { supports as backgroundSupports } from '../background';
+import { supports as customStyleSupports } from '../custom-style';
+import { supports as borderAndShadowSupports } from '../border-and-shadow';
+
+export const supports = {
+	...sizeSupports,
+	...iconSupports,
+	...mouseSupports,
+	...layoutSupports,
+	...effectsSupports,
+	...spacingSupports,
+	...positionSupports,
+	...flexChildSupports,
+	...typographySupports,
+	...backgroundSupports,
+	...customStyleSupports,
+	...borderAndShadowSupports,
+};
