a44a68f53eac575de9127163587f33927105d14a
rezaelahidev <rezaelahi.dev@gmail.com>
2024-04-25 20:28:35 +0330

    fix: block-states default values

--- packages/extensions/src/libs/block-states/attributes.js
+++ packages/extensions/src/libs/block-states/attributes.js
@@ -1,30 +1,15 @@
-/**
- * Blockera dependencies
- */
-import { defaultItemValue } from '@blockera/controls/src/libs/repeater-control';
-
-/**
- * Internal dependencies
- */
-import defaultStates from './states';
-import getBreakpoints from './default-breakpoints';
-import { blockStatesValueCleanup } from './helpers';
-
 export const attributes = {
 	blockeraBlockStates: {
 		type: 'object',
-		default: blockStatesValueCleanup({
+		default: {
 			normal: {
-				...defaultStates.normal,
-				...defaultItemValue,
-				isOpen: false,
-				display: false,
-				deletable: false,
-				selectable: true,
-				isSelected: true,
-				visibilitySupport: false,
-				breakpoints: getBreakpoints('normal'),
+				isVisible: true,
+				breakpoints: {
+					laptop: {
+						attributes: {},
+					},
+				},
 			},
-		}),
+		},
 	},
 };
