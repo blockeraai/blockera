8d05b247d8f0010e782634c90db749fbd03f26a0
Ali Aghdam <betteraliaghdam@gmail.com>
2024-05-06 12:50:02 +0330

    feat: spacing changed to be unlocked by default

--- packages/editor-extensions/js/libs/spacing/attributes.js
+++ packages/editor-extensions/js/libs/spacing/attributes.js
@@ -2,12 +2,14 @@ export const attributes = {
 	blockeraSpacing: {
 		type: 'object',
 		default: {
+			marginLock: 'none',
 			margin: {
 				top: '',
 				right: '',
 				bottom: '',
 				left: '',
 			},
+			paddingLock: 'none',
 			padding: {
 				top: '',
 				right: '',
