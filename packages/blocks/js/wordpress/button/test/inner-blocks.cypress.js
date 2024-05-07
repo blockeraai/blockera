540e9d214dcb7e04dfdc15262ae1d5eef0a9a1c2
Ali Aghdam <betteraliaghdam@gmail.com>
2024-05-07 18:06:38 +0330

    test: core/button block → inner blocks test

---
+++ packages/blocks/js/wordpress/button/test/inner-blocks.cypress.js
@@ -0,0 +1,33 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	createPost,
+	openInnerBlocksExtension,
+	openMoreFeaturesControl,
+} from '../../../../../../cypress/helpers';
+
+describe('Button Block → Inner Blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Should not have inner blocks', () => {
+		appendBlocks(`<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">button 1</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">button 2</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->`);
+
+		cy.getBlock('core/button').first().click();
+
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'not.exist'
+		);
+	});
+});
