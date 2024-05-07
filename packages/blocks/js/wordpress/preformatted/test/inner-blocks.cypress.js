b34fd069418f09ea76f775335daa5e33ad416974
Ali Aghdam <betteraliaghdam@gmail.com>
2024-05-07 16:05:15 +0330

    test: core/preformatted block → inner blocks test

---
+++ packages/blocks/js/wordpress/preformatted/test/inner-blocks.cypress.js
@@ -0,0 +1,27 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	createPost,
+	openInnerBlocksExtension,
+	openMoreFeaturesControl,
+} from '../../../../../../cypress/helpers';
+
+describe('Preformatted Block → Inner Blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Should not have inner blocks', () => {
+		appendBlocks(`<!-- wp:preformatted -->
+<pre class="wp-block-preformatted">test content...</pre>
+<!-- /wp:preformatted -->`);
+
+		cy.getBlock('core/preformatted').click();
+
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'not.exist'
+		);
+	});
+});
