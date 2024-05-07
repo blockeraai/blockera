4e80b6cf218f19356f9a2d2998b51ba1d49e004a
Ali Aghdam <betteraliaghdam@gmail.com>
2024-05-07 12:01:37 +0330

    test: core/gallery block → inner blocks existence

---
+++ packages/blocks/js/wordpress/gallery/test/inner-blocks.cypress.js
@@ -0,0 +1,52 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	createPost,
+	openInnerBlocksExtension,
+	openMoreFeaturesControl,
+} from '../../../../../../cypress/helpers';
+
+describe('Gallery Block → Inner Blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Should add all inner blocks to block settings', () => {
+		appendBlocks(
+			`<!-- wp:gallery {"linkTo":"none"} -->
+<figure class="wp-block-gallery has-nested-images columns-default is-cropped"><!-- wp:image {"id":7144,"sizeSlug":"large","linkDestination":"none","className":"blockera-core-block blockera-core-block-0e1eb5b2-0332-49c4-8ab7-d6da3510191d","blockeraFlexWrap":{"reverse":false},"blockeraPropsId":"47111938578","blockeraCompatId":"47111938578"} -->
+<figure class="wp-block-image size-large blockera-core-block blockera-core-block-0e1eb5b2-0332-49c4-8ab7-d6da3510191d"><img src="https://placehold.co/600x400" alt="" class="wp-image-7144"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":7139,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="https://placehold.co/600x400" alt="" class="wp-image-7139"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":7123,"sizeSlug":"large","linkDestination":"none","className":"blockera-core-block blockera-core-block-4df1fe55-0eae-4065-b060-4675d2c0304a","blockeraFlexWrap":{"reverse":false},"blockeraPropsId":"47112011175","blockeraCompatId":"47112011175"} -->
+<figure class="wp-block-image size-large blockera-core-block blockera-core-block-4df1fe55-0eae-4065-b060-4675d2c0304a"><img src="https://placehold.co/600x400" alt="" class="wp-image-7123"/><figcaption class="wp-element-caption">Image test caption</figcaption></figure>
+<!-- /wp:image --><figcaption class="blocks-gallery-caption wp-element-caption">Gallery test caption...</figcaption></figure>
+<!-- /wp:gallery -->
+			`
+		);
+
+		// Select target block
+		cy.getBlock('core/image').click();
+
+		cy.getByAriaLabel('Select Gallery').click();
+
+		// open inner block settings
+		openInnerBlocksExtension();
+
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').within(
+			() => {
+				cy.getByAriaLabel('Gallery Caption Customize').should('exist');
+				cy.getByAriaLabel('Images Customize').should('exist');
+				cy.getByAriaLabel('Images Captions Customize').should('exist');
+
+				cy.getByAriaLabel('H1s Customize').should('not.exist');
+			}
+		);
+	});
+});
