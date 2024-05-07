c01410cd0569f06a9d0c1d2c3ff5e4048e1fa931
Ali Aghdam <betteraliaghdam@gmail.com>
2024-05-07 19:02:06 +0330

    test: core/video block → inner blocks test

---
+++ packages/blocks/js/wordpress/video/test/inner-blocks.cypress.js
@@ -0,0 +1,27 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	createPost,
+	openInnerBlocksExtension,
+	openMoreFeaturesControl,
+} from '../../../../../../cypress/helpers';
+
+describe('Video Block → Inner Blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Should not have inner blocks', () => {
+		appendBlocks(`<!-- wp:video -->
+<figure class="wp-block-video"></figure>
+<!-- /wp:video -->`);
+
+		cy.getBlock('core/video').click();
+
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'not.exist'
+		);
+	});
+});
