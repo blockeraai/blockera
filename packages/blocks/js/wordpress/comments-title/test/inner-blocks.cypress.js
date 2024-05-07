8dea0164d11aded564c79ed1cc7128b0f31c07e5
Ali Aghdam <betteraliaghdam@gmail.com>
2024-05-07 10:00:41 +0330

    test: core/comments-title block → test to check not have inner block

---
+++ packages/blocks/js/wordpress/comments-title/test/inner-blocks.cypress.js
@@ -0,0 +1,31 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	createPost,
+	openInnerBlocksExtension,
+	openMoreFeaturesControl,
+} from '../../../../../../cypress/helpers';
+
+/**
+ * Internal dependencies
+ */
+import { testContent } from './test-content';
+
+describe('Comments Title Block → Inner Blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Should not have inner blocks', () => {
+		appendBlocks(testContent);
+
+		// Select target block
+		cy.getBlock('core/comments-title').first().click();
+
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'not.exist'
+		);
+	});
+});
