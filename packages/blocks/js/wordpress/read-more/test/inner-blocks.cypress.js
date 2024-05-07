4340d75d0c87e7f1b95b1964445aa1e9023691f0
Ali Aghdam <betteraliaghdam@gmail.com>
2024-05-07 19:07:10 +0330

    test: core/read-more block → inner blocks test

---
+++ packages/blocks/js/wordpress/read-more/test/inner-blocks.cypress.js
@@ -0,0 +1,25 @@
+/**
+ * Cypress dependencies
+ */
+import {
+	appendBlocks,
+	createPost,
+	openInnerBlocksExtension,
+	openMoreFeaturesControl,
+} from '../../../../../../cypress/helpers';
+
+describe('Read More Block → Inner Blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Should not have inner blocks', () => {
+		appendBlocks(`<!-- wp:read-more {"content":"Read more..."} /-->`);
+
+		cy.getBlock('core/read-more').click();
+
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'not.exist'
+		);
+	});
+});
