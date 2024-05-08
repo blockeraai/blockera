cbd1d418de5da18a5d79bdf8596051a490ad1107
Ali Aghdam <betteraliaghdam@gmail.com>
2024-05-08 18:54:07 +0330

    test: box shadow feature to test promoter and single shadow

--- packages/editor-extensions/js/libs/border-and-shadow/test/box-shadow.cypress.js
+++ packages/editor-extensions/js/libs/border-and-shadow/test/box-shadow.cypress.js
@@ -79,6 +79,16 @@ describe('Box Shadow → Functionality', () => {
 			}).to.be.deep.equal(getSelectedBlock(data, 'blockeraBoxShadow'));
 		});
 
+		/* try to add another box shadow */
+		cy.get('@container').within(() => {
+			cy.get('[aria-label="Add New Box Shadow"]').click({
+				force: true,
+			});
+		});
+
+		// promotion popover should appear
+		cy.get('.blockera-component-promotion-popover').should('exist');
+
 		//Check frontend
 		savePage();
 
@@ -88,131 +98,6 @@ describe('Box Shadow → Functionality', () => {
 			'have.css',
 			'box-shadow',
 			'rgba(197, 238, 240, 0.67) 10px 50px 30px 40px'
-		);
-	});
-
-	it('should update correctly, when adding multiple shadow', () => {
-		cy.get('@container').within(() => {
-			cy.multiClick('[aria-label="Add New Box Shadow"]', 2);
-			cy.getByDataCy('group-control-header').eq(0).click();
-		});
-
-		cy.getByDataTest('popover-body').within(() => {
-			cy.getByDataTest('box-shadow-x-input').clear({ force: true });
-			cy.getByDataTest('box-shadow-x-input').type(10, { force: true });
-
-			cy.getByDataTest('box-shadow-y-input').clear({ force: true });
-			cy.getByDataTest('box-shadow-y-input').type(50, { force: true });
-
-			cy.getByDataTest('box-shadow-blur-input').clear({ force: true });
-			cy.getByDataTest('box-shadow-blur-input').type(30, { force: true });
-
-			cy.getByDataTest('box-shadow-spread-input').clear({ force: true });
-			cy.getByDataTest('box-shadow-spread-input').type(40, {
-				force: true,
-			});
-
-			cy.getByDataTest('box-shadow-color-control').click({ force: true });
-		});
-
-		cy.getByDataTest('popover-body')
-			.last()
-			.within(() => {
-				cy.get('input[maxlength="9"]').clear({ force: true });
-				cy.get('input[maxlength="9"]').type('c5eef0ab', {
-					force: true,
-					delay: 0,
-				});
-			});
-
-		cy.get('@container').within(() => {
-			cy.getByDataCy('group-control-header').eq(1).click();
-		});
-
-		cy.getByDataTest('popover-body')
-			.last()
-			.within(() => {
-				cy.getByAriaLabel('Inner').click({ force: true });
-
-				cy.getByDataTest('box-shadow-x-input').clear({ force: true });
-				cy.getByDataTest('box-shadow-x-input').type(20, {
-					force: true,
-				});
-
-				cy.getByDataTest('box-shadow-y-input').clear({ force: true });
-				cy.getByDataTest('box-shadow-y-input').type(0, { force: true });
-
-				cy.getByDataTest('box-shadow-blur-input').clear({
-					force: true,
-				});
-				cy.getByDataTest('box-shadow-blur-input').type(50, {
-					force: true,
-				});
-
-				cy.getByDataTest('box-shadow-spread-input').clear({
-					force: true,
-				});
-				cy.getByDataTest('box-shadow-spread-input').type(5, {
-					force: true,
-				});
-
-				cy.getByDataTest('box-shadow-color-control').click({
-					force: true,
-				});
-			});
-
-		cy.getByDataTest('popover-body')
-			.last()
-			.within(() => {
-				cy.get('input[maxlength="9"]').clear({ force: true });
-				cy.get('input[maxlength="9"]').type('6a6969e6', {
-					force: true,
-					delay: 0,
-				});
-			});
-
-		//Check block
-		cy.getBlock('core/paragraph').should(
-			'have.css',
-			'box-shadow',
-			'rgba(197, 238, 240, 0.67) 10px 50px 30px 40px, rgba(106, 105, 105, 0.9) 20px 0px 50px 5px inset'
-		);
-
-		//Check store
-		getWPDataObject().then((data) => {
-			expect({
-				'outer-0': {
-					isVisible: true,
-					type: 'outer',
-					x: '10px',
-					y: '50px',
-					blur: '30px',
-					spread: '40px',
-					color: '#c5eef0ab',
-					order: 0,
-				},
-				'inner-0': {
-					isVisible: true,
-					type: 'inner',
-					x: '20px',
-					y: '0px',
-					blur: '50px',
-					spread: '5px',
-					color: '#6a6969e6',
-					order: 1,
-				},
-			}).to.be.deep.equal(getSelectedBlock(data, 'blockeraBoxShadow'));
-		});
-
-		//Check frontend
-		savePage();
-
-		redirectToFrontPage();
-
-		cy.get('.blockera-core-block').should(
-			'have.css',
-			'box-shadow',
-			'rgba(197, 238, 240, 0.67) 10px 50px 30px 40px, rgba(106, 105, 105, 0.9) 20px 0px 50px 5px inset'
 		);
 	});
 });
