680453b8b4341308fc6b504a1360ae22b8c5725a
rezaelahidev <rezaelahi.dev@gmail.com>
2024-05-16 19:21:46 +0330

    feat: add blockera-admin menus

--- packages/blockera/php/app.php
+++ packages/blockera/php/app.php
@@ -16,4 +16,6 @@ if ( ! defined( 'BLOCKERA_APP_MODE' ) && 'development' === BLOCKERA_APP_MODE ) {
 	blockera_load( 'hooks', [], __DIR__ );
 }
 
+blockera_load( 'wordpress.php.Admin.Menu.hooks', [], dirname( __DIR__, 2 ) );
+
 $blockera->bootstrap();
