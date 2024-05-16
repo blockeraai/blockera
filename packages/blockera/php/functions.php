b567e96cf71e3f0e3aba2534a4b483afaae7dbb7
rezaelahidev <rezaelahi.dev@gmail.com>
2024-05-16 19:19:11 +0330

    feat: support new core config

--- packages/blockera/php/functions.php
+++ packages/blockera/php/functions.php
@@ -30,6 +30,8 @@ if ( ! function_exists( 'blockera_core_config' ) ) {
 
 		$configIncludes = array(
 			'app'         => $config_dir . '/config/app.php',
+			'menu'        => $config_dir . '/config/menu.php',
+			'panel'        => $config_dir . '/config/panel.php',
 			'assets'      => $config_dir . '/config/assets.php',
 			'entities'    => $config_dir . '/config/entities.php',
 			'breakpoints' => $config_dir . '/config/breakpoints.php',
