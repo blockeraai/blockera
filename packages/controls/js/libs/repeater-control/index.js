a4d375b64b6525f172f5075e03fc88c96b0e706f
rezaelahidev <rezaelahi.dev@gmail.com>
2024-05-18 00:19:23 +0330

    fix: repeater add new item disabler

--- packages/controls/js/libs/repeater-control/index.js
+++ packages/controls/js/libs/repeater-control/index.js
@@ -346,6 +346,7 @@ export default function RepeaterControl(
 											'btn-add',
 											{
 												'is-deactivate':
+													disableProHints &&
 													disableAddNewItem,
 											}
 										)}
@@ -415,6 +416,7 @@ export default function RepeaterControl(
 											'btn-add',
 											{
 												'is-deactivate':
+													disableProHints &&
 													disableAddNewItem,
 											}
 										)}
