aafae1464183c5d1e36682730aef049a03773588
rezaelahidev <rezaelahi.dev@gmail.com>
2024-05-21 12:52:13 +0330

    feat: add block-visibility option for user roles

--- packages/blockera-admin/js/components/panels/general-panel/index.js
+++ packages/blockera-admin/js/components/panels/general-panel/index.js
@@ -5,14 +5,23 @@
  */
 import { __ } from '@wordpress/i18n';
 import type { MixedElement } from 'react';
+import { applyFilters } from '@wordpress/hooks';
 import { useContext } from '@wordpress/element';
-import { __experimentalVStack as VStack } from '@wordpress/components';
 
 /**
  * Blockera dependencies
  */
-import { SettingsContext, TabsContext } from '@blockera/wordpress';
-import { ControlContextProvider, ToggleControl } from '@blockera/controls';
+import {
+	TabsContext,
+	SettingsContext,
+	AdminFeatureWrapper,
+} from '@blockera/wordpress';
+import {
+	ToggleControl,
+	CheckboxControl,
+	ControlContextProvider,
+} from '@blockera/controls';
+import { Flex } from '@blockera/components';
 
 // here store fallback default values for tab general settings.
 const fallbackDefaultValue = {
@@ -20,14 +29,155 @@ const fallbackDefaultValue = {
 };
 
 export const GeneralPanel = (): MixedElement => {
-	const { defaultSettings } = useContext(SettingsContext);
+	const { defaultSettings, config } = useContext(SettingsContext);
 	const { settings, setSettings, setHasUpdates } = useContext(TabsContext);
 	const generalSettings =
 		settings?.general || defaultSettings?.general || fallbackDefaultValue;
 
+	const BlockVisibility = (): MixedElement => (
+		<Flex
+			direction={'column'}
+			className={'blockera-settings-general section'}
+		>
+			<h3 className={'blockera-settings-general section-title'}>
+				🚫 {__('Restricts Blocks by User Roles', 'blockera')}
+			</h3>
+
+			<p className={'blockera-settings-general section-desc'}>
+				{__(
+					'By default, all users with block editing capabilities can utilize the  features of Blockera blocks. You can specify which user roles should  have access to Blockera block settings within the Editor. Administrators  will always retain permission for these settings.',
+					'blockera'
+				)}
+			</p>
+			<div className={'blockera-settings-general control-wrapper'}>
+				<Flex direction={'column'}>
+					<AdminFeatureWrapper
+						config={config.general.restrictBlockVisibility}
+					>
+						<ControlContextProvider
+							value={{
+								name: 'toggleRestrictBlockVisibility',
+								value: generalSettings.disableRestrictBlockVisibility,
+							}}
+						>
+							<ToggleControl
+								// TODO: Convert to advanced labelType. to display for user is changed value or not!
+								labelType={'self'}
+								id={'toggleRestrictBlockVisibility'}
+								className={'blockera-settings-general control'}
+								defaultValue={
+									generalSettings.disableRestrictBlockVisibility
+								}
+								onChange={(checked: boolean) => {
+									setHasUpdates(
+										!generalSettings.disableRestrictBlockVisibility
+											? checked
+											: !checked
+									);
+
+									setSettings({
+										...settings,
+										general: {
+											...generalSettings,
+											disableRestrictBlockVisibility:
+												checked,
+										},
+									});
+								}}
+								label={
+									<strong
+										className={
+											'blockera-settings-general control-label'
+										}
+									>
+										{__(
+											'Restrict block visibility controls to selected user roles.',
+											'blockera'
+										)}
+									</strong>
+								}
+							/>
+						</ControlContextProvider>
+						<div
+							className={
+								'blockera-settings-block-visibility user-roles'
+							}
+						>
+							<ControlContextProvider
+								value={{
+									type: 'nested',
+									name: 'userRoles',
+									value: Object.fromEntries(
+										Object.entries(
+											generalSettings.allowedUserRoles
+										).map(
+											([id, userRole]: [
+												string,
+												{
+													name: string,
+													checked: boolean,
+												}
+											]): [string, boolean] => {
+												return [id, userRole.checked];
+											}
+										)
+									),
+								}}
+							>
+								{Object.entries(
+									generalSettings.allowedUserRoles
+								).map(
+									(
+										[id, userRole]: [
+											string,
+											{
+												name: string,
+												checked: boolean,
+											}
+										],
+										index: number
+									): MixedElement => {
+										return (
+											<CheckboxControl
+												id={id}
+												key={userRole.name + index}
+												checkboxLabel={userRole.name}
+												defaultValue={userRole.checked}
+												onChange={(
+													checked: boolean
+												): void => {
+													applyFilters(
+														'blockera.admin.general.panel.restrictBlockVisibility.userRole.onChange',
+														() => {},
+														checked,
+														id,
+														settings,
+														setSettings,
+														generalSettings
+													)(checked);
+												}}
+											/>
+										);
+									}
+								)}
+							</ControlContextProvider>
+						</div>
+					</AdminFeatureWrapper>
+				</Flex>
+			</div>
+		</Flex>
+	);
+
 	return (
-		<VStack className={'blockera-settings-panel-container'}>
-			<VStack className={'blockera-settings-general section'}>
+		<Flex
+			direction={'column'}
+			className={'blockera-settings-panel-container'}
+		>
+			<BlockVisibility />
+			<Flex
+				direction={'column'}
+				className={'blockera-settings-general section'}
+			>
 				<h3 className={'blockera-settings-general section-title'}>
 					⚙️ {__('Other Settings', 'blockera')}
 				</h3>
@@ -82,7 +232,7 @@ export const GeneralPanel = (): MixedElement => {
 						/>
 					</ControlContextProvider>
 				</div>
-			</VStack>
-		</VStack>
+			</Flex>
+		</Flex>
 	);
 };
