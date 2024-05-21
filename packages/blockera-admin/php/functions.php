fe07ffab724e2fbf50454481f4ad09ac6efca1ec
rezaelahidev <rezaelahi.dev@gmail.com>
2024-05-21 12:48:12 +0330

    feat: php new helper function in admin package

--- packages/blockera-admin/php/functions.php
+++ packages/blockera-admin/php/functions.php
@@ -12,3 +12,33 @@ if ( ! function_exists( 'blockera_settings_page_template' ) ) {
 		echo '<div id="blockera-admin-settings-container"></div>';
 	}
 }
+
+if ( ! function_exists( 'blockera_normalized_user_roles' ) ) {
+
+	/**
+	 * Preparing user roles and normalizing that with blockera standards.
+	 * this used for restrict block visibility controls.
+	 *
+	 * @return array the normalized user roles.
+	 */
+	function blockera_normalized_user_roles(): array {
+
+		$user_roles = [];
+
+		foreach ( wp_roles()->get_names() as $id => $name ) {
+
+			// Skip administrator, because this role is specific other restrict block visibility.
+			if ( 'administrator' === $id ) {
+
+				continue;
+			}
+
+			$user_roles[ $id ] = [
+				'name'    => $name,
+				'checked' => false,
+			];
+		}
+
+		return $user_roles;
+	}
+}
