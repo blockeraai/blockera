a77f5e962f3a1914c5a0ea1ab0ebaea3438ad278
rezaelahidev <rezaelahi.dev@gmail.com>
2024-05-22 23:43:40 +0330

    feat: add hook to before init blockera admin app

--- packages/blockera-admin/js/index.js
+++ packages/blockera-admin/js/index.js
@@ -7,12 +7,15 @@ import { __ } from '@wordpress/i18n';
 import { dispatch } from '@wordpress/data';
 import domReady from '@wordpress/dom-ready';
 import { createRoot } from '@wordpress/element';
+import { addFilter, applyFilters } from '@wordpress/hooks';
 import { store as coreDataStore } from '@wordpress/core-data';
 import { registerCoreBlocks } from '@wordpress/block-library';
 
 /**
  * Blockera dependencies
  */
+import { noop } from '@blockera/utils';
+import { initializer } from '@blockera/bootstrap';
 import { unstableBootstrapServerSideEntities } from '@blockera/data';
 
 /**
@@ -20,28 +23,43 @@ import { unstableBootstrapServerSideEntities } from '@blockera/data';
  */
 import { Dashboard } from './dashboard';
 
-domReady(() => {
-	registerCoreBlocks();
-	const { addEntities } = dispatch(coreDataStore);
-	const { unstableBlockeraBootstrapServerSideEntities } = window;
+const initializeBlockeraAdmin = (): void => {
+	domReady(() => {
+		applyFilters('blockera.admin.before.bootstrap', noop)();
 
-	unstableBootstrapServerSideEntities(
-		unstableBlockeraBootstrapServerSideEntities
-	);
+		registerCoreBlocks();
+		const { addEntities } = dispatch(coreDataStore);
+		const { unstableBlockeraBootstrapServerSideEntities } = window;
 
-	// Adding entities into WordPress core data.
-	addEntities([
-		{
-			label: __('Blockera Settings', 'blockera'),
-			kind: 'blockera/v1',
-			name: 'settings',
-			baseURL: '/blockera/v1/settings',
-		},
-	]);
+		unstableBootstrapServerSideEntities(
+			unstableBlockeraBootstrapServerSideEntities
+		);
 
-	const root = createRoot(
-		document.getElementById('blockera-admin-settings-container')
-	);
+		// Adding entities into WordPress core data.
+		addEntities([
+			{
+				label: __('Blockera Settings', 'blockera'),
+				kind: 'blockera/v1',
+				name: 'settings',
+				baseURL: '/blockera/v1/settings',
+			},
+		]);
 
-	root.render(<Dashboard />);
-});
+		const root = createRoot(
+			document.getElementById('blockera-admin-settings-container')
+		);
+
+		root.render(<Dashboard />);
+	});
+};
+
+/**
+ * Initialize blockera react application.
+ */
+addFilter(
+	'blockera.bootstrapper',
+	'blockera.bootstrap',
+	initializeBlockeraAdmin
+);
+
+initializer();
