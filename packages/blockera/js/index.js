8f75f348c83283e3f9bd028487ce252808cd700b
rezaelahidev <rezaelahi.dev@gmail.com>
2024-05-22 23:42:45 +0330

    feat: add hook to before init blockera app

--- packages/blockera/js/index.js
+++ packages/blockera/js/index.js
@@ -3,7 +3,7 @@
 /**
  * External dependencies
  */
-import { addFilter } from '@wordpress/hooks';
+import { addFilter, applyFilters } from '@wordpress/hooks';
 
 /**
  * Blockera dependencies
@@ -12,6 +12,7 @@ import {
 	reregistrationBlocks,
 	registerThirdPartyExtensionDefinitions,
 } from '@blockera/blocks';
+import { noop } from '@blockera/utils';
 import { initializer } from '@blockera/bootstrap';
 import { applyHooks, defineGlobalProps } from '@blockera/editor';
 
@@ -19,6 +20,8 @@ import { applyHooks, defineGlobalProps } from '@blockera/editor';
  * Initialize blockera react application.
  */
 addFilter('blockera.bootstrapper', 'blockera.bootstrap', () => {
+	applyFilters('blockera.before.bootstrap', noop)();
+
 	defineGlobalProps();
 
 	applyHooks(() => {
