ac04147bb3d84d365afab8d68d245f4a1d0a15d9
Ali Aghdam <betteraliaghdam@gmail.com>
2024-06-16 16:45:44 +0330

    fix: "Icon" item in WP exports is not icon and should be removed

--- packages/icons/js/library-wp/icons.js
+++ packages/icons/js/library-wp/icons.js
@@ -6,13 +6,15 @@
 import * as _rawIcons from '@wordpress/icons';
 
 const WPIcons: Object = Object.fromEntries(
-	Object.entries(_rawIcons).map(([key, value]) => [
-		key
-			.replace(/([a-z])([A-Z])/g, '$1-$2')
-			.replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
-			.toLowerCase(),
-		value,
-	])
+	Object.entries(_rawIcons)
+		.map(([key, value]: [string, any]) => [
+			key
+				.replace(/([a-z])([A-Z])/g, '$1-$2')
+				.replace(/([A-Z])([A-Z][a-z])/g, '$1-$2')
+				.toLowerCase(),
+			value,
+		])
+		.filter(([key]) => key !== 'icon')
 );
 
 export { WPIcons };
