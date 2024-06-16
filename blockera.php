7c0d4104e438a8e868c31c33ca0dd76b4100a01d
Ali Aghdam <betteraliaghdam@gmail.com>
2024-06-17 02:30:52 +0330

    feat: v1.1.5 BUMP

--- blockera.php
+++ blockera.php
@@ -8,7 +8,7 @@
  * Requires PHP: 7.4
  * Author: Blockera AI
  * Author URI: https://blockera.ai/about-us/
- * Version: 1.1.4
+ * Version: 1.1.5
  * Text Domain: blockera
  * License: GPLv3 or later
  *
