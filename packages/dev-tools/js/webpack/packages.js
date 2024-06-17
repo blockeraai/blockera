464f1576adcc790a492abbe059ecb2cc7cd6c139
rezaelahidev <rezaelahi.dev@gmail.com>
2024-06-17 16:21:00 +0330

    build(webpack): refactor dev-tools webpack/packages to dynamically

--- packages/dev-tools/js/webpack/packages.js
+++ packages/dev-tools/js/webpack/packages.js
@@ -1,24 +1,20 @@
 /**
  * External dependencies
  */
-const { join, resolve } = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { join, resolve } = require('path');
 
 /**
  * WordPress dependencies
  */
-const postcssPlugins = require('@wordpress/postcss-plugins-preset');
-const {
-	camelCaseDash,
-} = require('@wordpress/dependency-extraction-webpack-plugin/lib/util');
-const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const postcssPlugins = require('@wordpress/postcss-plugins-preset');
+const DependencyExtractionWebpackPlugin = require('@wordpress/dependency-extraction-webpack-plugin');
 
 /**
  * Internal dependencies
  */
-const { dependencies } = require('../../../../package');
 const styleDependencies = require('./packages-styles');
 
 /**
@@ -49,71 +45,6 @@ defaultConfig.module.rules
 		}
 	});
 
-const exportDefaultPackages = [];
-const BLOCKERA_NAMESPACE = '@blockera/';
-const blockeraPackages = Object.keys(dependencies)
-	.filter((packageName) => packageName.startsWith(BLOCKERA_NAMESPACE))
-	.map((packageName) => packageName.replace(BLOCKERA_NAMESPACE, ''));
-const blockeraPackagesVersion = Object.fromEntries(
-	blockeraPackages.map((packageName) => {
-		let parentDirectory = '';
-		let name = packageName;
-
-		if (-1 !== packageName.indexOf('blocks-')) {
-			parentDirectory = 'blocks/';
-			name = name.split('blocks-')[1];
-		}
-
-		const {
-			version,
-		} = require(`../../../../packages/${parentDirectory}${name}/package.json`);
-
-		return [packageName, version.replace(/\./g, '_')];
-	})
-);
-const blockeraEntries = blockeraPackages.reduce((memo, packageName) => {
-	// Exclude dev packages.
-	if (-1 !== packageName.indexOf('dev-')) {
-		return memo;
-	}
-
-	if (!blockeraPackagesVersion[packageName]) {
-		return memo;
-	}
-
-	let parentDirectory = '';
-	let _packageName = packageName;
-
-	if (-1 !== packageName.indexOf('blocks-')) {
-		parentDirectory = 'blocks/';
-		_packageName = _packageName.split('blocks-')[1];
-	}
-	const version = blockeraPackagesVersion[packageName];
-
-	let name = packageName.startsWith('blockera')
-		? camelCaseDash(packageName + '_' + version)
-		: camelCaseDash('blockera-' + packageName + '_' + version);
-
-	if ('icons' === packageName) {
-		name = packageName.startsWith('blockera')
-			? camelCaseDash(packageName)
-			: camelCaseDash('blockera-' + packageName);
-	}
-
-	return {
-		...memo,
-		[packageName]: {
-			import: `./packages/${parentDirectory}${_packageName}`,
-			library: {
-				name,
-				type: 'var',
-				export: exportDefaultPackages.includes(packageName)
-					? 'default'
-					: undefined,
-			},
-		},
-	};
-}, {});
 const scssLoaders = ({ isLazy }) => [
 	{
 		loader: 'style-loader',
@@ -139,11 +70,11 @@ module.exports = (env, argv) => {
 		mode: argv.mode,
 		name: 'packages',
 		entry: {
-			...blockeraEntries,
+			...argv.entry,
 			...styleDependencies.entry,
 		},
 		output: {
-			devtoolNamespace: 'blockera',
+			devtoolNamespace: argv.devtoolNamespace,
 			filename: isProduction
 				? './dist/[name]/[name].min.js'
 				: './dist/[name]/[name].js',
@@ -185,25 +116,6 @@ module.exports = (env, argv) => {
 			: {
 					devtool: 'source-map',
 			  }),
-		externals: {
-			// Externalize the local packages.
-			'@blockera/icons': 'blockeraIcons',
-			'@blockera/data': 'blockeraData_' + blockeraPackagesVersion.data,
-			'@blockera/utils': 'blockeraUtils_' + blockeraPackagesVersion.utils,
-			'@blockera/editor':
-				'blockeraEditor_' + blockeraPackagesVersion.editor,
-			'@blockera/core-blocks':
-				'blockeraBlocksCore_' + blockeraPackagesVersion['blocks-core'],
-			'@blockera/controls':
-				'blockeraControls_' + blockeraPackagesVersion.controls,
-			'@blockera/bootstrap':
-				'blockeraBootstrap_' + blockeraPackagesVersion.bootstrap,
-			'@blockera/wordpress':
-				'blockeraWordpress_' + blockeraPackagesVersion.wordpress,
-			'@blockera/classnames':
-				'blockeraClassnames_' + blockeraPackagesVersion.classnames,
-			'@blockera/data-editor':
-				'blockeraDataEditor_' + blockeraPackagesVersion['data-editor'],
-		},
+		externals: argv.externals,
 	};
 };
