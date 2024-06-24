bba37c049ee7e225d0ef356656dfa2b789c8f42a
rezaelahidev <rezaelahi.dev@gmail.com>
2024-06-24 19:10:10 +0330

    fix: outline default value

--- packages/controls/js/libs/outline-control/index.js
+++ packages/controls/js/libs/outline-control/index.js
@@ -25,9 +25,12 @@ export default function OutlineControl({
 	popoverTitle = __('Outline', 'blockera'),
 	defaultRepeaterItemValue = {
 		border: {
-			width: '2px',
-			style: 'solid',
-			color: '#b6b6b6',
+			type: 'all',
+			all: {
+				width: '2px',
+				style: 'solid',
+				color: '#b6b6b6',
+			},
 		},
 		offset: '2px',
 		isVisible: true,
