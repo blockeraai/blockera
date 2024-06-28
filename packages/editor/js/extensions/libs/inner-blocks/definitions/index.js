d5298663b6c8507b4ca4a541d4614a0608df6d82
Ali Aghdam <betteraliaghdam@gmail.com>
2024-06-28 08:48:17 +0330

    fix: heading inner block definition disabled temporarily

--- packages/editor/js/extensions/libs/inner-blocks/definitions/index.js
+++ packages/editor/js/extensions/libs/inner-blocks/definitions/index.js
@@ -3,48 +3,48 @@
 /**
  * External dependencies
  */
-import { dispatch } from '@wordpress/data';
-import { applyFilters } from '@wordpress/hooks';
+// import { dispatch } from '@wordpress/data';
+// import { applyFilters } from '@wordpress/hooks';
 
 /**
  * Internal dependencies
  */
-import { heading } from './heading';
-import * as config from '../../base/config';
-import { STORE_NAME } from '../../base/store/constants';
+// import { heading } from './heading';
+// import * as config from '../../base/config';
+// import { STORE_NAME } from '../../base/store/constants';
 
-export const definitionTypes = {
-	heading: {
-		...config,
-		backgroundConfig: {
-			...config.backgroundConfig,
-			...heading,
-		},
-	},
-};
+// export const definitionTypes = {
+// 	heading: {
+// 		...config,
+// 		backgroundConfig: {
+// 			...config.backgroundConfig,
+// 			...heading,
+// 		},
+// 	},
+// };
 
-export const __experimentalRegistrationInnerBlockExtensionCustomConfigDefinition =
-	(): void => {
-		/**
-		 * Filterable definitionTypes list before registration process.
-		 *
-		 * hook: `blockeraCore.extensions.innerBlocks.definitionTypes`
-		 *
-		 * @since 1.0.0
-		 *
-		 * @type {Object}
-		 */
-		const filteredDefinitions = applyFilters(
-			'blockeraCore.extensions.innerBlocks.definitionTypes',
-			definitionTypes
-		);
+// export const __experimentalRegistrationInnerBlockExtensionCustomConfigDefinition =
+// 	(): void => {
+// 		/**
+// 		 * Filterable definitionTypes list before registration process.
+// 		 *
+// 		 * hook: `blockeraCore.extensions.innerBlocks.definitionTypes`
+// 		 *
+// 		 * @since 1.0.0
+// 		 *
+// 		 * @type {Object}
+// 		 */
+// 		const filteredDefinitions = applyFilters(
+// 			'blockeraCore.extensions.innerBlocks.definitionTypes',
+// 			definitionTypes
+// 		);
 
-		Object.keys(filteredDefinitions).forEach((name: string): void => {
-			const { addDefinition } = dispatch(STORE_NAME);
+// 		Object.keys(filteredDefinitions).forEach((name: string): void => {
+// 			const { addDefinition } = dispatch(STORE_NAME);
 
-			addDefinition({
-				name,
-				extensions: filteredDefinitions[name],
-			});
-		});
-	};
+// 			addDefinition({
+// 				name,
+// 				extensions: filteredDefinitions[name],
+// 			});
+// 		});
+// 	};
