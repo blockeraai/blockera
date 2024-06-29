811742ed87bc0f75778d17ba23648f20001597b7
Ali Aghdam <betteraliaghdam@gmail.com>
2024-06-29 20:03:29 +0330

    test(cypress): decrease defaultCommandTimeout for checking CI run time

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
 	chromeWebSecurity: false,
-	defaultCommandTimeout: 20000,
+	defaultCommandTimeout: 5000,
 	e2e: {
 		setupNodeEvents(on, config) {
 			require('./packages/dev-cypress/js/plugins/index.js')(on, {
