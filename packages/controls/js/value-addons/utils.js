e831d9bac591e30f521b754822e4fecf5866afd4
Ali Aghdam <betteraliaghdam@gmail.com>
2024-07-02 01:37:53 +0330

    chore: fix data type

--- packages/controls/js/value-addons/utils.js
+++ packages/controls/js/value-addons/utils.js
@@ -1,5 +1,10 @@
 // @flow
 
+/**
+ * Internal dependencies
+ */
+import type { ValueAddon } from './types';
+
 export function isValid(value: ValueAddon | string): boolean {
 	//$FlowFixMe
 	return !!value?.isValueAddon;
