87a83ec2c7bc268f453c5f9a35bf5126d66cd0f2
rezaelahidev <rezaelahi.dev@gmail.com>
2024-07-08 01:19:08 +0330

    test: update toggle advanced label tests for experimental divider

--- packages/editor/js/components/editor-advanced-label/test/advanced-label-toggle.cypress.js
+++ packages/editor/js/components/editor-advanced-label/test/advanced-label-toggle.cypress.js
@@ -3,111 +3,114 @@ import {
 	createPost,
 	setDeviceType,
 } from '../../../../../dev-cypress/js/helpers';
+import { experimental } from '@blockera/env';
 
-describe('Toggle Control', () => {
-	beforeEach(() => {
-		createPost();
+if (experimental().get('editor.extensions.effectsExtension.divider')) {
+	describe('Toggle Control', () => {
+		beforeEach(() => {
+			createPost();
 
-		addBlockToPost('core/paragraph', true, 'blockera-paragraph');
+			addBlockToPost('core/paragraph', true, 'blockera-paragraph');
 
-		// Add divider
-		cy.getByAriaLabel('Add New Divider').click();
+			// Add divider
+			cy.getByAriaLabel('Add New Divider').click();
 
-		//Alias
-		cy.getParentContainer('Animation').within(() => {
-			cy.get('input[type="checkbox"]').as('animation-checkbox');
+			//Alias
+			cy.getParentContainer('Animation').within(() => {
+				cy.get('input[type="checkbox"]').as('animation-checkbox');
+			});
+
+			cy.getByDataTest('divider-item-header').as('divider-item');
 		});
 
-		cy.getByDataTest('divider-item-header').as('divider-item');
+		it('should display changed value on Divider/Animation -> Normal -> Laptop', () => {
+			// Assert label before set value
+			cy.getByAriaLabel('Animation').should(
+				'not.have.class',
+				'changed-in-normal-state'
+			);
+
+			// Set value
+			cy.get('@animation-checkbox').click({ force: true });
+
+			// Assert label after set value
+			cy.getByAriaLabel('Animation').should(
+				'have.class',
+				'changed-in-normal-state'
+			);
+
+			// Assert control
+			cy.get('@animation-checkbox')
+				.parent()
+				.should('have.class', 'is-checked');
+
+			/**
+			 * Tablet device
+			 */
+			setDeviceType('Tablet');
+			cy.get('@divider-item').click();
+
+			// Assert label
+			cy.getByAriaLabel('Animation').should(
+				'have.class',
+				'changed-in-normal-state'
+			);
+
+			// Assert control
+			cy.get('@animation-checkbox')
+				.parent()
+				.should('have.class', 'is-checked');
+
+			// Assert state graph
+			cy.checkStateGraph('', 'Animation', { laptop: ['Normal'] }, true);
+		});
+
+		it('should display changed value on Divider/Animation -> Normal -> Tablet', () => {
+			setDeviceType('Tablet');
+			cy.get('@divider-item').click();
+
+			// Assert label before set value
+			cy.getByAriaLabel('Animation').should(
+				'not.have.class',
+				'changed-in-normal-state'
+			);
+
+			// Set value
+			cy.get('@animation-checkbox').click({ force: true });
+
+			// Assert label after set value
+			cy.getByAriaLabel('Animation').should(
+				'have.class',
+				'changed-in-normal-state'
+			);
+
+			// Assert control
+			cy.get('@animation-checkbox')
+				.parent()
+				.should('have.class', 'is-checked');
+
+			/**
+			 * Laptop device
+			 */
+			setDeviceType('Laptop');
+			cy.get('@divider-item').click();
+
+			// Assert label
+			cy.getByAriaLabel('Animation').should(
+				'have.class',
+				'changed-in-other-state'
+			);
+
+			// Assert control
+			cy.get('@animation-checkbox')
+				.parent()
+				.should('not.have.class', 'is-checked');
+
+			// Assert state graph
+			cy.checkStateGraph('', 'Animation', { tablet: ['Normal'] }, true);
+		});
+
+		// TODO: add pseudo state test if this control is used in features which are available in pseudo states
+		// TODO: reset
 	});
-
-	it('should display changed value on Divider/Animation -> Normal -> Laptop', () => {
-		// Assert label before set value
-		cy.getByAriaLabel('Animation').should(
-			'not.have.class',
-			'changed-in-normal-state'
-		);
-
-		// Set value
-		cy.get('@animation-checkbox').click({ force: true });
-
-		// Assert label after set value
-		cy.getByAriaLabel('Animation').should(
-			'have.class',
-			'changed-in-normal-state'
-		);
-
-		// Assert control
-		cy.get('@animation-checkbox')
-			.parent()
-			.should('have.class', 'is-checked');
-
-		/**
-		 * Tablet device
-		 */
-		setDeviceType('Tablet');
-		cy.get('@divider-item').click();
-
-		// Assert label
-		cy.getByAriaLabel('Animation').should(
-			'have.class',
-			'changed-in-normal-state'
-		);
-
-		// Assert control
-		cy.get('@animation-checkbox')
-			.parent()
-			.should('have.class', 'is-checked');
-
-		// Assert state graph
-		cy.checkStateGraph('', 'Animation', { laptop: ['Normal'] }, true);
-	});
-
-	it('should display changed value on Divider/Animation -> Normal -> Tablet', () => {
-		setDeviceType('Tablet');
-		cy.get('@divider-item').click();
-
-		// Assert label before set value
-		cy.getByAriaLabel('Animation').should(
-			'not.have.class',
-			'changed-in-normal-state'
-		);
-
-		// Set value
-		cy.get('@animation-checkbox').click({ force: true });
-
-		// Assert label after set value
-		cy.getByAriaLabel('Animation').should(
-			'have.class',
-			'changed-in-normal-state'
-		);
-
-		// Assert control
-		cy.get('@animation-checkbox')
-			.parent()
-			.should('have.class', 'is-checked');
-
-		/**
-		 * Laptop device
-		 */
-		setDeviceType('Laptop');
-		cy.get('@divider-item').click();
-
-		// Assert label
-		cy.getByAriaLabel('Animation').should(
-			'have.class',
-			'changed-in-other-state'
-		);
-
-		// Assert control
-		cy.get('@animation-checkbox')
-			.parent()
-			.should('not.have.class', 'is-checked');
-
-		// Assert state graph
-		cy.checkStateGraph('', 'Animation', { tablet: ['Normal'] }, true);
-	});
-
-	// TODO: add pseudo state test if this control is used in features which are available in pseudo states
-	// TODO: reset
-});
+}
