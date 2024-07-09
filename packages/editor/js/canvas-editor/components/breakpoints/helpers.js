1c729800f14e6f57d6af8107324a8bcc1b65ea43
Ali Aghdam <betteraliaghdam@gmail.com>
2024-07-09 18:39:41 +0330

    feat: getLargestBreakpoint helper function

--- packages/editor/js/canvas-editor/components/breakpoints/helpers.js
+++ packages/editor/js/canvas-editor/components/breakpoints/helpers.js
@@ -1,9 +1,23 @@
 // @flow
 
 /**
+ * External dependencies
+ */
+import { default as memoize } from 'fast-memoize';
+
+/**
  * Blockera dependencies
  */
-import type { TBreakpoint } from '../../../extensions/libs/block-states/types';
+import { isUndefined } from '@blockera/utils';
+
+/**
+ * Internal dependencies
+ */
+import type {
+	TBreakpoint,
+	BreakpointTypes,
+} from '../../../extensions/libs/block-states/types';
+import { default as defaultBreakpoints } from '../../../extensions/libs/block-states/default-breakpoints';
 
 /**
  * Check if given breakpoint is base breakpoint
@@ -60,4 +74,45 @@ export const getSmallestBreakpoint: (breakpoints?: {
 	}
 
 	return minBreakpointId;
+});
+
+/**
+ * Get largest breakpoint id.
+ *
+ * @param {Object} breakpoints Optional breakpoints object and if not provided will use default
+ * @return {string} the breakpoint ID.
+ */
+export const getLargestBreakpoint: (breakpoints?: {
+	[key: TBreakpoint]: BreakpointTypes,
+}) => TBreakpoint = memoize(function (breakpoints?: {
+	[key: TBreakpoint]: BreakpointTypes,
+}): TBreakpoint {
+	if (isUndefined(breakpoints)) breakpoints = defaultBreakpoints();
+
+	let maxBreakpoint = -Infinity;
+
+	let maxBreakpointId = 'desktop';
+
+	for (const key in breakpoints) {
+		const typedKey: TBreakpoint = (key: any);
+
+		const min = breakpoints[typedKey].settings.min;
+		const max = breakpoints[typedKey].settings.max;
+
+		if (max && !isNaN(parseInt(max, 10))) {
+			const maxValue = parseInt(max, 10);
+			if (maxValue > maxBreakpoint) {
+				maxBreakpoint = maxValue;
+				maxBreakpointId = typedKey;
+			}
+		} else if (!max && min && !isNaN(parseInt(min, 10))) {
+			const minValue = parseInt(min, 10);
+			if (minValue > maxBreakpoint) {
+				maxBreakpoint = minValue;
+				maxBreakpointId = typedKey;
+			}
+		}
+	}
+
+	return maxBreakpointId;
 });
