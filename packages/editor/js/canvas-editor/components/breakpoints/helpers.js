205b591e3c75d73fc53de5467498f6c9ef0de34a
Ali Aghdam <betteraliaghdam@gmail.com>
2024-07-09 18:32:04 +0330

    feat: getSmallestBreakpoint helper function

--- packages/editor/js/canvas-editor/components/breakpoints/helpers.js
+++ packages/editor/js/canvas-editor/components/breakpoints/helpers.js
@@ -20,3 +20,44 @@ export const isBaseBreakpoint = (breakPoint: TBreakpoint): boolean =>
  * @return {string} the breakpoint identifier.
  */
 export const getBaseBreakpoint = (): TBreakpoint => 'desktop';
+
+/**
+ * Get smallest breakpoint id.
+ *
+ * @param {Object} breakpoints Optional breakpoints object and if not provided will use default
+ * @return {string} the breakpoint identifier.
+ */
+export const getSmallestBreakpoint: (breakpoints?: {
+	[key: TBreakpoint]: BreakpointTypes,
+}) => TBreakpoint = memoize(function (breakpoints?: {
+	[key: TBreakpoint]: BreakpointTypes,
+}): TBreakpoint {
+	if (isUndefined(breakpoints)) breakpoints = defaultBreakpoints();
+
+	let minBreakpoint = Infinity;
+
+	let minBreakpointId: TBreakpoint = 'desktop';
+
+	for (const key in breakpoints) {
+		const typedKey: TBreakpoint = (key: any);
+
+		const min = breakpoints[typedKey].settings.min;
+		const max = breakpoints[typedKey].settings.max;
+
+		if (min && !isNaN(parseInt(min, 10))) {
+			const minValue = parseInt(min, 10);
+			if (minValue < minBreakpoint) {
+				minBreakpoint = minValue;
+				minBreakpointId = typedKey;
+			}
+		} else if (!min && max && !isNaN(parseInt(max, 10))) {
+			const maxValue = parseInt(max, 10);
+			if (maxValue < minBreakpoint) {
+				minBreakpoint = maxValue;
+				minBreakpointId = typedKey;
+			}
+		}
+	}
+
+	return minBreakpointId;
+});
