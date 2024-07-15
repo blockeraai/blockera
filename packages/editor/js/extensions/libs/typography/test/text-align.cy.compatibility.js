0fab7d88227b5d7cce6de2653c1d0f6b1d50a044
Ali Aghdam <betteraliaghdam@gmail.com>
2024-07-15 13:38:31 +0330

    test(text align feature): new test for heading block

--- packages/editor/js/extensions/libs/typography/test/text-align.cy.compatibility.js
+++ packages/editor/js/extensions/libs/typography/test/text-align.cy.compatibility.js
@@ -15,11 +15,11 @@ describe('Text Align → WP Compatibility', () => {
 	});
 
 	describe('Paragraph Block', () => {
-		it('Simple value', () => {
+		it('in paragraph the attribute is align', () => {
 			appendBlocks(
-				'<!-- wp:paragraph {"align":"center"} -->\n' +
-					'<p class="has-text-align-center">Test paragraph</p>\n' +
-					'<!-- /wp:paragraph -->'
+				`<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Test paragraph...</p>
+<!-- /wp:paragraph -->`
 			);
 
 			// Select target block
@@ -89,6 +89,93 @@ describe('Text Align → WP Compatibility', () => {
 				);
 
 				expect(undefined).to.be.equal(getSelectedBlock(data, 'align'));
+			});
+		});
+	});
+
+	describe('Heading Block', () => {
+		it('in heading the attribute is textAlign', () => {
+			appendBlocks(
+				`<!-- wp:heading {"textAlign":"center","level":1,"fontSize":"x-large"} -->
+<h1 class="wp-block-heading has-text-align-center has-x-large-font-size">A commitment to innovation and sustainability</h1>
+<!-- /wp:heading -->`
+			);
+
+			// Select target block
+			cy.getBlock('core/heading').click();
+
+			// Open more settings
+			openMoreFeaturesControl('More typography settings');
+
+			cy.getParentContainer('Text Align').as('container');
+
+			//
+			// Test 1: WP data to Blockera
+			//
+
+			// WP data should come to Blockera
+			getWPDataObject().then((data) => {
+				expect('center').to.be.equal(
+					getSelectedBlock(data, 'blockeraTextAlign')
+				);
+
+				expect('center').to.be.equal(
+					getSelectedBlock(data, 'textAlign')
+				);
+			});
+
+			//
+			// Test 2: Blockera value to WP data
+			//
+
+			cy.get('@container').within(() => {
+				cy.getByAriaLabel('Right').click();
+			});
+
+			// Blockera value should be moved to WP data
+			getWPDataObject().then((data) => {
+				expect('right').to.be.equal(
+					getSelectedBlock(data, 'blockeraTextAlign')
+				);
+
+				expect('right').to.be.equal(
+					getSelectedBlock(data, 'textAlign')
+				);
+			});
+
+			// justify is not valid for wp
+			cy.get('@container').within(() => {
+				cy.getByAriaLabel('Justify').click();
+			});
+
+			// Blockera value should NOT moved to WP data
+			getWPDataObject().then((data) => {
+				expect('justify').to.be.equal(
+					getSelectedBlock(data, 'blockeraTextAlign')
+				);
+
+				expect(undefined).to.be.equal(
+					getSelectedBlock(data, 'textAlign')
+				);
+			});
+
+			//
+			// Test 3: Clear Blockera value and check WP data
+			//
+
+			// clear value
+			cy.get('@container').within(() => {
+				cy.getByAriaLabel('Justify').click();
+			});
+
+			getWPDataObject().then((data) => {
+				expect('').to.be.equal(
+					getSelectedBlock(data, 'blockeraTextAlign')
+				);
+
+				expect(undefined).to.be.equal(
+					getSelectedBlock(data, 'textAlign')
+				);
 			});
 		});
 	});
