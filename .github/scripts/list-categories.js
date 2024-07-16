8a57478c354dd202fc78c44b48821551a022b191
Ali Aghdam <betteraliaghdam@gmail.com>
2024-07-16 06:27:11 +0330

    ci: try to run e2e test dynamically on parallel

---
+++ .github/scripts/list-categories.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+const getFiles = (dir, pattern) => {
+	const files = fs.readdirSync(dir);
+	let allFiles = [];
+
+	files.forEach((file) => {
+		const filePath = path.join(dir, file);
+		if (fs.statSync(filePath).isDirectory()) {
+			allFiles = [...allFiles, ...getFiles(filePath, pattern)];
+		} else if (file.match(pattern)) {
+			allFiles.push(filePath);
+		}
+	});
+
+	return allFiles;
+};
+
+const main = () => {
+	const testFiles = getFiles('packages', /\.cy\.e2e\.(.*?)\.js/);
+	const categories = new Set();
+
+	testFiles.forEach((file) => {
+		const match = file.match(/\.cy\.e2e\.(.*?)\.js/);
+		if (match && match[1]) {
+			categories.add(match[1]);
+		}
+	});
+
+	console.log(Array.from(categories).join(','));
+};
+
+main();
