646df31e268dd03d1241b12eafe8c057d27fea01
rezaelahidev <rezaelahi.dev@gmail.com>
2024-07-17 20:32:25 +0330

    refactor: access to inner block selectors

--- packages/editor/js/style-engine/hooks/use-computed-css-props.js
+++ packages/editor/js/style-engine/hooks/use-computed-css-props.js
@@ -11,7 +11,6 @@ import {
 	EffectsStyles,
 	PositionStyles,
 	FlexChildStyles,
-	useBlockContext,
 	BackgroundStyles,
 	TypographyStyles,
 	BorderAndShadowStyles,
@@ -38,7 +37,6 @@ export const useComputedCssProps = ({
 	const {
 		blocks: { getBlockType },
 	} = useStoreSelectors();
-	const { blockeraInnerBlocks } = useBlockContext();
 	const calculatedProps = {
 		...params,
 		state,
@@ -102,7 +100,7 @@ export const useComputedCssProps = ({
 					...calculatedProps,
 					state: stateType,
 					masterState,
-					selectors: blockeraInnerBlocks[blockType]?.selectors || {},
+					selectors: selectors?.innerBlocks[blockType] || {},
 					attributes: {
 						...defaultAttributes,
 						...breakpointItem?.attributes,
@@ -117,7 +115,7 @@ export const useComputedCssProps = ({
 			...calculatedProps,
 			state: 'normal',
 			masterState,
-			selectors: blockeraInnerBlocks[blockType]?.selectors || {},
+			selectors: selectors?.innerBlocks[blockType] || {},
 			attributes: {
 				...defaultAttributes,
 				...attributes,
