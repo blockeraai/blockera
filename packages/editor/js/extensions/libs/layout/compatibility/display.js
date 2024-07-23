054cc5635e5f87d74eb8afd02646c09630134e18
Ali Aghdam <betteraliaghdam@gmail.com>
2024-07-23 05:15:36 +0330

    feat(layout extension): compatibility the attaching display to block variation + advanced scenarios
    `core/group` and `core/buttons`

--- packages/editor/js/extensions/libs/layout/compatibility/display.js
+++ packages/editor/js/extensions/libs/layout/compatibility/display.js
@@ -3,28 +3,56 @@
 export function displayFromWPCompatibility({
 	attributes,
 	blockId,
+	defaultValue,
+	activeVariation,
 }: {
 	attributes: Object,
 	blockId?: string,
+	defaultValue?: string,
+	activeVariation?: string,
 }): Object {
-	if (attributes?.blockeraDisplay !== '') {
-		return attributes;
-	}
-
+	/**
+	 * custom display compatibility for blocks because of WP blocks have
+	 * different defaults and behaviors and we need to compatible with all possibles.
+	 */
 	switch (blockId) {
 		case 'core/group':
-			if (attributes?.layout?.type !== 'constrained') {
+			// different default values base on block variation
+			switch (activeVariation) {
+				case 'group-grid':
+					defaultValue = 'grid';
+					break;
+				case 'group-stack':
+				case 'group-row':
+					defaultValue = 'flex';
+					break;
+				default:
+					defaultValue = 'constrained';
+					break;
+			}
+
+			if (attributes?.blockeraDisplay === defaultValue) {
+				return attributes;
+			}
+
+			if (attributes?.layout?.type === 'constrained') {
+				// it means the variation is switched and the value was remain from old variation and should be cleared
+				// for special display value other than `flex` and `grid` it should not cleared!
+				if (['flex', 'grid'].includes(attributes.blockeraDisplay)) {
+					attributes.blockeraDisplay = '';
+				}
+			} else {
 				attributes.blockeraDisplay = attributes?.layout?.type;
 			}
 
 			return attributes;
+	}
 
-		case 'core/buttons':
-			if (attributes?.layout?.type !== 'constrained') {
-				attributes.blockeraDisplay = attributes?.layout?.type;
-			}
-
-			return attributes;
+	if (
+		attributes?.blockeraDisplay === defaultValue &&
+		attributes?.layout?.type !== 'constrained'
+	) {
+		attributes.blockeraDisplay = attributes?.layout?.type;
 	}
 
 	return attributes;
@@ -34,22 +62,47 @@ export function displayToWPCompatibility({
 	newValue,
 	ref,
 	blockId,
+	activeVariation,
 }: {
 	newValue: Object,
 	ref?: Object,
 	blockId: string,
+	activeVariation?: string,
 }): Object {
 	switch (blockId) {
 		case 'core/group':
+			//
+			// Reset to default based on active variation
+			//
 			if ('reset' === ref?.current?.action) {
-				return {
-					layout: {
-						type: 'constrained',
-					},
-				};
+				switch (activeVariation) {
+					case 'group-grid':
+						return {
+							layout: {
+								type: 'grid',
+							},
+						};
+
+					case 'group-stack':
+					case 'group-row':
+						return {
+							layout: {
+								type: 'flex',
+							},
+						};
+
+					case 'group':
+					default:
+						return {
+							layout: {
+								type: 'constrained',
+							},
+						};
+				}
 			}
 
-			if (newValue === undefined || newValue !== 'flex') {
+			// prevent error
+			if (!['flex', 'grid'].includes(newValue)) {
 				return {
 					layout: {
 						type: 'constrained',
@@ -64,15 +117,7 @@ export function displayToWPCompatibility({
 			};
 
 		case 'core/buttons':
-			if ('reset' === ref?.current?.action) {
-				return {
-					layout: {
-						type: 'flex',
-					},
-				};
-			}
-
-			if (newValue === undefined || newValue === '') {
+			if ('reset' === ref?.current?.action || newValue === '') {
 				return {
 					layout: {
 						type: 'flex',
