a033df0726c20fa97001ff2c854bdcf782d19d37
rezaelahidev <rezaelahi.dev@gmail.com>
2024-07-31 10:24:58 +0330

    Revert "fix: bad set state in repeater on add new item"
    This reverts commit 895d93ad181dbdf06099cf8263e150c7c6cfaa11.

--- packages/controls/js/libs/repeater-control/index.js
+++ packages/controls/js/libs/repeater-control/index.js
@@ -108,7 +108,7 @@ export default function RepeaterControl(
 
 	const {
 		value: repeaterItems,
-		dispatch: { addRepeaterItem },
+		dispatch: { addRepeaterItem, modifyControlValue },
 		controlInfo: { name: controlId, attribute, blockName },
 		getControlPath,
 		resetToDefault,
@@ -203,6 +203,11 @@ export default function RepeaterControl(
 				...clonedRepeaterItems,
 				[newItemId]: value || defaultRepeaterItemValue,
 			};
+
+			modifyControlValue({
+				controlId,
+				value: newValue,
+			});
 
 			repeaterOnChange(newValue, {
 				onChange,
