3863337160781526db24f147c7f56939edcf8d77
Ali Aghdam <betteraliaghdam@gmail.com>
2024-07-31 19:10:47 +0330

    feat: BlockType improvement

--- packages/blocks/core/js/type/index.js
+++ packages/blocks/core/js/type/index.js
@@ -8,10 +8,14 @@ import type { MixedElement } from 'react';
  * Blockera dependencies
  */
 import type { InnerBlocks } from '@blockera/editor/js/extensions/libs/inner-blocks/types';
+import type { BlockStateType } from '@blockera/editor/js/extensions/libs/block-states/states';
 
 export type BlockType = {
 	name: string,
 	targetBlock: string,
-	blockeraInnerBlocks: InnerBlocks,
+	blockeraInnerBlocks?: InnerBlocks,
 	edit: (props: Object) => MixedElement,
+	availableBlockStates?: {
+		[key: string]: BlockStateType,
+	},
 };
