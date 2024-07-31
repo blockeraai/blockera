3722471ab13b494120c92dcd6762ae14adb1a3de
rezaelahidev <rezaelahi.dev@gmail.com>
2024-07-31 18:02:34 +0330

    test: updated block-states and calculate-current-attributes tests

--- packages/editor/js/hooks/use-calculate-current-attributes/test/use-calculate-current-attributes.cy.e2e.functionality.js
+++ packages/editor/js/hooks/use-calculate-current-attributes/test/use-calculate-current-attributes.cy.e2e.functionality.js
@@ -46,7 +46,7 @@ describe('Should calculate current attributes correctly:', () => {
 
 	it('For Paragraph -> Normal -> Desktop -> Link -> Normal -> Desktop', () => {
 		appendBlocks(
-			'<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"className":"blockera-block blockera-block-84d2a73b-edb9-483e-82a6-17cb82da38c4","blockeraBackgroundColor":"#000000","blockeraInnerBlocks":{"link":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}},"blockeraPropsId":"216143049173","blockeraCompatId":"216143049173"} -->\n' +
+			'<!-- wp:paragraph {"style":{"typography":{"fontSize":"22px"}},"className":"blockera-block blockera-block-84d2a73b-edb9-483e-82a6-17cb82da38c4","blockeraBackgroundColor":"#000000","blockeraInnerBlocks":{"elements/link":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}},"blockeraPropsId":"216143049173","blockeraCompatId":"216143049173"} -->\n' +
 				'<p class="blockera-block blockera-block-84d2a73b-edb9-483e-82a6-17cb82da38c4" style="font-size:22px">test</p>\n' +
 				'<!-- /wp:paragraph -->'
 		);
@@ -62,7 +62,7 @@ describe('Should calculate current attributes correctly:', () => {
 
 	it('For Paragraph -> Normal -> Tablet -> Link -> Normal', () => {
 		appendBlocks(
-			'<!-- wp:paragraph {"className":"blockera-block blockera-block-e66a14eb-6ac9-4ee2-a4d6-50875c606346","blockeraBlockStates":{"normal":{"breakpoints":{"tablet":{"attributes":{"blockeraInnerBlocks":{"link":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}}}}},"isVisible":true}},"blockeraPropsId":"216152855361","blockeraCompatId":"216152855361"} -->\n' +
+			'<!-- wp:paragraph {"className":"blockera-block blockera-block-e66a14eb-6ac9-4ee2-a4d6-50875c606346","blockeraBlockStates":{"normal":{"breakpoints":{"tablet":{"attributes":{"blockeraInnerBlocks":{"elements/link":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}}}}},"isVisible":true}},"blockeraPropsId":"216152855361","blockeraCompatId":"216152855361"} -->\n' +
 				'<p class="blockera-block blockera-block-e66a14eb-6ac9-4ee2-a4d6-50875c606346">test</p>\n' +
 				'<!-- /wp:paragraph -->'
 		);
@@ -79,7 +79,7 @@ describe('Should calculate current attributes correctly:', () => {
 
 	it('For Paragraph -> Hover -> Desktop -> Link -> Normal -> Desktop', () => {
 		appendBlocks(
-			'<!-- wp:paragraph {"className":"blockera-block blockera-block-7802e72e-59b5-4052-859a-82c1fddd6c28","blockeraBlockStates":{"hover":{"breakpoints":{"desktop":{"attributes":{"blockeraInnerBlocks":{"link":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}}}}},"isVisible":true}},"blockeraPropsId":"216155741902","blockeraCompatId":"216155741902"} -->\n' +
+			'<!-- wp:paragraph {"className":"blockera-block blockera-block-7802e72e-59b5-4052-859a-82c1fddd6c28","blockeraBlockStates":{"hover":{"breakpoints":{"desktop":{"attributes":{"blockeraInnerBlocks":{"elements/link":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}}}}},"isVisible":true}},"blockeraPropsId":"216155741902","blockeraCompatId":"216155741902"} -->\n' +
 				'<p class="blockera-block blockera-block-7802e72e-59b5-4052-859a-82c1fddd6c28"></p>\n' +
 				'<!-- /wp:paragraph -->'
 		);
@@ -101,7 +101,7 @@ describe('Should calculate current attributes correctly:', () => {
 
 	it('For Paragraph -> Hover -> Tablet -> Link -> Normal -> Tablet', () => {
 		appendBlocks(
-			'<!-- wp:paragraph {"className":"blockera-block blockera-block-7802e72e-59b5-4052-859a-82c1fddd6c28","blockeraBlockStates":{"hover":{"breakpoints":{"tablet":{"attributes":{"blockeraInnerBlocks":{"link":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}}}}},"isVisible":true}},"blockeraPropsId":"216155741902","blockeraCompatId":"216155741902"} -->\n' +
+			'<!-- wp:paragraph {"className":"blockera-block blockera-block-7802e72e-59b5-4052-859a-82c1fddd6c28","blockeraBlockStates":{"hover":{"breakpoints":{"tablet":{"attributes":{"blockeraInnerBlocks":{"elements/link":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}}}}},"isVisible":true}},"blockeraPropsId":"216155741902","blockeraCompatId":"216155741902"} -->\n' +
 				'<p class="blockera-block blockera-block-7802e72e-59b5-4052-859a-82c1fddd6c28"></p>\n' +
 				'<!-- /wp:paragraph -->'
 		);
@@ -125,7 +125,7 @@ describe('Should calculate current attributes correctly:', () => {
 
 	it('For Paragraph -> Normal -> Desktop -> Link -> Hover -> Desktop', () => {
 		appendBlocks(
-			'<!-- wp:paragraph {"className":"blockera-block blockera-block-97584dbc-298d-4500-8831-3da0762c3456","blockeraInnerBlocks":{"link":{"attributes":{"blockeraBlockStates":{"hover":{"breakpoints":{"desktop":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}},"isVisible":true}}}}},"blockeraPropsId":"21617126465","blockeraCompatId":"21617126465"} -->\n' +
+			'<!-- wp:paragraph {"className":"blockera-block blockera-block-97584dbc-298d-4500-8831-3da0762c3456","blockeraInnerBlocks":{"elements/link":{"attributes":{"blockeraBlockStates":{"hover":{"breakpoints":{"desktop":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}},"isVisible":true}}}}},"blockeraPropsId":"21617126465","blockeraCompatId":"21617126465"} -->\n' +
 				'<p class="blockera-block blockera-block-97584dbc-298d-4500-8831-3da0762c3456">test</p>\n' +
 				'<!-- /wp:paragraph -->'
 		);
@@ -142,7 +142,7 @@ describe('Should calculate current attributes correctly:', () => {
 
 	it('For Paragraph -> Normal -> Tablet -> Link -> Hover -> Tablet', () => {
 		appendBlocks(
-			'<!-- wp:paragraph {"className":"blockera-block blockera-block-05359826-41fa-43dd-880e-fcebf2a85252","blockeraBlockStates":{"normal":{"breakpoints":{"tablet":{"attributes":{"blockeraInnerBlocks":{"link":{"attributes":{"blockeraBlockStates":{"hover":{"breakpoints":{"tablet":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}}}}}}}}}}}},"blockeraPropsId":"6814023600","blockeraCompatId":"6814023600"} -->\n' +
+			'<!-- wp:paragraph {"className":"blockera-block blockera-block-05359826-41fa-43dd-880e-fcebf2a85252","blockeraBlockStates":{"normal":{"breakpoints":{"tablet":{"attributes":{"blockeraInnerBlocks":{"elements/link":{"attributes":{"blockeraBlockStates":{"hover":{"breakpoints":{"tablet":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}}}}}}}}}}}},"blockeraPropsId":"6814023600","blockeraCompatId":"6814023600"} -->\n' +
 				'<p class="blockera-block blockera-block-2762a1c8-98b1-49a8-afe2-61d176d55923">test</p>\n' +
 				'<!-- /wp:paragraph -->'
 		);
@@ -162,7 +162,7 @@ describe('Should calculate current attributes correctly:', () => {
 
 	it('For Paragraph -> Hover -> Desktop -> Link -> Hover -> Desktop', () => {
 		appendBlocks(
-			'<!-- wp:paragraph {"className":"blockera-block blockera-block-cfc3ffa9-2b93-4f98-a66a-511559797d7f","blockeraBlockStates":{"normal":{"breakpoints":{"desktop":{"attributes":{}}},"isVisible":true,"isSelected":false},"hover":{"breakpoints":{"desktop":{"attributes":{"blockeraInnerBlocks":{"link":{"attributes":{"blockeraBlockStates":{"normal":{"breakpoints":{"desktop":{"attributes":{}}},"isVisible":true,"isSelected":false},"hover":{"breakpoints":{"desktop":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}},"isVisible":true,"isSelected":true}}}}}}}},"isVisible":true,"isSelected":true}},"blockeraPropsId":"217145427439","blockeraCompatId":"217145427439"} -->\n' +
+			'<!-- wp:paragraph {"className":"blockera-block blockera-block-cfc3ffa9-2b93-4f98-a66a-511559797d7f","blockeraBlockStates":{"normal":{"breakpoints":{"desktop":{"attributes":{}}},"isVisible":true,"isSelected":false},"hover":{"breakpoints":{"desktop":{"attributes":{"blockeraInnerBlocks":{"elements/link":{"attributes":{"blockeraBlockStates":{"normal":{"breakpoints":{"desktop":{"attributes":{}}},"isVisible":true,"isSelected":false},"hover":{"breakpoints":{"desktop":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}},"isVisible":true,"isSelected":true}}}}}}}},"isVisible":true,"isSelected":true}},"blockeraPropsId":"217145427439","blockeraCompatId":"217145427439"} -->\n' +
 				'<p class="blockera-block blockera-block-cfc3ffa9-2b93-4f98-a66a-511559797d7f">Test</p>\n' +
 				'<!-- /wp:paragraph -->'
 		);
@@ -182,7 +182,7 @@ describe('Should calculate current attributes correctly:', () => {
 
 	it('For Paragraph -> Hover -> Mobile -> Link -> Hover -> Mobile', () => {
 		appendBlocks(
-			'<!-- wp:paragraph {"className":"blockera-block blockera-block-e3c7c4e8-bd79-41c2-ade6-cd6dabdc0186","blockeraBlockStates":{"hover":{"breakpoints":{"mobile":{"attributes":{"blockeraInnerBlocks":{"link":{"attributes":{"blockeraBlockStates":{"hover":{"breakpoints":{"mobile":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}},"isVisible":true}}}}}}}},"isVisible":true}},"blockeraPropsId":"21714588632","blockeraCompatId":"21714588632"} -->\n' +
+			'<!-- wp:paragraph {"className":"blockera-block blockera-block-e3c7c4e8-bd79-41c2-ade6-cd6dabdc0186","blockeraBlockStates":{"hover":{"breakpoints":{"mobile":{"attributes":{"blockeraInnerBlocks":{"elements/link":{"attributes":{"blockeraBlockStates":{"hover":{"breakpoints":{"mobile":{"attributes":{"blockeraBackgroundColor":"#eeeeee"}}},"isVisible":true}}}}}}}},"isVisible":true}},"blockeraPropsId":"21714588632","blockeraCompatId":"21714588632"} -->\n' +
 				'<p class="blockera-block blockera-block-e3c7c4e8-bd79-41c2-ade6-cd6dabdc0186">Test</p>\n' +
 				'<!-- /wp:paragraph -->'
 		);
