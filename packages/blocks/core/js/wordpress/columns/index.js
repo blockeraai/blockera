d38d2ffab9aa547061935b1075bda6f068a410d9
Ali Aghdam <betteraliaghdam@gmail.com>
2024-08-02 09:40:32 +0330

    feat(Columns block): inner block settings + BlockType

--- packages/blocks/core/js/wordpress/columns/index.js
+++ packages/blocks/core/js/wordpress/columns/index.js
@@ -13,19 +13,23 @@ import { SharedBlockExtension } from '@blockera/editor';
 /**
  * Internal dependencies
  */
-import link from '../inners/link';
-import button from '../inners/button';
-import headings from '../inners/headings';
-import paragraph from '../inners/paragraph';
+import sharedInnerBlocks from '../inners/shared';
+import type { BlockType } from '../../type';
 
-export const Columns = {
+export const Columns: BlockType = {
 	name: 'blockeraColumns',
 	targetBlock: 'core/columns',
 	blockeraInnerBlocks: {
-		...link,
-		...paragraph,
-		...button,
-		...headings,
+		'core/heading': sharedInnerBlocks['core/heading'],
+		'core/paragraph': sharedInnerBlocks['core/paragraph'],
+		'elements/link': sharedInnerBlocks['elements/link'],
+		'core/button': sharedInnerBlocks['core/button'],
+		'core/heading-1': sharedInnerBlocks['core/heading-1'],
+		'core/heading-2': sharedInnerBlocks['core/heading-2'],
+		'core/heading-3': sharedInnerBlocks['core/heading-3'],
+		'core/heading-4': sharedInnerBlocks['core/heading-4'],
+		'core/heading-5': sharedInnerBlocks['core/heading-5'],
+		'core/heading-6': sharedInnerBlocks['core/heading-6'],
 	},
 	edit: (props: Object): MixedElement => {
 		return <SharedBlockExtension {...props} />;
