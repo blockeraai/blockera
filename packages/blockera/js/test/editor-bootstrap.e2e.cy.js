b5ecb98c5005ee39250e3e8a481c6f15bb02bacc
rezaelahidev <rezaelahi.dev@gmail.com>
2024-08-05 21:01:08 +0330

    test: added new test suite to check 3rd party plugin block types

--- packages/blockera/js/test/editor-bootstrap.e2e.cy.js
+++ packages/blockera/js/test/editor-bootstrap.e2e.cy.js
@@ -3,12 +3,18 @@
  */
 import {
 	createPost,
+	addBlockToPost,
+	getWPDataObject,
 	getWindowProperty,
+	getSelectedBlock,
+	getBlockType,
 } from '@blockera/dev-cypress/js/helpers';
 
 /**
  * Blockera editor bootstrap scenarios.
  * we should check ensure of added registration shared and specific block type attributes store apis is existing on blockerEditor_VERSION global variable.
+ *
+ * support 3rd party plugins block types. for example => 'woocommerce/product-price'
  */
 describe('Blockera editor bootstrapper', () => {
 	beforeEach(() => {
@@ -26,6 +32,26 @@ describe('Blockera editor bootstrapper', () => {
 				data.editor.hasOwnProperty(
 					'unstableRegistrationSharedBlockAttributes'
 				)
+			);
+		});
+	});
+
+	it('should added blockera supports on woocommerce/product-price', () => {
+		addBlockToPost('woocommerce/all-products');
+
+		cy.getBlock('woocommerce/all-products').click();
+
+		cy.getByAriaLabel(`Edit the layout of each product`).click();
+		cy.get(`[data-type="woocommerce/product-price"]`).click();
+
+		cy.getByAriaLabel('Add New Background').should('exist');
+
+		getWPDataObject().then((data) => {
+			expect(true).to.eq(
+				getBlockType(
+					data,
+					'woocommerce/product-price'
+				).attributes.hasOwnProperty('blockeraPropsId')
 			);
 		});
 	});
