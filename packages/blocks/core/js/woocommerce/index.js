232491590dc96bcd7bb309f67e14c94e803d2774
Ali Aghdam <betteraliaghdam@gmail.com>
2024-08-06 21:44:03 +0330

    feat(Product Results Count Block): support

--- packages/blocks/core/js/woocommerce/index.js
+++ packages/blocks/core/js/woocommerce/index.js
@@ -3,5 +3,6 @@
 export * from './product-button';
 export * from './product-image';
 export * from './product-price';
+export * from './product-results-count';
 export * from './product-title';
 export * from './store-notices';
