5b5fdbe2d38963aa533aec82d615766ce8ed11f7
Ali Aghdam <betteraliaghdam@gmail.com>
2024-08-07 19:18:09 +0330

    feat(Order Totals Section Block): support

--- packages/blocks/core/js/woocommerce/index.js
+++ packages/blocks/core/js/woocommerce/index.js
@@ -4,6 +4,7 @@ export * from './catalog-sorting';
 export * from './order-confirmation-status';
 export * from './order-confirmation-summary';
 export * from './order-confirmation-totals';
+export * from './order-confirmation-totals-wrapper';
 export * from './product-button';
 export * from './product-image';
 export * from './product-price';
