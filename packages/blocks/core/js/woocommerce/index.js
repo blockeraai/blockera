62920ebc6ee5433e59ab5faf0b5e2ea8e6b80799
Ali Aghdam <betteraliaghdam@gmail.com>
2024-08-07 19:26:52 +0330

    feat(Order Downloads Block): support

--- packages/blocks/core/js/woocommerce/index.js
+++ packages/blocks/core/js/woocommerce/index.js
@@ -1,6 +1,7 @@
 // @flow
 
 export * from './catalog-sorting';
+export * from './order-confirmation-downloads';
 export * from './order-confirmation-downloads-wrapper';
 export * from './order-confirmation-status';
 export * from './order-confirmation-summary';
