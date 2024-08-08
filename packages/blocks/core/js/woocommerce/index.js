498ac5b99c12094ff96387508505c68635773e95
Ali Aghdam <betteraliaghdam@gmail.com>
2024-08-08 05:20:06 +0330

    feat(Product Categories List Block): support

--- packages/blocks/core/js/woocommerce/index.js
+++ packages/blocks/core/js/woocommerce/index.js
@@ -53,6 +53,7 @@ export * from './order-confirmation-totals-wrapper';
 export * from './page-content-wrapper';
 export * from './proceed-to-checkout-block';
 export * from './product-button';
+export * from './product-categories';
 export * from './product-details';
 export * from './product-image';
 export * from './product-image-gallery';
