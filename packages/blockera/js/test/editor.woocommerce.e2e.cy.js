8887241c661f647c5580de8d9484b61716f878a7
Ali Aghdam <betteraliaghdam@gmail.com>
2024-08-14 10:06:06 +0330

    test: WooCommerce test extracted to separate test

---
+++ packages/blockera/js/test/editor.woocommerce.e2e.cy.js
@@ -0,0 +1,45 @@
+/**
+ * Blockera dependencies
+ */
+import {
+	createPost,
+	getBlockType,
+	addBlockToPost,
+	getWPDataObject,
+	getWindowProperty,
+} from '@blockera/dev-cypress/js/helpers';
+
+/**
+ * Blockera editor bootstrap scenarios.
+ * we should check ensure of added registration shared and specific block type attributes store apis is existing on blockerEditor_VERSION global variable.
+ *
+ * support 3rd party plugins block types. for example => 'woocommerce/product-price'
+ */
+describe('Blockera editor bootstrapper', () => {
+	it('should added blockera supports on woocommerce/product-price', () => {
+		createPost({ postType: 'product' });
+
+		cy.get('input[name="post_title"]').type('Cap', { delay: 0 });
+		cy.get('input#publish').click();
+
+		createPost();
+
+		addBlockToPost('woocommerce/all-products');
+
+		cy.getBlock('woocommerce/all-products').click();
+
+		cy.getByAriaLabel(`Edit the layout of each product`).click();
+		cy.get(`[data-type="woocommerce/product-price"]`).click();
+
+		cy.getByAriaLabel('Add New Background').should('exist');
+
+		getWPDataObject().then((data) => {
+			expect(true).to.eq(
+				getBlockType(
+					data,
+					'woocommerce/product-price'
+				).attributes.hasOwnProperty('blockeraPropsId')
+			);
+		});
+	});
+});
