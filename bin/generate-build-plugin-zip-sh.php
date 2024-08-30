3546f53bf8736192554c3fbf29c7ba6cecc79fd3
rezaelahidev <rezaelahi.dev@gmail.com>
2024-08-30 10:56:35 +0330

    refactor: auto generator build-plugin-zip bash script

--- bin/generate-build-plugin-zip-sh.php
+++ bin/generate-build-plugin-zip-sh.php
@@ -7,103 +7,104 @@
  * @package blockera-build
  */
 
-$f = fopen(dirname(__DIR__) . '/bin/build-plugin-zip.sh', 'r');
+$f = fopen( dirname( __DIR__ ) . '/bin/build-plugin-zip.sh', 'r' );
 
 $packages = array_map(
-    function (string $package_name) {
-        $core_suffix = '-core';
+	function ( string $package_name ) {
 
-        $package_name = str_replace(dirname(__DIR__) . '/packages/', '', $package_name);
+		$core_suffix = '-core';
 
-        if('blocks' === $package_name) {
-            $package_name .= '-core';
-        }
+		$package_name = str_replace( dirname( __DIR__ ) . '/packages/', '', $package_name );
 
-        return $package_name;
-    },
-    array_filter(
-        glob(dirname(__DIR__) . '/packages/*'),
-        function (string $package_name): string {
-            // filter dev tools packages.
-            if(preg_match('/dev-(.*)/', $package_name)) {
+		if ( 'blocks' === $package_name ) {
+			$package_name .= '-core';
+		}
 
-                return false;
-            }
+		return $package_name;
+	},
+	array_filter(
+		glob( dirname( __DIR__ ) . '/packages/*' ),
+		function ( string $package_name ): string {
 
-            // filter invalid packages.
-            if(!is_dir($package_name . '/php') && !is_dir($package_name . '/core/php')) {
+			// filter dev tools packages.
+			if ( preg_match( '/dev-(.*)/', $package_name ) ) {
 
-                return false;
-            }
+				return false;
+			}
 
-            return true;
-        }
-    )
+			// filter invalid packages.
+			if ( ! is_dir( $package_name . '/php' ) && ! is_dir( $package_name . '/core/php' ) ) {
+
+				return false;
+			}
+
+			return true;
+		}
+	)
 );
 
 $internal_packages = array_filter(
-    $packages,
-    function (string $package_name): string {
-        if(preg_match('/-sdk$/', $package_name)) {
-            return false;
-        }
+	$packages,
+	function ( string $package_name ): string {
 
-        return true;
-    }
+		if ( preg_match( '/-sdk$/', $package_name ) ) {
+			return false;
+		}
+
+		return true;
+	}
 );
 
-$sdks = array_diff($packages, $internal_packages);
+$sdks = array_diff( $packages, $internal_packages );
 
 $inside_pattern_block = false;
 
-while (true) {
-    $line = fgets($f);
-    if (false === $line) {
-        break;
-    }
+while ( true ) {
+	$line = fgets( $f );
+	if ( false === $line ) {
+		break;
+	}
 
-    switch (trim($line)) {
+	switch ( trim( $line ) ) {
 
-        case '### END AUTO-GENERATED VENDOR PACKAGES PATH PATTERN':
-            $inside_pattern_block = false;
-            echo $line;
-            break;
+		case '### END AUTO-GENERATED VENDOR PACKAGES PATH PATTERN':
+			$inside_pattern_block = false;
+			break;
 
-        case '### BEGIN AUTO-GENERATED VENDOR PACKAGES PATH PATTERN':
-            $inside_pattern_block = true;
+		case '### BEGIN AUTO-GENERATED VENDOR PACKAGES PATH PATTERN':
+			$inside_pattern_block = true;
 
-            echo $line;
+			echo implode( PHP_EOL, array_map( function ( string $name ): string {
+				return sprintf(
+					'	$(find ./vendor/blockera/%1$s/ -type f \( -name "*.php" -o -name "*.json" \)) \\',
+					$name
+				);
+			}, $internal_packages ) );
 
-            echo implode(PHP_EOL, array_map(function (string $name): string {
-                return sprintf(
-                    '	$(find ./vendor/blockera/%1$s/ -type f \( -name "*.php" -o -name "*.json" \)) \\',
-                    $name
-                );
-            }, $internal_packages));
+			echo PHP_EOL;
 
-            echo PHP_EOL;
+			echo implode(
+				PHP_EOL,
+				array_map( function ( string $name ): string {
 
-            echo implode(
-                PHP_EOL,
-                array_map(function (string $name): string {
-                    return sprintf(
-                        '	$(find ./vendor/blockera/%1$s/) \\',
-                        $name
-                    );
-                }, $sdks)
-            );
+					return sprintf(
+						'	$(find ./vendor/blockera/%1$s/) \\',
+						$name
+					);
+				}, $sdks )
+			);
 
-            echo PHP_EOL;
+			echo PHP_EOL;
 
-            break;
+			break;
 
-        default:
-            if(!$inside_pattern_block) {
+		default:
+			if ( ! $inside_pattern_block ) {
 
-                echo $line;
-            }
-            break;
-    }
+				echo $line;
+			}
+			break;
+	}
 }
 
-fclose($f);
+fclose( $f );
