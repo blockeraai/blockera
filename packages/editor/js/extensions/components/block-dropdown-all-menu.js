a67473d602313d5e634686b466a04c58db87a6c1
rezaelahidev <rezaelahi.dev@gmail.com>
2024-09-05 01:57:27 +0330

    feat: implements BlockApp component, provider, and hooks
    # Conflicts:
    #	packages/editor/js/extensions/components/block-app.js
    #	packages/editor/js/extensions/components/block-dropdown-all-menu.js
    #	packages/editor/js/extensions/hooks/block-settings.js

--- packages/editor/js/extensions/components/block-dropdown-all-menu.js
+++ packages/editor/js/extensions/components/block-dropdown-all-menu.js
@@ -16,6 +16,11 @@ import { Flex } from '@blockera/controls';
 import { Icon } from '@blockera/icons';
 import { classNames } from '@blockera/classnames';
 
+/**
+ * Internal dependencies
+ */
+import { useBlockSections } from './block-app';
+
 export const BlockDropdownAllMenu = ({
 	isActive,
 	setActive,
@@ -23,6 +28,9 @@ export const BlockDropdownAllMenu = ({
 	isActive: boolean,
 	setActive: (isActive: boolean) => void,
 }): MixedElement => {
+	const { blockSections, updateBlockSections } = useBlockSections();
+	const { expandAll, collapseAll, focusMode } = blockSections;
+
 	const { setBlockExtensionsStatus } = dispatch('blockera/extensions');
 
 	return (
@@ -37,53 +45,140 @@ export const BlockDropdownAllMenu = ({
 		>
 			{({ onClose }) => {
 				return (
-					<MenuGroup label={__('Block Settings', 'blockera')}>
-						<MenuItem
-							data-test={'advanced-mode-block'}
-							icon={isActive ? check : ''}
-							onClick={() => {
-								setBlockExtensionsStatus(!isActive);
-								setActive(!isActive);
-								onClose();
-							}}
-							className={classNames({
-								'blockera-block-menu-item': true,
-								'blockera-block-menu-item-selected': isActive,
-							})}
-						>
-							<Flex alignItems="center" gap="10px">
-								<Icon
-									library="blockera"
-									icon="blockera"
-									iconSize="18"
-								/>
-								{__('Advanced Mode', 'blockera')}
-							</Flex>
-						</MenuItem>
+					<>
+						<MenuGroup label={__('Block Settings', 'blockera')}>
+							<MenuItem
+								data-test={'advanced-mode-block'}
+								icon={isActive ? check : ''}
+								onClick={() => {
+									setBlockExtensionsStatus(!isActive);
+									setActive(!isActive);
+									onClose();
+								}}
+								className={classNames({
+									'blockera-block-menu-item': true,
+									'blockera-block-menu-item-selected':
+										isActive,
+								})}
+							>
+								<Flex alignItems="center" gap="10px">
+									<Icon
+										library="blockera"
+										icon="blockera"
+										iconSize="18"
+									/>
+									{__('Advanced Mode', 'blockera')}
+								</Flex>
+							</MenuItem>
 
-						<MenuItem
-							data-test={'basic-mode-block'}
-							icon={isActive ? '' : check}
-							onClick={() => {
-								setBlockExtensionsStatus(!isActive);
-								setActive(!isActive);
-								onClose();
-							}}
-							className={classNames({
-								'blockera-block-menu-item': true,
-								'blockera-block-menu-item-selected': !isActive,
-							})}
-						>
-							<Flex alignItems="center" gap="10px">
-								<Icon
-									library="wp"
-									icon="wordpress"
-									iconSize="18"
-								/>
-								{__('Basic Mode', 'blockera')}
-							</Flex>
-						</MenuItem>
-					</MenuGroup>
+							<MenuItem
+								data-test={'basic-mode-block'}
+								icon={isActive ? '' : check}
+								onClick={() => {
+									setBlockExtensionsStatus(!isActive);
+									setActive(!isActive);
+									onClose();
+								}}
+								className={classNames({
+									'blockera-block-menu-item': true,
+									'blockera-block-menu-item-selected':
+										!isActive,
+								})}
+							>
+								<Flex alignItems="center" gap="10px">
+									<Icon
+										library="wp"
+										icon="wordpress"
+										iconSize="18"
+									/>
+									{__('Basic Mode', 'blockera')}
+								</Flex>
+							</MenuItem>
+						</MenuGroup>
+						<MenuGroup label={__('Block Sections', 'blockera')}>
+							<MenuItem
+								data-test={'Expand All'}
+								icon={expandAll ? check : ''}
+								onClick={() => {
+									updateBlockSections({
+										focusMode: false,
+										collapseAll: false,
+										expandAll: !expandAll,
+									});
+									setActive(!expandAll);
+									onClose();
+								}}
+								className={classNames({
+									'blockera-block-menu-item': true,
+									'blockera-block-menu-item-selected':
+										expandAll,
+								})}
+							>
+								<Flex alignItems="center" gap="10px">
+									<Icon
+										library="ui"
+										icon="expand-all"
+										iconSize="18"
+									/>
+									{__('Expand All', 'blockera')}
+								</Flex>
+							</MenuItem>
+							<MenuItem
+								data-test={'Collapse All'}
+								icon={collapseAll ? check : ''}
+								onClick={() => {
+									updateBlockSections({
+										expandAll: false,
+										focusMode: false,
+										collapseAll: !collapseAll,
+									});
+									setActive(!collapseAll);
+									onClose();
+								}}
+								className={classNames({
+									'blockera-block-menu-item': true,
+									'blockera-block-menu-item-selected':
+										collapseAll,
+								})}
+							>
+								<Flex alignItems="center" gap="10px">
+									<Icon
+										library="ui"
+										icon="collapse-all"
+										iconSize="18"
+									/>
+									{__('Collapse All', 'blockera')}
+								</Flex>
+							</MenuItem>
+							<MenuItem
+								data-test={'Focus Mode'}
+								icon={focusMode ? check : ''}
+								onClick={() => {
+									updateBlockSections({
+										expandAll: false,
+										collapseAll: false,
+										focusMode: !focusMode,
+									});
+									setActive(!focusMode);
+									onClose();
+								}}
+								className={classNames({
+									'blockera-block-menu-item': true,
+									'blockera-block-menu-item-selected':
+										focusMode,
+								})}
+							>
+								<Flex alignItems="center" gap="10px">
+									<Icon
+										library="ui"
+										icon="focus-mode"
+										iconSize="18"
+									/>
+									{__('Focus Mode', 'blockera')}
+								</Flex>
+							</MenuItem>
+						</MenuGroup>
+					</>
 				);
 			}}
 		</DropdownMenu>
