d5d0a6842e139d0f9ee86f5ae892163e9caa5808
Ali Aghdam <betteraliaghdam@gmail.com>
2024-09-10 18:04:26 +0330

    test: add new test case for nested items + dynamic selector change in style engine

--- packages/blocks/core/js/wordpress/group/test/gap-type-gap-and-margin.e2e.cy.js
+++ packages/blocks/core/js/wordpress/group/test/gap-type-gap-and-margin.e2e.cy.js
@@ -342,6 +342,189 @@ describe('Gap → Functionality (Type: gap-and-margin)', () => {
 				.should('not.have.css', 'align-items', 'flex-start')
 				.should('not.have.css', 'justify-content', 'flex-start');
 		});
+
+		it('Nested group + dynamic change in CSS selector - the css property should be gap', () => {
+			appendBlocks(
+				`<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Paragraph 1</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Paragraph 2</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>Paragraph 3</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Paragraph 4</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+</div>
+<!-- /wp:group -->`
+			);
+
+			//
+			// Parent Group
+			//
+
+			// Select target block
+			cy.getBlock('core/paragraph').first().click();
+
+			// Switch to parent block
+			cy.getByAriaLabel('Select parent block: Group').click();
+
+			// switch to style tab
+			cy.getByDataTest('style-tab').click();
+
+			cy.getParentContainer('Display').within(() => {
+				cy.getByAriaLabel('Flex').click();
+			});
+
+			cy.getParentContainer('Flex Layout').within(() => {
+				cy.getByAriaLabel('Row').click();
+			});
+
+			cy.getByDataTest('matrix-top-left-normal').click();
+
+			cy.getParentContainer('Gap').within(() => {
+				cy.get('input').type(100);
+			});
+
+			//
+			// Asset group block CSS
+			//
+			cy.getBlock('core/group').should('have.css', 'gap', '100px');
+
+			//
+			// Assert child block to have valid property for gap
+			//
+
+			cy.getBlock('core/paragraph')
+				.eq(0)
+				.should('have.css', 'margin-block-start', '0px')
+				.should('not.have.css', 'flex-direction', 'column')
+				.should('not.have.css', 'align-items', 'flex-start')
+				.should('not.have.css', 'justify-content', 'flex-start');
+
+			cy.getBlock('core/paragraph')
+				.eq(1)
+				.should('have.css', 'margin-block-start', '0px')
+				.should('not.have.css', 'flex-direction', 'column')
+				.should('not.have.css', 'align-items', 'flex-start')
+				.should('not.have.css', 'justify-content', 'flex-start');
+
+			//
+			// Inner Group
+			//
+
+			cy.getBlock('core/paragraph').eq(3).click();
+
+			// Switch to parent block
+			cy.getByAriaLabel('Select parent block: Group').click();
+
+			// switch to style tab
+			cy.getByDataTest('style-tab').click();
+
+			cy.getParentContainer('Display').within(() => {
+				cy.getByAriaLabel('Flex').click();
+			});
+
+			cy.getParentContainer('Flex Layout').within(() => {
+				cy.getByAriaLabel('Row').click();
+			});
+
+			cy.getByDataTest('matrix-top-right-normal').click();
+
+			cy.getParentContainer('Gap').within(() => {
+				cy.get('input').type(200);
+			});
+
+			//
+			// Asset group block CSS
+			//
+			cy.getBlock('core/group').eq(1).should('have.css', 'gap', '200px');
+
+			//
+			// Assert child block to have valid property for gap
+			//
+
+			cy.getBlock('core/paragraph')
+				.eq(2)
+				.should('have.css', 'margin-block-start', '0px')
+				.should('not.have.css', 'flex-direction', 'column')
+				.should('not.have.css', 'align-items', 'flex-start')
+				.should('not.have.css', 'justify-content', 'flex-start');
+
+			cy.getBlock('core/paragraph')
+				.eq(3)
+				.should('have.css', 'margin-block-start', '0px')
+				.should('not.have.css', 'flex-direction', 'column')
+				.should('not.have.css', 'align-items', 'flex-start')
+				.should('not.have.css', 'justify-content', 'flex-end');
+
+			//Check frontend
+			savePage();
+
+			redirectToFrontPage();
+
+			//
+			// Asset group block CSS
+			//
+
+			//
+			// Parent Group
+			//
+
+			cy.get('.wp-block-group.blockera-block')
+				.eq(0)
+				.should('have.css', 'gap', '100px');
+
+			//
+			// Assert child block to have valid property for gap
+			//
+			cy.get('.wp-block-group.blockera-block p')
+				.eq(0)
+				.should('have.css', 'margin-block-start', '0px')
+				.should('not.have.css', 'flex-direction', 'column')
+				.should('not.have.css', 'align-items', 'flex-start')
+				.should('not.have.css', 'justify-content', 'flex-start');
+
+			cy.get('.wp-block-group.blockera-block p')
+				.eq(1)
+				.should('have.css', 'margin-block-start', '0px')
+				.should('not.have.css', 'flex-direction', 'column')
+				.should('not.have.css', 'align-items', 'flex-start')
+				.should('not.have.css', 'justify-content', 'flex-start');
+
+			//
+			// Inner Group
+			//
+
+			cy.get('.wp-block-group.blockera-block')
+				.eq(0)
+				.should('have.css', 'gap', '200px');
+
+			//
+			// Assert child block to have valid property for gap
+			//
+			cy.get('.wp-block-group.blockera-block p')
+				.eq(2)
+				.should('have.css', 'margin-block-start', '0px')
+				.should('not.have.css', 'flex-direction', 'column')
+				.should('not.have.css', 'align-items', 'flex-start')
+				.should('not.have.css', 'justify-content', 'flex-end');
+
+			cy.get('.wp-block-group.blockera-block p')
+				.eq(3)
+				.should('have.css', 'margin-block-start', '0px')
+				.should('not.have.css', 'flex-direction', 'column')
+				.should('not.have.css', 'align-items', 'flex-start')
+				.should('not.have.css', 'justify-content', 'flex-end');
+		});
 	});
 
 	context('Variation: Stack', () => {
