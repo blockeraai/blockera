f14cb9cba95ec83d910cac24c4deab83f2c636ef
Ali Aghdam <betteraliaghdam@gmail.com>
2024-09-18 11:59:39 +0330

    test: refactor tests to make sure it waits for ajax call done

--- packages/blocks/core/js/wordpress/navigation-submenu/test/block.blocks.e2e.cy.js
+++ packages/blocks/core/js/wordpress/navigation-submenu/test/block.blocks.e2e.cy.js
@@ -7,57 +7,74 @@ import {
 	openInnerBlocksExtension,
 } from '@blockera/dev-cypress/js/helpers';
 
-describe('Navigation Link Block', () => {
-	beforeEach(() => {
-		createPost();
-	});
-
-	it('Block should be supported + switch to parent should work', () => {
-		appendBlocks('<!-- wp:navigation /-->');
-
-		cy.getBlock('core/navigation').click();
-
-		// Open blocks menu
-		cy.get('[aria-label="Add block"]').first().click();
-
-		// search for custom link
-		cy.get('input[type="search"]:focus').last().type('Submenu');
-
-		// insert
-		cy.get('button.editor-block-list-item-navigation-submenu')
-			.last()
-			.click();
-
-		// enter link value
-		cy.get('input[type="text"]:focus').type('#test{enter}');
-
-		// switch to target block
-		cy.getBlock('core/navigation-submenu').first().click();
-
-		// assert block card
-		cy.get('.blockera-extension-block-card.master-block-card').should(
-			'exist'
-		);
-
-		// switch to parent navigation block
-		cy.get('.blockera-extension-block-card.master-block-card').within(
-			() => {
-				cy.get('button[data-test="back-to-parent-navigation"]').should(
-					'exist'
-				);
-				cy.get('button[data-test="back-to-parent-navigation"]').click();
-			}
-		);
-
-		//
-		// Assert block switched to parent navigation block
-		//
-		cy.get('.blockera-extension-block-card.master-block-card').should(
-			'not.exist'
-		);
-		cy.get('.block-editor-block-card').should('exist');
-		cy.get('.block-editor-block-card').within(() => {
-			cy.get('.block-editor-block-card__title').contains('Navigation');
+describe(
+	'Navigation Submenu Block',
+	{
+		defaultCommandTimeout: 50000,
+	},
+	() => {
+		beforeEach(() => {
+			createPost();
 		});
-	});
-});
+
+		it('Block should be supported + switch to parent should work', () => {
+			appendBlocks('<!-- wp:navigation /-->');
+
+			cy.getBlock('core/navigation').click();
+
+			// Make sure the tree is visible (Ajax call done)
+			cy.get('.block-editor-list-view-tree').should('be.visible');
+
+			cy.get('.block-editor-list-view-tree').within(() => {
+				// Open blocks menu
+				cy.get('[aria-label="Add block"]').first().click();
+			});
+
+			// search for Submenu
+			cy.get('.block-editor-inserter__popover input[type="search"]').type(
+				'Submenu'
+			);
+
+			// insert
+			cy.get('button.editor-block-list-item-navigation-submenu')
+				.last()
+				.click();
+
+			// enter link value
+			cy.get('input[type="text"]:focus').type('#test{enter}');
+
+			// switch to target block
+			cy.getBlock('core/navigation-submenu').first().click();
+
+			// assert block card
+			cy.get('.blockera-extension-block-card.master-block-card').should(
+				'exist'
+			);
+
+			// switch to parent navigation block
+			cy.get('.blockera-extension-block-card.master-block-card').within(
+				() => {
+					cy.get(
+						'button[data-test="back-to-parent-navigation"]'
+					).should('exist');
+					cy.get(
+						'button[data-test="back-to-parent-navigation"]'
+					).click();
+				}
+			);
+
+			//
+			// Assert block switched to parent navigation block
+			//
+			cy.get('.blockera-extension-block-card.master-block-card').should(
+				'not.exist'
+			);
+			cy.get('.block-editor-block-card').should('exist');
+			cy.get('.block-editor-block-card').within(() => {
+				cy.get('.block-editor-block-card__title').contains(
+					'Navigation'
+				);
+			});
+		});
+	}
+);
