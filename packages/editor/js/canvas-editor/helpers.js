4a40007cca4ea27ef9810632887b7d83075d6c68
rezaelahidev <rezaelahi.dev@gmail.com>
2024-10-19 11:07:29 +0330

    test: add new test suits for canvas editor for rendering breakpoints

--- packages/editor/js/canvas-editor/helpers.js
+++ packages/editor/js/canvas-editor/helpers.js
@@ -11,9 +11,9 @@ import { isLoadedSiteEditor } from '@blockera/utils';
 import type { GetTarget } from './types';
 
 // Compatibility for WordPress supported versions.
-export const getTarget = (version: string): GetTarget => {
+export const getTargets = (version: string): GetTarget => {
 	// Regular expression to detect wp all alpha or beta versioning pattern.
-	const regexp = /(\d+\.\d+\.\d+)-.*/gi;
+	const regexp = /(\d+\.\d+(|\.\d+))-.*/gi;
 	const matches = regexp.exec(version);
 
 	if (matches && matches.length > 1) {
@@ -21,14 +21,23 @@ export const getTarget = (version: string): GetTarget => {
 		version = matches[1];
 	}
 
+	const dotsMatches = /\./g.exec(version);
+	version = version?.replace(/\./g, '');
+
+	const targets = {
+		header: '.editor-header__center',
+		previewDropdown:
+			'.editor-preview-dropdown, a.components-button[aria-label="View Post"], a.components-button[aria-label="View Page"]',
+		postPreviewElement: 'a[aria-label="View Post"]',
+	};
+
 	// For WordPress version equals or bigger than 6.6.1 version.
-	if (Number(version?.replace(/\./g, '')) >= 661) {
-		return {
-			header: '.editor-header__center',
-			previewDropdown:
-				'.editor-preview-dropdown, a.components-button[aria-label="View Post"], a.components-button[aria-label="View Page"]',
-			postPreviewElement: 'a[aria-label="View Post"]',
-		};
+	if (dotsMatches) {
+		if (dotsMatches.length > 2 && Number(version) >= 661) {
+			return targets;
+		} else if (Number(version) > 66) {
+			return targets;
+		}
 	}
 
 	// For less than WordPress 6.6.1 versions.
