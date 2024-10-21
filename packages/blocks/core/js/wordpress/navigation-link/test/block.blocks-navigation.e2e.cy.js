cd083d45b149c5ac6a0ff225983459a686ce3a16
Ali Aghdam <betteraliaghdam@gmail.com>
2024-10-21 16:46:53 +0330

    test(Navigation link block): inner block existence test

--- packages/blocks/core/js/wordpress/navigation-link/test/block.blocks-navigation.e2e.cy.js
+++ packages/blocks/core/js/wordpress/navigation-link/test/block.blocks-navigation.e2e.cy.js
@@ -17,6 +17,57 @@ describe(
 			createPost();
 		});
 
+		it('Inner blocks existence', () => {
+			appendBlocks('<!-- wp:navigation /-->');
+
+			cy.getBlock('core/navigation').click();
+
+			// Make sure the tree is visible (Ajax call done)
+			cy.get('.block-editor-list-view-tree').should('be.visible');
+
+			cy.get('.block-editor-list-view-tree').within(() => {
+				// Open blocks menu
+				cy.get('[aria-label="Add block"]').first().click();
+			});
+
+			// search for custom link
+			cy.get('.block-editor-inserter__popover input[type="search"]').type(
+				'Custom Link'
+			);
+
+			// insert
+			cy.get('button.editor-block-list-item-navigation-link')
+				.last()
+				.click();
+
+			// enter link value
+			cy.get('input[type="text"]:focus').type('#test{enter}');
+
+			// switch to target block
+			cy.getBlock('core/navigation-link').first().click();
+
+			// assert block card
+			cy.get('.blockera-extension-block-card.master-block-card').should(
+				'exist'
+			);
+
+			//
+			// 1. Inner blocks existence
+			//
+
+			// open inner block settings
+			openInnerBlocksExtension();
+
+			cy.get(
+				'.blockera-extension.blockera-extension-inner-blocks'
+			).within(() => {
+				cy.getByDataTest('elements/link').should('exist');
+
+				// no other item
+				cy.getByDataTest('core/paragraph').should('not.exist');
+			});
+		});
+
 		it('Block should be supported + switch to parent should work', () => {
 			appendBlocks('<!-- wp:navigation /-->');
 
