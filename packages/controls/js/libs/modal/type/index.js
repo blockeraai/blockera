0439c8f8bd229cf8ac194e25ffe4f96799768d17
Ali Aghdam <betteraliaghdam@gmail.com>
2024-10-27 15:43:40 +0330

    feat(Modal Component): add prop types

---
+++ packages/controls/js/libs/modal/type/index.js
@@ -0,0 +1,12 @@
+//@flow
+
+export type ModalProps = {
+	children?: any,
+	visible?: boolean,
+	onClose?: () => void,
+	onRequestClose?: () => void,
+	headerIcon?: any,
+	headerTitle?: string,
+	className?: string,
+	size?: 'small' | 'medium' | 'large' | 'fill',
+};
