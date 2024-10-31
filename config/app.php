77eb7a31cdd40affc0a29a19979c5ffdb2ff4d27
rezaelahidev <rezaelahi.dev@gmail.com>
2024-10-31 14:35:51 +0330

    feat: add the dashboard_page value in app config

--- config/app.php
+++ config/app.php
@@ -13,25 +13,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 $env_mode = 'development' === blockera_core_env( 'APP_MODE', 'production' );
 
 return [
-	'root_url'      => BLOCKERA_CORE_URI,
-	'root_path'     => BLOCKERA_CORE_PATH,
-	'name'          => 'blockera',
-	'dist_url'      => BLOCKERA_CORE_URI . 'dist/',
-	'dist_path'     => BLOCKERA_CORE_PATH . 'dist/',
-	'packages_url'  => BLOCKERA_CORE_URI . 'packages/',
-	'packages_path' => blockera_core_env( 'APP_MODE', 'production' ) === 'development' ? BLOCKERA_CORE_PATH . 'packages/' : BLOCKERA_CORE_PATH . 'vendor/',
-	'vendor_path'   => BLOCKERA_CORE_PATH . 'vendor/',
-	'version'       => defined( 'BLOCKERA_VERSION' ) ? BLOCKERA_VERSION : blockera_core_env( 'VERSION' ),
-	'namespaces'    => [
+	'root_url'       => BLOCKERA_CORE_URI,
+	'root_path'      => BLOCKERA_CORE_PATH,
+	'name'           => 'blockera',
+	'dashboard_page' => 'blockera-settings-dashboard',
+	'dist_url'       => BLOCKERA_CORE_URI . 'dist/',
+	'dist_path'      => BLOCKERA_CORE_PATH . 'dist/',
+	'packages_url'   => BLOCKERA_CORE_URI . 'packages/',
+	'packages_path'  => blockera_core_env( 'APP_MODE', 'production' ) === 'development' ? BLOCKERA_CORE_PATH . 'packages/' : BLOCKERA_CORE_PATH . 'vendor/',
+	'vendor_path'    => BLOCKERA_CORE_PATH . 'vendor/',
+	'version'        => defined( 'BLOCKERA_VERSION' ) ? BLOCKERA_VERSION : blockera_core_env( 'VERSION' ),
+	'namespaces'     => [
 		'controllers' => '\Blockera\Setup\Http\Controllers\\',
 	],
-	'debug'         => defined( 'BLOCKERA_APP_MODE' ) && 'development' === BLOCKERA_APP_MODE && $env_mode,
+	'debug'          => defined( 'BLOCKERA_APP_MODE' ) && 'development' === BLOCKERA_APP_MODE && $env_mode,
 	/**
 	 * Extendable blockera application providers by external developers.
 	 *
 	 * @since 1.0.0
 	 */
-	'providers'     => apply_filters(
+	'providers'      => apply_filters(
 		'blockera.application.providers',
 		[
 			\Blockera\Admin\Providers\AdminProvider::class,
