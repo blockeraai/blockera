bb45336e9754be602ef649588a1e39f7893db20d
rezaelahidev <rezaelahi.dev@gmail.com>
2025-01-18 14:52:13 +0330

    fix: wrong condition

--- packages/blockera/php/app.php
+++ packages/blockera/php/app.php
@@ -16,7 +16,7 @@ global $blockera;
 $blockera = new \Blockera\Setup\Blockera();
 
 // LOADING: other bootstrap files ...
-if ( ! defined( 'BLOCKERA_APP_MODE' ) && 'development' === BLOCKERA_APP_MODE ) {
+if ( defined( 'BLOCKERA_APP_MODE' ) && 'development' === BLOCKERA_APP_MODE ) {
 
 	// Experimental filter for variables.
 	blockera_load( 'hooks', __DIR__ );
