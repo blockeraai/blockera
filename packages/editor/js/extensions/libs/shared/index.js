2163b631d92b79f4f2a29c3a3f153327a4a63ebf
Ali Aghdam <betteraliaghdam@gmail.com>
2025-01-27 18:29:01 +0330

    feat: rename block settings tab to general

--- packages/editor/js/extensions/libs/shared/index.js
+++ packages/editor/js/extensions/libs/shared/index.js
@@ -967,8 +967,8 @@ export const SharedBlockExtension: ComponentType<Props> = memo(
 		const tabs = [
 			{
 				name: 'settings',
-				title: __('Settings', 'blockera'),
-				tooltip: __('Block Settings', 'blockera'),
+				title: __('General', 'blockera'),
+				tooltip: __('General Block Settings', 'blockera'),
 				className: 'settings-tab',
 				icon: <Icon icon="gear" iconSize="20" />,
 			},
