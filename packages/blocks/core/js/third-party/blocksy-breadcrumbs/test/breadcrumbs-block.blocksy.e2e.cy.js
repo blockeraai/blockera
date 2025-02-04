6829b3040b875c2edd89c83e57d77eb01b2d9fc9
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-04 15:07:24 +0330

    test(Blocksy Breadcrumbs Block): text inner block style tests

--- packages/blocks/core/js/third-party/blocksy-breadcrumbs/test/breadcrumbs-block.blocksy.e2e.cy.js
+++ packages/blocks/core/js/third-party/blocksy-breadcrumbs/test/breadcrumbs-block.blocksy.e2e.cy.js
@@ -2,12 +2,13 @@
  * Blockera dependencies
  */
 import {
+	savePage,
+	redirectToFrontPage,
 	appendBlocks,
 	getSelectedBlock,
 	getWPDataObject,
 	createPost,
 	setInnerBlock,
-	setParentBlock,
 } from '@blockera/dev-cypress/js/helpers';
 
 describe('Blocksy → Breadcrumbs Block → WP Compatibility', () => {
@@ -16,194 +17,262 @@ describe('Blocksy → Breadcrumbs Block → WP Compatibility', () => {
 	});
 
 	describe('Text Color Inner Block', () => {
-		it('Simple Value', () => {
-			appendBlocks(
-				`<!-- wp:blocksy/breadcrumbs {"customTextColor":"#ff0000"} /-->`
-			);
-
-			// Select target block
-			cy.getBlock('blocksy/breadcrumbs').first().click();
-
-			//
-			// Test 1: WP data to Blockera
-			//
-
-			// WP data should come to Blockera
-			getWPDataObject().then((data) => {
-				expect(undefined).to.be.equal(
-					getSelectedBlock(data, 'textColor')
+		describe('Data Compatibility', () => {
+			it('Simple Value', () => {
+				appendBlocks(
+					`<!-- wp:blocksy/breadcrumbs {"customTextColor":"#ff0000"} /-->`
 				);
 
-				expect('#ff0000').to.be.equal(
-					getSelectedBlock(data, 'customTextColor')
-				);
+				// Select target block
+				cy.getBlock('blocksy/breadcrumbs').first().click();
 
-				expect('#ff0000').to.be.equal(
-					getSelectedBlock(data, 'blockeraInnerBlocks')[
-						'elements/text'
-					]?.attributes?.blockeraFontColor
-				);
+				//
+				// Test 1: WP data to Blockera
+				//
+
+				// WP data should come to Blockera
+				getWPDataObject().then((data) => {
+					expect(undefined).to.be.equal(
+						getSelectedBlock(data, 'textColor')
+					);
+
+					expect('#ff0000').to.be.equal(
+						getSelectedBlock(data, 'customTextColor')
+					);
+
+					expect('#ff0000').to.be.equal(
+						getSelectedBlock(data, 'blockeraInnerBlocks')[
+							'elements/text'
+						]?.attributes?.blockeraFontColor
+					);
+				});
+
+				//
+				// Test 2: Blockera value to WP data
+				//
+				setInnerBlock('elements/text');
+
+				cy.setColorControlValue('Text Color', '666666');
+
+				//
+				// Check
+				//
+				getWPDataObject().then((data) => {
+					expect(undefined).to.be.equal(
+						getSelectedBlock(data, 'textColor')
+					);
+
+					expect('#666666').to.be.equal(
+						getSelectedBlock(data, 'customTextColor')
+					);
+
+					expect('#666666').to.be.equal(
+						getSelectedBlock(data, 'blockeraInnerBlocks')[
+							'elements/text'
+						]?.attributes?.blockeraFontColor
+					);
+				});
+
+				//
+				// Test 3: Clear Blockera value and check WP data
+				//
+
+				cy.resetBlockeraAttribute('Typography', 'Text Color', 'reset');
+
+				getWPDataObject().then((data) => {
+					expect(undefined).to.be.equal(
+						getSelectedBlock(data, 'textColor')
+					);
+					expect(undefined).to.be.equal(
+						getSelectedBlock(data, 'customTextColor')
+					);
+
+					expect(undefined).to.be.equal(
+						getSelectedBlock(data, 'blockeraInnerBlocks')[
+							'elements/text'
+						]?.attributes?.blockeraFontColor
+					);
+				});
 			});
 
-			//
-			// Test 2: Blockera value to WP data
-			//
-			setInnerBlock('elements/text');
-
-			cy.setColorControlValue('Text Color', '666666');
-
-			//
-			// Check
-			//
-			getWPDataObject().then((data) => {
-				expect(undefined).to.be.equal(
-					getSelectedBlock(data, 'textColor')
+			it('Variable Value', () => {
+				appendBlocks(
+					`<!-- wp:blocksy/breadcrumbs {"textColor":"palette-color-1"} /-->`
 				);
 
-				expect('#666666').to.be.equal(
-					getSelectedBlock(data, 'customTextColor')
-				);
+				// Select target block
+				cy.getBlock('blocksy/breadcrumbs').first().click();
 
-				expect('#666666').to.be.equal(
-					getSelectedBlock(data, 'blockeraInnerBlocks')[
-						'elements/text'
-					]?.attributes?.blockeraFontColor
-				);
-			});
+				//
+				// Test 1: WP data to Blockera
+				//
 
-			//
-			// Test 3: Clear Blockera value and check WP data
-			//
+				// WP data should come to Blockera
+				getWPDataObject().then((data) => {
+					expect('palette-color-1').to.be.equal(
+						getSelectedBlock(data, 'textColor')
+					);
 
-			cy.resetBlockeraAttribute('Typography', 'Text Color', 'reset');
+					expect(undefined).to.be.equal(
+						getSelectedBlock(data, 'customTextColor')
+					);
 
-			getWPDataObject().then((data) => {
-				expect(undefined).to.be.equal(
-					getSelectedBlock(data, 'textColor')
-				);
-				expect(undefined).to.be.equal(
-					getSelectedBlock(data, 'customTextColor')
-				);
+					expect({
+						settings: {
+							name: 'Palette Color 1',
+							id: 'palette-color-1',
+							value: 'var(--theme-palette-color-1, #2872fa)',
+							reference: {
+								type: 'theme',
+								theme: 'Blocksy',
+							},
+							type: 'color',
+							var: '--wp--preset--color--palette-color-1',
+						},
+						name: 'Palette Color 1',
+						isValueAddon: true,
+						valueType: 'variable',
+					}).to.be.deep.equal(
+						getSelectedBlock(data, 'blockeraInnerBlocks')[
+							'elements/text'
+						]?.attributes?.blockeraFontColor
+					);
+				});
 
-				expect(undefined).to.be.equal(
-					getSelectedBlock(data, 'blockeraInnerBlocks')[
-						'elements/text'
-					]?.attributes?.blockeraFontColor
-				);
+				//
+				// Test 2: Blockera value to WP data
+				//
+				setInnerBlock('elements/text');
+
+				cy.getParentContainer('Text Color').within(() => {
+					cy.clickValueAddonButton();
+				});
+
+				// change variable
+				cy.selectValueAddonItem('palette-color-2');
+
+				//
+				// Check
+				//
+				getWPDataObject().then((data) => {
+					expect('palette-color-2').to.be.equal(
+						getSelectedBlock(data, 'textColor')
+					);
+
+					expect(undefined).to.be.equal(
+						getSelectedBlock(data, 'customTextColor')
+					);
+
+					expect({
+						settings: {
+							name: 'Palette Color 2',
+							id: 'palette-color-2',
+							value: 'var(--theme-palette-color-2, #1559ed)',
+							reference: {
+								type: 'theme',
+								theme: 'Blocksy',
+							},
+							type: 'color',
+							var: '--wp--preset--color--palette-color-2',
+						},
+						name: 'Palette Color 2',
+						isValueAddon: true,
+						valueType: 'variable',
+					}).to.be.deep.equal(
+						getSelectedBlock(data, 'blockeraInnerBlocks')[
+							'elements/text'
+						]?.attributes?.blockeraFontColor
+					);
+				});
+
+				//
+				// Test 3: Clear Blockera value and check WP data
+				//
+
+				cy.getParentContainer('Text Color').within(() => {
+					cy.removeValueAddon();
+				});
+
+				getWPDataObject().then((data) => {
+					expect(undefined).to.be.equal(
+						getSelectedBlock(data, 'textColor')
+					);
+					expect(undefined).to.be.equal(
+						getSelectedBlock(data, 'customTextColor')
+					);
+
+					expect(undefined).to.be.equal(
+						getSelectedBlock(data, 'blockeraInnerBlocks')[
+							'elements/text'
+						]?.attributes?.blockeraFontColor
+					);
+				});
 			});
 		});
 
-		it('Variable Value', () => {
-			appendBlocks(
-				`<!-- wp:blocksy/breadcrumbs {"textColor":"palette-color-1"} /-->`
-			);
+		it.only('Inner blocks existence + CSS selectors in editor and front-end', () => {
+			appendBlocks(`<!-- wp:blocksy/breadcrumbs /-->`);
 
-			// Select target block
 			cy.getBlock('blocksy/breadcrumbs').first().click();
 
 			//
-			// Test 1: WP data to Blockera
+			// 1. Edit Inner Blocks
 			//
 
-			// WP data should come to Blockera
-			getWPDataObject().then((data) => {
-				expect('palette-color-1').to.be.equal(
-					getSelectedBlock(data, 'textColor')
-				);
-
-				expect(undefined).to.be.equal(
-					getSelectedBlock(data, 'customTextColor')
-				);
-
-				expect({
-					settings: {
-						name: 'Palette Color 1',
-						id: 'palette-color-1',
-						value: 'var(--theme-palette-color-1, #2872fa)',
-						reference: {
-							type: 'theme',
-							theme: 'Blocksy',
-						},
-						type: 'color',
-						var: '--wp--preset--color--palette-color-1',
-					},
-					name: 'Palette Color 1',
-					isValueAddon: true,
-					valueType: 'variable',
-				}).to.be.deep.equal(
-					getSelectedBlock(data, 'blockeraInnerBlocks')[
-						'elements/text'
-					]?.attributes?.blockeraFontColor
-				);
-			});
-
 			//
-			// Test 2: Blockera value to WP data
+			// 1.1. Text inner block
 			//
 			setInnerBlock('elements/text');
 
-			cy.getParentContainer('Text Color').within(() => {
-				cy.clickValueAddonButton();
-			});
+			//
+			// 1.1.1. BG color
+			//
+			cy.setColorControlValue('Text Color', 'cccccc');
 
-			// change variable
-			cy.selectValueAddonItem('palette-color-2');
+			cy.getBlock('blocksy/breadcrumbs')
+				.first()
+				.within(() => {
+					cy.get('> span:last-child').should(
+						'have.css',
+						'color',
+						'rgb(204, 204, 204)'
+					);
+
+					// the block adds the text color to root so the svg inherits it too!
+					cy.get('svg')
+						.first()
+						.should('have.css', 'color', 'rgb(204, 204, 204)');
+
+					// this is link element
+					cy.get('> span:first-child > span').should(
+						'not.have.css',
+						'color',
+						'rgb(204, 204, 204)'
+					);
+				});
 
 			//
-			// Check
+			// 2. Assert inner blocks selectors in front end
 			//
-			getWPDataObject().then((data) => {
-				expect('palette-color-2').to.be.equal(
-					getSelectedBlock(data, 'textColor')
+			savePage();
+			redirectToFrontPage();
+
+			cy.get('.blockera-block').within(() => {
+				cy.get('> span:last-child').should(
+					'have.css',
+					'color',
+					'rgb(204, 204, 204)'
 				);
 
-				expect(undefined).to.be.equal(
-					getSelectedBlock(data, 'customTextColor')
-				);
+				// the block adds the text color to root so the svg inherits it too!
+				cy.get('svg')
+					.first()
+					.should('have.css', 'color', 'rgb(204, 204, 204)');
 
-				expect({
-					settings: {
-						name: 'Palette Color 2',
-						id: 'palette-color-2',
-						value: 'var(--theme-palette-color-2, #1559ed)',
-						reference: {
-							type: 'theme',
-							theme: 'Blocksy',
-						},
-						type: 'color',
-						var: '--wp--preset--color--palette-color-2',
-					},
-					name: 'Palette Color 2',
-					isValueAddon: true,
-					valueType: 'variable',
-				}).to.be.deep.equal(
-					getSelectedBlock(data, 'blockeraInnerBlocks')[
-						'elements/text'
-					]?.attributes?.blockeraFontColor
-				);
-			});
-
-			//
-			// Test 3: Clear Blockera value and check WP data
-			//
-
-			cy.getParentContainer('Text Color').within(() => {
-				cy.removeValueAddon();
-			});
-
-			getWPDataObject().then((data) => {
-				expect(undefined).to.be.equal(
-					getSelectedBlock(data, 'textColor')
-				);
-				expect(undefined).to.be.equal(
-					getSelectedBlock(data, 'customTextColor')
-				);
-
-				expect(undefined).to.be.equal(
-					getSelectedBlock(data, 'blockeraInnerBlocks')[
-						'elements/text'
-					]?.attributes?.blockeraFontColor
-				);
+				// this is link element
+				cy.get('a')
+					.first()
+					.should('not.have.css', 'color', 'rgb(204, 204, 204)');
 			});
 		});
 	});
