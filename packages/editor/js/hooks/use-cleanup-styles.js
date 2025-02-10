cb802cf7a070c28c8924504c4007afb80dd7ff39
rezaelahidev <rezaelahi.dev@gmail.com>
2025-02-10 18:21:30 +0330

    feat: implemented useCleanuupStyles react custom hook

---
+++ packages/editor/js/hooks/use-cleanup-styles.js
@@ -0,0 +1,73 @@
+// @flow
+
+/**
+ * External dependencies
+ */
+import { useState, useEffect } from '@wordpress/element';
+
+/**
+ * Blockera dependencies
+ */
+import { isEmpty, getSmallHash, getIframeTag } from '@blockera/utils';
+import { classNames } from '@blockera/classnames';
+
+export const useCleanupStyles = (
+	{ clientId }: { clientId: string },
+	dependencies: Array<any>
+): Object => {
+	const [inlineStyles, setInlineStyles] = useState({});
+	const {
+		blockeraSettings: {
+			labAndExperimental: { enableCleanupStyles },
+		},
+	} = window;
+
+	useEffect(() => {
+		if (!enableCleanupStyles) {
+			return;
+		}
+
+		const el = getIframeTag('body').querySelector(`#block-${clientId}`);
+
+		if (!el) {
+			return;
+		}
+
+		// Get all elements with inline styles.
+		const elementsWithStyles = el.querySelectorAll('[style]');
+		const inlineStyles: { [key: string]: Object } = {};
+
+		// Extract inline styles from main element if it has any
+		if (el.hasAttribute('style')) {
+			inlineStyles[`#block-${clientId}`] = el.getAttribute('style');
+			el.removeAttribute('style');
+		}
+
+		// Extract and remove inline styles from child elements
+		elementsWithStyles.forEach((element) => {
+			let elementSelector = classNames(element.getAttribute('class'), {
+				'blockera-block': true,
+				[`blockera-block-${getSmallHash(clientId)}`]: true,
+			}).replace(/\s+/g, '.');
+			elementSelector = `.${elementSelector}`;
+
+			if (elementSelector) {
+				inlineStyles[elementSelector] = element.getAttribute('style');
+				element.removeAttribute('style');
+			}
+		});
+
+		// Store extracted styles in state if not empty
+		if (!isEmpty(inlineStyles)) {
+			// Here you would need to implement state management
+			// For example using setState or dispatch to store
+			// the inlineStyles object
+
+			setInlineStyles(inlineStyles);
+		}
+
+		// eslint-disable-next-line
+	}, dependencies);
+
+	return inlineStyles;
+};
