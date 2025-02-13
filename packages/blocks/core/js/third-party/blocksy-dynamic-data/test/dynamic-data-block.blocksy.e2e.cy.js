01cc8315bfa8cc3e060da050a313f40e4b2a9b42
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-13 14:30:19 +0330

    test(Blocksy Dynamic Data Block): fix base block functionality test

--- packages/blocks/core/js/third-party/blocksy-dynamic-data/test/dynamic-data-block.blocksy.e2e.cy.js
+++ packages/blocks/core/js/third-party/blocksy-dynamic-data/test/dynamic-data-block.blocksy.e2e.cy.js
@@ -20,7 +20,7 @@ describe('Blocksy → Dynamic Data Block → WP Compatibility', () => {
 
 	it('Inner blocks existence + CSS selectors in editor and front-end', () => {
 		appendBlocks(
-			`<!-- wp:blocksy/dynamic-data {"blockeraPropsId":"c7920348-5ad7-4e2d-9699-fff8e781e06a","blockeraCompatId":"1912856297","blockeraBackgroundColor":{"value":"#ffa6a6"},"blockeraInnerBlocks":{"value":{"elements/link":{"attributes":{"blockeraBackgroundColor":"#ff0000"}}}},"blockeraFontColor":{"value":"#0044ff"},"field":"wp:author","style":{"elements":{"overlay":{"color":{"background":"#000000"}},"link":{"color":{"text":"#0044ff"}}},"color":{"text":"#0044ff","background":"#ffa6a6"}},"has_field_link":"yes","className":"blockera-block blockera-block\u002d\u002d63bmhe"} /-->`
+			`<!-- wp:blocksy/dynamic-data {"blockeraPropsId":"c7920348-5ad7-4e2d-9699-fff8e781e06a","blockeraCompatId":"1912856297","blockeraBackgroundColor":{"value":"#ffa6a6"},"blockeraBackgroundClip":{"value":"padding-box"},"blockeraInnerBlocks":{"value":{"elements/link":{"attributes":{"blockeraBackgroundColor":"#ff0000"}}}},"blockeraFontColor":{"value":"#0044ff"},"field":"wp:author","style":{"elements":{"overlay":{"color":{"background":"#000000"}},"link":{"color":{"text":"#0044ff"}}},"color":{"text":"#0044ff","background":"#ffa6a6"}},"has_field_link":"yes","className":"blockera-block blockera-block\u002d\u002d63bmhe"} /-->`
 		);
 
 		cy.getBlock('blocksy/dynamic-data').first().click();
@@ -32,8 +32,8 @@ describe('Blocksy → Dynamic Data Block → WP Compatibility', () => {
 		//
 		cy.getBlock('blocksy/dynamic-data').should(
 			'have.css',
-			'background-color',
-			'rgb(255, 166, 166)'
+			'background-clip',
+			'padding-box'
 		);
 
 		cy.getBlock('blocksy/dynamic-data')
@@ -54,8 +54,8 @@ describe('Blocksy → Dynamic Data Block → WP Compatibility', () => {
 
 		cy.get('.blockera-block').should(
 			'have.css',
-			'background-color',
-			'rgb(255, 166, 166)'
+			'background-clip',
+			'padding-box'
 		);
 
 		cy.get('.blockera-block').within(() => {
