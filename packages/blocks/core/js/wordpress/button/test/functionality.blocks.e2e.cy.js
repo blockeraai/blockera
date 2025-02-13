19082957f29324279ecb5d6aabbc2dddcd8f47b4
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-13 20:05:41 +0330

    test(Button Block): add full functionality test

---
+++ packages/blocks/core/js/wordpress/button/test/functionality.blocks.e2e.cy.js
@@ -0,0 +1,70 @@
+/**
+ * Blockera dependencies
+ */
+import {
+	appendBlocks,
+	createPost,
+	savePage,
+	redirectToFrontPage,
+} from '@blockera/dev-cypress/js/helpers';
+
+describe('Button Block → Functionality + Inner blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Functionality + Inner blocks', () => {
+		appendBlocks(`<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">button 1</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">button 2</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->`);
+
+		cy.getBlock('core/button').first().click();
+
+		// Block supported is active
+		cy.get('.blockera-extension-block-card').should('be.visible');
+
+		// No inner blocks
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'not.exist'
+		);
+
+		//
+		// 1. Block Styles
+		//
+		cy.getParentContainer('Clipping').within(() => {
+			cy.customSelect('Clip to Padding');
+		});
+
+		cy.getBlock('core/button')
+			.first()
+			.within(() => {
+				cy.get('.wp-element-button').should(
+					'have.css',
+					'background-clip',
+					'padding-box'
+				);
+			});
+
+		//
+		// 2. Assert front end
+		//
+		savePage();
+		redirectToFrontPage();
+
+		cy.get('.blockera-block.wp-block-button')
+			.first()
+			.within(() => {
+				cy.get('.wp-element-button').should(
+					'have.css',
+					'background-clip',
+					'padding-box'
+				);
+			});
+	});
+});
