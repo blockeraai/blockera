99250611325ffa11516d830f7fb905243033421e
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-13 20:17:04 +0330

    test(Calendar Block): add full functionality test

---
+++ packages/blocks/core/js/wordpress/calendar/test/functionality.blocks.e2e.cy.js
@@ -0,0 +1,57 @@
+/**
+ * Blockera dependencies
+ */
+import {
+	savePage,
+	createPost,
+	appendBlocks,
+	setInnerBlock,
+	redirectToFrontPage,
+} from '@blockera/dev-cypress/js/helpers';
+
+describe('Calendar Block → Functionality + Inner blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Functionality + Inner blocks', () => {
+		appendBlocks(`<!-- wp:calendar /-->`);
+
+		// Select target block
+		cy.getBlock('core/calendar').click();
+
+		// Block supported is active
+		cy.get('.blockera-extension-block-card').should('be.visible');
+
+		// No inner blocks
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'not.exist'
+		);
+
+		//
+		// 1.0. Block Styles
+		//
+
+		cy.getParentContainer('Clipping').within(() => {
+			cy.customSelect('Clip to Padding');
+		});
+
+		cy.getBlock('core/calendar').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		//
+		// Assert front end
+		//
+		savePage();
+		redirectToFrontPage();
+
+		cy.get('.blockera-block.wp-block-calendar').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+	});
+});
