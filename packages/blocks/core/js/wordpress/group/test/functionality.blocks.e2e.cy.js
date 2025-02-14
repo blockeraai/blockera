6b1baaac9f58cc7a5c544095c9d383f8934043be
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-14 13:44:09 +0330

    test(Group Block): add full functionality test

---
+++ packages/blocks/core/js/wordpress/group/test/functionality.blocks.e2e.cy.js
@@ -0,0 +1,96 @@
+/**
+ * Blockera dependencies
+ */
+import {
+	appendBlocks,
+	setInnerBlock,
+	createPost,
+	savePage,
+	redirectToFrontPage,
+} from '@blockera/dev-cypress/js/helpers';
+
+describe('Group Block → Functionality + Inner blocks', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Functionality + Inner blocks', () => {
+		appendBlocks(`<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph -->
+<p>This is a test paragraph!</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->`);
+
+		// Select target block
+		cy.getBlock('core/paragraph').first().click();
+
+		// Switch to parent block
+		cy.getByAriaLabel('Select Group').click();
+
+		// Block supported is active
+		cy.get('.blockera-extension-block-card').should('be.visible');
+
+		// Has inner blocks
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'exist'
+		);
+
+		//
+		// 1. Edit Inner Blocks
+		//
+
+		//
+		// 1.0. Block Styles
+		//
+		cy.getBlock('core/group').should(
+			'not.have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		cy.getParentContainer('Clipping').within(() => {
+			cy.customSelect('Clip to Padding');
+		});
+
+		cy.getBlock('core/group').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		//
+		// 1.1. Inner blocks
+		//
+		setInnerBlock('core/paragraph');
+
+		cy.setColorControlValue('BG Color', 'ff0000');
+
+		cy.getBlock('core/group').within(() => {
+			cy.get('.wp-block-paragraph').should(
+				'have.css',
+				'background-color',
+				'rgb(255, 0, 0)'
+			);
+		});
+
+		//
+		// 2. Assert inner blocks selectors in front end
+		//
+		savePage();
+		redirectToFrontPage();
+
+		cy.get('.blockera-block.wp-block-group').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		cy.get('.blockera-block.wp-block-group').within(() => {
+			cy.get('p').should(
+				'have.css',
+				'background-color',
+				'rgb(255, 0, 0)'
+			);
+		});
+	});
+});
