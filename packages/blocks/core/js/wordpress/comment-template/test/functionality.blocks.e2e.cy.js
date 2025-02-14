3ddeb479d2a717096fc67c24f0346568d56dd9ba
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-14 11:30:00 +0330

    test(Comment Template Block): add full functionality test

---
+++ packages/blocks/core/js/wordpress/comment-template/test/functionality.blocks.e2e.cy.js
@@ -0,0 +1,60 @@
+/**
+ * Blockera dependencies
+ */
+import {
+	editPost,
+	appendBlocks,
+	savePage,
+	redirectToFrontPage,
+} from '@blockera/dev-cypress/js/helpers';
+
+/**
+ * Internal dependencies
+ */
+import { testContent } from './test-content';
+
+describe('Comment Template Block → Functionality + Inner blocks', () => {
+	beforeEach(() => {
+		editPost({ postID: 1 });
+	});
+
+	it('Functionality + Inner blocks', () => {
+		appendBlocks(testContent + ' comment template block ' + Math.random());
+
+		// Select target block
+		cy.getBlock('core/comment-template').first().click();
+
+		// Block supported is active
+		cy.get('.blockera-extension-block-card').should('be.visible');
+
+		// No inner blocks
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'not.exist'
+		);
+
+		//
+		// 1.0. Block Styles
+		//
+		cy.getParentContainer('Clipping').within(() => {
+			cy.customSelect('Clip to Padding');
+		});
+
+		cy.getBlock('core/comment-template').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		//
+		// 2. Assert front end
+		//
+		savePage();
+		redirectToFrontPage();
+
+		cy.get('.blockera-block.wp-block-comment-template').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+	});
+});
