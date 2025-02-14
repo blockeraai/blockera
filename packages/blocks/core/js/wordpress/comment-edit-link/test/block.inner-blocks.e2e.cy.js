70781295e9ff2db3864304b52262df2171721ffa
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-14 11:17:33 +0330

    test(Comment Edit Link Block): add full functionality test

--- packages/blocks/core/js/wordpress/comment-edit-link/test/block.inner-blocks.e2e.cy.js
+++ packages/blocks/core/js/wordpress/comment-edit-link/test/block.inner-blocks.e2e.cy.js
@@ -4,7 +4,9 @@
 import {
 	editPost,
 	appendBlocks,
-	openInnerBlocksExtension,
+	savePage,
+	redirectToFrontPage,
+	setInnerBlock,
 } from '@blockera/dev-cypress/js/helpers';
 
 /**
@@ -12,27 +14,79 @@ import {
  */
 import { testContent } from './test-content';
 
-describe('Comment Edit Link Block → Inner Blocks', () => {
+describe('Comment Edit Link Block → Functionality + Inner blocks', () => {
 	beforeEach(() => {
 		editPost({ postID: 1 });
 	});
 
-	it('Inner blocks existence', () => {
-		appendBlocks(testContent);
+	it('Functionality + Inner blocks', () => {
+		appendBlocks(testContent + ' comment edit link block ' + Math.random());
 
 		// Select target block
 		cy.getBlock('core/comment-edit-link').first().click();
 
-		// open inner block settings
-		openInnerBlocksExtension();
+		// Block supported is active
+		cy.get('.blockera-extension-block-card').should('be.visible');
 
-		cy.get('.blockera-extension.blockera-extension-inner-blocks').within(
-			() => {
-				cy.getByDataTest('elements/link').should('exist');
-
-				// no other item
-				cy.getByDataTest('core/paragraph').should('not.exist');
-			}
+		// Has inner blocks
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'exist'
 		);
+
+		//
+		// 1.0. Block Styles
+		//
+		cy.getParentContainer('Clipping').within(() => {
+			cy.customSelect('Clip to Padding');
+		});
+
+		cy.getBlock('core/comment-edit-link').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		//
+		// 1.1. Inner blocks
+		//
+		setInnerBlock('elements/link');
+
+		cy.setColorControlValue('BG Color', 'ffdbdb');
+
+		cy.getBlock('core/comment-edit-link')
+			.first()
+			.within(() => {
+				cy.get('a')
+					.first()
+					.should(
+						'have.css',
+						'background-color',
+						'rgb(255, 219, 219)'
+					);
+			});
+
+		//
+		// 2. Assert front end
+		//
+		savePage();
+		redirectToFrontPage();
+
+		cy.get('.blockera-block.wp-block-comment-edit-link').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		cy.get('.blockera-block.wp-block-comment-edit-link')
+			.first()
+			.within(() => {
+				cy.get('a')
+					.first()
+					.should(
+						'have.css',
+						'background-color',
+						'rgb(255, 219, 219)'
+					);
+			});
 	});
 });
