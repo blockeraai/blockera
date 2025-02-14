20534dce2e16a51fe6e8667c094ff5f7d97aaa8d
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-14 20:05:23 +0330

    test(Post Navigation Link Block): add full functionality test

---
+++ packages/blocks/core/js/wordpress/post-navigation-link/test/functionality.blocks.e2e.cy.js
@@ -0,0 +1,108 @@
+/**
+ * Blockera dependencies
+ */
+import {
+	createPost,
+	appendBlocks,
+	openInnerBlocksExtension,
+	setInnerBlock,
+	savePage,
+	redirectToFrontPage,
+} from '@blockera/dev-cypress/js/helpers';
+
+describe('Post Navigation Link Block', () => {
+	beforeEach(() => {
+		createPost();
+	});
+
+	it('Functionality + Inner blocks', () => {
+		appendBlocks(
+			'<!-- wp:post-navigation-link {"type":"previous","label":"Prev"} /--> '
+		);
+
+		// Select target block
+		cy.getBlock('core/post-navigation-link').click();
+
+		// Block supported is active
+		cy.get('.blockera-extension-block-card').should('be.visible');
+
+		// Has inner blocks
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'exist'
+		);
+
+		// open inner block settings
+		openInnerBlocksExtension();
+
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').within(
+			() => {
+				cy.getByDataTest('elements/link').should('exist');
+
+				// no other item
+				cy.getByDataTest('core/heading').should('not.exist');
+			}
+		);
+
+		//
+		// 1. Edit Block
+		//
+
+		//
+		// 1.0. Block Styles
+		//
+
+		cy.getBlock('core/post-navigation-link').should(
+			'not.have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		cy.getParentContainer('Clipping').within(() => {
+			cy.customSelect('Clip to Padding');
+		});
+
+		cy.getBlock('core/post-navigation-link').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		//
+		// 1.1. elements/link inner block
+		//
+		setInnerBlock('elements/link');
+
+		//
+		// 1.1.1. BG color
+		//
+		cy.setColorControlValue('BG Color', 'ff0000');
+
+		cy.getBlock('core/post-navigation-link').within(() => {
+			cy.get('a').should(
+				'have.css',
+				'background-color',
+				'rgb(255, 0, 0)'
+			);
+		});
+
+		//
+		// 2. Assert inner blocks selectors in front end
+		//
+		savePage();
+		redirectToFrontPage();
+
+		cy.get('.wp-block-post-navigation-link.blockera-block').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		cy.get('.wp-block-post-navigation-link.blockera-block').within(() => {
+			cy.get('a').should(
+				'have.css',
+				'background-color',
+				'rgb(255, 0, 0)'
+			);
+		});
+	});
+});
