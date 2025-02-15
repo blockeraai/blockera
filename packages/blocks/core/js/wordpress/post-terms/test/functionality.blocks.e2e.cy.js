c6df2ca613e6cf886cdd2b773799468abd939d7c
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-15 13:11:00 +0330

    test(Post Terms Block): add full functionality test

--- packages/blocks/core/js/wordpress/post-terms/test/functionality.blocks.e2e.cy.js
+++ packages/blocks/core/js/wordpress/post-terms/test/functionality.blocks.e2e.cy.js
@@ -4,18 +4,34 @@
 import {
 	savePage,
 	createPost,
+	createTerm,
 	appendBlocks,
 	setInnerBlock,
 	setParentBlock,
 	redirectToFrontPage,
+	setCategoriesBySlug,
 } from '@blockera/dev-cypress/js/helpers';
 
-describe('Post Terms → Inner Blocks', () => {
+describe('Post Terms Block', () => {
 	beforeEach(() => {
+		//
+		// Setup terms
+		//
+		createTerm('Category 1');
+		createTerm('Category 2');
+
+		//
+		// Create post
+		//
 		createPost();
+
+		//
+		// Set categories by slug
+		//
+		setCategoriesBySlug(['category-1', 'category-2']);
 	});
 
-	it('Inner blocks existence + CSS selectors in editor and front-end', () => {
+	it('Functionality + Inner blocks', () => {
 		appendBlocks(
 			'<!-- wp:post-terms {"term":"category","prefix":"prefix text","suffix":"suffix text"} /-->'
 		);
@@ -23,27 +39,50 @@ describe('Post Terms → Inner Blocks', () => {
 		// Select target block
 		cy.getBlock('core/post-terms').click();
 
+		// Block supported is active
+		cy.get('.blockera-extension-block-card').should('be.visible');
+
+		// Has inner blocks
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'exist'
+		);
+
 		//
-		// 1. Edit Inner Blocks
+		// 1. Edit Block
 		//
 
 		//
-		// 1.1. core/separator
+		// 1.0. Block Styles
 		//
-		setInnerBlock('core/separator');
+
+		cy.getBlock('core/post-terms').should(
+			'not.have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		cy.getParentContainer('Clipping').within(() => {
+			cy.customSelect('Clip to Padding');
+		});
+
+		cy.getBlock('core/post-terms').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		//
+		// 1.1. elements/separator
+		//
+		setInnerBlock('elements/separator');
 
 		//
 		// 1.1.1. BG color
 		//
 		cy.setColorControlValue('BG Color', 'ff0000');
 
-		// cy.getBlock('core/post-terms')
-		// 	.first()
-		// 	.within(() => {
-		// 		cy.get('.wp-block-post-terms__separator')
-		// 			.first()
-		// 			.should('have.css', 'background-color', 'rgb(255, 0, 0)');
-		// 	});
+		// there is an issue that blocks are not updated immediately to see the terms
+		// so we check the separator color in the front end
 
 		//
 		// 1.2. elements/prefix
@@ -89,15 +128,17 @@ describe('Post Terms → Inner Blocks', () => {
 		savePage();
 		redirectToFrontPage();
 
-		cy.get('.blockera-block').within(() => {
-			// // core/separator
-			// cy.get('.wp-block-post-terms__separator')
-			// 	.first()
-			// 	.should(
-			// 		'have.css',
-			// 		'background-color',
-			// 		'rgb(255, 0, 0)'
-			// 	);
+		cy.get('.blockera-block.wp-block-post-terms').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		cy.get('.blockera-block.wp-block-post-terms').within(() => {
+			// core/separator
+			cy.get('.wp-block-post-terms__separator')
+				.first()
+				.should('have.css', 'background-color', 'rgb(255, 0, 0)');
 
 			// elements/prefix
 			cy.get('.wp-block-post-terms__prefix')
