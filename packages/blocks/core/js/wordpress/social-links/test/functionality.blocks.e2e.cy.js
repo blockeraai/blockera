c4f35de62a94769cfe832491199f9dc4df414a85
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-15 17:45:57 +0330

    test(Social Links Block): add full functionality test

--- packages/blocks/core/js/wordpress/social-links/test/functionality.blocks.e2e.cy.js
+++ packages/blocks/core/js/wordpress/social-links/test/functionality.blocks.e2e.cy.js
@@ -10,12 +10,12 @@ import {
 	redirectToFrontPage,
 } from '@blockera/dev-cypress/js/helpers';
 
-describe('Social Links Block → Inner Blocks', () => {
+describe('Social Links Block', () => {
 	beforeEach(() => {
 		createPost();
 	});
 
-	it('Should add all inner blocks to block settings', () => {
+	it('Functionality + inner blocks', () => {
 		appendBlocks(`<!-- wp:social-links {"showLabels":true,"size":"has-normal-icon-size","className":"is-style-default","layout":{"type":"flex","justifyContent":"left","flexWrap":"nowrap"}} -->
 <ul class="wp-block-social-links has-normal-icon-size has-visible-labels is-style-default"><!-- wp:social-link {"url":"#test","service":"wordpress"} /-->
 
@@ -30,17 +30,44 @@ describe('Social Links Block → Inner Blocks', () => {
 		// Switch to parent block
 		cy.getByAriaLabel('Select parent block: Social Icons').click();
 
+		// Block supported is active
+		cy.get('.blockera-extension-block-card').should('be.visible');
+
+		// Has inner blocks
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'exist'
+		);
+
 		//
 		// 1. Edit Inner Blocks
 		//
 
 		//
-		// 1.1. elements/item-containers
+		// 1.1. Block styles
+		//
+		cy.getBlock('core/social-links').should(
+			'have.css',
+			'background-clip',
+			'border-box'
+		);
+
+		cy.getParentContainer('Clipping').within(() => {
+			cy.customSelect('Clip to Padding');
+		});
+
+		cy.getBlock('core/social-links').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		//
+		// 1.2. elements/item-containers
 		//
 		setInnerBlock('elements/item-containers');
 
 		//
-		// 1.1.1. BG color
+		// 1.2.1. BG color
 		//
 		cy.setColorControlValue('BG Color', 'ff0000');
 
@@ -53,13 +80,13 @@ describe('Social Links Block → Inner Blocks', () => {
 			});
 
 		//
-		// 1.2. elements/item-icons
+		// 1.3. elements/item-icons
 		//
 		setParentBlock();
 		setInnerBlock('elements/item-icons');
 
 		//
-		// 1.2.1. BG color
+		// 1.3.1. BG color
 		//
 		cy.setColorControlValue('BG Color', 'ff2020');
 
@@ -72,13 +99,13 @@ describe('Social Links Block → Inner Blocks', () => {
 			});
 
 		//
-		// 1.3. elements/item-icons
+		// 1.4. elements/item-names
 		//
 		setParentBlock();
 		setInnerBlock('elements/item-names');
 
 		//
-		// 1.3.1. BG color
+		// 1.4.1. BG color
 		//
 		cy.setColorControlValue('BG Color', 'ff2020');
 
@@ -96,18 +123,28 @@ describe('Social Links Block → Inner Blocks', () => {
 		savePage();
 		redirectToFrontPage();
 
+		cy.get('.blockera-block.wp-block-social-links').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
 		// elements/item-containers
-		cy.get('.wp-block-social-link')
+		cy.get('.blockera-block.wp-block-social-links .wp-block-social-link')
 			.first()
 			.should('have.css', 'background-color', 'rgb(255, 0, 0)');
 
 		// elements/item-icons
-		cy.get('.wp-block-social-link svg')
+		cy.get(
+			'.blockera-block.wp-block-social-links .wp-block-social-link svg'
+		)
 			.first()
 			.should('have.css', 'background-color', 'rgb(255, 32, 32)');
 
 		// elements/item-names
-		cy.get('.wp-block-social-link .wp-block-social-link-label')
+		cy.get(
+			'.blockera-block.wp-block-social-links .wp-block-social-link .wp-block-social-link-label'
+		)
 			.first()
 			.should('have.css', 'background-color', 'rgb(255, 32, 32)');
 	});
