f138f3790622369e74076f58a43de9038e8c99aa
Ali Aghdam <betteraliaghdam@gmail.com>
2025-02-15 15:24:56 +0330

    test(Query Pagination Numbers Block): add full functionality test

--- packages/blocks/core/js/wordpress/query-pagination-numbers/test/functionality.blocks.e2e.cy.js
+++ packages/blocks/core/js/wordpress/query-pagination-numbers/test/functionality.blocks.e2e.cy.js
@@ -2,10 +2,12 @@
  * Blockera dependencies
  */
 import {
+	savePage,
 	createPost,
 	appendBlocks,
 	setInnerBlock,
 	setParentBlock,
+	redirectToFrontPage,
 } from '@blockera/dev-cypress/js/helpers';
 
 /**
@@ -13,26 +15,53 @@ import {
  */
 import { testContent } from './test-content';
 
-describe('Query Pagination Numbers Block → Inner Blocks', () => {
+describe('Query Pagination Numbers Block', () => {
 	beforeEach(() => {
 		createPost();
 	});
 
-	it('Inner blocks existence + CSS selector in editor', () => {
+	it('Functionality + inner blocks', () => {
 		appendBlocks(testContent);
 
 		// Select target block
 		cy.getBlock('core/query-pagination-numbers').click();
+
+		// Block supported is active
+		cy.get('.blockera-extension-block-card').should('be.visible');
+
+		// Has inner blocks
+		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
+			'exist'
+		);
 
 		//
 		// 1. Edit Inner Blocks
 		//
 
 		//
+		// 1.0. Block styles
+		//
+
+		cy.getBlock('core/query-pagination-numbers').should(
+			'not.have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		cy.getParentContainer('Clipping').within(() => {
+			cy.customSelect('Clip to Padding');
+		});
+
+		cy.getBlock('core/query-pagination-numbers').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		//
 		// 1.1. elements/numbers inner block
 		//
 		setInnerBlock('elements/numbers');
-
 		//
 		// 1.1.1. BG color
 		//
@@ -89,5 +118,41 @@ describe('Query Pagination Numbers Block → Inner Blocks', () => {
 					'rgb(255, 64, 64)'
 				);
 			});
+
+		//
+		// 2. Assert inner blocks selectors in front end
+		//
+
+		savePage();
+		redirectToFrontPage();
+
+		cy.get('.blockera-block.wp-block-query-pagination-numbers').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		cy.get('.blockera-block.wp-block-query-pagination-numbers').within(
+			() => {
+				// not current and not dots because they have different styles
+				cy.get('.page-numbers:not(.dots):not(.current)').should(
+					'have.css',
+					'background-color',
+					'rgb(204, 204, 204)'
+				);
+
+				cy.get('.page-numbers.current').should(
+					'have.css',
+					'background-color',
+					'rgb(255, 32, 32)'
+				);
+
+				cy.get('.page-numbers.dots').should(
+					'have.css',
+					'background-color',
+					'rgb(255, 64, 64)'
+				);
+			}
+		);
 	});
 });
