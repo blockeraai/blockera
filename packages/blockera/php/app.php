8864244a82780a7ac1576e8de94121a4d41aacfb
rezaelahidev <rezaelahi.dev@gmail.com>
2025-03-05 20:41:08 +0330

    perf: first use of blockera object cache

--- packages/blockera/php/app.php
+++ packages/blockera/php/app.php
@@ -13,12 +13,34 @@ if (! defined('ABSPATH')) {
 
 global $blockera;
 
+// Initialize static cache.
+$blockera_cache = wp_cache_get('blockera_instance');
+
+if ($blockera_cache !== false) {
+    $blockera = $blockera_cache;
+    return;
+}
+
+// Optimize class initialization.
 $blockera = new \Blockera\Setup\Blockera();
+
+// Cache the instance.
+wp_cache_set('blockera_instance', $blockera);
 
 $external_dir = blockera_core_config('app.vendor_path') . 'blockera/';
 
-blockera_load('blockera-admin.php.hooks', $external_dir);
-blockera_load('wordpress.php.Admin.Menu.hooks', $external_dir);
+// Conditional loading based on context.
+if (is_admin()) {
+    blockera_load('blockera-admin.php.hooks', $external_dir);
+    blockera_load('wordpress.php.Admin.Menu.hooks', $external_dir);
+}
+
 blockera_load('telemetry.php.hooks', $external_dir);
 
+// Initialize core components with optimized bootstrap.
 $blockera->bootstrap();
+
+// Register shutdown function for cleanup.
+register_shutdown_function(function() {
+    wp_cache_delete('blockera_instance');
+});
