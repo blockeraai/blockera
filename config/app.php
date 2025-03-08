64a79736c59a2bc0ddf8b37d7907edc00d26e0a8
rezaelahidev <rezaelahi.dev@gmail.com>
2025-03-08 12:16:30 +0330

    Revert "perf: managed the service providers based on requirements"
    This reverts commit e4f44c7fd0bc1294658bf688b13151fcd441b5a4.

--- config/app.php
+++ config/app.php
@@ -35,18 +35,13 @@ return [
      */
     'providers'      => apply_filters(
         'blockera.application.providers',
-        is_admin() ? [
-			\Blockera\Admin\Providers\AdminProvider::class,
+        [
+            \Blockera\Admin\Providers\AdminProvider::class,
             \Blockera\Setup\Providers\EditorAssetsProvider::class,
             \Blockera\Setup\Providers\RestAPIProvider::class,
             \Blockera\Editor\Providers\StyleProviders::class,
             \Blockera\Setup\Providers\AppServiceProvider::class,
             \Blockera\Admin\Providers\AdminAssetsProvider::class,
-		] : [
-            \Blockera\Setup\Providers\EditorAssetsProvider::class,
-            \Blockera\Setup\Providers\RestAPIProvider::class,
-            \Blockera\Editor\Providers\StyleProviders::class,
-            \Blockera\Setup\Providers\AppServiceProvider::class,
         ]
     ),
 ];
