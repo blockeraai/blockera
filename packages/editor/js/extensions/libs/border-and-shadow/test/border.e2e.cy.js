af3fefa5cb2c4a91e73babd26102d17bc652578e
Ali Aghdam <betteraliaghdam@gmail.com>
2025-03-10 11:19:13 +0330

    test(Border Control): variable use functionality

--- packages/editor/js/extensions/libs/border-and-shadow/test/border.e2e.cy.js
+++ packages/editor/js/extensions/libs/border-and-shadow/test/border.e2e.cy.js
@@ -374,4 +374,561 @@ describe('Border → Functionality', () => {
 			);
 		});
 	});
+
+	describe('Variable Value', () => {
+		it('should update border when add same data to all side', () => {
+			cy.get('@container').within(() => {
+				cy.getByDataTest('border-control-width').clear();
+				cy.getByDataTest('border-control-width').type(5, {
+					force: true,
+				});
+			});
+
+			cy.get('@container').within(() => {
+				cy.openValueAddon();
+			});
+
+			cy.selectValueAddonItem('contrast');
+
+			//Check block
+			cy.getIframeBody().within(() => {
+				cy.get('#blockera-styles-wrapper')
+					.invoke('text')
+					.should(
+						'include',
+						'border: 5px solid var(--wp--preset--color--contrast)'
+					);
+			});
+
+			//Check store
+			getWPDataObject().then((data) => {
+				expect({
+					type: 'all',
+					all: {
+						width: '5px',
+						style: '',
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+					},
+				}).to.be.deep.equal(getSelectedBlock(data, 'blockeraBorder'));
+			});
+
+			//Check frontend
+			savePage();
+			redirectToFrontPage();
+
+			cy.get('style#blockera-inline-css')
+				.invoke('text')
+				.should(
+					'include',
+					'border: 5px solid var(--wp--preset--color--contrast)'
+				);
+		});
+
+		it('Add variable and then remove to make sure remove works correctly', () => {
+			cy.get('@container').within(() => {
+				cy.getByDataTest('border-control-width').clear();
+				cy.getByDataTest('border-control-width').type(5, {
+					force: true,
+				});
+			});
+
+			cy.get('@container').within(() => {
+				cy.openValueAddon();
+			});
+
+			cy.selectValueAddonItem('contrast');
+
+			//Check block
+			cy.getIframeBody().within(() => {
+				cy.get('#blockera-styles-wrapper')
+					.invoke('text')
+					.should(
+						'include',
+						'border: 5px solid var(--wp--preset--color--contrast)'
+					);
+			});
+
+			//Check store
+			getWPDataObject().then((data) => {
+				expect({
+					type: 'all',
+					all: {
+						width: '5px',
+						style: '',
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+					},
+				}).to.be.deep.equal(getSelectedBlock(data, 'blockeraBorder'));
+			});
+
+			cy.get('@container').within(() => {
+				cy.openValueAddon();
+			});
+
+			//Check store
+			getWPDataObject().then((data) => {
+				expect({
+					type: 'all',
+					all: {
+						width: '5px',
+						style: '',
+						color: '',
+					},
+				}).to.be.deep.equal(getSelectedBlock(data, 'blockeraBorder'));
+			});
+		});
+
+		it('custom borders', () => {
+			//
+			// Top Border
+			//
+			cy.get('@container').within(() => {
+				cy.getByAriaLabel('Custom Box Border').click();
+				cy.getByDataTest('border-control-component')
+					.eq(0)
+					.within(() => {
+						cy.getByDataTest('border-control-width').clear();
+						cy.getByDataTest('border-control-width').type(1, {
+							force: true,
+						});
+
+						cy.get('[aria-haspopup="listbox"]').trigger('click');
+						cy.get('div').eq(0).trigger('click');
+					});
+			});
+
+			cy.get('@container').within(() => {
+				cy.getByDataTest('border-control-component')
+					.eq(0)
+					.within(() => {
+						cy.openValueAddon();
+					});
+			});
+
+			cy.selectValueAddonItem('contrast');
+
+			//Check block
+			cy.getIframeBody().within(() => {
+				cy.get('#blockera-styles-wrapper')
+					.invoke('text')
+					.should(
+						'include',
+						'border-top: 1px solid var(--wp--preset--color--contrast)'
+					);
+			});
+
+			//Check store
+			getWPDataObject().then((data) => {
+				expect({
+					type: 'custom',
+					all: { color: '', style: '', width: '' },
+					left: { color: '', style: '', width: '' },
+					right: { color: '', style: '', width: '' },
+					bottom: { color: '', style: '', width: '' },
+					top: {
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+						style: '',
+						width: '1px',
+					},
+				}).to.be.deep.equal(getSelectedBlock(data, 'blockeraBorder'));
+			});
+
+			//
+			// Right Border
+			//
+			cy.get('@container').within(() => {
+				cy.getByDataTest('border-control-component')
+					.eq(1)
+					.within(() => {
+						cy.getByDataTest('border-control-width').clear({
+							force: true,
+						});
+						cy.getByDataTest('border-control-width').type(2, {
+							force: true,
+						});
+
+						cy.get('[aria-haspopup="listbox"]').trigger('click');
+						cy.get('div[aria-selected="false"]')
+							.eq(1)
+							.trigger('click');
+					});
+			});
+
+			cy.get('@container').within(() => {
+				cy.getByDataTest('border-control-component')
+					.eq(1)
+					.within(() => {
+						cy.openValueAddon();
+					});
+			});
+
+			cy.selectValueAddonItem('contrast');
+
+			//Check block
+			cy.getIframeBody().within(() => {
+				cy.get('#blockera-styles-wrapper')
+					.invoke('text')
+					.should(
+						'include',
+						'border-right: 2px dashed var(--wp--preset--color--contrast)'
+					);
+			});
+
+			//Check store
+			getWPDataObject().then((data) => {
+				expect({
+					type: 'custom',
+					all: { color: '', style: '', width: '' },
+					left: { color: '', style: '', width: '' },
+					right: {
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+						style: 'dashed',
+						width: '2px',
+					},
+					bottom: { color: '', style: '', width: '' },
+					top: {
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+						style: '',
+						width: '1px',
+					},
+				}).to.be.deep.equal(getSelectedBlock(data, 'blockeraBorder'));
+			});
+
+			//
+			// Bottom Border
+			//
+			cy.get('@container').within(() => {
+				cy.getByDataTest('border-control-component')
+					.eq(2)
+					.within(() => {
+						cy.getByDataTest('border-control-width').clear({
+							force: true,
+						});
+						cy.getByDataTest('border-control-width').type(3, {
+							force: true,
+						});
+
+						cy.get('[aria-haspopup="listbox"]').trigger('click');
+						cy.get('div[aria-selected="false"]')
+							.eq(2)
+							.trigger('click');
+					});
+			});
+
+			cy.get('@container').within(() => {
+				cy.getByDataTest('border-control-component')
+					.eq(2)
+					.within(() => {
+						cy.openValueAddon();
+					});
+			});
+
+			cy.selectValueAddonItem('contrast');
+
+			//Check block
+			cy.getIframeBody().within(() => {
+				cy.get('#blockera-styles-wrapper')
+					.invoke('text')
+					.should(
+						'include',
+						'border-bottom: 3px dotted var(--wp--preset--color--contrast)'
+					);
+			});
+
+			//Check store
+			getWPDataObject().then((data) => {
+				expect({
+					type: 'custom',
+					all: { color: '', style: '', width: '' },
+					left: { color: '', style: '', width: '' },
+					right: {
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+						style: 'dashed',
+						width: '2px',
+					},
+					bottom: {
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+						style: 'dotted',
+						width: '3px',
+					},
+					top: {
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+						style: '',
+						width: '1px',
+					},
+				}).to.be.deep.equal(getSelectedBlock(data, 'blockeraBorder'));
+			});
+
+			//
+			// Left Border
+			//
+			cy.get('@container').within(() => {
+				cy.getByDataTest('border-control-component')
+					.eq(3)
+					.within(() => {
+						cy.getByDataTest('border-control-width').clear({
+							force: true,
+						});
+						cy.getByDataTest('border-control-width').type(4, {
+							force: true,
+						});
+
+						cy.get('[aria-haspopup="listbox"]').trigger('click');
+						cy.get('div[aria-selected="false"]')
+							.eq(3)
+							.trigger('click');
+					});
+			});
+
+			cy.get('@container').within(() => {
+				cy.getByDataTest('border-control-component')
+					.eq(3)
+					.within(() => {
+						cy.openValueAddon();
+					});
+			});
+
+			cy.selectValueAddonItem('contrast');
+
+			//Check block
+			cy.getIframeBody().within(() => {
+				cy.get('#blockera-styles-wrapper')
+					.invoke('text')
+					.should(
+						'include',
+						'border-left: 4px double var(--wp--preset--color--contrast)'
+					);
+			});
+
+			//Check store
+			getWPDataObject().then((data) => {
+				expect({
+					type: 'custom',
+					all: { color: '', style: '', width: '' },
+					left: {
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+						style: 'double',
+						width: '4px',
+					},
+					right: {
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+						style: 'dashed',
+						width: '2px',
+					},
+					bottom: {
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+						style: 'dotted',
+						width: '3px',
+					},
+					top: {
+						color: {
+							settings: {
+								name: 'Contrast',
+								id: 'contrast',
+								value: '#111111',
+								reference: {
+									type: 'theme',
+									theme: 'Twenty Twenty-Five',
+								},
+								type: 'color',
+								var: '--wp--preset--color--contrast',
+							},
+							name: 'Contrast',
+							isValueAddon: true,
+							valueType: 'variable',
+						},
+						style: '',
+						width: '1px',
+					},
+				}).to.be.deep.equal(getSelectedBlock(data, 'blockeraBorder'));
+			});
+
+			// 	Check frontend
+			savePage();
+			redirectToFrontPage();
+
+			cy.get('style#blockera-inline-css')
+				.invoke('text')
+				.should(
+					'include',
+					'border-top: 1px solid var(--wp--preset--color--contrast)'
+				)
+				.and(
+					'include',
+					'border-right: 2px dashed var(--wp--preset--color--contrast)'
+				)
+				.and(
+					'include',
+					'border-bottom: 3px dotted var(--wp--preset--color--contrast)'
+				)
+				.and(
+					'include',
+					'border-left: 4px double var(--wp--preset--color--contrast)'
+				);
+		});
+	});
 });
