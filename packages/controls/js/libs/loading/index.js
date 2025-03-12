6813a3199b0ed0efe9dba9ec055683fe17111855
rezaelahidev <rezaelahi.dev@gmail.com>
2025-03-12 15:51:28 +0330

    feat: added the new loading component

--- packages/controls/js/libs/loading/index.js
+++ packages/controls/js/libs/loading/index.js
@@ -6,31 +6,78 @@
 import type { MixedElement } from 'react';
 
 export const LoadingComponent = ({
-	loadingDescription,
+	color,
 }: {
-	loadingDescription: string,
-}): MixedElement => (
-	<div style={{ textAlign: 'center', padding: '50px 0' }}>
-		<h1>{loadingDescription}</h1>
-		<div
-			className="blockera-loading-circle"
-			style={{
-				width: '40px',
-				height: '40px',
-				margin: '20px auto',
-				border: '4px solid #f3f3f3',
-				borderTop: '4px solid #3498db',
-				borderRadius: '50%',
-				animation: 'blockera-spin 1s linear infinite',
-			}}
-		/>
-		<style>
-			{`
-						@keyframes blockera-spin {
-							0% { transform: rotate(0deg); }
-							100% { transform: rotate(360deg); }
-						}
-					`}
-		</style>
-	</div>
-);
+	color: string,
+}): MixedElement => {
+	const style = `
+		.container {
+    --uib-size: 60px;
+    --uib-color: ${color};
+    --uib-speed: 2.5s;
+    --uib-dot-size: calc(var(--uib-size) * 0.18);
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: var(--uib-size);
+    height: var(--uib-dot-size);
+  }
+
+  .dot {
+    position: absolute;
+    top: calc(50% - var(--uib-dot-size) / 2);
+    left: calc(0px - var(--uib-dot-size) / 2);
+    display: block;
+    height: var(--uib-dot-size);
+    width: var(--uib-dot-size);
+    border-radius: 50%;
+    background-color: var(--uib-color);
+    animation: stream var(--uib-speed) linear infinite both;
+    transition: background-color 0.3s ease;
+  }
+
+  .dot:nth-child(2) {
+    animation-delay: calc(var(--uib-speed) * -0.2);
+  }
+
+  .dot:nth-child(3) {
+    animation-delay: calc(var(--uib-speed) * -0.4);
+  }
+
+  .dot:nth-child(4) {
+    animation-delay: calc(var(--uib-speed) * -0.6);
+  }
+
+  .dot:nth-child(5) {
+    animation-delay: calc(var(--uib-speed) * -0.8);
+  }
+
+  @keyframes stream {
+    0%,
+    100% {
+      transform: translateX(0) scale(0);
+    }
+
+    50% {
+      transform: translateX(calc(var(--uib-size) * 0.5)) scale(1);
+    }
+
+    99.999% {
+      transform: translateX(calc(var(--uib-size))) scale(0);
+    }
+  }`;
+	return (
+		<>
+			<div className="container">
+				<div className="dot"></div>
+				<div className="dot"></div>
+				<div className="dot"></div>
+				<div className="dot"></div>
+				<div className="dot"></div>
+			</div>
+
+			<style>{style}</style>
+		</>
+	);
+};
