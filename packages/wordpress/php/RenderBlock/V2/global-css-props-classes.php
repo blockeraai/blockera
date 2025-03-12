b99f01a687390d0d47638301210ac58c3e165662
Ali Aghdam <betteraliaghdam@gmail.com>
2025-03-12 13:54:05 +0330

    feat: add more css props as classes

--- packages/wordpress/php/RenderBlock/V2/global-css-props-classes.php
+++ packages/wordpress/php/RenderBlock/V2/global-css-props-classes.php
@@ -2,4 +2,11 @@
 
 return [
 	'flex-basis' => 'blockera-has-flex-basis',
+	'text-decoration' => 'blockera-has-text-decoration',
+	'font-weight' => 'blockera-has-font-weight',
+	'font-family' => 'blockera-has-font-family',
+	'font-size' => 'blockera-has-font-size',
+	'line-height' => 'blockera-has-line-height',
+	'font-style' => 'blockera-has-font-style',
+	'letter-spacing' => 'blockera-has-letter-spacing',
 ];
