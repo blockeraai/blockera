3c450d0b160bb9425dcf3f4ffbcb9195bac2c14d
Ali Aghdam <betteraliaghdam@gmail.com>
2025-03-15 14:15:04 +0330

    test(Post Content Block): add more test senarios

--- packages/blocks/core/js/wordpress/post-content/test/functionality.blocks.e2e.cy.js
+++ packages/blocks/core/js/wordpress/post-content/test/functionality.blocks.e2e.cy.js
@@ -10,6 +10,7 @@ import {
 	openInnerBlocksExtension,
 	redirectToFrontPage,
 } from '@blockera/dev-cypress/js/helpers';
+import { experimental } from '@blockera/env';
 
 describe('Post Content Block', () => {
 	beforeEach(() => {
@@ -21,7 +22,11 @@ describe('Post Content Block', () => {
 		});
 	});
 
-	it('Functionality + Inner blocks', () => {
+	const enabledOptimizeStyleGeneration = experimental().get(
+		'earlyAccessLab.optimizeStyleGeneration'
+	);
+
+	it('Functionality + Inner blocks (Post content inside a Group block)', () => {
 		//
 		// 1. Edit Post Template (with no title)
 		//
@@ -171,6 +176,109 @@ describe('Post Content Block', () => {
 			'have.css',
 			'background-clip',
 			'padding-box'
+		);
+	});
+
+	it('Post content block in root', () => {
+		//
+		// 1. Edit Post Template (with no title)
+		//
+		appendBlocks(`<!-- wp:template-part {"slug":"header","theme":"twentytwentyfive"} /-->
+
+<!-- wp:post-content {"style":{"spacing":{"padding":{"top":"0px","right":"","bottom":"","left":""},"margin":{"top":"0px","right":"","bottom":"","left":""}}}} /-->
+
+<!-- wp:template-part {"slug":"footer","theme":"twentytwentyfive","area":"footer"} /-->
+		`);
+
+		// Select target block
+		cy.getBlock('core/post-content').click();
+
+		//
+		// 1.1. Edit Post Block
+		//
+
+		//
+		// 1.1.1. Block Styles
+		//
+		cy.getBlock('core/post-content').should(
+			'not.have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		cy.getParentContainer('Clipping').within(() => {
+			cy.customSelect('Clip to Padding');
+		});
+
+		cy.getBlock('core/post-content').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		//
+		// 1.2. Inner blocks
+		//
+		setInnerBlock('elements/link');
+		cy.setColorControlValue('BG Color', 'ff0000');
+
+		//
+		// 1.2. Save template
+		//
+		savePage();
+
+		//
+		// 2. Create a new page with no title template to test post content blo
+		//
+		createPost({ postType: 'page' });
+
+		appendBlocks(`
+			<!-- wp:paragraph -->
+			<p>This is a test <a href="#a">paragraph</a>...</p>
+			<!-- /wp:paragraph -->
+		`);
+
+		cy.get('button[data-tab-id="edit-post/document"]').click();
+
+		//
+		// Change post template
+		//
+		cy.get('[aria-label="Template options"]').click();
+		cy.get('button').contains('Swap template').click();
+		cy.get('div#page-no-title[role="option"]').click();
+
+		//
+		// 2. Assert inner blocks selectors in front end
+		//
+		savePage();
+		redirectToFrontPage();
+
+		//
+		// 2.1. Post Content Block
+		//
+		cy.get('.blockera-block.wp-block-post-content').should(
+			'have.css',
+			'background-clip',
+			'padding-box'
+		);
+
+		// Check if optimize style generation is enabled
+		// then style attribute should not be present
+		// this is a workaround to check if the feature is enabled and working
+		if (enabledOptimizeStyleGeneration) {
+			cy.get('.blockera-block.wp-block-post-content').should(
+				'not.have.attr',
+				'style'
+			);
+		}
+
+		//
+		// 2.2. Post Content Block → Link Inner blocks
+		//
+		cy.get('.blockera-block.wp-block-post-content a').should(
+			'have.css',
+			'background-color',
+			'rgb(255, 0, 0)'
 		);
 	});
 });
