1425f809d8ef56a5135821e9f49c1b7ef1df6c4e
Ali Aghdam <betteraliaghdam@gmail.com>
2025-03-23 12:34:46 +0330

    feat: improve command to support element for opening side

--- packages/dev-cypress/js/helpers/controls-box-spacing.js
+++ packages/dev-cypress/js/helpers/controls-box-spacing.js
@@ -37,6 +37,32 @@ export function clearBoxSpacingSide(side) {
  *
  * @param {string}  side   Name of the panel to open
  */
-export function openBoxSpacingSide(side) {
-	cy.get(`[data-cy="box-spacing-${side}"] [data-cy="label-control"]`).click();
+export function openBoxSpacingSide(side, element = 'label') {
+	if (element === 'label') {
+		cy.get(
+			`[data-cy="box-spacing-${side}"] [data-cy="label-control"]`
+		).click();
+	} else if (element === 'shape') {
+		cy.get(`[data-cy="box-spacing-control"]`).within(() => {
+			//
+			// Following code is used to click on the shape element and make sure it works
+			//
+			cy.get(
+				`path.blockera-control-spacing-shape-side.side-${side}`
+			).trigger('mousedown', 'topLeft', {
+				force: true,
+				which: 1,
+			});
+			cy.wait(100);
+			cy.get(
+				`path.blockera-control-spacing-shape-side.side-${side}`
+			).trigger('mouseup', 'topLeft', {
+				force: true,
+				which: 1,
+			});
+			cy.get(
+				`path.blockera-control-spacing-shape-side.side-${side}`
+			).trigger('click', { force: true });
+		});
+	}
 }
