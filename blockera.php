cfd41313c68caf3dec637854718b6e91e470efa3
blockerabot <blockeraai+githubbot@gmail.com>
2025-04-12 16:58:41 +0000

    Bump plugin version to 1.7.0

--- blockera.php
+++ blockera.php
@@ -8,7 +8,7 @@
  * Requires PHP: 7.4
  * Author: Blockera AI
  * Author URI: https://blockera.ai/about/
- * Version: 1.6.0
+ * Version: 1.7.0
  * Text Domain: blockera
  * Domain Path: /languages
  * License: GPLv3 or later
