2ac51f815fd52897bb9c61b4a4be89efd29c1c5f
rezaelahidev <rezaelahi.dev@gmail.com>
2025-04-19 18:31:40 +0330

    feat: appended the blockeraCustomCss attribute value as inline style

--- packages/editor/js/extensions/components/block-base.js
+++ packages/editor/js/extensions/components/block-base.js
@@ -314,6 +314,10 @@ export const BlockBase: ComponentType<any> = memo((): Element<any> | null => {
 		blockName: name,
 		currentAttributes,
 		defaultAttributes,
+		customCss: attributes.blockeraCustomCSS.replace(
+			/(.|#)block/gi,
+			`#block-${clientId}`
+		),
 		activeDeviceType: getDeviceType(),
 	};
 
