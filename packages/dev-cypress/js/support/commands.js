736fd2ede481e215d04099977c997f6fcc6710db
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-02 10:56:01 +0330

    fix: command

--- packages/dev-cypress/js/support/commands.js
+++ packages/dev-cypress/js/support/commands.js
@@ -345,9 +345,11 @@ export const registerCommands = () => {
 			// Open label state graph
 			if (repeaterItem) {
 				// for repeater inner labels
-				cy.getByDataTest('popover-body').within(() => {
-					cy.getByAriaLabel(label).click({ force: true });
-				});
+				cy.getByDataTest('popover-body')
+					.last()
+					.within(() => {
+						cy.getByAriaLabel(label).click({ force: true });
+					});
 			} else
 				cy.get('h2')
 					.contains(content)
