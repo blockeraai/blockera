4f83a149728f0ea3c5cf41e18bff827e4ac8c6e2
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-05 14:19:55 +0330

    feat(Navigation Link): customize states

--- packages/blocks/core/js/wordpress/navigation-link/index.js
+++ packages/blocks/core/js/wordpress/navigation-link/index.js
@@ -1,9 +1,14 @@
 // @flow
 
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Blockera dependencies
  */
-import { SharedBlockExtension } from '@blockera/editor';
+import { SharedBlockExtension, generalBlockStates } from '@blockera/editor';
 
 /**
  * Internal dependencies
@@ -16,6 +21,42 @@ export const NavigationLink: BlockType = {
 	targetBlock: 'core/navigation-link',
 	blockeraInnerBlocks: {
 		'elements/link': sharedInnerBlocks['elements/link'],
+	},
+	availableBlockStates: {
+		...generalBlockStates,
+		'current-page': {
+			type: 'current-page',
+			label: __('Is Current Page', 'blockera'),
+			category: 'special',
+			breakpoints: {},
+			priority: 10,
+			force: true,
+			settings: {
+				color: 'var(--blockera-controls-states-color)',
+			},
+		},
+		active: {
+			type: 'active',
+			label: __('Active', 'blockera'),
+			category: 'interactive-states',
+			breakpoints: {},
+			priority: 25,
+			force: false,
+			settings: {
+				color: 'var(--blockera-controls-states-color)',
+			},
+		},
+		visited: {
+			type: 'visited',
+			label: __('Visited', 'blockera'),
+			category: 'interactive-states',
+			breakpoints: {},
+			priority: 25,
+			force: false,
+			settings: {
+				color: 'var(--blockera-controls-states-color)',
+			},
+		},
 	},
 	edit: (props) => {
 		return <SharedBlockExtension {...props} />;
