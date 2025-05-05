8c39804f528579291e2ab9a71820c587e01bc94b
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-05 14:19:36 +0330

    feat(Button Block): customize states

--- packages/blocks/core/js/wordpress/button/index.js
+++ packages/blocks/core/js/wordpress/button/index.js
@@ -1,9 +1,14 @@
 // @flow
 
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Blockera dependencies
  */
-import { SharedBlockExtension } from '@blockera/editor';
+import { SharedBlockExtension, generalBlockStates } from '@blockera/editor';
 
 /**
  * Internal dependencies
@@ -15,5 +20,34 @@ export const Button: BlockType = {
 	targetBlock: 'core/button',
 	edit: (props) => {
 		return <SharedBlockExtension {...props} />;
+	},
+	availableBlockStates: {
+		...generalBlockStates,
+		focus: {
+			...generalBlockStates.focus,
+			force: true,
+		},
+		active: {
+			type: 'active',
+			label: __('Active', 'blockera'),
+			category: 'interactive-states',
+			breakpoints: {},
+			priority: 25,
+			force: true,
+			settings: {
+				color: 'var(--blockera-controls-states-color)',
+			},
+		},
+		visited: {
+			type: 'visited',
+			label: __('Visited', 'blockera'),
+			category: 'interactive-states',
+			breakpoints: {},
+			priority: 25,
+			force: false,
+			settings: {
+				color: 'var(--blockera-controls-states-color)',
+			},
+		},
 	},
 };
