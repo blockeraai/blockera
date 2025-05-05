0cf0a20d6d161a3106c2fb97d3605fcdd203990c
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-05 21:40:21 +0330

    feat(Buttons): improve inner block

--- packages/blocks/core/js/wordpress/buttons/index.js
+++ packages/blocks/core/js/wordpress/buttons/index.js
@@ -1,6 +1,11 @@
 // @flow
 
 /**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
  * Blockera dependencies
  */
 import { SharedBlockExtension } from '@blockera/editor';
@@ -15,7 +20,10 @@ export const Buttons: BlockType = {
 	name: 'blockeraButtons',
 	targetBlock: 'core/buttons',
 	blockeraInnerBlocks: {
-		'core/button': sharedInnerBlocks['core/button'],
+		'core/button': {
+			...sharedInnerBlocks['core/button'],
+			label: __('Single Button', 'blockera'),
+		},
 	},
 	edit: (props) => {
 		return <SharedBlockExtension {...props} />;
