8ab937b218cddfedc4d176a961e20d670fa20e38
rezaelahidev <rezaelahi.dev@gmail.com>
2025-05-07 18:45:47 +0330

    feat: improved the use-editor-store hook

--- packages/editor/js/hooks/use-editor-store/index.js
+++ packages/editor/js/hooks/use-editor-store/index.js
@@ -11,22 +11,35 @@ import { useSelect } from '@wordpress/data';
 import { STORE_NAME } from '../../store/constants';
 
 export const useEditorStore = (): Object => {
-	const { availableStates, availableBreakpoints, getState } = useSelect(
-		(select) => {
-			const { getAvailableStates, getAvailableBreakpoints, getState } =
-				select(STORE_NAME);
+	const {
+		getState,
+		getInnerState,
+		availableStates,
+		availableBreakpoints,
+		getAvailableInnerState,
+	} = useSelect((select) => {
+		const {
+			getState,
+			getInnerState,
+			getAvailableStates,
+			getAvailableInnerState,
+			getAvailableBreakpoints,
+		} = select(STORE_NAME);
 
-			return {
-				getState,
-				availableStates: getAvailableStates(),
-				availableBreakpoints: getAvailableBreakpoints(),
-			};
-		}
-	);
+		return {
+			getState,
+			getInnerState,
+			getAvailableInnerState,
+			availableStates: getAvailableStates(),
+			availableBreakpoints: getAvailableBreakpoints(),
+		};
+	});
 
 	return {
 		getState,
+		getInnerState,
 		availableStates,
 		availableBreakpoints,
+		getAvailableInnerState,
 	};
 };
