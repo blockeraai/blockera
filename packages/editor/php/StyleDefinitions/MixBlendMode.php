49ca67117d30cd0d66601c06702ea2cdb2cb0e5f
rezaelahidev <rezaelahi.dev@gmail.com>
2025-05-11 15:06:44 +0330

    fix: missed import statement

--- packages/editor/php/StyleDefinitions/MixBlendMode.php
+++ packages/editor/php/StyleDefinitions/MixBlendMode.php
@@ -2,6 +2,7 @@
 
 namespace Blockera\Editor\StyleDefinitions;
 
+use Blockera\Editor\StyleDefinitions\Contracts\StandardDefinition;
 use Blockera\Editor\StyleDefinitions\Traits\SimpleDefinitionTrait;
 
 /**
