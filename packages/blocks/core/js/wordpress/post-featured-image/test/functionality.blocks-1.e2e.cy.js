1095ce579ac70e1b3e1d625c76990a0033c51c4b
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-18 10:41:02 +0330

    test(post-featured-image Block): improve test to check block card active items

--- packages/blocks/core/js/wordpress/post-featured-image/test/functionality.blocks-1.e2e.cy.js
+++ packages/blocks/core/js/wordpress/post-featured-image/test/functionality.blocks-1.e2e.cy.js
@@ -38,10 +38,7 @@ describe('Post Featured Image Block', () => {
 		// Block supported is active
 		cy.get('.blockera-extension-block-card').should('be.visible');
 
-		// No inner blocks
-		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
-			'not.exist'
-		);
+		cy.checkBlockCardItems(['normal', 'hover']);
 
 		//
 		// 1. Edit Block
