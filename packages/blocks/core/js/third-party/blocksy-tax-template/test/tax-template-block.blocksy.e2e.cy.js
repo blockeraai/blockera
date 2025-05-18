80b87492444f9cb7ce5749f0b66d48580140c976
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-18 11:50:43 +0330

    test(Blocksy → blocksy-tax-template Block): improve test to check block card active items

--- packages/blocks/core/js/third-party/blocksy-tax-template/test/tax-template-block.blocksy.e2e.cy.js
+++ packages/blocks/core/js/third-party/blocksy-tax-template/test/tax-template-block.blocksy.e2e.cy.js
@@ -37,9 +37,7 @@ describe('Blocksy → Tax Template Block → Block support', () => {
 
 		cy.get('.blockera-extension-block-card').should('be.visible');
 
-		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
-			'not.exist'
-		);
+		cy.checkBlockCardItems(['normal', 'hover']);
 
 		//
 		// 1. Edit Blocks
