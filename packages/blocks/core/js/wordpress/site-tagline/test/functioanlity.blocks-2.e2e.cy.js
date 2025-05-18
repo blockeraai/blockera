2b8dab4aa293be49fd224d0ca9ae1f27dab09ad4
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-18 10:42:36 +0330

    test(site-tagline Block): improve test to check block card active items

--- packages/blocks/core/js/wordpress/site-tagline/test/functioanlity.blocks-2.e2e.cy.js
+++ packages/blocks/core/js/wordpress/site-tagline/test/functioanlity.blocks-2.e2e.cy.js
@@ -21,10 +21,7 @@ describe('Site Tagline Block', () => {
 		// Block supported is active
 		cy.get('.blockera-extension-block-card').should('be.visible');
 
-		// No inner blocks
-		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
-			'not.exist'
-		);
+		cy.checkBlockCardItems(['normal', 'hover']);
 
 		//
 		// 1. Edit block
