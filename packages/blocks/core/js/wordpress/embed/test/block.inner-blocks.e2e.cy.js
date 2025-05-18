40ebe869d077a0e8c6d6e47c5e5e9352ed0e7006
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-18 10:39:13 +0330

    test(embed Block): improve test to check block card active items

--- packages/blocks/core/js/wordpress/embed/test/block.inner-blocks.e2e.cy.js
+++ packages/blocks/core/js/wordpress/embed/test/block.inner-blocks.e2e.cy.js
@@ -25,10 +25,7 @@ https://www.youtube.com/watch?v=H_oJZ2Cv7a0
 		// Block supported is active
 		cy.get('.blockera-extension-block-card').should('be.visible');
 
-		// No inner blocks
-		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
-			'not.exist'
-		);
+		cy.checkBlockCardItems(['normal', 'hover']);
 
 		//
 		// 1.0. Block Styles
