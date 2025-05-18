a2a511253b1f184788fb4321a2413fafbebd81a9
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-18 10:38:12 +0330

    test(comment-author-name Block): improve test to check block card active items

--- packages/blocks/core/js/wordpress/comment-author-name/test/functionality.blocks-1.e2e.cy.js
+++ packages/blocks/core/js/wordpress/comment-author-name/test/functionality.blocks-1.e2e.cy.js
@@ -28,6 +28,9 @@ describe('Comment Author Name Block', () => {
 
 		// Block supported is active
 		cy.get('.blockera-extension-block-card').should('be.visible');
+
+		cy.checkBlockCardItems(['normal', 'hover']);
+
 		//
 		// 1.0. Block Styles
 		//
@@ -51,6 +54,8 @@ describe('Comment Author Name Block', () => {
 		// 1.1. Inner blocks
 		//
 		setInnerBlock('elements/link');
+
+		cy.checkBlockCardItems(['normal', 'hover', 'focus', 'active'], true);
 
 		cy.setColorControlValue('BG Color', 'ffdbdb');
 
