1a940be0896b9dee1c2b74437c4289c95bae2d0b
Ali Aghdam <betteraliaghdam@gmail.com>
2025-05-18 11:50:41 +0330

    test(Blocksy → blocksy-tax-query Block): improve test to check block card active items

--- packages/blocks/core/js/third-party/blocksy-tax-query/test/tax-query-block.blocksy.e2e.cy.js
+++ packages/blocks/core/js/third-party/blocksy-tax-query/test/tax-query-block.blocksy.e2e.cy.js
@@ -39,9 +39,7 @@ describe('Blocksy → Advanced Taxonomies (Tax Query) Block → Block support', 
 
 		cy.get('.blockera-extension-block-card').should('be.visible');
 
-		cy.get('.blockera-extension.blockera-extension-inner-blocks').should(
-			'not.exist'
-		);
+		cy.checkBlockCardItems(['normal', 'hover']);
 
 		//
 		// 1. Edit Blocks
