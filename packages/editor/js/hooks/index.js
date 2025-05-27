ec519439987dfa4a7692235f5b407a05ab1908c7
rezaelahidev <rezaelahi.dev@gmail.com>
2025-05-27 11:41:39 +0330

    refactor: improved the use-block-side-effects hook

--- packages/editor/js/hooks/index.js
+++ packages/editor/js/hooks/index.js
@@ -10,8 +10,10 @@ export { useExtensionsStore } from './use-extensions-store';
 export { useBlockExtensions } from './use-block-extensions';
 export { useInnerBlocksInfo } from './use-inner-blocks-info';
 export { useStoreDispatchers } from './use-store-dispatchers';
-export { useBlockSideEffects } from './use-block-side-effects';
 export { useAdvancedLabelProps } from './use-advanced-label-props';
+export {
+	useBlockSideEffects,
+	useBlockSideEffectsRestore,
+} from './use-block-side-effects';
 export { useDisplayBlockControls } from './use-display-block-controls';
-export { useBlockSideEffectsRestore } from './use-block-side-effects-restore';
 export { useCalculateCurrentAttributes } from './use-calculate-current-attributes';
