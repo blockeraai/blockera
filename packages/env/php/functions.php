1cb8dbdeaa70598836648a70a0a5671be6f662ee
rezaelahidev <rezaelahi.dev@gmail.com>
2025-06-28 19:23:08 +0330

    chore: modified the experimental.config.json

--- packages/env/php/functions.php
+++ packages/env/php/functions.php
@@ -10,15 +10,16 @@ if ( ! function_exists( 'blockera_get_experimental' ) ) {
 	/**
 	 * Get the blockera experimental support configuration.
 	 *
-	 * @param array $support_path the array of paths to access support configuration.
+	 * @param array  $support_path the array of paths to access support configuration.
+	 * @param string $plugin_slug the plugin slug.
 	 *
 	 * @return mixed the value of experimental support.
 	 */
-	function blockera_get_experimental( array $support_path ) {
+	function blockera_get_experimental( array $support_path, string $plugin_slug = 'blockera' ) {
 		static $blockera_experimental_config_cache = null;
 
 		if ( null === $blockera_experimental_config_cache ) {
-			$experimental_config_file = dirname( __DIR__, 3 ) . '/experimental.config.json';
+			$experimental_config_file = WP_PLUGIN_DIR . '/' . $plugin_slug . '/experimental.config.json';
 
 			if ( ! file_exists( $experimental_config_file ) ) {
 				
