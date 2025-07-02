681f6ecb380854caadfe7ba134c49c1265c12274
rezaelahidev <rezaelahi.dev@gmail.com>
2025-07-02 08:21:24 +0330

    feat: added the supports to check experimental features status

--- packages/features/src/Modules/Icon/Icon.php
+++ packages/features/src/Modules/Icon/Icon.php
@@ -4,8 +4,9 @@ namespace Blockera\Features\Modules\Icon;
 
 use Blockera\Features\Traits\Singleton;
 use Blockera\Features\Contracts\FeatureInterface;
+use Blockera\Features\Contracts\EditableBlockHTML;
 
-class Icon implements FeatureInterface {
+class Icon implements FeatureInterface, EditableBlockHTML {
 
     use Singleton;
 
@@ -44,8 +45,21 @@ class Icon implements FeatureInterface {
 		return true;
     }
 
+	/**
+	 * Manipulate the html of the block.
+	 *
+	 * @param array $data the data of the block.
+	 *
+	 * @return string the manipulated html.
+	 */
 	public function htmlManipulate( array $data ): string {
 		
+		// If icon feature is experimental, check if it's enabled.
+		if (isset($data['experimental-features-status']['icon']) && ! $data['experimental-features-status']['icon']) {
+			
+			return $data['html'] ?? '';
+		}
+
 		return $this->edit_block_html->htmlManipulate( $data );
 	}
 }
