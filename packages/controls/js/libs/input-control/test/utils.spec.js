97d374febf542e7074a98af3cc869aaf3bf5a0c3
Ali Aghdam <betteraliaghdam@gmail.com>
2025-07-12 10:15:44 +0330

    test(Input): check media query unite type

--- packages/controls/js/libs/input-control/test/utils.spec.js
+++ packages/controls/js/libs/input-control/test/utils.spec.js
@@ -1807,6 +1807,31 @@ describe('Util functions', () => {
 			]);
 		});
 
+		test('media-query', () => {
+			expect(getCSSUnits('media-query')).toStrictEqual([
+				{
+					label: 'Common Value',
+					options: [
+						{
+							value: 'px',
+							label: 'PX',
+							format: 'number',
+						},
+					],
+				},
+				{
+					label: 'Advanced',
+					options: [
+						{
+							value: 'func',
+							label: 'CSS Functions',
+							format: 'text',
+						},
+					],
+				},
+			]);
+		});
+
 		test('line-height', () => {
 			expect(getCSSUnits('line-height')).toStrictEqual([
 				{
