d35682543ed144a0eac769918235f96f75d464b4
rezaelahidev <rezaelahi.dev@gmail.com>
2025-07-21 11:34:54 +0330

    style: eslint fix

--- packages/features/src/Js/components/extension-slot-fill.js
+++ packages/features/src/Js/components/extension-slot-fill.js
@@ -86,6 +86,7 @@ export const ExtensionSlotFill = (props: TExtensionSlotFillProps) => {
 		}
 
 		return mapped;
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [props]);
 
 	if (!mappedExtensions.length) {
