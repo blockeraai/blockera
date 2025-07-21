09360588f762053ee3cb9c0e75787531b1dc6f20
rezaelahidev <rezaelahi.dev@gmail.com>
2025-07-21 17:45:50 +0330

    feat: configure elements/icon inner block for icon feature

--- packages/features/src/Library/Icon/config.js
+++ packages/features/src/Library/Icon/config.js
@@ -1,5 +1,16 @@
 // @flow
 
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Blockera dependencies
+ */
+import { Icon } from '@blockera/icons';
+import { generalInnerBlockStates } from '@blockera/editor/js/extensions/libs/block-card/block-states/states';
+
 export const icon = {
 	block: {
 		status: true,
@@ -17,6 +28,31 @@ export const icon = {
 		contextualToolbar: {
 			status: false,
 			type: 'none',
+		},
+		innerBlocks: {
+			status: true,
+			items: {
+				'elements/icon': {
+					name: 'elements/icon',
+					label: __('Icons', 'blockera'),
+					description: __('All icon elements.', 'blockera'),
+					icon: <Icon icon="button" library="wp" iconSize="20" />,
+					settings: {
+						force: false,
+						priority: 10,
+					},
+					availableBlockStates: {
+						...generalInnerBlockStates,
+						focus: {
+							...generalInnerBlockStates.focus,
+							force: true,
+						},
+					},
+				},
+			},
+		},
+		blockStates: {
+			status: false,
 		},
 	},
 };
