8102dc1c34470c6e072b8e54fa8e80d8d16f4327
Ali Aghdam <betteraliaghdam@gmail.com>
2025-07-23 10:06:45 +0330

    feat(Icon): show settings only if an icon was picked

--- packages/features/src/Library/Icon/extension/extension.js
+++ packages/features/src/Library/Icon/extension/extension.js
@@ -75,206 +75,263 @@ export const IconExtension: ComponentType<{
 				className={extensionClassNames('icon')}
 			>
 				{isActiveField(blockeraIcon) && (
-					<ControlContextProvider
-						value={{
-							name: generateExtensionId(block, 'icon'),
-							value: icon,
-							attribute: 'blockeraIcon',
-							blockName: block.blockName,
-						}}
-					>
-						<IconControl
-							columns="columns-1"
-							suggestionsQuery={() => {
-								return 'button';
-							}}
-							onChange={(newValue, ref) =>
-								handleOnChangeAttributes(
-									'blockeraIcon',
-									newValue,
-									{ ref }
-								)
-							}
-							{...extensionProps.blockeraIcon}
-						/>
-					</ControlContextProvider>
-				)}
-
-				{isActiveField(blockeraIconOptions) && (
 					<>
-						<BaseControl
-							label={__('Style', 'blockera')}
-							columns="1fr 3fr"
+						<ControlContextProvider
+							value={{
+								name: generateExtensionId(block, 'icon'),
+								value: icon,
+								attribute: 'blockeraIcon',
+								blockName: block.blockName,
+							}}
 						>
-							{isActiveField(blockeraIconPosition) && (
-								<ControlContextProvider
-									value={{
-										name: generateExtensionId(
-											block,
-											'icon-position'
-										),
-										value: iconPosition,
-										attribute: 'blockeraIconPosition',
-										blockName: block.blockName,
-									}}
-								>
-									<ToggleSelectControl
-										label={__('Position', 'blockera')}
-										columns="columns-2"
-										options={[
-											{
-												label: __('Left', 'blockera'),
-												value: 'left',
-												icon: (
-													<Icon
-														icon="icon-position-left"
-														iconSize="18"
-													/>
-												),
-											},
-											{
-												label: __('Right', 'blockera'),
-												value: 'right',
-												icon: (
-													<Icon
-														icon="icon-position-right"
-														iconSize="18"
-													/>
-												),
-											},
-										]}
-										isDeselectable={true}
-										//
-										defaultValue=""
-										onChange={(newValue, ref) =>
-											handleOnChangeAttributes(
-												'blockeraIconPosition',
-												newValue,
-												{ ref }
-											)
-										}
-										{...extensionProps.blockeraIconPosition}
-									/>
-								</ControlContextProvider>
-							)}
-
-							{isActiveField(blockeraIconGap) && (
-								<ControlContextProvider
-									value={{
-										name: generateExtensionId(
-											block,
-											'icon-gap'
-										),
-										value: iconGap,
-										attribute: 'blockeraIconGap',
-										blockName: block.blockName,
-									}}
-								>
-									<InputControl
-										label={__('Gap', 'blockera')}
-										columns="columns-2"
-										unitType="essential"
-										defaultValue=""
-										min={0}
-										onChange={(newValue, ref) =>
-											handleOnChangeAttributes(
-												'blockeraIconGap',
-												newValue,
-												{ ref }
-											)
-										}
-										{...extensionProps.blockeraIconGap}
-									/>
-								</ControlContextProvider>
-							)}
-
-							{isActiveField(blockeraIconSize) && (
-								<ControlContextProvider
-									value={{
-										name: generateExtensionId(
-											block,
-											'icon-size'
-										),
-										value: iconSize,
-										attribute: 'blockeraIconSize',
-										blockName: block.blockName,
-									}}
-								>
-									<InputControl
-										label={__('Size', 'blockera')}
-										columns="columns-2"
-										unitType="essential"
-										defaultValue=""
-										min={0}
-										onChange={(newValue, ref) =>
-											handleOnChangeAttributes(
-												'blockeraIconSize',
-												newValue,
-												{ ref }
-											)
-										}
-										{...extensionProps.blockeraIconSize}
-									/>
-								</ControlContextProvider>
-							)}
-
-							{isActiveField(blockeraIconColor) && (
-								<ControlContextProvider
-									value={{
-										name: generateExtensionId(
-											block,
-											'icon-color'
-										),
-										value: iconColor,
-										attribute: 'blockeraIconColor',
-										blockName: block.blockName,
-									}}
-								>
-									<ColorControl
-										label={__('Color', 'blockera')}
-										columns="columns-2"
-										//
-										defaultValue=""
-										onChange={(newValue, ref) =>
-											handleOnChangeAttributes(
-												'blockeraIconColor',
-												newValue,
-												{ ref }
-											)
-										}
-										{...extensionProps.blockeraIconColor}
-									/>
-								</ControlContextProvider>
-							)}
-						</BaseControl>
-
-						{isActiveField(blockeraIconLink) && (
-							<ControlContextProvider
-								value={{
-									name: generateExtensionId(
-										block,
-										'icon-link'
-									),
-									value: iconLink,
-									attribute: 'blockeraIconLink',
-									blockName: block.blockName,
+							<IconControl
+								columns="columns-1"
+								suggestionsQuery={() => {
+									return 'button';
 								}}
-							>
-								<LinkControl
-									columns="1fr 3fr"
-									label={__('Link', 'blockera')}
-									id={generateExtensionId(block, 'icon-link')}
-									//
-									onChange={(newValue, ref) =>
-										handleOnChangeAttributes(
-											'blockeraIconLink',
-											newValue,
-											{ ref }
-										)
-									}
-									{...extensionProps.blockeraIconLink}
-								/>
-							</ControlContextProvider>
+								onChange={(newValue, ref) =>
+									handleOnChangeAttributes(
+										'blockeraIcon',
+										newValue,
+										{ ref }
+									)
+								}
+								{...extensionProps.blockeraIcon}
+							/>
+						</ControlContextProvider>
+
+						{(icon?.icon || icon?.uploadSVG) && (
+							<>
+								{isActiveField(blockeraIconOptions) && (
+									<>
+										<BaseControl
+											label={__('Style', 'blockera')}
+											columns="1fr 3fr"
+										>
+											{isActiveField(
+												blockeraIconPosition
+											) && (
+												<ControlContextProvider
+													value={{
+														name: generateExtensionId(
+															block,
+															'icon-position'
+														),
+														value: iconPosition,
+														attribute:
+															'blockeraIconPosition',
+														blockName:
+															block.blockName,
+													}}
+												>
+													<ToggleSelectControl
+														label={__(
+															'Position',
+															'blockera'
+														)}
+														columns="columns-2"
+														options={[
+															{
+																label: __(
+																	'Left',
+																	'blockera'
+																),
+																value: 'left',
+																icon: (
+																	<Icon
+																		icon="icon-position-left"
+																		iconSize="18"
+																	/>
+																),
+															},
+															{
+																label: __(
+																	'Right',
+																	'blockera'
+																),
+																value: 'right',
+																icon: (
+																	<Icon
+																		icon="icon-position-right"
+																		iconSize="18"
+																	/>
+																),
+															},
+														]}
+														isDeselectable={true}
+														//
+														defaultValue=""
+														onChange={(
+															newValue,
+															ref
+														) =>
+															handleOnChangeAttributes(
+																'blockeraIconPosition',
+																newValue,
+																{ ref }
+															)
+														}
+														{...extensionProps.blockeraIconPosition}
+													/>
+												</ControlContextProvider>
+											)}
+
+											{isActiveField(blockeraIconGap) && (
+												<ControlContextProvider
+													value={{
+														name: generateExtensionId(
+															block,
+															'icon-gap'
+														),
+														value: iconGap,
+														attribute:
+															'blockeraIconGap',
+														blockName:
+															block.blockName,
+													}}
+												>
+													<InputControl
+														label={__(
+															'Gap',
+															'blockera'
+														)}
+														columns="columns-2"
+														unitType="essential"
+														defaultValue=""
+														min={0}
+														onChange={(
+															newValue,
+															ref
+														) =>
+															handleOnChangeAttributes(
+																'blockeraIconGap',
+																newValue,
+																{ ref }
+															)
+														}
+														{...extensionProps.blockeraIconGap}
+													/>
+												</ControlContextProvider>
+											)}
+
+											{isActiveField(
+												blockeraIconSize
+											) && (
+												<ControlContextProvider
+													value={{
+														name: generateExtensionId(
+															block,
+															'icon-size'
+														),
+														value: iconSize,
+														attribute:
+															'blockeraIconSize',
+														blockName:
+															block.blockName,
+													}}
+												>
+													<InputControl
+														label={__(
+															'Size',
+															'blockera'
+														)}
+														columns="columns-2"
+														unitType="essential"
+														defaultValue=""
+														min={0}
+														onChange={(
+															newValue,
+															ref
+														) =>
+															handleOnChangeAttributes(
+																'blockeraIconSize',
+																newValue,
+																{ ref }
+															)
+														}
+														{...extensionProps.blockeraIconSize}
+													/>
+												</ControlContextProvider>
+											)}
+
+											{isActiveField(
+												blockeraIconColor
+											) && (
+												<ControlContextProvider
+													value={{
+														name: generateExtensionId(
+															block,
+															'icon-color'
+														),
+														value: iconColor,
+														attribute:
+															'blockeraIconColor',
+														blockName:
+															block.blockName,
+													}}
+												>
+													<ColorControl
+														label={__(
+															'Color',
+															'blockera'
+														)}
+														columns="columns-2"
+														//
+														defaultValue=""
+														onChange={(
+															newValue,
+															ref
+														) =>
+															handleOnChangeAttributes(
+																'blockeraIconColor',
+																newValue,
+																{ ref }
+															)
+														}
+														{...extensionProps.blockeraIconColor}
+													/>
+												</ControlContextProvider>
+											)}
+										</BaseControl>
+
+										{isActiveField(blockeraIconLink) && (
+											<ControlContextProvider
+												value={{
+													name: generateExtensionId(
+														block,
+														'icon-link'
+													),
+													value: iconLink,
+													attribute:
+														'blockeraIconLink',
+													blockName: block.blockName,
+												}}
+											>
+												<LinkControl
+													columns="1fr 3fr"
+													label={__(
+														'Link',
+														'blockera'
+													)}
+													id={generateExtensionId(
+														block,
+														'icon-link'
+													)}
+													//
+													onChange={(newValue, ref) =>
+														handleOnChangeAttributes(
+															'blockeraIconLink',
+															newValue,
+															{ ref }
+														)
+													}
+													{...extensionProps.blockeraIconLink}
+												/>
+											</ControlContextProvider>
+										)}
+									</>
+								)}
+							</>
 						)}
 					</>
 				)}
