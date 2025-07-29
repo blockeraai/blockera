3af80049d6194f63d8026a719ebb08c85a04d921
Ali Aghdam <betteraliaghdam@gmail.com>
2025-07-29 13:11:46 +0330

    feat(Icons Search): config moved to separate file

--- packages/icons/js/icon-search.js
+++ packages/icons/js/icon-search.js
@@ -18,6 +18,8 @@ import {
 import { isValidIcon } from './icon';
 import { type IconLibraryTypes } from './types';
 
+const searchConfig = require('./search-config.json');
+
 export function iconSearch({
 	query,
 	library = 'all',
@@ -32,33 +34,9 @@ export function iconSearch({
 	}
 
 	const getMemoizedResult = memoize(() => {
-		// We define which keys in our objects should be searched and how much weight they should have.
-		// A higher weight means matches in that key are more relevant.
-		const options = {
-			// `includeScore` is useful for debugging or advanced sorting.
-			includeScore: true,
-			// `threshold` determines how "fuzzy" the search is. 0.0 is a perfect match, 1.0 matches anything.
-			threshold: 0.4,
-			// `keys` specifies the properties to search in.
-			keys: [
-				{
-					name: 'title', // Search the 'title' property
-					weight: 2, // Give it a high weight, as titles are very important.
-				},
-				{
-					name: 'tags', // Search the 'tags' array
-					weight: 1.5, // Tags are also very important.
-				},
-				{
-					name: 'iconName', // Search the 'iconName'
-					weight: 1, // Less important than title/tags, but still relevant.
-				},
-			],
-		};
-
 		const fuse = new Fuse(
 			getIconLibrarySearchData(library),
-			options,
+			searchConfig,
 			getIconLibrariesSearchIndex()
 		);
 
