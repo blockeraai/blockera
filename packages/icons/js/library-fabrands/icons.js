20d78b737fc5d2b9a6edd19272198856b32702db
Ali Aghdam <betteraliaghdam@gmail.com>
2025-08-13 12:49:34 +0330

    fix(Icons fa-brands): icon keys and search data

--- packages/icons/js/library-fabrands/icons.js
+++ packages/icons/js/library-fabrands/icons.js
@@ -20,47 +20,47 @@ const FaBrandsIcons: Object = Object.fromEntries(
 
 			switch (newKey) {
 				case 'fa-5-0-0px':
-					newKey = '500px';
+					newKey = 'fa-500px';
 					break;
 
 				case 'fa-1-1ty':
-					newKey = '11ty';
+					newKey = 'fa-11ty';
 					break;
 
 				case 'fa-4-2-group':
-					newKey = '42-group';
+					newKey = 'fa-42-group';
 					break;
 
 				case 'fa-css-3':
-					newKey = 'css3';
+					newKey = 'fa-css3';
 					break;
 
 				case 'fa-css-3-alt':
-					newKey = 'css3-alt';
+					newKey = 'fa-css3-alt';
 					break;
 
 				case 'fa-html-5':
-					newKey = 'html5';
+					newKey = 'fa-html5';
 					break;
 
 				case 'fa-draft-2digital':
-					newKey = 'draft2digital';
+					newKey = 'fa-draft2digital';
 					break;
 
 				case 'fa-ns-8':
-					newKey = 'ns8';
+					newKey = 'fa-ns8';
 					break;
 
 				case 'fa-page-4':
-					newKey = 'page4';
+					newKey = 'fa-page4';
 					break;
 
 				case 'fa-typo-3':
-					newKey = 'typo3';
+					newKey = 'fa-typo3';
 					break;
 
 				case 'fa-w-3c':
-					newKey = 'w3c';
+					newKey = 'fa-w3c';
 					break;
 
 				default:
