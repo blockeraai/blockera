495da6904e9463beec169a80fd86413390b3eee5
rezaelahidev <rezaelahi.dev@gmail.com>
2025-08-31 20:21:18 +0330

    fix: needToUpdateHtml method functionality in Render v1

--- packages/wordpress/php/RenderBlock/V1/Render.php
+++ packages/wordpress/php/RenderBlock/V1/Render.php
@@ -285,8 +285,8 @@ class Render {
      */
     protected function needToUpdateHTML( string $block_classname, string $inner_html): bool {
 
-        // Imagine th block classname or classnames property is empty, so we should update html output.
-        if (empty($block_classname) || empty($this->classnames)) {
+        // Imagine the block classname and classnames property is empty, so we should update html output.
+        if (empty($block_classname) && empty($this->classnames)) {
 
             return true;
         }
