9fbd2df0354ddb2f46d3ce26c3705aba5f3f04e8
rezaelahidev <rezaelahi.dev@gmail.com>
2025-09-06 12:28:55 +0330

    fix: the button block selectors

--- packages/blocks-core/php/libs/wordpress/button/block.php
+++ packages/blocks-core/php/libs/wordpress/button/block.php
@@ -14,7 +14,7 @@ return array_merge(
 			$args['selectors'] ?? [],
 			[
 				'blockera/elements/icon' => [
-					'root' => ' .wp-block-button__link .blockera-icon',
+					'root' => ' .wp-block-button__link:before',
 				],
 				'htmlEditable' => [
 					'root' => '.wp-block-button .wp-block-button__link div[role="textbox"][contenteditable="true"], .wp-block-button .wp-block-button__link',
