ed3b6e2eb313e5ff97713ce7d4d641a07085c2b3
rezaelahidev <rezaelahi.dev@gmail.com>
2025-09-15 17:53:32 +0330

    feat: enhanced the BlockGlobalStylesPanelScreen component

--- packages/editor/js/canvas-editor/components/block-global-styles-panel-screen/index.js
+++ packages/editor/js/canvas-editor/components/block-global-styles-panel-screen/index.js
@@ -6,7 +6,8 @@
 import type { MixedElement } from 'react';
 import { getBlockType } from '@wordpress/blocks';
 import { select, dispatch } from '@wordpress/data';
-import { useEffect, createPortal } from '@wordpress/element';
+import { store as blockEditorStore } from '@wordpress/block-editor';
+import { useState, useEffect, createPortal } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -22,11 +23,15 @@ export const BlockGlobalStylesPanelScreen = ({
 }): MixedElement => {
 	const className = 'blockera-extensions-wrapper';
 
+	const { getSelectedBlock } = select(blockEditorStore);
 	const { getSelectedBlockStyle } = select(STORE_NAME);
 	const { setSelectedBlockStyle, setSelectedBlockStyleVariation } =
 		dispatch(STORE_NAME);
+	const selectedBlock = getSelectedBlock();
 	const selectedBlockStyle = getSelectedBlockStyle();
-	const blockType = getBlockType(selectedBlockStyle);
+	const [blockType, setBlockType] = useState(
+		getBlockType(selectedBlockStyle)
+	);
 	const screenElement = document.querySelector(screen);
 	const hasBlockeraExtensions = blockType?.attributes?.blockeraPropsId;
 
@@ -37,24 +42,39 @@ export const BlockGlobalStylesPanelScreen = ({
 	});
 
 	useEffect(() => {
+		if (
+			(!selectedBlockStyle && selectedBlock) ||
+			(selectedBlockStyle &&
+				selectedBlock &&
+				selectedBlock?.name !== selectedBlockStyle)
+		) {
+			setBlockType(getBlockType(selectedBlock?.name));
+			setSelectedBlockStyle(selectedBlock?.name);
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [selectedBlock, selectedBlockStyle]);
+
+	useEffect(() => {
 		if (!hasBlockeraExtensions && selectedBlockStyle) {
 			screenElement.classList.remove('has-blockera-extensions');
 			screenElement.classList.add('has-not-blockera-extensions');
 		}
-
-		return () => screenElement.removeChild(`.${className}`);
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
+	}, [selectedBlockStyle, hasBlockeraExtensions]);
 
 	if (!hasBlockeraExtensions) {
 		return <></>;
 	}
 
-	screenElement.classList.add('has-blockera-extensions');
-
-	if (screenElement.querySelector(`.${className}`)) {
+	if (
+		selectedBlockStyle &&
+		selectedBlock &&
+		selectedBlock?.name !== selectedBlockStyle
+	) {
 		return <></>;
 	}
+
+	screenElement.classList.add('has-blockera-extensions');
 
 	return createPortal(
 		<div className={className}>
