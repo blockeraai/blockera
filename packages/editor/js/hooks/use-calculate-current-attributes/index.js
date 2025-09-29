5def7872500e72378195ed4fc072a121ae322c9f
rezaelahidev <rezaelahi.dev@gmail.com>
2025-09-29 09:23:45 +0330

    feat: stabilized the custom hooks

--- packages/editor/js/hooks/use-calculate-current-attributes/index.js
+++ packages/editor/js/hooks/use-calculate-current-attributes/index.js
@@ -1,6 +1,11 @@
 // @flow
 
 /**
+ * External dependencies
+ */
+import { useMemo } from '@wordpress/element';
+
+/**
  * Blockera dependencies
  */
 import { prepare } from '@blockera/data-editor';
@@ -24,41 +29,57 @@ export const useCalculateCurrentAttributes = ({
 	currentInnerBlock,
 	blockeraInnerBlocks,
 }: CalculateCurrentAttributesProps): Object => {
-	let currentAttributes: Object = {};
 	const { currentBlock, currentState, currentBreakpoint } =
 		useExtensionsStore({ name, clientId });
-	const blockAttributesDefaults =
-		prepareBlockeraDefaultAttributesValues(blockAttributes);
 
-	// Assume block is inner block type.
-	if (isInnerBlock(currentBlock)) {
-		currentAttributes = {
-			...blockAttributesDefaults,
-			...currentInnerBlock,
-		};
-	}
-	// Assume master block in normal state.
-	else if (isNormalStateOnBaseBreakpoint(currentState, currentBreakpoint)) {
-		currentAttributes = {
-			...blockAttributesDefaults,
-			...attributes,
-		};
-	}
-	// Assume master block is not in normal state and base breakpoint.
-	else if (!isNormalStateOnBaseBreakpoint(currentState, currentBreakpoint)) {
-		currentAttributes = {
-			...blockAttributesDefaults,
-			...attributes,
-			...(prepare(
-				`blockeraBlockStates[${currentState}].breakpoints[${currentBreakpoint}].attributes`,
-				attributes
-			) || {}),
-		};
-	}
+	return useMemo(() => {
+		let currentAttributes: Object = {};
 
-	if (!Object(currentAttributes?.blockeraInnerBlocks).length) {
-		currentAttributes.blockeraInnerBlocks = { ...blockeraInnerBlocks };
-	}
+		const blockAttributesDefaults =
+			prepareBlockeraDefaultAttributesValues(blockAttributes);
 
-	return currentAttributes;
+		// Assume block is inner block type.
+		if (isInnerBlock(currentBlock)) {
+			currentAttributes = {
+				...blockAttributesDefaults,
+				...currentInnerBlock,
+			};
+		}
+		// Assume master block in normal state.
+		else if (
+			isNormalStateOnBaseBreakpoint(currentState, currentBreakpoint)
+		) {
+			currentAttributes = {
+				...blockAttributesDefaults,
+				...attributes,
+			};
+		}
+		// Assume master block is not in normal state and base breakpoint.
+		else if (
+			!isNormalStateOnBaseBreakpoint(currentState, currentBreakpoint)
+		) {
+			currentAttributes = {
+				...blockAttributesDefaults,
+				...attributes,
+				...(prepare(
+					`blockeraBlockStates[${currentState}].breakpoints[${currentBreakpoint}].attributes`,
+					attributes
+				) || {}),
+			};
+		}
+
+		if (!Object(currentAttributes?.blockeraInnerBlocks).length) {
+			currentAttributes.blockeraInnerBlocks = { ...blockeraInnerBlocks };
+		}
+
+		return currentAttributes;
+	}, [
+		attributes,
+		currentBlock,
+		currentState,
+		blockAttributes,
+		currentBreakpoint,
+		currentInnerBlock,
+		blockeraInnerBlocks,
+	]);
 };
