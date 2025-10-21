899bddb666669abaa03fb57d8393a1e8058d1a97
rezaelahidev <rezaelahi.dev@gmail.com>
2025-10-21 16:13:43 +0330

    test(global-styles): style engine new test

--- packages/editor/js/style-engine/test/style-engine.global-styles.e2e.cy.js
+++ packages/editor/js/style-engine/test/style-engine.global-styles.e2e.cy.js
@@ -1,6 +1,7 @@
 import {
 	goTo,
 	savePage,
+	setBlockState,
 	getWPDataObject,
 	redirectToFrontPage,
 	getSelectedBlockStyle,
@@ -50,6 +51,76 @@ describe('Style Engine → Global Styles', () => {
 				'include',
 				`:root :where(p) {
  font-size: 10px !important; 
+}`
+			);
+	});
+
+	it('should generate css for normal and hover states of all paragraphs cross website pages', () => {
+		cy.getByDataTest('style-default').click();
+
+		cy.getParentContainer('Size').within(() => {
+			cy.get('input[type="text"]').clear();
+			cy.get('input[type="text"]').type(10, {
+				force: true,
+			});
+		});
+
+		//Check block
+		cy.getBlock('core/paragraph').should('have.css', 'font-size', '10px');
+
+		//Check store
+		getWPDataObject().then((data) => {
+			expect('10px').to.be.equal(
+				getSelectedBlockStyle(data, 'core/paragraph', 'default')
+					?.blockeraFontSize?.value
+			);
+		});
+
+		setBlockState('Hover');
+
+		cy.getParentContainer('Size').within(() => {
+			cy.get('input[type="text"]').clear();
+			cy.get('input[type="text"]').type(20, {
+				force: true,
+			});
+		});
+
+		//Check block
+		cy.getBlock('core/paragraph').realHover();
+		cy.getBlock('core/paragraph').should('have.css', 'font-size', '20px');
+
+		//Check store
+		getWPDataObject().then((data) => {
+			expect('20px').to.be.equal(
+				getSelectedBlockStyle(data, 'core/paragraph', 'default')
+					?.blockeraFontSize?.value
+			);
+		});
+
+		//Check frontend
+		savePage();
+
+		redirectToFrontPage();
+
+		cy.get('.blockera-block').should('have.css', 'font-size', '10px');
+		cy.get('.blockera-block').realHover();
+		cy.get('.blockera-block').should('have.css', 'font-size', '20px');
+
+		cy.get('#global-styles-inline-css')
+			.invoke('text')
+			.should(
+				'include',
+				`:root :where(p) {
+ font-size: 10px !important; 
+}`
+			);
+
+		cy.get('#global-styles-inline-css')
+			.invoke('text')
+			.should(
+				'include',
+				`:root :where(p:hover) {
+ font-size: 20px !important; 
 }`
 			);
 	});
