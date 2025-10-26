f1695fe3d01a1ddd02a2838a600396c4c0c63b14
Ali Aghdam <betteraliaghdam@gmail.com>
2025-10-26 19:48:15 +0330

    test(Dashboard): improve to make it more relaiable

--- packages/blockera-admin/js/test/blockera-dashboard.visual.cy.js
+++ packages/blockera-admin/js/test/blockera-dashboard.visual.cy.js
@@ -4,6 +4,12 @@ describe('Visual Regression Test', () => {
 	it('screenshot dashboard', () => {
 		goTo('/wp-admin/admin.php?page=blockera-settings-dashboard');
 
+		// disable wp footer to avoid screenshot issue
+		cy.get('#wpfooter').invoke('css', 'display', 'none');
+
+		// disable sticky menu to avoid screenshot issue
+		cy.get('#adminmenuwrap').invoke('css', 'position', 'relative');
+
 		cy.compareSnapshot({
 			name: 'dashboard',
 			testThreshold: 0.2,
