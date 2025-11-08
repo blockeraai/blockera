f564af212c2f8524e4943c1f48d592fa2a80be9b
rezaelahidev <rezaelahi.dev@gmail.com>
2025-11-08 13:59:29 +0330

    refactor(inner-headings): updated the selectors

--- packages/blocks-core/php/libs/wordpress/inners/headings.php
+++ packages/blocks-core/php/libs/wordpress/inners/headings.php
@@ -11,22 +11,22 @@ return [
 	'blockera/core/heading' => [
 		'root' => ':is(h1,h2,h3,h4,h5,h6).wp-block-heading',
 	],
-	'heading1'              => [
+	'blockera/core/heading-1'              => [
 		'root' => 'h1.wp-block-heading',
 	],
-	'heading2'              => [
+	'blockera/core/heading-2'              => [
 		'root' => 'h2.wp-block-heading',
 	],
-	'heading3'              => [
+	'blockera/core/heading-3'              => [
 		'root' => 'h3.wp-block-heading',
 	],
-	'heading4'              => [
+	'blockera/core/heading-4'              => [
 		'root' => 'h4.wp-block-heading',
 	],
-	'heading5'              => [
+	'blockera/core/heading-5'              => [
 		'root' => 'h5.wp-block-heading',
 	],
-	'heading6'              => [
+	'blockera/core/heading-6'              => [
 		'root' => 'h6.wp-block-heading',
 	],
 ];
