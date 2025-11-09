5386a7b1ba0e4e26c1e1150061c034d69213f2ec
Ali Aghdam <betteraliaghdam@gmail.com>
2025-11-09 18:47:35 +0330

    feat(TextWrap): enable front style generator

--- packages/editor/php/Providers/StyleDefinitionsProvider.php
+++ packages/editor/php/Providers/StyleDefinitionsProvider.php
@@ -66,6 +66,7 @@ class StyleDefinitionsProvider extends ServiceProvider {
 				'Overflow' => \Blockera\Editor\StyleDefinitions\Overflow::class,
 				'Outline' => \Blockera\Editor\StyleDefinitions\Outline::class,
 				'TextIndent' => \Blockera\Editor\StyleDefinitions\TextIndent::class,
+				'TextWrap' => \Blockera\Editor\StyleDefinitions\TextWrap::class,
 				'Flex' => \Blockera\Editor\StyleDefinitions\Flex::class,
 				'Position' => \Blockera\Editor\StyleDefinitions\Position::class,
 				'Spacing' => \Blockera\Editor\StyleDefinitions\Spacing::class,
