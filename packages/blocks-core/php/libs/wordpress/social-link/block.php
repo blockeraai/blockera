fad880ac94cae2cda6d3774807b95d48f777c39c
Ali Aghdam <betteraliaghdam@gmail.com>
2025-12-07 17:26:42 +0330

    feat(Social Links Block): improve support

--- packages/blocks-core/php/libs/wordpress/social-link/block.php
+++ packages/blocks-core/php/libs/wordpress/social-link/block.php
@@ -13,6 +13,9 @@ return array_merge(
 		'selectors' => array_merge(
 			$args['selectors'] ?? [],
 			[
+				'layout' => '.wp-block-social-link .wp-block-social-link-anchor',
+				'flex-child' => '.wp-block-social-link',
+				// inner blocks selectors.
 				'blockera/elements/item-icon' => [
 					'root' => 'svg',
 				],
