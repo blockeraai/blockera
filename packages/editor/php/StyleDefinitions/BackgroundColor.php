d42684c8ed7c1bda60caed9e21df8a309087be86
Ali Aghdam <betteraliaghdam@gmail.com>
2025-12-11 02:37:15 +0330

    feat(BackgroundColor definition): optimize code for performance

--- packages/editor/php/StyleDefinitions/BackgroundColor.php
+++ packages/editor/php/StyleDefinitions/BackgroundColor.php
@@ -7,18 +7,14 @@ use Blockera\Editor\StyleDefinitions\BaseStyleDefinition;
 class BackgroundColor extends BaseStyleDefinition {
 
     protected function css( array $setting): array {
-        
-		$declaration = [];
-		$cssProperty = $setting['type'];
+		$cssProperty = $setting['type'] ?? null;
 
-		if ( empty( $cssProperty ) || empty( $setting[ $cssProperty ] ) || 'background-color' !== $cssProperty ) {
-
-			return $declaration;
+		if ( 'background-color' !== $cssProperty || ! isset( $setting[ $cssProperty ] ) || '' === $setting[ $cssProperty ] ) {
+			return [];
 		}
 
-		$this->setDeclaration($cssProperty, blockera_get_value_addon_real_value($setting[ $cssProperty ]));	
-
-		$this->setCss($this->declarations);
+		$this->setDeclaration( $cssProperty, blockera_get_value_addon_real_value( $setting[ $cssProperty ] ) );
+		$this->setCss( $this->declarations );
 
 		return $this->css;
     }
