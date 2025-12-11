fc6b379d572dab87d4bc4e2d05ce84565f6b58f2
Ali Aghdam <betteraliaghdam@gmail.com>
2025-12-11 04:24:17 +0330

    feat(Order definition): optimize code for performance

--- packages/editor/php/StyleDefinitions/Order.php
+++ packages/editor/php/StyleDefinitions/Order.php
@@ -8,28 +8,32 @@ class Order extends BaseStyleDefinition implements CustomStyle {
 
     protected function css( array $setting): array {
 
-        $declaration = [];
+        if (! isset($setting['type'])) {
+            return [];
+        }
+
         $cssProperty = $setting['type'];
 
-        if (empty($cssProperty) || empty($setting[ $cssProperty ]) || 'order' !== $cssProperty) {
+        if ('order' !== $cssProperty || ! isset($setting[ $cssProperty ]) || empty($setting[ $cssProperty ])) {
+            return [];
+        }
 
-            return $declaration;
+        if (! isset($setting['order'])) {
+            return [];
         }
 
         $orderType = $setting['order'];
 
-        switch ($orderType) {
-            case 'first':
-                $this->setDeclaration('order', '-1');
-                break;
-
-            case 'last':
-                $this->setDeclaration('order', '100');
-                break;
-
-            case 'custom':
-                $this->setDeclaration('order', $setting['custom'] ? blockera_get_value_addon_real_value($setting['custom']) : '100');
-                break;
+        if ('first' === $orderType) {
+            $this->declarations['order'] = '-1';
+        } elseif ('last' === $orderType) {
+            $this->declarations['order'] = '100';
+        } elseif ('custom' === $orderType) {
+            if (isset($setting['custom']) && $setting['custom']) {
+                $this->declarations['order'] = blockera_get_value_addon_real_value($setting['custom']);
+            } else {
+                $this->declarations['order'] = '100';
+            }
         }
 
         $this->setCss($this->declarations);
@@ -48,31 +52,29 @@ class Order extends BaseStyleDefinition implements CustomStyle {
      */
     public function getCustomSettings( array $settings, string $settingName, string $cssProperty): array {
 
-        $settings                  = blockera_get_sanitize_block_attributes($settings);
-		$currentBreakpointSettings = $this->getCurrentBreakpointSettings();
+        $settings = blockera_get_sanitize_block_attributes($settings);
 
         if (isset($settings['value']) && 'custom' === $settings['value'] && 'order' === $cssProperty) {
+            $currentBreakpointSettings = $this->getCurrentBreakpointSettings();
+            $orderValue                = $currentBreakpointSettings['blockeraFlexChildOrder']['value'] ?? $currentBreakpointSettings['blockeraFlexChildOrder'] ?? 'custom';
+            $customValue               = $currentBreakpointSettings['blockeraFlexChildOrderCustom']['value'] ?? $currentBreakpointSettings['blockeraFlexChildOrderCustom'] ?? '';
 
-            $setting = [
+            return [
                 [
                     'isVisible'  => true,
                     'type'       => $cssProperty,
-                    $cssProperty => $currentBreakpointSettings['blockeraFlexChildOrder']['value'] ?? $currentBreakpointSettings['blockeraFlexChildOrder'] ?? 'custom',
-                    'custom'     => $currentBreakpointSettings['blockeraFlexChildOrderCustom']['value'] ?? $currentBreakpointSettings['blockeraFlexChildOrderCustom'] ?? '',
-                ],
-            ];
-
-        } else {
-
-            $setting = [
-                [
-                    'isVisible'  => true,
-                    'type'       => $cssProperty,
-                    $cssProperty => $settings['value'] ?? [],
+                    $cssProperty => $orderValue,
+                    'custom'     => $customValue,
                 ],
             ];
         }
 
-        return $setting;
+        return [
+            [
+                'isVisible'  => true,
+                'type'       => $cssProperty,
+                $cssProperty => $settings['value'] ?? [],
+            ],
+        ];
     }
 }
