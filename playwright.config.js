7fef3313ef4164dc69843fb4323e7a3d4051dcf2
rezaelahidev <rezaelahi.dev@gmail.com>
2025-12-30 13:08:47 +0330

    chore: added main configuration for playwright e2e tests

---
+++ playwright.config.js
@@ -0,0 +1,35 @@
+/**
+ * External dependencies
+ */
+const { fileURLToPath } = require('url');
+const { defineConfig } = require('@playwright/test');
+
+/**
+ * WordPress dependencies
+ */
+const baseConfig = require('@wordpress/scripts/config/playwright.config.js');
+
+const config = defineConfig({
+	...baseConfig,
+	testDir: '.',
+	testMatch: '**/*.ply.js',
+	reporter: process.env.CI
+		? [
+				['github'],
+				['./packages/dev-playwright/js/config/flaky-tests-reporter.ts'],
+		  ]
+		: 'list',
+	workers: 1,
+	globalSetup: fileURLToPath(
+		new URL(
+			'./packages/dev-playwright/js/config/global-setup.ts',
+			'file:' + __filename
+		).href
+	),
+	webServer: {
+		...baseConfig.webServer,
+		command: 'npm run env:start',
+	},
+});
+
+export default config;
