fcb3be02c68f19775a460e40c6bbe3e1fc29fa4f
rezaelahidev <rezaelahi.dev@gmail.com>
2026-01-09 19:47:38 +0330

    style: reformat codes

--- packages/editor/js/extensions/components/get-compatible-attributes.js
+++ packages/editor/js/extensions/components/get-compatible-attributes.js
@@ -47,7 +47,7 @@ export const getCompatibleAttributes = ({
 			? mergeObject(
 					{ ...attributes },
 					prepareBlockeraDefaultAttributesValues(defaultAttributes)
-				)
+			  )
 			: { ...attributes },
 		args
 	);
