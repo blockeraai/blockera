b507a32aaaa812c33663dc48b79052991a1cf437
rezaelahidev <rezaelahi.dev@gmail.com>
2026-01-22 19:08:06 +0330

    test(global-styles-compatibility): modified the font size tests and must plugins

--- packages/editor/js/extensions/libs/typography/test/global-styles/fixtures/font-size-setup-3.php
+++ packages/editor/js/extensions/libs/typography/test/global-styles/fixtures/font-size-setup-3.php
@@ -19,7 +19,7 @@ add_filter('blockera_theme_json_data_theme', function($theme_json) {
 	
 	// Add font size configuration based on WordPress valid schema
 	// Using fontSize attribute for preset font size values (unknown variable)
-	$data['styles']['blocks']['core/paragraph']['fontSize'] = 'unknown';
+	$data['styles']['blocks']['core/paragraph']['fontSize'] = 'var:preset|font-size|unknown';
 	
 	// Update the theme.json data
 	return $data;
