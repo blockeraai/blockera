31884cbc4c25acf7db4b267ad1a38482b651fef7
Ali Aghdam <betteraliaghdam@gmail.com>
2026-03-05 10:31:59 +0330

    test(accordion-heading): fixed test

--- packages/blocks-core/js/libs/wordpress/accordion-heading/test/functionality.blocks-1.e2e.cy.js
+++ packages/blocks-core/js/libs/wordpress/accordion-heading/test/functionality.blocks-1.e2e.cy.js
@@ -11,37 +11,18 @@ import {
 	redirectToFrontPage,
 } from '@blockera/dev-cypress/js/helpers';
 
+/**
+ * Internal dependencies
+ */
+import { testSample } from '../../accordion/test/test-sample';
+
 describe('Accordion Heading Block', () => {
 	beforeEach(() => {
 		createPost();
 	});
 
 	it('Functionality + Inner blocks', () => {
-		appendBlocks(`<!-- wp:accordion -->
-<div role="group" class="wp-block-accordion"><!-- wp:accordion-item -->
-<div class="wp-block-accordion-item"><!-- wp:accordion-heading -->
-<h3 class="wp-block-accordion-heading"><button class="wp-block-accordion-heading__toggle"><span class="wp-block-accordion-heading__toggle-title">Accordion 1 title</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
-<!-- /wp:accordion-heading -->
-
-<!-- wp:accordion-panel -->
-<div role="region" class="wp-block-accordion-panel"><!-- wp:paragraph -->
-<p>Accordion 1 content</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:accordion-panel --></div>
-<!-- /wp:accordion-item -->
-
-<!-- wp:accordion-item {"openByDefault":true} -->
-<div class="wp-block-accordion-item is-open"><!-- wp:accordion-heading -->
-<h3 class="wp-block-accordion-heading"><button class="wp-block-accordion-heading__toggle"><span class="wp-block-accordion-heading__toggle-title">Accordion 2 title</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
-<!-- /wp:accordion-heading -->
-
-<!-- wp:accordion-panel {"openByDefault":true} -->
-<div role="region" class="wp-block-accordion-panel"><!-- wp:paragraph -->
-<p>Accordion 2 content</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:accordion-panel --></div>
-<!-- /wp:accordion-item --></div>
-<!-- /wp:accordion -->`);
+		appendBlocks(testSample);
 
 		cy.getBlock('core/accordion-heading').first().click();
 
