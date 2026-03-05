fb8a8106e048c06a04f900c7c318d116a09264eb
Ali Aghdam <betteraliaghdam@gmail.com>
2026-03-05 11:03:59 +0330

    test(accordion): fixed test

--- packages/blocks-core/js/libs/wordpress/accordion/test/test-sample.js
+++ packages/blocks-core/js/libs/wordpress/accordion/test/test-sample.js
@@ -12,12 +12,12 @@ export const testSample = `
 <!-- /wp:accordion-panel --></div>
 <!-- /wp:accordion-item -->
 
-<!-- wp:accordion-item -->
-<div class="wp-block-accordion-item"><!-- wp:accordion-heading -->
+<!-- wp:accordion-item {"openByDefault":true} -->
+<div class="wp-block-accordion-item is-open"><!-- wp:accordion-heading -->
 <h3 class="wp-block-accordion-heading"><button type="button" class="wp-block-accordion-heading__toggle"><span class="wp-block-accordion-heading__toggle-title">Accordion 2 title</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h3>
 <!-- /wp:accordion-heading -->
 
-<!-- wp:accordion-panel -->
+<!-- wp:accordion-panel {"openByDefault":true} -->
 <div role="region" class="wp-block-accordion-panel"><!-- wp:paragraph -->
 <p>Accordion 2 content</p>
 <!-- /wp:paragraph --></div>
