297b9ae6ee78476858f33a7c10a574885f682698
Ali Aghdam <betteraliaghdam@gmail.com>
2026-03-31 11:37:05 +0330

    fix(Heading inner blocks): selector specificity

--- packages/blocks-core/php/libs/wordpress/inners/headings.php
+++ packages/blocks-core/php/libs/wordpress/inners/headings.php
@@ -12,21 +12,21 @@ return [
 		'root' => ':where(:is(h1,h2,h3,h4,h5,h6).wp-block-heading)',
 	],
 	'blockera/core/heading-1'              => [
-		'root' => ':where(h1:is(h1.wp-block-heading))',
+		'root' => ':where(h1).wp-block-heading',
 	],
 	'blockera/core/heading-2'              => [
-		'root' => ':where(h2:is(h2.wp-block-heading))',
+		'root' => ':where(h2).wp-block-heading',
 	],
 	'blockera/core/heading-3'              => [
-		'root' => ':where(h3:is(h3.wp-block-heading))',
+		'root' => ':where(h3).wp-block-heading',
 	],
 	'blockera/core/heading-4'              => [
-		'root' => ':where(h4:is(h4.wp-block-heading))',
+		'root' => ':where(h4).wp-block-heading',
 	],
 	'blockera/core/heading-5'              => [
-		'root' => ':where(h5:is(h5.wp-block-heading))',
+		'root' => ':where(h5).wp-block-heading',
 	],
 	'blockera/core/heading-6'              => [
-		'root' => ':where(h6:is(h6.wp-block-heading))',
+		'root' => ':where(h6).wp-block-heading',
 	],
 ];
