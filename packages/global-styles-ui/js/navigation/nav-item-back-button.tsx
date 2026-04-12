b707efa3a22b3ce82b9cf8a66e3da3cd419d1b66
Ali Aghdam <betteraliaghdam@gmail.com>
2026-04-13 00:12:19 +0330

    feat(global-styles-ui): show branding in NavItemBackButton
    - Use blockera-back-navigation class and optional showBranding prop
    
    - Add styles to position branding for correct tooltip behavior
    
    - Import navigation stylesheet from the back button module

--- packages/global-styles-ui/js/navigation/nav-item-back-button.tsx
+++ packages/global-styles-ui/js/navigation/nav-item-back-button.tsx
@@ -8,23 +8,34 @@ import { Navigator } from '@wordpress/components';
  * Blockera dependencies
  */
 import { Icon } from '@blockera/icons';
-import { extensionClassNames } from '@blockera/classnames';
+import { classNames } from '@blockera/classnames';
+
+/**
+ * Internal dependencies
+ */
+import { BlockeraBranding } from '../components/blockera-branding';
+
+import './style.scss';
 
 export const NavItemBackButton = ({
 	backLabel,
 	closeCallback,
+	showBranding = true,
 }: {
 	backLabel: string;
+	showBranding?: boolean;
 	closeCallback?: () => void;
 }): ReactElement => {
 	return (
-		<div className={extensionClassNames('back-navigation')}>
+		<div className={classNames('blockera-back-navigation')}>
 			<Navigator.BackButton
 				onClick={closeCallback}
 				icon={<Icon icon="chevron-left" library="wp" />}
 			>
 				{backLabel}
 			</Navigator.BackButton>
+
+			{showBranding && <BlockeraBranding />}
 		</div>
 	);
 };
