f945009c3ec56f1b0445ef15625dc889ad71f552
Ali Aghdam <betteraliaghdam@gmail.com>
2026-04-15 15:56:14 +0330

    feat(controls): configurable popover placement and offset on position button
    Expose popoverPlacement and popoverOffset; forward labelDescription to the
    alignment matrix inside the popover.

--- packages/controls/js/libs/position-button/index.js
+++ packages/controls/js/libs/position-button/index.js
@@ -30,6 +30,8 @@ export default function PositionButtonControl({
 	singularId,
 	buttonLabel,
 	popoverTitle = __('Setting', 'blockera'),
+	popoverPlacement = 'left-start',
+	popoverOffset = 121,
 	alignmentMatrixLabel = __('Position', 'blockera'),
 	id,
 	onChange,
@@ -109,8 +111,8 @@ export default function PositionButtonControl({
 				<Popover
 					design="highlight"
 					title={popoverTitle}
-					offset={121}
-					placement="left"
+					offset={popoverOffset}
+					placement={popoverPlacement}
 					className={controlInnerClassNames('position-popover')}
 					onClose={() => {
 						setIsPopoverActive(false);
@@ -121,6 +123,7 @@ export default function PositionButtonControl({
 						repeaterItem={repeaterItem}
 						id={id}
 						label={alignmentMatrixLabel}
+						labelDescription={labelDescription}
 						columns={
 							alignmentMatrixLabel ? 'columns-2' : 'columns-1'
 						}
--- packages/controls/js/libs/position-button/types/control-types.js
+++ packages/controls/js/libs/position-button/types/control-types.js
@@ -3,6 +3,7 @@
  * Internal dependencies
  */
 import type { ControlGeneralTypes, ControlSize } from '../../../types';
+import type { PopoverPlacement } from '../../popover/types';
 
 export type TPositionButtonProps = {
 	...ControlGeneralTypes,
@@ -12,6 +13,18 @@ export type TPositionButtonProps = {
 	 * Label for popover
 	 */
 	popoverTitle?: string,
+	/**
+	 * Placement of the popover
+	 *
+	 * @default "left-start"
+	 */
+	popoverPlacement?: PopoverPlacement,
+	/**
+	 * Offset of the popover
+	 *
+	 * @default 121
+	 */
+	popoverOffset?: number,
 	/**
 	 * Label for field alignment-matrix. If you pass empty value the field will not be added and simple control will be rendered
 	 *
