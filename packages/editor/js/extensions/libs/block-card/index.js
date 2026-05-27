df754744cb2213fefdd7e711d4f1c6b1b163876a
Ali Aghdam <betteraliaghdam@gmail.com>
2026-05-27 10:57:56 +0330

    feat(block-card): export pattern block card APIs
    Export PatternBlockCard, usePatternEditSection, and pattern edit helpers.

--- packages/editor/js/extensions/libs/block-card/index.js
+++ packages/editor/js/extensions/libs/block-card/index.js
@@ -1,6 +1,14 @@
 // @flow
 export { BlockCard } from './components/block-card';
 export { InnerBlockCard } from './components/inner-block-card';
+export { PatternBlockCard } from './components/pattern-block-card';
+export { usePatternEditSection } from './hooks/use-pattern-edit-section';
+export {
+	findPatternSectionClientId,
+	isPatternSectionBlock,
+	isCoreExitPatternEditModeVisible,
+	stopPatternContentOnlyEdit,
+} from './helpers/pattern-edit-section';
 export {
 	DEFAULT_STYLE_VARIATION_BLOCK_CARD_SLOT_NAME,
 	getStyleVariationBlockCardSlotNames,
