8097e5b627fb8d363eba2e7ee8f4428ddf27b7d9
Ali Aghdam <betteraliaghdam@gmail.com>
2026-06-02 18:42:34 +0330

    feat(blocks): enable size variations for core/post-terms
    Add hasSizeVariations and size-surface supportsExtensions config matching core/button.

--- packages/blocks-core/js/libs/wordpress/post-terms/index.js
+++ packages/blocks-core/js/libs/wordpress/post-terms/index.js
@@ -10,6 +10,7 @@ import { __ } from '@wordpress/i18n';
  */
 import { SharedBlockExtension } from '@blockera/editor';
 import { Icon } from '@blockera/icons';
+import { mergeObjects } from '@blockera/utils';
 
 /**
  * Internal dependencies
@@ -20,6 +21,7 @@ import type { BlockType } from '../../../type';
 export const PostTerms: BlockType = {
 	name: 'blockeraPostTerms',
 	targetBlock: 'core/post-terms',
+	hasSizeVariations: true,
 	blockeraInnerBlocks: {
 		'elements/link': {
 			...sharedInnerBlocks['elements/link'],
@@ -60,5 +62,124 @@ export const PostTerms: BlockType = {
 	},
 	edit: (props) => {
 		return <SharedBlockExtension {...props} />;
+	},
+	supportsExtensions: (
+		blockName: string,
+		current: Object,
+		variationSurface: 'size' | 'style'
+	): Object => {
+		if (variationSurface === 'style') {
+			return current;
+		}
+
+		return mergeObjects(current, {
+			advancedSettingsConfig: {
+				status: false,
+			},
+			backgroundConfig: {
+				status: false,
+			},
+			borderAndShadowConfig: {
+				blockeraBoxShadow: {
+					status: false,
+				},
+				blockeraOutline: {
+					status: false,
+				},
+				blockeraBorder: {
+					status: false,
+				},
+			},
+			clickAnimationConfig: {
+				status: false,
+			},
+			conditionsConfig: {
+				status: false,
+			},
+			customStyleConfig: {
+				status: true,
+			},
+			effectsConfig: {
+				status: false,
+			},
+			entranceAnimationConfig: {
+				status: false,
+			},
+			flexChildConfig: {
+				status: false,
+			},
+			gridChildConfig: {
+				status: false,
+			},
+			layoutConfig: {
+				blockeraDisplay: {
+					status: false,
+				},
+				blockeraFlexLayout: {
+					status: false,
+				},
+				blockeraGap: {
+					status: false,
+				},
+				blockeraFlexWrap: {
+					status: false,
+				},
+			},
+			typographyConfig: {
+				blockeraFontFamily: {
+					status: false,
+				},
+				blockeraFontColor: {
+					status: false,
+				},
+				blockeraTextShadow: {
+					status: false,
+				},
+				blockeraTextAlign: {
+					status: false,
+				},
+				blockeraTextTransform: {
+					status: false,
+				},
+				blockeraTextDecoration: {
+					status: false,
+				},
+				blockeraDirection: {
+					status: false,
+				},
+				blockeraTextIndent: {
+					status: false,
+				},
+				blockeraTextOrientation: {
+					status: false,
+				},
+				blockeraTextColumns: {
+					status: false,
+				},
+				blockeraTextStroke: {
+					status: false,
+				},
+				blockeraWordBreak: {
+					status: false,
+				},
+				blockeraTextWrap: {
+					status: false,
+				},
+			},
+			sizeConfig: {
+				blockeraOverflow: {
+					status: false,
+				},
+			},
+			mouseConfig: {
+				status: false,
+			},
+			positionConfig: {
+				status: false,
+			},
+			scrollAnimationConfig: {
+				status: false,
+			},
+		});
 	},
 };
