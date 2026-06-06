2d02f1648d1be7e1ed358d0081f042ca88a77ab2
Ali Aghdam <betteraliaghdam@gmail.com>
2026-06-06 21:43:33 +0330

    feat(icon-control): add Clear all button to recently used icons
    Add a header action in the icon picker modal to wipe the recently used
    icons list, with layout and styling for the title row.

--- packages/controls/js/libs/icon-control/components/icon-picker/recent-icons.js
+++ packages/controls/js/libs/icon-control/components/icon-picker/recent-icons.js
@@ -7,18 +7,27 @@ import { useMemo, useContext } from '@wordpress/element';
 /**
  * Blockera dependencies
  */
-import { controlInnerClassNames } from '@blockera/classnames';
+import {
+	controlInnerClassNames,
+	controlClassNames,
+} from '@blockera/classnames';
 import { Icon } from '@blockera/icons';
 
 /**
  * Internal dependencies
  */
+import { Button } from '../../../index';
 import { IconContext } from '../../context';
 import { buildRecentIconElements } from '../../utils';
 
 export default function RecentIcons() {
-	const { recentIcons, removeRecentIcon, handleIconSelect, isCurrentIcon } =
-		useContext(IconContext);
+	const {
+		recentIcons,
+		removeRecentIcon,
+		clearRecentIcons,
+		handleIconSelect,
+		isCurrentIcon,
+	} = useContext(IconContext);
 
 	const iconElements = useMemo(
 		() =>
@@ -37,16 +46,38 @@ export default function RecentIcons() {
 
 	return (
 		<div
-			className={controlInnerClassNames(
+			className={controlClassNames(
 				'icon-library',
 				'library-recent',
 				'icon-picker-recent-icons',
 				'is-rendered'
 			)}
 		>
-			<div className={controlInnerClassNames('library-header')}>
-				<Icon icon="fa-clock" library="faregular" iconSize="22" />
-				{__('Recently used', 'blockera')}
+			<div
+				className={controlClassNames(
+					'library-header',
+					'recent-icons-header'
+				)}
+			>
+				<div
+					className={controlClassNames('recent-icons-header__title')}
+				>
+					<Icon icon="fa-clock" library="faregular" iconSize="22" />
+					{__('Recently used', 'blockera')}
+				</div>
+
+				<Button
+					variant="tertiary"
+					size="extra-small"
+					className={controlInnerClassNames('recent-icons-clear')}
+					aria-label={__('Clear all recently used icons', 'blockera')}
+					onClick={(event) => {
+						event.stopPropagation();
+						clearRecentIcons();
+					}}
+				>
+					{__('Clear all', 'blockera')}
+				</Button>
 			</div>
 
 			<div className={controlInnerClassNames('library-body', 'no-fade')}>
