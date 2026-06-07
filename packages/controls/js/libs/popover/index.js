a496df5adb56f4a270f7cef302c683db56b9bddf
rezaelahidev <rezaelahi.dev@gmail.com>
2026-06-07 17:06:21 +0330

    fix(controls): sync Popover primary color from scoped StateContainer
    Replace ad-hoc store reads with anchor-aware StateContainer CSS variable
    sync so portaled popovers follow inner block, block state, and style vs size
    variation surface colors.

--- packages/controls/js/libs/popover/index.js
+++ packages/controls/js/libs/popover/index.js
@@ -2,8 +2,6 @@
 /**
  * External dependencies
  */
-import { select } from '@wordpress/data';
-import { useEffect } from '@wordpress/element';
 import type { MixedElement } from 'react';
 
 /**
@@ -11,61 +9,23 @@ import type { MixedElement } from 'react';
  */
 import { PopoverCore } from './core';
 import { DraggablePopover } from './draggable';
+import { useStateContainerActiveColorStyle } from './use-state-container-active-color-style';
 import type { TPopoverProps } from './types';
 
 export default function Popover({
 	draggable = true,
+	style,
 	...props
 }: TPopoverProps): MixedElement {
-	const { getSelectedBlock = () => ({ name: '', clientId: '' }) } =
-		select('core/block-editor') || {};
-	const { name, clientId } = getSelectedBlock() || {};
-
-	const {
-		getActiveInnerState = () => 'normal',
-		getActiveMasterState = () => 'normal',
-		getExtensionCurrentBlock = () => 'master',
-	} = select('blockera/extensions') || {};
-	const { getState } = select('blockera/editor') || {
-		getState: () => ({ settings: { color: '#cc0000' } }),
+	const stateContainerStyle = useStateContainerActiveColorStyle(props.anchor);
+	const mergedStyle = {
+		...stateContainerStyle,
+		...(style || {}),
 	};
 
-	let activeColor = getState(getActiveMasterState(clientId, name))?.settings
-		?.color;
-
-	if (
-		'master' !== getExtensionCurrentBlock() &&
-		'normal' === getActiveInnerState(clientId, getExtensionCurrentBlock())
-	) {
-		activeColor = '#cc0000';
-	} else if ('master' !== getExtensionCurrentBlock()) {
-		activeColor = getState(
-			getActiveInnerState(clientId, getExtensionCurrentBlock())
-		)?.settings?.color;
-	}
-
-	useEffect(() => {
-		const container = document.querySelector(
-			'.components-popover__fallback-container'
-		);
-
-		if (container) {
-			container.style.setProperty('color', 'inherit');
-			container.style.setProperty(
-				'--blockera-controls-primary-color',
-				activeColor
-			);
-			container.style.setProperty(
-				'--blockera-tab-panel-active-color',
-				activeColor
-			);
-		}
-		// eslint-disable-next-line
-	}, []);
-
 	return draggable && props?.title ? (
-		<DraggablePopover {...props} />
+		<DraggablePopover {...props} style={mergedStyle} />
 	) : (
-		<PopoverCore {...props} />
+		<PopoverCore {...props} style={mergedStyle} />
 	);
 }
