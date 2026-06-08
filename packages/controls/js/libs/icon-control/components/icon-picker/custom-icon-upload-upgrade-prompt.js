926ba61c29cc09713b33acb3c611e289b9b4df4c
Ali Aghdam <betteraliaghdam@gmail.com>
2026-06-08 16:41:32 +0330

    feat(controls): add custom icon upload upgrade prompt component

---
+++ packages/controls/js/libs/icon-control/components/icon-picker/custom-icon-upload-upgrade-prompt.js
@@ -0,0 +1,47 @@
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Blockera dependencies
+ */
+import { Icon } from '@blockera/icons';
+
+/**
+ * Internal dependencies
+ */
+import { UpgradePrompt, Flex } from '../../../';
+
+export default function CustomIconUploadUpgradePrompt({
+	isOpen = false,
+	onClose = () => {},
+}) {
+	return (
+		<UpgradePrompt
+			lockedFeature={{
+				icon: <Icon icon="upload" library="wp" iconSize={22} />,
+				title: __('Custom SVG Icons', 'blockera'),
+				description: (
+					<Flex direction="column" gap="6px">
+						{__('Upload unlimited custom SVG icons', 'blockera')}
+						<Flex direction="column" gap="6px">
+							<span className="blockera-free-plan-hint">
+								{__('Free: No uploads allowed', 'blockera')}
+							</span>
+							<span className="blockera-pro-plan-hint">
+								{__(
+									'Pro: Upload unlimited custom icons',
+									'blockera'
+								)}
+							</span>
+						</Flex>
+					</Flex>
+				),
+			}}
+			isOpen={isOpen}
+			onClose={onClose}
+			type="modal"
+		/>
+	);
+}
