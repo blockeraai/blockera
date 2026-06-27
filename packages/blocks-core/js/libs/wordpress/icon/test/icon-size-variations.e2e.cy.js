6be0aec7d6dfaea40f109e992a9886e646c7e6ca
Ali Aghdam <betteraliaghdam@gmail.com>
2026-06-27 10:45:32 +0330

    test(Icon Block + Size Variation): fix test

--- packages/blocks-core/js/libs/wordpress/icon/test/icon-size-variations.e2e.cy.js
+++ packages/blocks-core/js/libs/wordpress/icon/test/icon-size-variations.e2e.cy.js
@@ -4,27 +4,12 @@
 import { appendBlocks, createPost } from '@blockera/dev-cypress/js/helpers';
 
 describe('core/icon → size variations support', () => {
-	before(function () {
-		cy.window().then((win) => {
-			const blockTypes = win.wp?.blocks?.getBlockTypes?.() || [];
-			const hasCoreIcon = blockTypes.some(
-				(block) => block.name === 'core/icon'
-			);
-
-			if (!hasCoreIcon) {
-				this.skip();
-			}
-		});
-	});
-
 	beforeEach(() => {
 		createPost();
 	});
 
 	it('exposes Blockera size variation surface for core/icon', () => {
-		appendBlocks(`<!-- wp:icon {"icon":"wordpress"} -->
-<div class="wp-block-icon"></div>
-<!-- /wp:icon -->`);
+		appendBlocks(`<!-- wp:icon {"icon":"wordpress"} /-->`);
 
 		cy.getBlock('core/icon').first().click();
 
