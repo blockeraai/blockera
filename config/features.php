f7420035341cbb0a3203f6c0a0af6b67680cb54a
rezaelahidev <rezaelahi.dev@gmail.com>
2026-06-30 16:15:19 +0330

    fix(config): add ABSPATH guards to config entry files
    Prevent direct HTTP access to features and telemetry config PHP files.

--- config/features.php
+++ config/features.php
@@ -1,6 +1,16 @@
 <?php
+/**
+ * Blockera features config.
+ *
+ * @package Blockera
+ */
 
 use Blockera\Feature\Icon\Icon;
+
+// direct access is not allowed.
+if (! defined('ABSPATH')) {
+    exit;
+}
 
 return [
     'icon' => Icon::getInstance(),
