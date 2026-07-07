897338e3188b6c82e7ed9c5788a59155470fbcd7
rezaelahidev <rezaelahi.dev@gmail.com>
2026-07-07 10:46:55 +0330

    test(repeater-control): updated codes

--- packages/controls/js/libs/repeater-control/test/repeater-control.cy.js
+++ packages/controls/js/libs/repeater-control/test/repeater-control.cy.js
@@ -704,16 +704,12 @@ describe('repeater control component testing', () => {
 						'first-0': { ...value['first-0'], order: 0 },
 						'second-0': { ...value['second-0'], order: 1 },
 						'second-2': {
-							...value['second-1'],
+							...value['second-0'],
 							order: 2,
 							creatingStep: false,
 						},
 						'first-1': { ...value['first-1'], order: 3 },
-						'second-1': {
-							...value['second-2'],
-							order: 4,
-							creatingStep: false,
-						},
+						'second-1': { ...value['second-1'], order: 4 },
 					});
 				});
 			});
