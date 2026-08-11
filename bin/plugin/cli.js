d0e42c09939e82752532f438b11547297ad7540f
rezaelahidev <rezaelahi.dev@gmail.com>
2026-08-11 21:49:22 +0330

    refactor: consume shared dual-output changelog CLI from global-packages
    Thin bin/plugin wrappers with product config; pin global-packages to 6d46282
    for the shared modern changelog generator.

--- bin/plugin/cli.js
+++ bin/plugin/cli.js
@@ -1,57 +1,29 @@
 #!/usr/bin/env node
 
 /**
- * External dependencies
+ * Internal dependencies
  */
-const program = require('commander');
+const config = require('./config');
+const {
+	createPluginCli,
+} = require('../../packages/global-packages/packages/dev-tools/bin/plugin/cli');
 
-const catchException = (command) => {
-	return async (...args) => {
-		try {
-			await command(...args);
-		} catch (error) {
-			console.error(error);
-			process.exitCode = 1;
-		}
-	};
-};
-
-const releaseType = ['--releaseType <releaseType>', 'Release Type'];
-
-const { getReleaseChangelog } = require('./commands/changelog');
-const { updatePackagesChangelog } = require('./commands/packages');
-const { testImport } = require('./commands/testImport');
-
-program
-	.command('update-packages-changelog')
-	.option('-v, --version <version>', 'Version')
-	.option(...releaseType)
-	.description('Blockera plugin and packages changelogs publishes to git.')
-	.action(catchException(updatePackagesChangelog));
-
-program
-	.command('release-plugin-changelog')
-	.alias('changelog')
-	.option('-f, --file <file>', 'File')
-	.option('-v, --version <version>', 'Version')
-	.option('-m, --milestone <milestone>', 'Milestone')
-	.option('-t, --token <token>', 'GitHub token')
-	.option(
-		'-u, --unreleased',
-		"Only include PRs that haven't been included in a release yet"
-	)
-	.description('Generates a changelog from merged Pull Requests')
-	.action(catchException(getReleaseChangelog));
-
-program
-	.command('test:snapshots:import')
-	.option(
-		'-d, --dry-run',
-		'Show what would be done without actually creating posts'
-	)
-	.description(
-		'Import all input.html files from tests/fixtures as WordPress posts'
-	)
-	.action(catchException(testImport));
-
-program.parse(process.argv);
+createPluginCli(config, {
+	extraCommands(program, catchException) {
+		program
+			.command('test:snapshots:import')
+			.option(
+				'-d, --dry-run',
+				'Show what would be done without actually creating posts'
+			)
+			.description(
+				'Import all input.html files from tests/fixtures as WordPress posts'
+			)
+			.action(
+				catchException(async (...args) => {
+					const { testImport } = require('./commands/testImport');
+					return testImport(...args);
+				})
+			);
+	},
+});
--- bin/plugin/config.js
+++ bin/plugin/config.js
@@ -1,29 +1,13 @@
+/**
+ * Internal dependencies
+ */
+const {
+	createPluginCliConfig,
+} = require('../../packages/global-packages/packages/dev-tools/bin/plugin/create-config');
+
 const gitRepoOwner = 'blockeraai';
 
-/**
- * @typedef WPPluginCLIConfig
- *
- * @property {string} slug                    Slug.
- * @property {string} name                    Name.
- * @property {string} team                    GitHub Team Name.
- * @property {string} versionMilestoneFormat  printf template for milestone
- *                                            version name. Expected to be called
- *                                            with a merged object of the config
- *                                            and semver-parsed version parts.
- * @property {string} githubRepositoryOwner   GitHub Repository Owner.
- * @property {string} githubRepositoryName    GitHub Repository Name.
- * @property {string} pluginEntryPoint        Plugin Entry Point File.
- * @property {string} buildZipCommand         Build Plugin ZIP command.
- * @property {string} githubRepositoryURL     GitHub Repository URL.
- * @property {string} wpRepositoryReleasesURL WordPress Repository Tags URL.
- * @property {string} gitRepositoryURL        Git Repository URL.
- * @property {string} svnRepositoryURL        SVN Repository URL.
- */
-
-/**
- * @type {WPPluginCLIConfig}
- */
-const config = {
+module.exports = createPluginCliConfig({
 	slug: 'blockera',
 	name: 'Blockera',
 	team: 'Blockeraai',
@@ -36,6 +20,9 @@ const config = {
 	wpRepositoryReleasesURL: 'https://github.com/blockeraai/blockera/releases/',
 	gitRepositoryURL: 'https://github.com/' + gitRepoOwner + '/blockera.git',
 	svnRepositoryURL: 'https://plugins.svn.wordpress.org/blockera',
-};
-
-module.exports = config;
+	changelog: {
+		archiveUrl: 'https://community.blockera.ai/changelog-9l8hbrv0',
+		archiveLabel: 'Blockera',
+		includeCommitCount: true,
+	},
+});
