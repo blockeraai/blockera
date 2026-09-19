4dd63e01b468ceb655d911a1dd7e7d3cc62c66c7
rezaelahidev <rezaelahi.dev@gmail.com>
2026-09-19 13:23:15 +0330

    Revert "chore: drop auth package host wiring"
    This reverts commit 0d7c5bf3b836567d26f1737fedb90a1c1e73f863.

--- config/menu.php
+++ config/menu.php
@@ -74,7 +74,9 @@ $menu = apply_filters(
 
 $blockera_pro_active = false;
 
-if ( defined( 'ABSPATH' ) ) {
+if ( function_exists( 'blockera_auth_is_pro_plugin_active' ) ) {
+	$blockera_pro_active = blockera_auth_is_pro_plugin_active();
+} elseif ( defined( 'ABSPATH' ) ) {
 	if ( ! function_exists( 'is_plugin_active' ) ) {
 		require_once ABSPATH . 'wp-admin/includes/plugin.php';
 	}
