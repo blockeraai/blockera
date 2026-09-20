6c2491944351dcef78c2d85fe856a737646484b4
rezaelahidev <rezaelahi.dev@gmail.com>
2026-09-20 10:52:25 +0330

    fix: prune vendor/blockera from the zip vendor find

--- bin/build-plugin-zip.sh
+++ bin/build-plugin-zip.sh
@@ -197,7 +197,7 @@ build_files=$(
 )
 
 vendor_without_blockera=$(
-  find ./vendor -type f -not -path "./vendor/blockera" \
+  find ./vendor \( -path './vendor/blockera' -o -path './vendor/blockera/*' \) -prune -o -type f -print
 );
 
 main_plugin_file='blockera.php'
